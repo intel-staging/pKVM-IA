caedf475243b472863f89472580b4d551f8c1d80
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-20 23:59:48 +0800

    pkvm: x86: Add vmx capability check and vmx config setup
    Check minimal vmx capabilities requirement for pKVM on Intel platform.
    Setup vmcs config and vmx cap for later run VCPUs of deprivileged
    host VM.
    
    The vmx capabilities are checked through setup_vmcs_config_common,
    the same API as KVM. pKVM tries to pass-thru resource to host VM as
    much as possible, so it does not need to do a lot of emulations;
    but some capabilities are still required for pKVM's emulations
    (e.g. EPT, SHADOW_VMCS, MSR_BITMAP etc.), so pKVM has its own
    different check-set from KVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit 7f66ed27a21e913333b6642565bbaad3e1dd574c)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/include/pkvm.h
+++ arch/x86/kvm/vmx/pkvm/include/pkvm.h
@@ -6,8 +6,13 @@
 #ifndef _PKVM_H_
 #define _PKVM_H_
 
+#include <vmx/vmx.h>
+
 struct pkvm_hyp {
 	int num_cpus;
+
+	struct vmx_capability vmx_cap;
+	struct vmcs_config vmcs_config;
 };
 
 #define PKVM_PAGES (ALIGN(sizeof(struct pkvm_hyp), PAGE_SIZE) >> PAGE_SHIFT)
