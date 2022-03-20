7f66ed27a21e913333b6642565bbaad3e1dd574c
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-20 23:59:48 +0800

    pkvm: x86: Add vmx capability check and vmx config setup
    Check minimal vmx capabilities requirement for pKVM on Intel platform.
    Setup vmcs config and vmx cap for later run VCPUs of deprivileged
    host VM.
    
    The vmx capabilities are checked through setup_vmcs_config_common,
    the same API as KVM. pKVM tries to pass-thru resource to host VM as
    much as possible, so it does not need to do a lot of emulations;
    but some capabilities are still required for pKVM's emulations
    (e.g. EPT, SHADOW_VMCS, MSR_BITMAP etc.), so pKVM has its own
    different check-set from KVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -17,6 +17,64 @@ static void *pkvm_early_alloc_contig(int pages)
 	return alloc_pages_exact(pages << PAGE_SHIFT, GFP_KERNEL | __GFP_ZERO);
 }
 
+static void pkvm_early_free(void *ptr, int pages)
+{
+	free_pages_exact(ptr, pages << PAGE_SHIFT);
+}
+
+static __init int pkvm_host_check_and_setup_vmx_cap(struct pkvm_hyp *pkvm)
+{
+	struct vmcs_config *vmcs_config = &pkvm->vmcs_config;
+	struct vmx_capability *vmx_cap = &pkvm->vmx_cap;
+	int ret = 0;
+	struct vmcs_config_setting setting = {
+		.cpu_based_vm_exec_ctrl_req =
+			CPU_BASED_INTR_WINDOW_EXITING |
+			CPU_BASED_USE_MSR_BITMAPS |
+			CPU_BASED_ACTIVATE_SECONDARY_CONTROLS,
+		.cpu_based_vm_exec_ctrl_opt = 0,
+		.secondary_vm_exec_ctrl_req =
+			SECONDARY_EXEC_ENABLE_EPT |
+			SECONDARY_EXEC_SHADOW_VMCS,
+		.secondary_vm_exec_ctrl_opt =
+			SECONDARY_EXEC_ENABLE_VPID |
+			SECONDARY_EXEC_ENABLE_INVPCID |
+			SECONDARY_EXEC_ENABLE_XSAVES |
+			SECONDARY_EXEC_ENABLE_RDTSCP |
+			SECONDARY_EXEC_ENABLE_USR_WAIT_PAUSE,
+		.tertiary_vm_exec_ctrl_opt = 0,
+		.pin_based_vm_exec_ctrl_req = 0,
+		.pin_based_vm_exec_ctrl_opt = 0,
+		.vmexit_ctrl_req =
+			VM_EXIT_HOST_ADDR_SPACE_SIZE |
+			VM_EXIT_LOAD_IA32_PAT |
+			VM_EXIT_LOAD_IA32_EFER |
+			VM_EXIT_SAVE_IA32_PAT |
+			VM_EXIT_SAVE_IA32_EFER |
+			VM_EXIT_SAVE_DEBUG_CONTROLS,
+		.vmexit_ctrl_opt = 0,
+		.vmentry_ctrl_req =
+			VM_ENTRY_LOAD_DEBUG_CONTROLS |
+			VM_ENTRY_IA32E_MODE |
+			VM_ENTRY_LOAD_IA32_EFER |
+			VM_ENTRY_LOAD_IA32_PAT,
+		.vmentry_ctrl_opt = 0,
+	};
+
+	ret = setup_vmcs_config_common(vmcs_config, vmx_cap, &setting);
+	if (ret) {
+		pr_err("%s: fail with ret %d\n", __func__, ret);
+	} else {
+		pr_info("pin_based_exec_ctrl 0x%x\n", vmcs_config->pin_based_exec_ctrl);
+		pr_info("cpu_based_exec_ctrl 0x%x\n", vmcs_config->cpu_based_exec_ctrl);
+		pr_info("cpu_based_2nd_exec_ctrl 0x%x\n", vmcs_config->cpu_based_2nd_exec_ctrl);
+		pr_info("vmexit_ctrl 0x%x\n", vmcs_config->vmexit_ctrl);
+		pr_info("vmentry_ctrl 0x%x\n", vmcs_config->vmentry_ctrl);
+	}
+
+	return ret;
+}
+
 __init int pkvm_init(void)
 {
 	int ret = 0;
@@ -27,8 +85,16 @@ __init int pkvm_init(void)
 		goto out;
 	}
 
+	ret = pkvm_host_check_and_setup_vmx_cap(pkvm);
+	if (ret)
+		goto out_free_pkvm;
+
 	pkvm->num_cpus = num_possible_cpus();
 
+	return 0;
+
+out_free_pkvm:
+	pkvm_early_free(pkvm, PKVM_PAGES);
 out:
 	return ret;
 }
