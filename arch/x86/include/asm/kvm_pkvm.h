db1f37d11a117a8bd58766261fff694e0c4661a3
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-24 18:43:34 +0800

    pkvm: x86: Define hypervisor runtime VA/PA APIs
    pKVM has its own MMU page table, to maintain the mapping between VA &
    PA under hypervisor runtime (VMX root mode).
    
    Setup the translation between VA & PA for hypervisor runtime:
    keep same direct mapping as Linux kernel based on __page_base_offset.
    also keep same pkvm symbol mapping as Linux kernel based on
    __symbol_base_offset.
    
    direct mapping translation:
    - pkvm_phys_to_virt: phys + __page_base_offset
    - pkvm_virt_to_phys: virt - __page_base_offset
    symbol mapping translation:
    - pkvm_virt_to_symbol_phys: virt - __symbol_base_offset
    
    The following patches will setup pKVM's MMU page table based on above
    translation.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit 582f21bd5a57ce1c0c5ac0c0d54ddc994c661124)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+
+#ifndef _ASM_X86_KVM_PKVM_H
+#define _ASM_X86_KVM_PKVM_H
+
+#ifdef CONFIG_PKVM_INTEL
+
+void *pkvm_phys_to_virt(unsigned long phys);
+unsigned long pkvm_virt_to_phys(void *virt);
+
+#define __pkvm_pa(virt)	pkvm_virt_to_phys((void *)(virt))
+#define __pkvm_va(phys)	pkvm_phys_to_virt((unsigned long)(phys))
+#endif
+
+#endif
