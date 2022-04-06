c566295d36dc00172af2450416c80cdfb5ac91cc
Jason Chen CJ <jason.cj.chen@intel.com>
2022-04-07 01:40:50 +0800

    pkvm: x86: Early alloc from reserved memory
    pKVM's data strcuture shall be allocated from its own memory, to
    ensure it can be isolated from host Linux.
    
    Add early page allocator which fetch pages from reserved memory of
    pKVM. For pkvm_early_alloc_config(), replace alloc_pages_exact()
    with the new added early page allocator. And as the allocation is
    from reserved memory, pkvm_init failure make reserved memory useless,
    there is no need to free such allocated pages anymore.
    
    TODO: return reserved memory to host Linux if pkvm_init fails.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/early_alloc.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+#ifndef __PKVM_EARLY_ALLOC_H
+#define __PKVM_EARLY_ALLOC_H
+
+void *pkvm_early_alloc_contig(unsigned int nr_pages);
+void *pkvm_early_alloc_page(void);
+void pkvm_early_alloc_init(void *virt, unsigned long size);
+
+#endif
