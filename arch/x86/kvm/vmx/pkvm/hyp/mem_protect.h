89e53f1c25459318b65f48d3b168b4816dd8c35e
Shaoqin Huang <shaoqin.huang@intel.com>
2022-05-05 16:31:08 -0600

    pkvm: x86: Use SW bits to track page state
    pKVM already had a mechnism to record page ownership in host EPT, it
    still needs a place to record page state based on the ownership; the page
    state is used to identify the exact state of corresponding page:
    
    - PKVM_NOPAGE: the page has no mapping in page table (also invalid pte).
                   And under this page state, host EPT is using the pte
    	       ignored bits to record owner_id.
    - PKVM_PAGE_OWNED: the page is owned exclusively by the page-table owner.
    - PKVM_PAGE_SHARED_OWNED: the page is owned by the page-table owner, but
    			  is shared with another entity.
    - PKVM_PAGE_SHARED_BORROWED: the page is shared with, but not owned by
    			     the page-table owner.
    
    The bit[56,57] of a EPT page table are ignored in the PTE, so use these
    two bits as SW bits to record the page state.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/mem_protect.h
+++ arch/x86/kvm/vmx/pkvm/hyp/mem_protect.h
@@ -5,8 +5,47 @@
 #ifndef __PKVM_MEM_PROTECT_H__
 #define __PKVM_MEM_PROTECT_H__
 
+/*
+ * enum pkvm_pgtable_prot - The ignored bits in page-table.
+ * pkvm will use these ignored bits as software bits to
+ * identify the page status.
+ */
+enum pkvm_pgtable_prot {
+	PKVM_PGTABLE_PROT_SW0		= BIT(56),
+	PKVM_PGTABLE_PROT_SW1		= BIT(57),
+};
+
+/*
+ * Using the ignored bits in page-table as SW bits.
+ * SW bits 0-1 are used to track the memory ownership state of each page:
+ *   00: The page has no mapping in page table (also invalid pte). And under
+ *   this page state, host ept is using the pte ignored bits to record owner_id.
+ *   01: The page is owned exclusively by the page-table owner.
+ *   10: The page is owned by the page-table owner, but is shared
+ *   	with another entity.
+ *   11: The page is shared with, but not owned by the page-table owner.
+ */
+enum pkvm_page_state {
+	PKVM_NOPAGE			= 0ULL,
+	PKVM_PAGE_OWNED			= PKVM_PGTABLE_PROT_SW0,
+	PKVM_PAGE_SHARED_OWNED		= PKVM_PGTABLE_PROT_SW1,
+	PKVM_PAGE_SHARED_BORROWED	= PKVM_PGTABLE_PROT_SW0 |
+					  PKVM_PGTABLE_PROT_SW1,
+};
+
+#define PKVM_PAGE_STATE_PROT_MASK	(PKVM_PGTABLE_PROT_SW0 | PKVM_PGTABLE_PROT_SW1)
 /* use 20 bits[12~31] - not conflict w/ low 12 bits pte prot */
 #define PKVM_INVALID_PTE_OWNER_MASK	GENMASK(31, 12)
+
+static inline u64 pkvm_mkstate(u64 prot, enum pkvm_page_state state)
+{
+	return (prot & ~PKVM_PAGE_STATE_PROT_MASK) | state;
+}
+
+static inline enum pkvm_page_state pkvm_getstate(u64 pte)
+{
+	return pte & PKVM_PAGE_STATE_PROT_MASK;
+}
 
 typedef u32 pkvm_id;
 
