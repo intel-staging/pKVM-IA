993d34e73f61beffd0e84d77e1f05cb2485b532d
Shaoqin Huang <shaoqin.huang@intel.com>
2022-05-05 16:31:08 -0600

    pkvm: x86: using SW bits to track page state
    We already had a mechnism to record page ownership in host ept, we still
    need a place to record page state based on the ownership; the page state
    is used to identify the exact state of corresponding page:
    
    - PKVM_NOPAGE: the page has no mapping in page table (also invalid pte).
                   And under this page state, host ept is using the pte
    	       ignored bits to record owner_id.
    - PKVM_PAGE_OWNED: the page is owned exclusively by the page-table owner.
    - PKVM_PAGE_SHARED_OWNED: the page is owned by the page-table owner, but
    			  is shared with another entity.
    - PKVM_PAGE_SHARED_BORROWED: the page is shared with, but not owned by
    			     the page-table owner.
    
    The bit[56,57] of a IA page table are ignored in the pte, so use these
    two bits as SW bits to record the page state.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
+++ arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -10,7 +10,9 @@
 
 static u64 pkvm_init_invalid_leaf_owner(pkvm_id owner_id)
 {
-	return FIELD_PREP(PKVM_INVALID_PTE_OWNER_MASK, owner_id);
+	/* the page owned by others also means NOPAGE in page state */
+	return FIELD_PREP(PKVM_INVALID_PTE_OWNER_MASK, owner_id) |
+		FIELD_PREP(PKVM_PAGE_STATE_PROT_MASK, PKVM_NOPAGE);
 }
 
 int host_ept_set_owner(phys_addr_t addr, u64 size, pkvm_id owner_id)
@@ -18,6 +20,15 @@ int host_ept_set_owner(phys_addr_t addr, u64 size, pkvm_id owner_id)
 	u64 annotation = pkvm_init_invalid_leaf_owner(owner_id);
 	int ret;
 
+
+	/*
+	 * The memory [addr, addr + size) will be unmaped from host ept. At the
+	 * same time, the annotation with a NOPAGE flag will be put in the
+	 * invalid pte that has been unmaped. And these information record that
+	 * the page has been used by some guest and it's id can be read from
+	 * annotation. Also when later these page back to host, the annotation
+	 * will help to check the right page transition.
+	 */
 	ret = pkvm_pgtable_annotate(pkvm_hyp->host_vm.ept, addr, size, annotation);
 
 	return ret;
