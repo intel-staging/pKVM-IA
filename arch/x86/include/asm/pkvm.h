0d67c56e4dbd17f00e3d040d2cd9cccc456d9b71
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-06-09 12:09:27 +0800

    IOMMU: use pkvm supported PASID size
    pkvm support limited PASID size due to memory consumption. So make
    the linux IOMMU driver to use the supported PASID size.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -16,8 +16,12 @@
 #define PKVM_HC_TEARDOWN_SHADOW_VCPU	5
 #define PKVM_HC_MMIO_ACCESS		6
 
-/* 15bits for PASID */
-#define PKVM_MAX_PASID	0x8000
+/*
+ * 15bits for PASID, DO NOT change it, based on it,
+ * the size of PASID DIR table can kept as one page
+ */
+#define PKVM_MAX_PASID_BITS	15
+#define PKVM_MAX_PASID		(1 << PKVM_MAX_PASID_BITS)
 
 #ifdef CONFIG_PKVM_INTEL
 DECLARE_PER_CPU_READ_MOSTLY(bool, pkvm_enabled);
@@ -72,7 +76,9 @@ static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 		 */
 		*cap |= 1 << 7;
 
-	if (ecap)
+	if (ecap) {
+		u64 tmp;
+
 		/*
 		 * Some IOMMU capabilities cannot be directly used by the linux
 		 * IOMMU driver after the linux is deprivileged, which is because after
@@ -94,6 +100,12 @@ static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 		 * So disable SLTS and Nest.
 		 */
 		*ecap &= ~((1UL << 46) | (1UL << 26));
+
+		/* limit PASID to reduce the memory consumptions */
+		tmp = min_t(u64, (PKVM_MAX_PASID_BITS - 1),
+			    (*ecap & GENMASK_ULL(39, 35)) >> 35);
+		*ecap = (*ecap & ~GENMASK_ULL(39, 35)) | (tmp << 35);
+	}
 }
 #endif
 
