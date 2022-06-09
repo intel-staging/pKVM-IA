a13fba891a8e335bcd7751f1462095e715ffcba5
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-06-09 13:42:46 +0800

    pkvm: x86: shadow IOMMU page table
    pkvm IOMMU leverage VT-d nested translation capability. Host vIOMMU
    works in first-level only translation and pkvm IOMMU works in nested
    translation. In this working mode, pkvm IOMMU use host vIOMMU
    first-level page table directly and re-use EPT as its second-level
    to reduce the memory consumption and simplify the implementations, which
    no need to shadow IO translation page tables. But we still need to
    shadow some paging strcture translation to guarantee host won't be able
    to bypass the second-level. The shadow includes the root/context/pasid page
    tables.
    
    Reuse the pgtable walker framework and take the
    ((bdf << MAX_NR_PASID_BITS) | passid) as the input vaddr for the walker.
    iommu_paging_ops provides the apis for walker to be able to walk the
    root/context/pasid page tables.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit 996ced6dd90c170cccaecae5a9009c13d4fb97c7)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -30,6 +30,8 @@ enum sm_level {
 	IOMMU_SM_LEVEL_NUM,
 };
 
+#define PASID_PTE_PRESENT	1
+#define PASID_PTE_FPD		2
 #define MAX_NR_PASID_BITS	PKVM_MAX_PASID_BITS
 
 #define PASIDTAB_BITS		6
@@ -59,5 +61,138 @@ struct pasid_dir_entry {
 struct pasid_entry {
 	u64 val[8];
 };
+
+static inline void entry_set_bits(u64 *ptr, u64 mask, u64 bits)
+{
+	u64 old;
+
+	old = READ_ONCE(*ptr);
+	WRITE_ONCE(*ptr, (old & ~mask) | bits);
+}
+
+/* Get PRESENT bit of a PASID table entry. */
+static inline bool pasid_pte_is_present(struct pasid_entry *pte)
+{
+	return READ_ONCE(pte->val[0]) & PASID_PTE_PRESENT;
+}
+
+/* Get PGTT field of a PASID table entry */
+static inline u16 pasid_pte_get_pgtt(struct pasid_entry *pte)
+{
+	return (u16)((READ_ONCE(pte->val[0]) >> 6) & 0x7);
+}
+
+/*
+ * Interfaces for PASID table entry manipulation:
+ */
+static inline void pasid_clear_entry(struct pasid_entry *pe)
+{
+	WRITE_ONCE(pe->val[0], 0);
+	WRITE_ONCE(pe->val[1], 0);
+	WRITE_ONCE(pe->val[2], 0);
+	WRITE_ONCE(pe->val[3], 0);
+	WRITE_ONCE(pe->val[4], 0);
+	WRITE_ONCE(pe->val[5], 0);
+	WRITE_ONCE(pe->val[6], 0);
+	WRITE_ONCE(pe->val[7], 0);
+}
+
+/*
+ * Get domain ID value of a scalable mode PASID entry.
+ */
+static inline u16
+pasid_get_domain_id(struct pasid_entry *pe)
+{
+	return (u16)(READ_ONCE(pe->val[1]) & GENMASK_ULL(15, 0));
+}
+
+/*
+ * Setup the SLPTPTR(Second Level Page Table Pointer) field (Bit 12~63)
+ * of a scalable mode PASID entry.
+ */
+static inline void
+pasid_set_slptr(struct pasid_entry *pe, u64 value)
+{
+	entry_set_bits(&pe->val[0], VTD_PAGE_MASK, value);
+}
+
+/*
+ * Setup the AW(Address Width) field (Bit 2~4) of a scalable mode PASID
+ * entry.
+ */
+static inline void
+pasid_set_address_width(struct pasid_entry *pe, u64 value)
+{
+	entry_set_bits(&pe->val[0], GENMASK_ULL(4, 2), value << 2);
+}
+
+/*
+ * Setup the PGTT(PASID Granular Translation Type) field (Bit 6~8)
+ * of a scalable mode PASID entry.
+ */
+static inline void
+pasid_set_translation_type(struct pasid_entry *pe, u64 value)
+{
+	entry_set_bits(&pe->val[0], GENMASK_ULL(8, 6), value << 6);
+}
+
+/*
+ * Setup Page Walk Snoop bit (Bit 87) of a scalable mode PASID
+ * entry.
+ */
+static inline void pasid_set_page_snoop(struct pasid_entry *pe, bool value)
+{
+	entry_set_bits(&pe->val[1], 1 << 23, value << 23);
+}
+
+/*
+ * Setup the Page Snoop (PGSNP) field (Bit 88) of a scalable mode
+ * PASID entry.
+ */
+static inline void
+pasid_set_pgsnp(struct pasid_entry *pe)
+{
+	entry_set_bits(&pe->val[1], 1ULL << 24, 1ULL << 24);
+}
+
+#define PASID_ENTRY_PGTT_FL_ONLY        (1)
+#define PASID_ENTRY_PGTT_SL_ONLY        (2)
+#define PASID_ENTRY_PGTT_NESTED         (3)
+#define PASID_ENTRY_PGTT_PT             (4)
+
+/*
+ * Set the Second Stage Execute Enable field (Bit 5) of a scalable mode
+ * PASID entry.
+ */
+static inline void pasid_set_ssee(struct pasid_entry *pe, bool value)
+{
+	entry_set_bits(&pe->val[0], 1 << 5, value << 5);
+}
+
+/*
+ * Set the Second Stage Access/Dirty bit Enable field (Bit 9) of a scalable mode
+ * PASID entry.
+ */
+static inline void pasid_set_ssade(struct pasid_entry *pe, bool value)
+{
+	entry_set_bits(&pe->val[0], 1 << 9, value << 9);
+}
+
+static inline bool pasid_copy_entry(struct pasid_entry *to, struct pasid_entry *from)
+{
+	bool updated = false;
+	int i;
+
+	for (i = 0; i < 8; i++) {
+		u64 new = READ_ONCE(from->val[i]);
+
+		if (READ_ONCE(to->val[i]) != new) {
+			WRITE_ONCE(to->val[i], new);
+			updated = true;
+		}
+	}
+
+	return updated;
+}
 
 #endif
