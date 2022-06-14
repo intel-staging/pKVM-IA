690f3258d378b76ff0249710f9c9bbaab2aba3e3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-06-14 14:58:39 +0800

    KVM: x86: Introduce vm_type to differentiate default VMs from protected VMs.
    This patch is leveraging the implementation submitted for TDX[1].
    
    Unlike default VMs, protected VMs don't allow some operations
    (e.g., memory read/write, register state access, etc). So Introduce
    vm_type to track the type of the VM to x86 KVM.  Other arch KVMs
    already use vm_type, KVM_INIT_VM accepts vm_type, and x86 KVM callback
    vm_init accepts vm_type. So follow them. Further, a different policy
    can be made based on vm_type. Define KVM_X86_DEFAULT_VM for default
    VM as default and define KVM_X86_PROTECTED_VM for pkvm.
    
    Add a capability KVM_CAP_VM_TYPES to effectively allow device model,
    e.g. qemu, to query what VM types are supported by KVM.  This
    (introduce a new capability and add vm_type) is chosen to align with
    other arch KVMs that have VM types already. Other arch KVMs uses different
    name to query supported vm types and there is no common name for it, so
    new name was chosen. Add a capability to effectively allow userspace to query
    what VM types are supported by KVM.
    
    --
    [1]https://lore.kernel.org/all/24bfec5cbb1ecb8b9626aea7379f7f1bd62ea954.1645266955.git.isaku.yamahata@intel.com/
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1122,6 +1122,7 @@ enum kvm_apicv_inhibit {
 };
 
 struct kvm_arch {
+	unsigned long vm_type;
 	unsigned long n_used_mmu_pages;
 	unsigned long n_requested_mmu_pages;
 	unsigned long n_max_mmu_pages;
@@ -1401,6 +1402,7 @@ struct kvm_x86_ops {
 	void (*hardware_unsetup)(void);
 	bool (*has_emulated_msr)(struct kvm *kvm, u32 index);
 	void (*vcpu_after_set_cpuid)(struct kvm_vcpu *vcpu);
+	bool (*is_vm_type_supported)(unsigned long vm_type);
 
 	unsigned int vm_size;
 	int (*vm_init)(struct kvm *kvm);
