bbf59c710e7a6c0418095172289444ff6518c00c
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-01 00:26:40 +0800

    pkvm: x86: memory operations for for host vm
    this patch add below memory operations for host vm:
    - gva2gpa
    - read_gva/write_gva
    - read_gpa/write_gpa
    such ops will be used later for vmx instruction emulation, for example,
    vmxon instruction will put vmxon area's point in a guest memory, it
    means pkvm hypervisor need read its content from a gva.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Haiwei Li <haiwei.li@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/memory.h
+++ arch/x86/kvm/vmx/pkvm/hyp/memory.h
@@ -12,4 +12,14 @@
 unsigned long pkvm_virt_to_symbol_phys(void *virt);
 #define __pkvm_pa_symbol(x) pkvm_virt_to_symbol_phys((void *)x)
 
+#include <linux/kvm_host.h>
+void *host_gpa2hva(unsigned long gpa);
+int gva2gpa(struct kvm_vcpu *vcpu, gva_t gva, gpa_t *gpa,
+		u32 access, struct x86_exception *exception);
+int read_gva(struct kvm_vcpu *vcpu, gva_t gva, void *addr,
+		unsigned int bytes, struct x86_exception *exception);
+int write_gva(struct kvm_vcpu *vcpu, gva_t gva, void *addr,
+		unsigned int bytes, struct x86_exception *exception);
+int read_gpa(struct kvm_vcpu *vcpu, gpa_t gpa, void *addr, unsigned int bytes);
+int write_gpa(struct kvm_vcpu *vcpu, gpa_t gpa, void *addr, unsigned int bytes);
 #endif
