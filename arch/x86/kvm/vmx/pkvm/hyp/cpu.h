e63f998f8d2f9a3e3226f49567766de0c2e5becd
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-02 00:15:15 +0800

    pkvm: x86: Add msr ops for pKVM hypervisor
    Add pkvm msr ops, avoid using Linux msr ops directly to remove the
    dependency of link to EXTABLE.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/cpu.h
+++ arch/x86/kvm/vmx/pkvm/hyp/cpu.h
@@ -13,6 +13,29 @@ static inline u64 pkvm_msr_read(u32 reg)
 	return (((u64)msrh << 32U) | msrl);
 }
 
+#define pkvm_rdmsr(msr, low, high)              \
+do {                                            \
+	u64 __val = pkvm_msr_read(msr);         \
+	(void)((low) = (u32)__val);             \
+	(void)((high) = (u32)(__val >> 32));    \
+} while (0)
+
+#define pkvm_rdmsrl(msr, val)                   \
+	((val) = pkvm_msr_read((msr)))
+
+static inline void pkvm_msr_write(u32 reg, u64 msr_val)
+{
+	asm volatile (" wrmsr " : : "c" (reg), "a" ((u32)msr_val), "d" ((u32)(msr_val >> 32U)));
+}
+
+#define pkvm_wrmsr(msr, low, high)              	\
+do {                                            	\
+	u64 __val = (u64)(high) << 32 | (u64)(low); 	\
+	pkvm_msr_write(msr, __val);             	\
+} while (0)
+
+#define pkvm_wrmsrl(msr, val)   pkvm_msr_write(msr, val)
+
 #ifdef CONFIG_PKVM_INTEL_DEBUG
 #include <linux/smp.h>
 static inline u64 get_pcpu_id(void)
