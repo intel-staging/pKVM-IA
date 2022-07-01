10186982d57407efef2731d45479ac7ad2804818
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-01 20:24:52 +0800

    pkvm: x86: Add early allocator based mm_ops
    The buddy page allocator need setting up based on virtual memory mapping
    (vmemmap), it is based on MMU page table. Meanwhile MMU page table
    setup for virtual memory mapping needs page allocator as well.
    
    Before pKVM setup buddy page allocator, uses a early page allocator
    based mm_ops for MMU page table.
    
    This patch adds such mm_ops based on early page allocator, and it
    will be used later for MMU page table early setup.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit 789b7b22b568a4ce3f27cd4a1b5df09fe15592a8)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/early_alloc.c
+++ arch/x86/kvm/vmx/pkvm/hyp/early_alloc.c
@@ -7,11 +7,20 @@
 #include <asm/pkvm_spinlock.h>
 #include <pkvm.h>
 
+#include "pgtable.h"
+
 static unsigned long base;
 static unsigned long end;
 static unsigned long cur;
 
 static pkvm_spinlock_t early_lock = __PKVM_SPINLOCK_UNLOCKED;
+
+struct pkvm_mm_ops pkvm_early_alloc_mm_ops;
+
+unsigned long pkvm_early_alloc_nr_used_pages(void)
+{
+	return (cur - base) >> PAGE_SHIFT;
+}
 
 void *pkvm_early_alloc_contig(unsigned int nr_pages)
 {
@@ -40,8 +49,25 @@ void *pkvm_early_alloc_page(void)
 	return pkvm_early_alloc_contig(1);
 }
 
+static void pkvm_early_alloc_get_page(void *addr) { }
+static void pkvm_early_alloc_put_page(void *addr) { }
+static void pkvm_early_flush_tlb(void) { }
+
+static int pkvm_early_page_count(void *vaddr)
+{
+	return 512;
+}
+
 void pkvm_early_alloc_init(void *virt, unsigned long size)
 {
 	base = cur = (unsigned long)virt;
 	end = base + size;
+
+	pkvm_early_alloc_mm_ops.zalloc_page = pkvm_early_alloc_page;
+	pkvm_early_alloc_mm_ops.get_page = pkvm_early_alloc_get_page;
+	pkvm_early_alloc_mm_ops.put_page = pkvm_early_alloc_put_page;
+	pkvm_early_alloc_mm_ops.phys_to_virt = pkvm_phys_to_virt;
+	pkvm_early_alloc_mm_ops.virt_to_phys = pkvm_virt_to_phys;
+	pkvm_early_alloc_mm_ops.page_count = pkvm_early_page_count;
+	pkvm_early_alloc_mm_ops.flush_tlb = pkvm_early_flush_tlb;
 }
