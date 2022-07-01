dfe059cd518514bf8673c75350892d6523549a02
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-02 00:31:45 +0800

    pkvm: x86: Add VMXON/VMXOFF emulation
    Host VM keep the capability to launch its guests based on VMX, pKVM
    need to provide VMX emulation for it. This includes emulations for
    different VMX instructions - VMXON/VMXOFF, VMPTRLD/VMCLEAR,
    VMWRITE/VMREAD, and VMRESUME/VMLAUNCH.
    
    This patch introduces nested.c, and provide emulation for VMXON and
    VMXOFF vmx instructions for host VM.
    
    The emulation simply does state check and revision id validation for
    vmxarea passed from VMXON/VMXOFF instructions, the physical VMX is kept
    as enabled after the pKVM initialization.
    
    More permission check still leaves as TODO.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/nested.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+#ifndef __PKVM_NESTED_H
+#define __PKVM_NESTED_H
+
+int handle_vmxon(struct kvm_vcpu *vcpu);
+int handle_vmxoff(struct kvm_vcpu *vcpu);
+
+#endif
