bb560db1fef7e2e0c748d5b56a57abf16fa6bf18
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-01 20:24:52 +0800

    pkvm: x86: add mm_ops for early allocator
    This patch adds mm_ops for early allocator, it prepares for mmu
    pgtable mapping.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/early_alloc.h
+++ arch/x86/kvm/vmx/pkvm/hyp/early_alloc.h
@@ -5,8 +5,11 @@
 #ifndef __PKVM_EARLY_ALLOC_H
 #define __PKVM_EARLY_ALLOC_H
 
+unsigned long pkvm_early_alloc_nr_used_pages(void);
 void *pkvm_early_alloc_contig(unsigned int nr_pages);
 void *pkvm_early_alloc_page(void);
 void pkvm_early_alloc_init(void *virt, unsigned long size);
+
+extern struct pkvm_mm_ops pkvm_early_alloc_mm_ops;
 
 #endif
