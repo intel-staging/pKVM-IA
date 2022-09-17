3d28d32487a4a84a685fe8a1817c4c76d71ac02a
Zide Chen <zide.chen@intel.com>
2022-09-16 22:42:29 -0700

    pkvm: x86: define linker script alias conditionally
    Define linker alias only if the associated symbols are actually
    built into the binary, otherwise linker could find errors similar to:
    
    vmlinux.lds:189: symbol not found: physical_mask
    vmlinux.lds:190: symbol not found: sme_me_mask
    
    Signed-off-by: Zide Chen <zide.chen@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/include/asm/pkvm_image_vars.h
+++ arch/x86/include/asm/pkvm_image_vars.h
@@ -6,8 +6,15 @@
 #define __ASM_x86_PKVM_IMAGE_VARS_H
 
 #ifndef CONFIG_PKVM_INTEL_DEBUG
+
+#ifdef CONFIG_DYNAMIC_PHYSICAL_MASK
 PKVM_ALIAS(physical_mask);
+#endif
+
+#ifdef CONFIG_AMD_MEM_ENCRYPT
 PKVM_ALIAS(sme_me_mask);
+#endif
+
 PKVM_ALIAS(__default_kernel_pte_mask);
 PKVM_ALIAS(vmcs12_field_offsets);
 PKVM_ALIAS(nr_vmcs12_fields);
