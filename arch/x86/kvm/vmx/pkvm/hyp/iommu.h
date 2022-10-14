4e7dc2cc907b1a79ed6aed8b3f215cab9d635f46
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-10-14 14:11:10 +0800

    pkvm: x86: do iotlb flushing when flushing tlb for host EPT
    Host EPT is used as the second-stage page table when IOMMU is in nested
    translation mode, as well as the second-stage only mode. When doing TLB
    flushing due to unmap from host EPT, also needs to do IOTLB flushing to
    make sure devices won't access the unmapped memory through stale IOTLB.
    
    If there is memory to bunch the IOTLB invalidation requests, will flush
    the IOTLB per domain with page selective if it is supported. If there is
    no memory to bunch the requests, then just simply to do a global IOTLB
    flushing.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.h
@@ -11,5 +11,6 @@ bool is_mem_range_overlap_iommu(unsigned long start, unsigned long end);
 int pkvm_activate_iommu(void);
 int pkvm_iommu_sync(u16 bdf, u32 pasid);
 bool pkvm_iommu_coherency(u16 bdf, u32 pasid);
+void pkvm_iommu_flush_iotlb(struct pkvm_pgtable *pgt, unsigned long addr, unsigned long size);
 
 #endif
