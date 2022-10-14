9b2ffbeaf3d7e3bb640ec61c6b448f978cbffd2e
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-10-14 13:47:12 +0800

    pkvm: x86: Reserve more QI descriptor pages for each CPU
    These new reserved QI descriptor pages for each CPU will be used at
    runtime to bunch the cache invalidation requests by the later patch.
    Bunching can help to reduce the invalidation counts so that pkvm can
    submit multiple QI requests at one time, which can improve the
    efficiency.
    
    The reserved QI descriptor page size is calculated according to the
    IOMMU QI descriptor size(excludes the QI descriptor status as this is
    not needed to bunch the requests) and the CPU number. Each CPU can have
    its own reserved QI descriptor page so that multiple CPUs can bunch the
    QI requests at the same time.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -232,7 +232,9 @@ u64 pkvm_total_reserve_pages(void)
 	total += host_ept_pgtable_pages();
 	total += pkvm_iommu_pages(PKVM_MAX_PASID, PKVM_PASIDDEV_NUM,
 				  PKVM_PDEV_NUM, PKVM_IOMMU_NUM,
-				  PKVM_IOMMU_QI_SIZE);
+				  PKVM_IOMMU_QI_DESC_SIZE,
+				  PKVM_IOMMU_QI_DESC_STATUS_SIZE,
+				  num_possible_cpus());
 	total += pkvm_shadow_ept_pgtable_pages(PKVM_MAX_VM_NUM);
 
 	return total;
