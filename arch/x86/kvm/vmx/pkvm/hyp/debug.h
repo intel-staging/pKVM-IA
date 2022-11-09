256b8c2e0d81466c4ce4a8e6307e3dfc2ef161eb
Jason Chen CJ <jason.cj.chen@intel.com>
2022-11-10 01:21:35 +0800

    pkvm: x86: Define empty debug functions for hypervisor
    Use pkvm_dbg/pkvm_info/pkvm_err as debug functions for pKVM runtime.
    
    Define all these debug functions as empty functions now, it's also the
    configuration of final release, as no additional debug console support
    in pKVM now and such runtime debug functions shall avoid to access
    host kernel debug APIs (e.g. printk) for isolation purpose.
    
    Future will enable a debug mode for pKVM by taking use of linux
    printk with some extra addon to access host kernel code - it's a
    tmp quick debug method as it does not follow above rules for isolation.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit cb4ee77ee6471f88b1c6f0696219d3a41b65e23e)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/debug.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+
+#ifndef _PKVM_DEBUG_H_
+#define _PKVM_DEBUG_H_
+
+#define pkvm_dbg(x...)
+#define pkvm_info(x...)
+#define pkvm_err(x...)
+
+#endif
