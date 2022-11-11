77758ad20a8906d609ab749391a52cbc0781b960
Jason Chen CJ <jason.cj.chen@intel.com>
2022-11-11 18:13:08 +0800

    pkvm: x86: Build pKVM runtime as an independent binary
    pKVM runtime is running under VMX root mode, need isolating
    from host Linux, so it shall be built as an independent binary.
    
    Add pKVM link script to build an independent binary with separated
    sections, and add prefix __pkvm for all its symbols to ensure host
    Linux will not touch it and meantime ensure pKVM will not touch
    host Linux symbols as well. For special case that Linux need to call
    into pKVM's functions during init time, provide macros PKVM_DECLARE
    & pkvm_sym to intently add __pkvm prefix.
    
    To simplify, remove ftrace, Shadow Call Stack, CFI CFLAGS, and
    disable stack protector. pKVM shall not export any symbols, so
    disable 'EXPORT_SYMBOL'.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit e958a687c2cf79fead3e54e1cb5852067aa7e3ac)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/include/asm/pkvm_image.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2022 Intel Corporation
+ */
+
+#ifndef __X86_INTEL_PKVM_IMAGE_H
+#define __X86_INTEL_PKVM_IMAGE_H
+
+#ifdef __PKVM_HYP__
+/* No prefix will be added */
+#define PKVM_DECLARE(type, f)	type f
+#define pkvm_sym(sym)		sym
+#else
+/* prefix is added by Makefile */
+#define PKVM_DECLARE(type, f)	type __pkvm_##f
+#define pkvm_sym(sym)		__pkvm_##sym
+#endif
+
+#define __PKVM_CONCAT(a, b)	a ## b
+#define PKVM_CONCAT(a, b)	__PKVM_CONCAT(a, b)
+
+#ifdef LINKER_SCRIPT
+
+#define PKVM_SECTION_NAME(NAME)	.pkvm##NAME
+
+#define PKVM_SECTION_SYMBOL_NAME(NAME) \
+	PKVM_CONCAT(__pkvm_section_, PKVM_SECTION_NAME(NAME))
+
+#define BEGIN_PKVM_SECTION(NAME)			\
+	PKVM_SECTION_NAME(NAME) : {			\
+		PKVM_SECTION_SYMBOL_NAME(NAME) = .;
+
+#define END_PKVM_SECTION				\
+	}
+
+#define PKVM_SECTION(NAME)			\
+	BEGIN_PKVM_SECTION(NAME)		\
+		*(NAME NAME##.*)		\
+	END_PKVM_SECTION
+
+#endif /* LINKER_SCRIPT */
+
+#endif /* __X86_INTEL_PKVM_IMAGE_H */
