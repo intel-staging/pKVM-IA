1d0328d7f7ee6c44c06809d49a3cf043892c3049
Jason Chen CJ <jason.cj.chen@intel.com>
2022-11-23 23:55:34 +0800

    pkvm: x86: Introduce MMU pgtable support
    Follow the native Linux setting, pKVM work under paging mode to
    access its memory, MMU page table need setting up.
    
    Add mmu.c for pKVM, which provide APIs to enable MMU pgtable mapping.
    
    As buddy page allocator is based on MMU pgtable mapping, MMU page table
    initialization is divided into two stages before and after buddy page
    allocator setup:
    - before buddy page allocator setup, MMU page table mm_ops is based on
      early page allocator, the initialization is done by
      pkvm_early_mmu_init(). After this initialization, MMU page table can
      be created, but its allocated pages usage is not tracked.
    - after buddy page allocator setup, the vmemmap MMU mapping is done
      through above early MMU, MMU page table mm_ops then can be switched to
      use buddy page allocator, and meantime the early allocated pages'
      refcount need fixing to ensure the track from buddy allocator.
    This patch only implements above first stage.
    
    Some alias of kernel-proper symbols also defined for pKVM usage, like
    physical_mask for page level mask, sme_me_mask & __default_kernel_pte_mask
    for page prot setting.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit bcb6b0cd57bb7c5862db18dbb05f2b36c94dc7fc)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/mmu.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+#ifndef _PKVM_MMU_H_
+#define _PKVM_MMU_H_
+
+int pkvm_mmu_map(unsigned long vaddr_start, unsigned long phys_start,
+		unsigned long size, int pgsz_mask, u64 prot);
+
+int pkvm_mmu_unmap(unsigned long vaddr_start, unsigned long phys_start,
+		unsigned long size);
+
+int pkvm_early_mmu_init(struct pkvm_pgtable_cap *cap,
+		void *mmu_pool_base, unsigned long mmu_pool_pages);
+
+#endif
