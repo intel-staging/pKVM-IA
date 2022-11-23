7ac58969337961e186f5a3d08da77b8499bdb643
Jason Chen CJ <jason.cj.chen@intel.com>
2022-11-23 23:55:34 +0800

    pkvm: x86: Introduce MMU pgtable support
    Follow the native Linux setting, pKVM work under paging mode to
    access its memory, MMU page table need setting up.
    
    Add mmu.c for pKVM, which provide APIs to enable MMU pgtable mapping.
    
    As buddy page allocator is based on MMU pgtable mapping, MMU page table
    initialization is divided into two stages before and after buddy page
    allocator setup:
    - before buddy page allocator setup, MMU page table mm_ops is based on
      early page allocator, the initialization is done by
      pkvm_early_mmu_init(). After this initialization, MMU page table can
      be created, but its allocated pages usage is not tracked.
    - after buddy page allocator setup, the vmemmap MMU mapping is done
      through above early MMU, MMU page table mm_ops then can be switched to
      use buddy page allocator, and meantime the early allocated pages'
      refcount need fixing to ensure the track from buddy allocator.
    This patch only implements above first stage.
    
    Some alias of kernel-proper symbols also defined for pKVM usage, like
    physical_mask for page level mask, sme_me_mask & __default_kernel_pte_mask
    for page prot setting.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/include/asm/pkvm_image_vars.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Intel Corporation
+ */
+#ifndef __ASM_x86_PKVM_IMAGE_VARS_H
+#define __ASM_x86_PKVM_IMAGE_VARS_H
+
+#ifdef CONFIG_DYNAMIC_PHYSICAL_MASK
+PKVM_ALIAS(physical_mask);
+#endif
+
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+PKVM_ALIAS(sme_me_mask);
+#endif
+
+PKVM_ALIAS(__default_kernel_pte_mask);
+
+#endif
