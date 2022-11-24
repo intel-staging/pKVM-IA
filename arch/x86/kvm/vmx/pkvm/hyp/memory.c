0184daa8c6842c67e4bf4b98c317b56c233f970a
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-11-24 16:34:34 +0800

    pkvm: x86: Introduce find_mem_range API
    Add find_mem_range API support to indicate if a memory address belonging
    to system memory. This is used to check if an EPT violation caused by
    memory accessing in the later patch.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/memory.c
+++ arch/x86/kvm/vmx/pkvm/hyp/memory.c
@@ -6,6 +6,8 @@
 #include <linux/types.h>
 #include <asm/kvm_pkvm.h>
 
+#include "memory.h"
+
 unsigned long __page_base_offset;
 unsigned long __symbol_base_offset;
 
@@ -25,4 +27,39 @@ unsigned long pkvm_virt_to_phys(void *virt)
 unsigned long pkvm_virt_to_symbol_phys(void *virt)
 {
 	return (unsigned long)virt - __symbol_base_offset;
+}
+
+bool find_mem_range(unsigned long addr, struct mem_range *range)
+{
+	int cur, left = 0, right = hyp_memblock_nr;
+	struct memblock_region *reg;
+	unsigned long end;
+
+	range->start = 0;
+	range->end = ULONG_MAX;
+
+	/* The list of memblock regions is sorted, binary search it */
+	while (left < right) {
+		cur = (left + right) >> 1;
+		reg = &hyp_memory[cur];
+		end = reg->base + reg->size;
+		if (addr < reg->base) {
+			right = cur;
+			range->end = reg->base;
+		} else if (addr >= end) {
+			left = cur + 1;
+			range->start = end;
+		} else {
+			range->start = reg->base;
+			range->end = end;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool mem_range_included(struct mem_range *child, struct mem_range *parent)
+{
+	return parent->start <= child->start && child->end <= parent->end;
 }
