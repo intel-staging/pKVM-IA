10a05a5c15a6395572628efe4dc18567c933f3c1
Jason Chen CJ <jason.cj.chen@intel.com>
2022-12-06 20:36:31 +0800

    pkvm: x86: Create MMU pgtable in init-finalise hypercall
    Create MMU pgtable for pKVM, this is the first stage to initialize
    MMU pgtable, which is based on early page allocator.
    
    Such early page allocator's memory pool is allocated from pKVM
    reserved memory, after removed data struct size which already be
    allocated during pkvm_init.
    
    Take use of pkvm_early_mmu_init() to initialize hyp_mmu pgtable in
    mmu.c, then do MMU page mapping based on it.
    
    The MMU page mapping is setup for each system memory block in
    hyp_memory[] array and each memory section from init-finalise hypercall
    parameter; the address translation is according to previous commit
    "Define hypervisor runtime VA/PA APIs". In summary, pKVM keep same
    direct & symbol mapping as Linux kernel.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    
    dmaluka@ This patch breaks the build: usage of __pkvm_pa_symbol() macro
    causes the following:
    
    ld.lld: error: undefined symbol: __pkvm_memcpy
    >>> referenced by usercopy_64.c
    >>>               vmlinux.o:(__pkvm___pkvm_init_finalise)
    
    even though __pkvm_pa_symbol() doesn't seem to do anything that may
    involve memcpy. After patch "pkvm: x86: Add memcpy lib" this problem
    disappears.
    
    To be checked what's going on.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    (cherry picked from commit d721540c747c63a98eb5cc7582f7f6b8a6270f95)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/include/pkvm.h
+++ arch/x86/kvm/vmx/pkvm/include/pkvm.h
@@ -48,10 +48,24 @@ struct pkvm_hyp {
 	struct pkvm_pgtable_cap mmu_cap;
 	struct pkvm_pgtable_cap ept_cap;
 
+	struct pkvm_pgtable *mmu;
+
 	struct pkvm_pcpu *pcpus[CONFIG_NR_CPUS];
 
 	struct pkvm_host_vm host_vm;
 };
+
+static inline struct pkvm_host_vcpu *vmx_to_pkvm_hvcpu(struct vcpu_vmx *vmx)
+{
+	return container_of(vmx, struct pkvm_host_vcpu, vmx);
+}
+
+static inline struct pkvm_host_vcpu *to_pkvm_hvcpu(struct kvm_vcpu *vcpu)
+{
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+
+	return vmx_to_pkvm_hvcpu(vmx);
+}
 
 struct pkvm_section {
 	unsigned long type;
