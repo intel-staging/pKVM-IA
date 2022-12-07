02af3ff5312259b472fc036d41a6eb66ee7ea0e1
Shaoqin Huang <shaoqin.huang@intel.com>
2022-12-07 00:36:12 -0800

    x86/tdx: Add ve_x86_ops to wrap different operation for virt exception
    Define a ve_x86_ops to provide different handler for virt exception
    related function. Later will add virt exception support for pkvm guest,
    which will use this ve_x86_ops.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -46,8 +46,6 @@ u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
 
 void tdx_get_ve_info(struct ve_info *ve);
 
-bool tdx_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve);
-
 void tdx_safe_halt(void);
 
 bool tdx_early_handle_ve(struct pt_regs *regs);
