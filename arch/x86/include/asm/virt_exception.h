6bc2f472d706391821b6b9601535ae11b00f2f8c
Shaoqin Huang <shaoqin.huang@intel.com>
2022-12-07 00:36:12 -0800

    x86/tdx: Move virt exception handler code to virt_exception.c
    The TDX using virt exception to handle mmio opeartion, since virt
    exception is a cpu feature, it can also being used by other cc_platform.
    So move the common virt exception handler code to virt_exception.c
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit a5ddc4a6a5beeb6c5d0f8390fc795d6432f4c34c)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/include/asm/virt_exception.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_VIRT_EXCEPTION_H
+#define _ASM_X86_VIRT_EXCEPTION_H
+
+#include <asm/ptrace.h>
+
+#ifndef __ASSEMBLY__
+
+/*
+ * Used by the #VE exception handler to gather the #VE exception
+ * info from the TDX module. This is a software only structure
+ * and not part of the TDX module/VMM ABI.
+ */
+struct ve_info {
+	u64 exit_reason;
+	u64 exit_qual;
+	/* Guest Linear (virtual) Address */
+	u64 gla;
+	/* Guest Physical Address */
+	u64 gpa;
+	u32 instr_len;
+	u32 instr_info;
+};
+
+int handle_mmio(struct pt_regs *regs, struct ve_info *ve);
+
+bool mmio_read(int size, unsigned long addr, unsigned long *val);
+bool mmio_write(int size, unsigned long addr, unsigned long val);
+
+#endif /* !__ASSEMBLY__ */
+#endif /* _ASM_X86_VIRT_EXCEPTION_H */
