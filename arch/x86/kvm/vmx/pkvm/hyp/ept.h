b4a85afa3a6d25cf2d9bb021ff81106782da96ed
Dmytro Maluka <dmy@semihalf.com>
2023-02-08 14:11:41 +0000

    pkvm: x86: Use shadow IOMMU page tables for legacy IOMMU
    Unlike scalable IOMMU, legacy IOMMU provides just one level of address
    translation. So in the cases when the host itself uses address
    translation for the given device (i.e. uses legacy IOMMU in
    non-passthrough mode), pKVM needs to shadow the host's vIOMMU page
    tables to ensure memory protection. This patch enables using shadow
    IOMMU page tables in legacy mode, replacing our temporary hack using
    vIOMMU page tables directly.
    
    TODO: this patch still does not ensure memory protection, since it
    does not implement syncing of shadow IOMMU page tables after a page is
    donated. Memory protection _usually_ takes effect some time later, when
    the (non-malicious) host itself triggers IOMMU sync. But since we do not
    trust the host, memory protection is not guaranteed at all.
    
    Performance considerations: as the host frequently flushes IOTLB when
    mapping/unmapping its DMA buffers, the shadow tables sync in pKVM during
    IOTLB flush emulation has high impact on performance. Fortunately the
    host typically uses page-selective IOTLB flushes. This patch implements
    two basic optimizations eliminating the biggest bottlenecks:
    1. Obvious one: for a page-selective flush, walk shadow page table
       entries for the needed pages only.
    2. For a page-selective or a domain-selective flush, walk root & context
       table entries for the needed BDFs only (i.e. for the ptdevs with the
       matching DID registered earlier), not the entire BDF space.
    
    Signed-off-by: Dmytro Maluka <dmy@semihalf.com>

--- arch/x86/kvm/vmx/pkvm/hyp/ept.h
+++ arch/x86/kvm/vmx/pkvm/hyp/ept.h
@@ -28,6 +28,8 @@ int pkvm_host_ept_map(unsigned long vaddr_start, unsigned long phys_start,
 		unsigned long size, int pgsz_mask, u64 prot);
 int pkvm_host_ept_unmap(unsigned long vaddr_start, unsigned long phys_start,
 		unsigned long size);
+void pkvm_host_ept_lookup(unsigned long vaddr, unsigned long *pphys,
+			  u64 *pprot, int *plevel);
 void pkvm_host_ept_destroy(void);
 int pkvm_host_ept_init(struct pkvm_pgtable_cap *cap, void *ept_pool_base,
 		unsigned long ept_pool_pages);
