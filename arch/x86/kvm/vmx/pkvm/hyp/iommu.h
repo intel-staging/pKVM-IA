ad869935a4956bff2df14afb2cf6cbbfe36a9b61
Chuanxiao Dong <chuanxiao.dong@intel.com>
2023-02-14 13:15:30 +0800

    pkvm: x86: Introduce iommu_coherency flag for ptdev
    This flag represents the coherency of the IOMMU unit this ptdev is on.
    It is configured when allocate a new ptdev, either during syncing IOMMU
    pasid/context or attaching a ptdev to a shadow VM. So a new interface is
    added in pKVM IOMMU driver to get the IOMMU coherency by bdf/pasid when
    attaching a ptdev. The current implementation does ptdev "get" and
    "allocate"(if not existing) in one single function pkvm_get_ptdev(). As
    ptdev "get" doesn't need to configure iommu_coherency and only "allocate"
    does, separate the "allocate" from pkvm_get_ptdev().
    
    The iommu_coherency flag is used by the next patch to configure the
    mm_ops for this ptdev's IOMMU page table.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit a6fc0fe82b4363ef9428f0f532f01c350e50f8cd)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.h
@@ -10,5 +10,6 @@ unsigned long pkvm_access_iommu(bool is_read, int len, unsigned long reg, unsign
 bool is_mem_range_overlap_iommu(unsigned long start, unsigned long end);
 int pkvm_activate_iommu(void);
 int pkvm_iommu_sync(u16 bdf, u32 pasid);
+bool pkvm_iommu_coherency(u16 bdf, u32 pasid);
 
 #endif
