808138a2451ef323df4aad06d270beade71cd448
Tinghao Zhang <tinghao.zhang@intel.com>
2023-03-20 23:01:26 -0700

    pkvm: introduce port I/O emulation framework
    Add io_emulate.c and its header to place I/O emulation code for host vm.
    
    Introduce a new data structure pkvm_pio_emul_table to store port I/O
    handler. Add a helper function to register port I/O handler into the
    emulation table. Define host_pio_emul_table for host port I/O emulation.
    
    Signed-off-by: Tinghao Zhang <tinghao.zhang@intel.com>
    (cherry picked from commit 5a5aaa21e9c2e103e2c35b43667aa385a6c27f0a)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/io_emulate.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2023 Intel Corporation
+ */
+#ifndef _PKVM_IO_EMULATE_H_
+#define _PKVM_IO_EMULATE_H_
+
+/* I/O direction */
+#define PKVM_IO_READ 0
+#define PKVM_IO_WRITE 1
+
+/* Max num of port I/O emulation handlers */
+#define PKVM_MAX_PIO_EMUL_NUM 32
+
+struct pkvm_pio_req {
+	unsigned int port;
+	int size;
+	bool direction;
+	unsigned long *value;
+};
+
+typedef int (*pio_handler_t)(struct kvm_vcpu *, struct pkvm_pio_req *);
+
+struct pkvm_pio_handler {
+	unsigned int port;
+	int size_mask;
+	pio_handler_t read;
+	pio_handler_t write;
+};
+
+struct pkvm_pio_emul_table {
+	struct pkvm_pio_handler table[PKVM_MAX_PIO_EMUL_NUM];
+	DECLARE_BITMAP(bitmap, PKVM_MAX_PIO_EMUL_NUM);
+};
+
+int register_host_pio_handler(struct pkvm_host_vm *host_vm, unsigned int port,
+	unsigned int size_mask, pio_handler_t read, pio_handler_t write);
+
+#endif
