840ae5d9b0c1ff5761560874096e6f8ef210cac6
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-11-06 13:37:26 +0800

    pkvm: x86: Introduce def.h to control defines for pkvm hypervisor
    Introduce a def.h to control defines for pkvm hypervisor. It is included
    on top of each pkvm hypervisor source code so that the defines in def.h
    can take effact at the beginning. This could be used by pkvm to undefine
    some unnecessary linux kernel configs to avoid introducing the unneeded
    symbols by using linux kernel macros, or re-define a linux kernel macro
    with pkvm's own implementation.
    
    The kernel config CONFIG_DEBUG_PREEMPT is undefined. This is used for
    the smp_processor_id() macro defined in linux kernel but is unnecessary
    when it is used by the pkvm hypervisor to get the CPU id.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/kvm/pkvm/def.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_DEF_H
+#define __PKVM_X86_DEF_H
+
+/*
+ * Special hack: pKVM runs in the highest privilege level, which is higher than
+ * the linux kernel. This means that pKVM cannot use any of the linux kernel
+ * symbols. To make pKVM being able to use the linux kernel headers without
+ * introducing additional symbols, some kernel configuration options are
+ * disabled. (This list needs to be extended when new variants are added.)
+ */
+#undef CONFIG_DEBUG_PREEMPT
+
+#endif /* __PKVM_X86_DEF_H */
