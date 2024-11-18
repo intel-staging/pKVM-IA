c2e37e1eb4a9cfcabde9c24f9a74cf8ddfb433b3
Dmytro Maluka <dmaluka@google.com>
2024-11-18 15:55:16 +0000

    modpost: add .pkvm.text to authorized sections to jump on fault
    Fix noisy modpost warnings caused by _ASM_EXTABLE() usage in pKVM's
    __vmcs_readl():
    
      WARNING: modpost: vmlinux: section mismatch in reference:
        (unknown)+0x2574 (section: __ex_table) ->  (section: .pkvm.text)
      WARNING: modpost: The relocation at __ex_table+0x2574 references
      section ".pkvm.text" which is not in the list of
      authorized sections.  If you're adding a new section
      and/or if this reference is valid, add ".pkvm.text" to the
      list of authorized sections to jump to on fault.
      This can be achieved by adding ".pkvm.text" to
      OTHER_TEXT_SECTIONS in scripts/mod/modpost.c.
    
    Fixes: 26a720df76fc ("pkvm: x86: Fix an issue with asm_goto_output usage")
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    (cherry picked from commit c3593573dedcc7ed78873dd1826cae1ef91cadbc)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -785,7 +785,7 @@ static void check_section(const char *modname, struct elf_info *elf,
 		".ltext", ".ltext.*"
 #define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
 		".fixup", ".entry.text", ".exception.text", \
-		".coldtext", ".softirqentry.text"
+		".coldtext", ".softirqentry.text", ".pkvm.text"
 
 #define ALL_TEXT_SECTIONS  ".init.text", ".exit.text", \
 		TEXT_SECTIONS, OTHER_TEXT_SECTIONS
@@ -812,9 +812,9 @@ enum mismatch {
  * @mismatch: Type of mismatch.
  */
 struct sectioncheck {
-	const char *fromsec[20];
-	const char *bad_tosec[20];
-	const char *good_tosec[20];
+	const char *fromsec[32];
+	const char *bad_tosec[32];
+	const char *good_tosec[32];
 	enum mismatch mismatch;
 };
 
