26fae0ef979cc21fb4c041e0fe0806cb1a236437
Dmytro Maluka <dmaluka@google.com>
2024-11-20 17:18:59 +0000

    pkvm: x86: Add pkvm_iommu_types.h for pkvm_constants
    Similarly to the previous commit, move the iommu related definitions
    used by pkvm_constants.c to a separate header, so that the host code
    does not include the hyp internal definitions it doesn't need.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -9,9 +9,7 @@
 #include <asm/pkvm.h>
 #include <asm/pkvm_spinlock.h>
 #include "pgtable.h"
-
-#define PKVM_QI_DESC_ALIGNED_SIZE		ALIGN(QI_LENGTH * sizeof(struct qi_desc), PAGE_SIZE)
-#define PKVM_QI_DESC_STATUS_ALIGNED_SIZE	ALIGN(QI_LENGTH * sizeof(int), PAGE_SIZE)
+#include "pkvm_iommu_types.h"
 
 struct viommu_reg {
 	u64 cap;
