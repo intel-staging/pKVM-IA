416c68e70356824aafceee8cbea654bf7f7eb430
Dmytro Maluka <dmaluka@google.com>
2024-11-21 19:22:58 +0000

    pkvm: x86: Copy pvmfw memory region into pVM memory
    Similarly to pKVM-ARM, when donating a page that is within the pvmfw
    address region in the pVM guest memory, copy corresponding page from the
    pvmfw region in the hypervisor memory into this guest page.
    
    Also squashing a minor fix from Dmytro:
    https://android.googlesource.com/kernel/common/+/5da3664c763928d8940249d8e7bf96a4886095c0/arch/x86/kvm/vmx/pkvm/hyp/pkvm.c#554
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    (cherry picked from commit 62d13254298abebca8e83252cfdb759b8bddef46)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/pkvm.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pkvm.c
@@ -543,3 +543,18 @@ int pkvm_add_ptdev(int shadow_vm_handle, u16 bdf, u32 pasid)
 
 	return ret;
 }
+
+int pkvm_load_pvmfw_pages(struct pkvm_shadow_vm *vm, u64 gpa, u64 phys, u64 size)
+{
+	u64 offset = gpa - vm->pvmfw_load_addr;
+
+	if (offset >= pvmfw_size)
+		return -EINVAL;
+
+	size = min(size, pvmfw_size - offset);
+	if (!PAGE_ALIGNED(size) || !PAGE_ALIGNED(offset))
+		return -EINVAL;
+
+	memcpy(__pkvm_va(phys), __pkvm_va(pvmfw_base + offset), size);
+	return 0;
+}
