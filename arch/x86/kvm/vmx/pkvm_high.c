97b26fa0a9fff397ff56f4c61d4cad88f3969212
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-12-17 08:05:39 +0800

    kvm: pkvm: Remove the host state settings from the pkvm_vcpu_load
    With the vcpu_run PV method, the VMLAUNCH/VMRESUME are not used any more
    thus the pkvm hypervisor won't use the nested code to emulation these
    instructions. The host state settings in the pkvm_vcpu_load can be
    removed.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_high.c
+++ arch/x86/kvm/vmx/pkvm_high.c
@@ -301,34 +301,8 @@ static void pkvm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	kvm_call_pkvm(vcpu_load, vcpu, cpu);
 
-	/*
-	 * FIXME: Currently the host VMM still uses VMLAUNCH/VMRESUME
-	 * instruction to run vcpu, which will trap to the pkvm hypervisor to
-	 * emulate. Once the emulation is done, the host VMM state will be
-	 * restored from VMCS12 before returning back to the host VMM. To make
-	 * sure VMCS12 having the right state of the host VMM, set below VMCS
-	 * fields in the host VMM. This can be removed once the vcpu run is
-	 * changed to the PV method.
-	 */
-	if (!already_loaded) {
-		void *gdt = get_current_gdt_ro();
-
-		/*
-		 * Linux uses per-cpu TSS and GDT, so set these when switching
-		 * processors.  See 22.2.4.
-		 */
-		vmcs_writel(HOST_TR_BASE,
-			    (unsigned long)&get_cpu_entry_area(cpu)->tss.x86_tss);
-		vmcs_writel(HOST_GDTR_BASE, (unsigned long)gdt);   /* 22.2.4 */
-
-		if (IS_ENABLED(CONFIG_IA32_EMULATION) || IS_ENABLED(CONFIG_X86_32)) {
-			/* 22.2.3 */
-			vmcs_writel(HOST_IA32_SYSENTER_ESP,
-				    (unsigned long)(cpu_entry_stack(cpu) + 1));
-		}
-
+	if (!already_loaded)
 		vmx->loaded_vmcs->cpu = cpu;
-	}
 
 	vmx_vcpu_pi_load(vcpu, cpu);
 }
