9e4526910f246cabc1165db98d97e74281e8a1ee
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-01-22 16:23:07 +0800

    pkvm: vmx: Handle monitor trap vmexit
    The monitor trap vmexit is handled by following the linux VMX code
    implementation.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/vmx/vmx.c
+++ arch/x86/kvm/pkvm/vmx/vmx.c
@@ -4874,8 +4874,7 @@ static int handle_pause(struct kvm_vcpu *vcpu)
 
 static int handle_monitor_trap(struct kvm_vcpu *vcpu)
 {
-	/* TODO */
-	return 0;
+	return 1;
 }
 
 static int handle_invpcid(struct kvm_vcpu *vcpu)
