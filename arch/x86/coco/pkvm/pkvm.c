61c01462776db730cba31b0225c16a3233c8c2ca
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-02-25 18:10:50 +0800

    x86/pkvm: Use hypercall to access MMIO
    Overwrite the MMIO PV ops with pkvm's virt MMIO operations which will
    use the hypercall to access the MMIO.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/coco/pkvm/pkvm.c
+++ arch/x86/coco/pkvm/pkvm.c
@@ -5,6 +5,7 @@
 
 #include <linux/cpufeature.h>
 #include <linux/kvm_para.h>
+#include <linux/io.h>
 #include <asm/coco.h>
 #include <asm/vmx.h>
 #include <asm/pkvm.h>
@@ -100,6 +101,86 @@ static bool pkvm_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
 	return true;
 }
 
+static unsigned long pkvm_virt_mmio(int size, bool write, unsigned long vaddr,
+				    unsigned long *val)
+{
+	unsigned long paddr;
+	pte_t *pte;
+	int level;
+
+	pte = lookup_address(vaddr, &level);
+	if (!pte)
+		return -EIO;
+
+	paddr = (pte_pfn(*pte) << PAGE_SHIFT) | (vaddr & ~page_level_mask(level));
+
+	return write ? mmio_write(size, paddr, *val) : mmio_read(size, paddr, val);
+}
+
+static unsigned char pkvm_mmio_readb(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (!pkvm_virt_mmio(1, false, (unsigned long)addr, &val))
+		return 0xff;
+	return val;
+}
+
+static unsigned short pkvm_mmio_readw(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (!pkvm_virt_mmio(2, false, (unsigned long)addr, &val))
+		return 0xffff;
+	return val;
+}
+
+static unsigned int pkvm_mmio_readl(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (!pkvm_virt_mmio(4, false, (unsigned long)addr, &val))
+		return 0xffffffff;
+	return val;
+}
+
+static u64 pkvm_mmio_readq(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (!pkvm_virt_mmio(8, false, (unsigned long)addr, &val))
+		return 0xffffffffffffffff;
+	return val;
+}
+
+static void pkvm_mmio_writeb(unsigned char v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(1, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writew(unsigned short v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(2, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writel(unsigned int v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(4, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writeq(u64 v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(8, true, (unsigned long)addr, &val);
+}
+
 __init void pkvm_guest_init_coco(void)
 {
 	cc_vendor = CC_VENDOR_PKVM;
@@ -110,4 +191,23 @@ __init void pkvm_guest_init_coco(void)
 	ve_x86_ops.mmio_write = mmio_write;
 	ve_x86_ops.handle_virt_exception = pkvm_handle_virt_exception;
 	ve_x86_ops.get_ve_info = pkvm_get_ve_info;
+
+	pv_ops.mmio.raw_readb = pkvm_mmio_readb;
+	pv_ops.mmio.raw_readw = pkvm_mmio_readw;
+	pv_ops.mmio.raw_readl = pkvm_mmio_readl;
+	pv_ops.mmio.raw_readb_relaxed = pkvm_mmio_readb;
+	pv_ops.mmio.raw_readw_relaxed = pkvm_mmio_readw;
+	pv_ops.mmio.raw_readl_relaxed = pkvm_mmio_readl;
+	pv_ops.mmio.raw_writeb = pkvm_mmio_writeb;
+	pv_ops.mmio.raw_writew = pkvm_mmio_writew;
+	pv_ops.mmio.raw_writel = pkvm_mmio_writel;
+	pv_ops.mmio.raw_writeb_relaxed = pkvm_mmio_writeb;
+	pv_ops.mmio.raw_writew_relaxed = pkvm_mmio_writew;
+	pv_ops.mmio.raw_writel_relaxed = pkvm_mmio_writel;
+#ifdef CONFIG_X86_64
+	pv_ops.mmio.raw_readq = pkvm_mmio_readq;
+	pv_ops.mmio.raw_readq_relaxed = pkvm_mmio_readq;
+	pv_ops.mmio.raw_writeq = pkvm_mmio_writeq;
+	pv_ops.mmio.raw_writeq_relaxed = pkvm_mmio_writeq;
+#endif
 }
