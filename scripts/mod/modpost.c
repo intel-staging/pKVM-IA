f566d214eb06b3c33f8b405ddce3d83d94f33a42
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-03-03 16:22:38 +0800

    modpost: add pkvm init/noinstr text to authorized sections
    modpost can warn due to the _ASM_EXTABLE() usage in pkvm's text section
    which is fixed by c2e37e1eb4a9. Beside the pkvm.text, the pkvm
    hypervisor also has .pkvm.init.text and .pkvm.noinstr.text section which
    require similar fix. So extend the modpost to authorize these two text
    as well.
    
    TODO: Fix this by supporting __ex_table in the pkvm hypervisor?
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -785,7 +785,8 @@ static void check_section(const char *modname, struct elf_info *elf,
 		".ltext", ".ltext.*"
 #define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
 		".fixup", ".entry.text", ".exception.text", \
-		".coldtext", ".softirqentry.text", ".pkvm.text"
+		".coldtext", ".softirqentry.text", ".pkvm.text", \
+		".pkvm.init.text", ".pkvm.noinstr.text"
 
 #define ALL_TEXT_SECTIONS  ".init.text", ".exit.text", \
 		TEXT_SECTIONS, OTHER_TEXT_SECTIONS
