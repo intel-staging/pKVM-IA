eca350caa6f72688897598a02d38bc7b68981077
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-03-11 14:13:44 +0800

    pkvm: vmx: Handle interrupt window vmexit
    The interrupt window vmexit is handled by the pkvm hypervisor to clear
    CPU_BASED_INTR_WINDOW_EXITING. Then return back to the host make request
    KVM_REQ_EVENT to inject any pending interrupt.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_high.c
+++ arch/x86/kvm/vmx/pkvm_high.c
@@ -331,8 +331,6 @@ static int handle_tpr_below_threshold(struct kvm_vcpu *vcpu)
 
 static int handle_interrupt_window(struct kvm_vcpu *vcpu)
 {
-	exec_controls_clearbit(to_vmx(vcpu), CPU_BASED_INTR_WINDOW_EXITING);
-
 	kvm_make_request(KVM_REQ_EVENT, vcpu);
 
 	++vcpu->stat.irq_window_exits;
