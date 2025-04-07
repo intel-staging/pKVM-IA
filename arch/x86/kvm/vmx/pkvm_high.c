intel-staging/pKVM-IA

commit 6024308d1c304e655e329aa0c0a629dc70e52833

kvm: pkvm: Use PV interface to do vcpu reset
Add vcpu_reset PV interface for the host KVM to reset the vcpu in the
pkvm hypervisor. As the host KVM still emulates the virtual lapic, the
cr8 and lapic are still handled by the host KVM.

Once the guest state is protected, resetting vcpu is not allowed via
this PV interface.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_high.c
@@ -290,6 +290,24 @@ static void pkvm_vcpu_free(struct kvm_vcpu *vcpu)
 	free_ve_info(vmx);
 }
 
+static void pkvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
+{
+	if (!vcpu->arch.guest_state_protected)
+		kvm_call_pkvm(vcpu_reset, vcpu, init_event);
+
+	/*
+	 * The CR0/CR4 guest-owned/rsvd bits are controlled by the pkvm
+	 * hypervisor. The host VMM can assume all the bits in CR0/CR4 are owned
+	 * by the guest.
+	 */
+	vcpu->arch.cr0_guest_owned_bits = ~0;
+	vcpu->arch.cr4_guest_owned_bits = ~0;
+	vcpu->arch.cr4_guest_rsvd_bits = 0;
+
+	kvm_set_cr8(vcpu, 0);
+	kvm_make_request(KVM_REQ_APIC_PAGE_RELOAD, vcpu);
+}
+
 static void pkvm_prepare_switch_to_guest(struct kvm_vcpu *vcpu) {}
 
 static void pkvm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
@@ -535,7 +553,7 @@ struct kvm_x86_ops pkvm_host_x86_ops __initdata = {
 	.vcpu_precreate = vmx_vcpu_precreate,
 	.vcpu_create = pkvm_vcpu_create,
 	.vcpu_free = pkvm_vcpu_free,
-	.vcpu_reset = vmx_vcpu_reset,
+	.vcpu_reset = pkvm_vcpu_reset,
 
 	.prepare_switch_to_guest = pkvm_prepare_switch_to_guest,
 	.vcpu_load = pkvm_vcpu_load,

arch/x86/include/asm/kvm_pkvm.h
@@ -261,6 +261,7 @@ enum pkvm_fn {
 	__pkvm__vm_init,
 	__pkvm__vm_destroy,
 	__pkvm__vcpu_create,
+	__pkvm__vcpu_reset,
 	__pkvm__vcpu_load,
 	__pkvm__vcpu_put,
 	__pkvm__vcpu_run,

arch/x86/kvm/pkvm/pkvm.c
@@ -516,6 +516,14 @@ static unsigned long pkvm_vcpu_after_set_cpuid(struct pkvm_vcpu *pkvm_vcpu, unsi
 	return ret;
 }
 
+static void pkvm_reset_vcpu(struct pkvm_vcpu *pkvm_vcpu, bool init_event)
+{
+	if (WARN_ON_ONCE(!pkvm_vcpu))
+		return;
+
+	kvm_vcpu_reset(to_kvm_vcpu(pkvm_vcpu), init_event);
+}
+
 static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 					       struct kvm_vcpu *shared_vcpu,
 					       unsigned long p2, unsigned  long p3)
@@ -537,6 +545,9 @@ static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 		break;
 	case __pkvm__vcpu_after_set_cpuid:
 		ret = pkvm_vcpu_after_set_cpuid(pkvm_vcpu, p2);
+		break;
+	case __pkvm__vcpu_reset:
+		pkvm_reset_vcpu(pkvm_vcpu, (bool)p2);
 		break;
 	default:
 		ret = -EINVAL;

commit 2d0269ccff8c968e3ac54e0fdfc55b9f5f38c89e

kvm: pkvm: Initialize pi_desc
The host response to inject interrupts to the guest. The pi_desc is the
key structure for the host to inject interrupts via the posted interrupt
mechanism. Its physical address is used for the POSTED_INTR_DESC_ADDR in
the VMCS by the pkvm hypervisor. Initialize the pi_desc when reset vcpu.

Fixes: 6024308d1c30 ("kvm: pkvm: Use PV interface to do vcpu reset")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_high.c
@@ -899,8 +899,20 @@ static void pkvm_vcpu_free(struct kvm_vcpu *vcpu)
 
 static void pkvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 {
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+
 	if (!vcpu->arch.guest_state_protected)
 		kvm_call_pkvm(vcpu_reset, vcpu, init_event);
+
+	/*
+	 * The host response to inject interrupts to the guest. The pi_desc is
+	 * the key structure for the host to inject interrupts via the posted
+	 * interrupt mechanism. Its physical address is used for the
+	 * POSTED_INTR_DESC_ADDR in the VMCS by the pkvm hypervisor. Initialize
+	 * the pi_desc when reset vcpu.
+	 */
+	vmx->pi_desc.nv = POSTED_INTR_VECTOR;
+	__pi_set_sn(&vmx->pi_desc);
 
 	/*
 	 * The CR0/CR4 guest-owned/rsvd bits are controlled by the pkvm