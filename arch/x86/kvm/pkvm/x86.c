intel-staging/pKVM-IA

commit e2f430e2b53c65208ab23c3dd8c8bf7955efdfc1

pkvm: x86: Switch the debug registers
Switch the debug registers in the pkvm hypervisor. Before entering the
guest, load the debug registers with the guest value. Before exiting to
the host, save the guest debug registers. Meanwhile clears the debug
registers to avoid leaking register values to the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/x86.c
@@ -2,6 +2,7 @@
 #include "x86.h"
 #include <asm/fpu/xcr.h>
 #include <asm/kvm_pkvm.h>
+#include <asm/debugreg.h>
 #include <cpuid.h>
 #include <linux/user-return-notifier.h>
 #include <trace.h>
@@ -3275,6 +3276,15 @@ unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_e
 
 	/* TODO: Save the host VMM fpu and load the guest fpu */
 
+	/* Switch to the guest debug registers. */
+	if (unlikely(vcpu->arch.switch_db_regs)) {
+		set_debugreg(0, 7);
+		set_debugreg(vcpu->arch.eff_db[0], 0);
+		set_debugreg(vcpu->arch.eff_db[1], 1);
+		set_debugreg(vcpu->arch.eff_db[2], 2);
+		set_debugreg(vcpu->arch.eff_db[3], 3);
+	}
+
 	for (;;) {
 		bool req_immediate_exit = false;
 
@@ -3326,6 +3336,21 @@ unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_e
 	}
 
 	kvm_x86_call(prepare_switch_to_host)(vcpu);
+
+	/* Save and clear the guest debug registers */
+	if (unlikely(vcpu->arch.switch_db_regs & KVM_DEBUGREG_WONT_EXIT)) {
+		WARN_ON(vcpu->guest_debug & KVM_GUESTDBG_USE_HW_BP);
+		kvm_x86_call(sync_dirty_debug_regs)(vcpu);
+
+		kvm_update_dr0123(vcpu);
+		set_debugreg(0, 0);
+		set_debugreg(0, 1);
+		set_debugreg(0, 2);
+		set_debugreg(0, 3);
+
+		kvm_update_dr7(vcpu);
+		set_debugreg(0, 7);
+	}
 
 	/* TODO: Restore the host VMM fpu and save the guest fpu */
 

arch/x86/kvm/pkvm/vmx/vmx.c
@@ -5011,6 +5011,25 @@ out:
 	return kvm_complete_insn_gp(vcpu, err);
 }
 
+void vmx_sync_dirty_debug_regs(struct kvm_vcpu *vcpu)
+{
+	get_debugreg(vcpu->arch.db[0], 0);
+	get_debugreg(vcpu->arch.db[1], 1);
+	get_debugreg(vcpu->arch.db[2], 2);
+	get_debugreg(vcpu->arch.db[3], 3);
+	get_debugreg(vcpu->arch.dr6, 6);
+	vcpu->arch.dr7 = vmcs_readl(GUEST_DR7);
+
+	vcpu->arch.switch_db_regs &= ~KVM_DEBUGREG_WONT_EXIT;
+	exec_controls_setbit(to_vmx(vcpu), CPU_BASED_MOV_DR_EXITING);
+
+	/*
+	 * exc_debug expects dr6 to be cleared after it runs, avoid that it sees
+	 * a stale dr6 from the guest.
+	 */
+	set_debugreg(DR6_RESERVED, 6);
+}
+
 void vmx_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
 {
 	vmcs_writel(GUEST_DR7, val);
@@ -8024,6 +8043,7 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.get_gdt = vmx_get_gdt,
 	.set_gdt = vmx_set_gdt,
 	.set_dr7 = vmx_set_dr7,
+	.sync_dirty_debug_regs = vmx_sync_dirty_debug_regs,
 	.cache_reg = vmx_cache_reg,
 	.get_rflags = vmx_get_rflags,
 	.set_rflags = vmx_set_rflags,

commit 5d11f08019ac4fd413780ecfe4c8c6026972cc2e

pkvm: x86: Enhance the debug registers switching
Before entering the vcpu run loop, save the current debug registers for
the host as the debug registers may be overwriting with the guest debug
registers. And restoring those debug registers after exiting the vcpu
run loop.

The guest debug registers are loaded before vcpu running if it is
necessary. And syncing the dirty debug registers for the guest in the
vcpu run loop so that the up-to-date dr variables in the vcpu->arch can
be referenced.

Fixes: e2f430e2b53c ("pkvm: x86: Switch the debug registers")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/x86.c
@@ -3264,8 +3264,9 @@ static void kvm_restore_user_return_msr(void)
 
 unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit)
 {
+	struct kvm_vcpu *hvcpu = this_cpu_read(host_vcpu);
 	fastpath_t exit_fastpath;
-	int ret;
+	int ret, i;
 
 	pkvm_reset_reqs_to_host(vcpu);
 
@@ -3276,14 +3277,10 @@ unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_e
 
 	/* TODO: Save the host VMM fpu and load the guest fpu */
 
-	/* Switch to the guest debug registers. */
-	if (unlikely(vcpu->arch.switch_db_regs)) {
-		set_debugreg(0, 7);
-		set_debugreg(vcpu->arch.eff_db[0], 0);
-		set_debugreg(vcpu->arch.eff_db[1], 1);
-		set_debugreg(vcpu->arch.eff_db[2], 2);
-		set_debugreg(vcpu->arch.eff_db[3], 3);
-	}
+	/* Save the host debug registers */
+	get_debugreg(hvcpu->arch.dr7, 7);
+	for (i = 0; i < KVM_NR_DB_REGS; i++)
+		get_debugreg(hvcpu->arch.db[i], i);
 
 	for (;;) {
 		bool req_immediate_exit = false;
@@ -3313,7 +3310,23 @@ unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_e
 		else
 			req_immediate_exit = force_immediate_exit;
 
+		if (unlikely(vcpu->arch.switch_db_regs)) {
+			set_debugreg(0, 7);
+			set_debugreg(vcpu->arch.eff_db[0], 0);
+			set_debugreg(vcpu->arch.eff_db[1], 1);
+			set_debugreg(vcpu->arch.eff_db[2], 2);
+			set_debugreg(vcpu->arch.eff_db[3], 3);
+		}
+
 		exit_fastpath = kvm_x86_call(vcpu_run)(vcpu, req_immediate_exit);
+
+		/* Sync the guest debug registers */
+		if (unlikely(vcpu->arch.switch_db_regs & KVM_DEBUGREG_WONT_EXIT)) {
+			WARN_ON(vcpu->guest_debug & KVM_GUESTDBG_USE_HW_BP);
+			kvm_x86_call(sync_dirty_debug_regs)(vcpu);
+			kvm_update_dr0123(vcpu);
+			kvm_update_dr7(vcpu);
+		}
 
 		/*
 		 * Make sure vcpu->mode is changed to OUTSIDE_GUEST_MODE after
@@ -3337,20 +3350,10 @@ unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_e
 
 	kvm_x86_call(prepare_switch_to_host)(vcpu);
 
-	/* Save and clear the guest debug registers */
-	if (unlikely(vcpu->arch.switch_db_regs & KVM_DEBUGREG_WONT_EXIT)) {
-		WARN_ON(vcpu->guest_debug & KVM_GUESTDBG_USE_HW_BP);
-		kvm_x86_call(sync_dirty_debug_regs)(vcpu);
-
-		kvm_update_dr0123(vcpu);
-		set_debugreg(0, 0);
-		set_debugreg(0, 1);
-		set_debugreg(0, 2);
-		set_debugreg(0, 3);
-
-		kvm_update_dr7(vcpu);
-		set_debugreg(0, 7);
-	}
+	/* Restore the host debug registers */
+	set_debugreg(hvcpu->arch.dr7, 7);
+	for (i = 0; i < KVM_NR_DB_REGS; i++)
+		set_debugreg(hvcpu->arch.db[i], i);
 
 	/* TODO: Restore the host VMM fpu and save the guest fpu */
 