intel-staging/pKVM-IA

commit b8c84942a7ac058f5116a58c2a6069e5498094da

kvm: pkvm: Use PV interface to set dr7
Add set_dr7 PV interface for the host to set dr7. Once the guest state
is protected, the dr7 is not allowed to be set via this PV interface.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -649,6 +649,14 @@ static void pkvm_access_idt_gdt(struct pkvm_vcpu *pkvm_vcpu, struct desc_ptr *de
 	}
 }
 
+static void pkvm_set_dr7(struct pkvm_vcpu *pkvm_vcpu, unsigned long val)
+{
+	if (WARN_ON_ONCE(!pkvm_vcpu))
+		return;
+
+	kvm_x86_call(set_dr7)(to_kvm_vcpu(pkvm_vcpu), val);
+}
+
 static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 					       struct kvm_vcpu *shared_vcpu,
 					       unsigned long p2, unsigned  long p3)
@@ -715,6 +723,9 @@ static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 	case __pkvm__set_gdt:
 		pkvm_access_idt_gdt(pkvm_vcpu, (struct desc_ptr *)kern_pkvm_va((void *)p2),
 				    true, false);
+		break;
+	case __pkvm__set_dr7:
+		pkvm_set_dr7(pkvm_vcpu, p2);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -278,6 +278,7 @@ enum pkvm_fn {
 	__pkvm__set_idt,
 	__pkvm__get_gdt,
 	__pkvm__set_gdt,
+	__pkvm__set_dr7,
 };
 
 union pkvm_pv_param {

arch/x86/kvm/vmx/pkvm_high.c
@@ -687,6 +687,14 @@ static void pkvm_set_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
 	pkvm_access_idt_gdt(vcpu, dt, true, false);
 }
 
+static void pkvm_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
+{
+	if (vcpu->arch.guest_state_protected)
+		return;
+
+	kvm_call_pkvm(set_dr7, vcpu, val);
+}
+
 void vmx_do_nmi_irqoff(void);
 
 static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
@@ -922,7 +930,7 @@ struct kvm_x86_ops pkvm_host_x86_ops __initdata = {
 	.set_idt = pkvm_set_idt,
 	.get_gdt = pkvm_get_gdt,
 	.set_gdt = pkvm_set_gdt,
-	.set_dr7 = vmx_set_dr7,
+	.set_dr7 = pkvm_set_dr7,
 	.sync_dirty_debug_regs = vmx_sync_dirty_debug_regs,
 	.cache_reg = vmx_cache_reg,
 	.get_rflags = vmx_get_rflags,

commit 5d604d15ccbe4e7654688307d98339420ace335b

pkvm: x86: Update switch_db_regs when set DR7
The switch_db_regs should be updated according to the DR7 value. It is
used by the pkvm hypervisor to determine if loads the guest debug regs
or not.

Fixes: b8c84942a7ac ("kvm: pkvm: Use PV interface to set dr7")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -829,10 +829,17 @@ static void pkvm_access_idt_gdt(struct pkvm_vcpu *pkvm_vcpu, struct desc_ptr *de
 
 static void pkvm_set_dr7(struct pkvm_vcpu *pkvm_vcpu, unsigned long val)
 {
+	unsigned long dr7 = val;
+	struct kvm_vcpu *vcpu;
+
 	if (WARN_ON_ONCE(!pkvm_vcpu))
 		return;
 
-	kvm_x86_call(set_dr7)(to_kvm_vcpu(pkvm_vcpu), val);
+	vcpu = to_kvm_vcpu(pkvm_vcpu);
+	kvm_x86_call(set_dr7)(vcpu, dr7);
+	vcpu->arch.switch_db_regs &= ~KVM_DEBUGREG_BP_ENABLED;
+	if (dr7 & DR7_BP_EN_MASK)
+		vcpu->arch.switch_db_regs |= KVM_DEBUGREG_BP_ENABLED;
 }
 
 static unsigned long pkvm_get_rflags(struct pkvm_vcpu *pkvm_vcpu)