intel-staging/pKVM-IA

commit da30eac3d7017b5a7faced76235ae9e9f7822cf1

kvm: pkvm: Use PV interface to inject exception for the npVM
Add inject_exception PV interface for the host to inject exceptions to
the npVM. This would make the host KVM being able to emulate more
exceptions for the npVM. Host injecting exception to the pVM is still
not allowed. Do KVM_BUG_ON for the pVM to cache these cases for the
future investigation.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_high.c
@@ -776,6 +776,7 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit
 		mds_clear_cpu_buffers();
 
 	vcpu->arch.nmi_injected = false;
+	kvm_clear_exception_queue(vcpu);
 	kvm_clear_interrupt_queue(vcpu);
 
 	vmx->exit_reason.full = 0xdead;
@@ -814,6 +815,9 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit
 		vmx->loaded_vmcs->vnmi_blocked_time +=
 			ktime_to_ns(ktime_sub(ktime_get(),
 					      vmx->loaded_vmcs->entry_time));
+
+	if (vcpu->arch.exception.injected)
+		kvm_make_request(KVM_REQ_EVENT, vcpu);
 
 	exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
 	if (reqs_to_host) {
@@ -860,6 +864,13 @@ static void pkvm_inject_nmi(struct kvm_vcpu *vcpu)
 	++vcpu->stat.nmi_injections;
 
 	kvm_call_pkvm(inject_nmi, vcpu);
+}
+
+static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
+{
+	KVM_BUG_ON(pkvm_is_protected_vcpu(vcpu), vcpu->kvm);
+
+	kvm_call_pkvm(inject_exception, vcpu);
 }
 
 static int pkvm_interrupt_allowed(struct kvm_vcpu *vcpu, bool for_injection)
@@ -1062,7 +1073,7 @@ struct kvm_x86_ops pkvm_host_x86_ops __initdata = {
 	.patch_hypercall = vmx_patch_hypercall,
 	.inject_irq = pkvm_inject_irq,
 	.inject_nmi = pkvm_inject_nmi,
-	.inject_exception = vmx_inject_exception,
+	.inject_exception = pkvm_inject_exception,
 	.cancel_injection = vmx_cancel_injection,
 	.interrupt_allowed = pkvm_interrupt_allowed,
 	.nmi_allowed = pkvm_nmi_allowed,

arch/x86/include/asm/kvm_pkvm.h
@@ -292,6 +292,7 @@ enum pkvm_fn {
 	__pkvm__nmi_allowed,
 	__pkvm__inject_irq,
 	__pkvm__inject_nmi,
+	__pkvm__inject_exception,
 };
 
 #define HOST_HANDLE_EXIT		0

arch/x86/kvm/pkvm/pkvm.c
@@ -580,6 +580,14 @@ static void pkvm_vcpu_share_state_to_host(struct pkvm_vcpu *pkvm_vcpu)
 		shared_vcpu->arch.cr4 = kvm_read_cr4(vcpu);
 		kvm_register_mark_available(shared_vcpu, VCPU_EXREG_CR4);
 		shared_vcpu->arch.efer = vcpu->arch.efer;
+
+		/*
+		 * Share the exception information to the host if there is any
+		 */
+		if (vcpu->arch.exception.pending || vcpu->arch.exception.injected) {
+			shared_vcpu->arch.exception = vcpu->arch.exception;
+			kvm_clear_exception_queue(vcpu);
+		}
 	}
 
 	pkvm_x86_call(sync_vcpu_state_pre_switch)(pkvm_vcpu);
@@ -916,6 +924,23 @@ static void pkvm_inject_nmi(struct pkvm_vcpu *pkvm_vcpu)
 	kvm_x86_call(inject_nmi)(to_kvm_vcpu(pkvm_vcpu));
 }
 
+static void pkvm_inject_exception(struct pkvm_vcpu *pkvm_vcpu)
+{
+	struct kvm_vcpu *vcpu, *shared_vcpu;
+
+	if (WARN_ON_ONCE(!pkvm_vcpu))
+		return;
+
+	vcpu = to_kvm_vcpu(pkvm_vcpu);
+	if (WARN_ON_ONCE(pkvm_is_protected_vcpu(vcpu)))
+		return;
+
+	shared_vcpu = pkvm_vcpu->shared_vcpu;
+	vcpu->arch.exception = shared_vcpu->arch.exception;
+
+	kvm_x86_call(inject_exception)(vcpu);
+}
+
 static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 					       struct kvm_vcpu *shared_vcpu,
 					       unsigned long p2, unsigned  long p3)
@@ -1024,6 +1049,9 @@ static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 		break;
 	case __pkvm__inject_nmi:
 		pkvm_inject_nmi(pkvm_vcpu);
+		break;
+	case __pkvm__inject_exception:
+		pkvm_inject_exception(pkvm_vcpu);
 		break;
 	default:
 		ret = -EINVAL;

commit b1bc2f6ec0e25990f991126c349e4b79ea387af6

kvm: pkvm: Check exception.pending
The pkvm hypervisor will share the un-injected exception information to
the host. So it is possible either the exception.pending or injected
being set. Check both these flags to determin if needs to make a event
request.

Fixes: da30eac3d701 ("kvm: pkvm: Use PV interface to inject exception for the npVM")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_high.c
@@ -1418,7 +1418,7 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit
 			ktime_to_ns(ktime_sub(ktime_get(),
 					      vmx->loaded_vmcs->entry_time));
 
-	if (vcpu->arch.exception.injected)
+	if (vcpu->arch.exception.pending || vcpu->arch.exception.injected)
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 
 	exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;