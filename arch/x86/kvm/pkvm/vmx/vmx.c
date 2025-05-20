intel-staging/pKVM-IA

commit f4df71744a99f673c376a58fb4a9572d0145f35d

kvm: pkvm: Add skip instruction function
For the pVM, skipping instruction from the host KVM side is not allowed
by the pkvm hypervisor.

For the npVM, as there is no VMCS in the host KVM, the instruction has
to be skipped via emulating rather than getting the instruction length
from the VMCS.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/vmx.c
@@ -5107,6 +5107,7 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 	case PKVM_GHC_IOREAD:
 	case PKVM_GHC_IOWRITE:
 		/* Hypercall for MMIO accessing should be forwared to the host */
+		kvm_skip_emulated_instruction(vcpu);
 		return 0;
 	default:
 		/* The other hypercalls are not supported */

arch/x86/kvm/vmx/pkvm_high.c
@@ -1440,6 +1440,20 @@ static int pkvm_handle_exit(struct kvm_vcpu *vcpu, fastpath_t exit_fastpath)
 	return ret;
 }
 
+static int pkvm_skip_emulated_instruction(struct kvm_vcpu *vcpu)
+{
+	if (vcpu->arch.guest_state_protected)
+		return 1;
+
+	if (!kvm_emulate_instruction(vcpu, EMULTYPE_SKIP))
+		return 0;
+
+	/* skipping an emulated instruction also counts */
+	kvm_call_pkvm(set_interrupt_shadow, vcpu, 0);
+
+	return 1;
+}
+
 static void pkvm_update_emulated_instruction(struct kvm_vcpu *vcpu) {}
 
 static void pkvm_set_interrupt_shadow(struct kvm_vcpu *vcpu, int mask)
@@ -1879,7 +1893,7 @@ struct kvm_x86_ops pkvm_host_x86_ops __initdata = {
 	.vcpu_pre_run = vmx_vcpu_pre_run,
 	.vcpu_run = pkvm_vcpu_run,
 	.handle_exit = pkvm_handle_exit,
-	.skip_emulated_instruction = vmx_skip_emulated_instruction,
+	.skip_emulated_instruction = pkvm_skip_emulated_instruction,
 	.update_emulated_instruction = pkvm_update_emulated_instruction,
 	.set_interrupt_shadow = pkvm_set_interrupt_shadow,
 	.get_interrupt_shadow = pkvm_get_interrupt_shadow,

arch/x86/kvm/x86.c
@@ -10058,12 +10058,6 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 		break;
 	}
 
-	/*
-	 * We assume calling this function will always success which will update
-	 * the GUEST_RIP to skip the current instruction.
-	 */
-	static_call(kvm_x86_skip_emulated_instruction)(vcpu);
-
 	return ret;
 }
 

commit 64036565a16e48cf33ce3844ea81ca05ca17636a

kvm: pkvm: Fix using sleeping function when skip instruction
The host can skip the instruction for the npVM by emulating which
accesses the guest memory. However this may happen in the irq-disabled
context for the WRMSR vmexit, and the code of accessing the guest memory
will result in following bugs:

[ 1739.883856] BUG: sleeping function called from invalid context at include/linux/uaccess.h:106
[ 1739.885906] in_atomic(): 1, irqs_disabled(): 1, non_block: 0, pid: 1218, name: crosvm_vcpu1
[ 1739.887909] preempt_count: 1, expected: 0
[ 1739.888759] RCU nest depth: 0, expected: 0
[ 1739.889008] 2 locks held by crosvm_vcpu1/1218:
[ 1739.889275]  #0: ffff88813bf780b8 (&vcpu->mutex){+.+.}-{3:3}, at: kvm_vcpu_ioctl+0x89/0xa20
[ 1739.889774]  #1: ffff88813bdfa070 (&kvm->srcu){.+.+}-{0:0}, at: handle_fastpath_set_msr_irqoff+0x77/0x310
[ 1739.890341] irq event stamp: 3043066
[ 1739.890576] hardirqs last  enabled at (3043065): [<ffffffff81067942>] vcpu_run+0xb22/0x15c0
[ 1739.891065] hardirqs last disabled at (3043066): [<ffffffff81067942>] vcpu_run+0xb22/0x15c0
[ 1739.891560] softirqs last  enabled at (3043014): [<ffffffff811ad29d>] __irq_exit_rcu+0x7d/0xa0
[ 1739.892066] softirqs last disabled at (3043009): [<ffffffff811ad29d>] __irq_exit_rcu+0x7d/0xa0
[ 1739.892576] CPU: 5 UID: 0 PID: 1218 Comm: crosvm_vcpu1 Tainted: G        W          6.12.0-pkvm-google+ #187
[ 1739.893154] Tainted: [W]=WARN
[ 1739.893337] Hardware name: QEMU Standard PC (Q35 + ICH9, 2009), BIOS rel-1.16.3-0-ga6ed6b701f0a-prebuilt.qemu.org 04/01/2014
[ 1739.893991] Call Trace:
[ 1739.894143]  <TASK>
[ 1739.894276]  dump_stack_lvl+0x9f/0x100
[ 1739.894527]  dump_stack+0x14/0x20
[ 1739.894732]  __might_resched+0x16f/0x290
[ 1739.894971]  __might_sleep+0x47/0x70
[ 1739.895189]  __might_fault+0x34/0x90
[ 1739.895409]  __kvm_read_guest_page+0x77/0xd0
[ 1739.895669]  kvm_vcpu_read_guest_page+0x39/0x50
[ 1739.895942]  kvm_fetch_guest_virt+0x91/0xd0
[ 1739.896196]  __do_insn_fetch_bytes+0x1ae/0x230
[ 1739.896467]  x86_decode_insn+0xce/0x14b0
[ 1739.896704]  ? ring_buffer_unlock_commit+0x2d/0x170
[ 1739.896999]  x86_decode_emulated_instruction+0x3f/0xf0
[ 1739.897306]  x86_emulate_instruction+0x710/0x720
[ 1739.897589]  kvm_emulate_instruction+0x1b/0x30
[ 1739.897859]  pkvm_skip_emulated_instruction+0x54/0x80
[ 1739.898161]  kvm_skip_emulated_instruction+0x25/0x60
[ 1739.898475]  pkvm_complete_emulated_msr+0x35/0x60
[ 1739.898759]  handle_fastpath_set_msr_irqoff+0x1ed/0x310
[ 1739.899072]  pkvm_vcpu_run+0x39d/0x540
[ 1739.899301]  vcpu_run+0xb22/0x15c0
[ 1739.899513]  ? kvm_skip_emulated_instruction+0x3b/0x60
[ 1739.899822]  kvm_arch_vcpu_ioctl_run+0x1f2/0x710
[ 1739.900099]  ? kvm_arch_vcpu_ioctl_run+0x1f2/0x710
[ 1739.900390]  kvm_vcpu_ioctl+0x239/0xa20
[ 1739.900625]  ? __fget_files+0xc6/0x1a0
[ 1739.900858]  __x64_sys_ioctl+0x99/0xd0
[ 1739.901087]  x64_sys_call+0x108d/0x2060
[ 1739.901321]  do_syscall_64+0x6d/0x140
[ 1739.901546]  entry_SYSCALL_64_after_hwframe+0x76/0x7e
[ 1739.901847] RIP: 0033:0x7faa23124ded
[ 1739.902065] Code: 04 25 28 00 00 00 48 89 45 c8 31 c0 48 8d 45 10 c7 45 b0 10 00 00 00 48 89 45 b8 48 8d 45 d0 48 89 45 c0 b8 10 00 00 00 0f 05 <89> c2 3d 00 f0 ff ff 77 1a 48 8b 45 c8 64 48 2b 04 25 28 00 00 00
[ 1739.903165] RSP: 002b:00007fa9a25aa360 EFLAGS: 00000246 ORIG_RAX: 0000000000000010
[ 1739.903613] RAX: ffffffffffffffda RBX: 0000000000000000 RCX: 00007faa23124ded
[ 1739.904031] RDX: 0000000000000000 RSI: 000000000000ae80 RDI: 0000000000000099
[ 1739.904451] RBP: 00007fa9a25aa3b0 R08: 000055ba74a9cc00 R09: 0000000000000000
[ 1739.904869] R10: 0000000000000000 R11: 0000000000000246 R12: 000055ba74a8e2e8
[ 1739.905289] R13: fffffffffffffdb8 R14: 000000000000001d R15: 00007fa9a25ad4c0
[ 1739.905717]  </TASK>

To resolve this, share the vmexit instruction length information of the
npVM with the host for certain vmexit reasons to simplify instruction
skip by advancing the rip directly.

Fixes: f4df71744a99 ("kvm: pkvm: Add skip instruction function")

arch/x86/kvm/pkvm/vmx/vmx.c
@@ -7866,6 +7866,8 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 	if (pkvm_is_protected_vcpu(vcpu))
 		return;
 
+	shared_vcpu->arch.event_exit_inst_len = 0;
+
 	switch (to_vmx(vcpu)->exit_reason.basic) {
 	case EXIT_REASON_EXCEPTION_NMI:
 		if ((vmx_get_intr_info(vcpu) & INTR_INFO_VECTOR_MASK) == DB_VECTOR) {
@@ -7876,6 +7878,15 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 	case EXIT_REASON_EPT_MISCONFIG:
 	case EXIT_REASON_EPT_VIOLATION:
 		to_vmx(shared_vcpu)->exit_gpa = vmcs_read64(GUEST_PHYSICAL_ADDRESS);
+		fallthrough;
+	case EXIT_REASON_IO_INSTRUCTION:
+	case EXIT_REASON_MSR_READ:
+	case EXIT_REASON_MSR_WRITE:
+	case EXIT_REASON_VMCALL:
+		/* The host will skip the instruction for certain vmexit reasons */
+		shared_vcpu->arch.event_exit_inst_len = vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
+		break;
+	default:
 		break;
 	}
 }

arch/x86/kvm/vmx/pkvm_high.c
@@ -1523,8 +1523,24 @@ static int pkvm_skip_emulated_instruction(struct kvm_vcpu *vcpu)
 	if (vcpu->arch.guest_state_protected)
 		return 1;
 
-	if (!kvm_emulate_instruction(vcpu, EMULTYPE_SKIP))
+	if (vcpu->arch.event_exit_inst_len) {
+		unsigned long rip, orig_rip;
+
+		orig_rip = kvm_rip_read(vcpu);
+		rip = orig_rip + vcpu->arch.event_exit_inst_len;
+#ifdef CONFIG_X86_64
+		/*
+		 * We need to mask out the high 32 bits of RIP if not in 64-bit
+		 * mode, but just finding out that we are in 64-bit mode is
+		 * quite expensive.  Only do it if there was a carry.
+		 */
+		if (unlikely(((rip ^ orig_rip) >> 31) == 3) && !is_64_bit_mode(vcpu))
+			rip = (u32)rip;
+#endif
+		kvm_rip_write(vcpu, rip);
+	} else if (!kvm_emulate_instruction(vcpu, EMULTYPE_SKIP)) {
 		return 0;
+	}
 
 	/* skipping an emulated instruction also counts */
 	kvm_call_pkvm(set_interrupt_shadow, vcpu, 0);