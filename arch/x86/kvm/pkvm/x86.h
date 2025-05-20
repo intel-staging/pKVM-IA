121be973cae7c26d100c8d20271b50788c861caa
Zhiyuan Lv <zhiyuan.lv@intel.com>
2025-05-20 14:58:06 +0800

    pkvm: x86: Remove x86.[c,h] in pkvm as they are not needed
    Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>

--- arch/x86/kvm/pkvm/x86.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __PKVM_X86_H
-#define __PKVM_X86_H
-
-#include <x86.h>
-
-unsigned long kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit);
-
-#endif /* __PKVM_X86_H */
