6185c8cf06309ec16deefca4c997be26582fa9b3
Dmytro Maluka <dmaluka@google.com>
2025-06-18 16:49:30 +0000

    ANDROID: pkvm: x86: Don't allocate vEPT in host
    Nail in the coffin of no-more-used virtual EPT page tables in KVM-high:
    don't allocate page table roots, and don't create kmem caches used to
    allocate page tables and auxiliary data structures for them.
    
    Since pKVM does not currently support SMM emulation and thus doesn't
    need to support multiple physical address spaces for the guest, it
    maintains the same stage-2 page table root throughout the VM lifecycle,
    i.e. doesn't need to support switching between different roots.
    So kvm_mmu_load() and kvm_mmu_unload() become almost no-ops. The most
    important thing kvm_mmu_load() still needs to do is to set the initial
    guest CR3 value at the VM startup.
    
    In kvm_mmu_load(), set mmu->root.hpa to a fake valid address 0 for
    simplicity, just to avoid the need to adjust various code in KVM that
    requires it to be different from INVALID_PAGE (see kvm_mmu_reload() as
    an example). In kvm_mmu_unload(), reset it to INVALID_PAGE for mere
    consistency.
    
    Bug: 391541180
    Test: run various VMs
    Upstream-Task: 402758258
    
    Change-Id: I8a0200cf3cf7577c26d820dfed4b10e2202689ca
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -5900,6 +5900,12 @@ int kvm_mmu_load(struct kvm_vcpu *vcpu)
 {
 	int r;
 
+	if (enable_pkvm) {
+		vcpu->arch.mmu->root.hpa = 0;	/* fake valid hpa */
+		kvm_mmu_load_pgd(vcpu);
+		return 0;
+	}
+
 	r = mmu_topup_memory_caches(vcpu, !vcpu->arch.mmu->root_role.direct);
 	if (r)
 		goto out;
@@ -5932,6 +5938,11 @@ out:
 void kvm_mmu_unload(struct kvm_vcpu *vcpu)
 {
 	struct kvm *kvm = vcpu->kvm;
+
+	if (enable_pkvm) {
+		vcpu->arch.mmu->root.hpa = INVALID_PAGE;
+		return;
+	}
 
 	kvm_mmu_free_roots(kvm, &vcpu->arch.root_mmu, KVM_MMU_ROOTS_ALL);
 	WARN_ON_ONCE(VALID_PAGE(vcpu->arch.root_mmu.root.hpa));
@@ -7478,8 +7489,10 @@ int kvm_mmu_vendor_module_init(void)
 {
 	int ret = -ENOMEM;
 
-	if (enable_pkvm)
+	if (enable_pkvm) {
 		pkvm_mmu_vendor_module_init();
+		return 0;
+	}
 
 	/*
 	 * MMU roles use union aliasing which is, generally speaking, an
@@ -7535,6 +7548,9 @@ void kvm_mmu_destroy(struct kvm_vcpu *vcpu)
 
 void kvm_mmu_vendor_module_exit(void)
 {
+	if (enable_pkvm)
+		return;
+
 	mmu_destroy_caches();
 	percpu_counter_destroy(&kvm_total_used_mmu_pages);
 	shrinker_free(mmu_shrinker);
