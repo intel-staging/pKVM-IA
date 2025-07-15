95eac7499553717a130a056764b9c9f0ba0e5d19
Dmytro Maluka <dmaluka@google.com>
2025-07-15 17:54:06 +0000

    ANDROID: pkvm: x86: Disable dirty logging
    For simplicity PV MMU will not initially support dirty logging, as there
    are no known pKVM use cases for it so far. Implementing it would require
    some extra complexity:
    
    - adding pKVM hypercall to modify page permissions
    - modifying KVM-high code paths to use this hypercall
    - to make it fast, add missing PML support in pKVM hypervisor
    
    Bug: 391541180
    Test: compile, boot and run VMs
    Upstream-Task: 402758258
    
    Change-Id: I58ce5d2074bbdbe8b9ac3e71e84734a44a148a3f
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1597,6 +1597,12 @@ static int check_memory_region_flags(struct kvm *kvm,
 	if (mem->flags & KVM_MEM_GUEST_MEMFD)
 		valid_flags &= ~KVM_MEM_LOG_DIRTY_PAGES;
 
+#ifdef CONFIG_PKVM_INTEL
+	/* pKVM on x86 does not currently support dirty logging. */
+	if (enable_pkvm)
+		valid_flags &= ~KVM_MEM_LOG_DIRTY_PAGES;
+#endif
+
 	/*
 	 * GUEST_MEMFD is incompatible with read-only memslots, as writes to
 	 * read-only memslots have emulated MMIO, not page fault, semantics,
