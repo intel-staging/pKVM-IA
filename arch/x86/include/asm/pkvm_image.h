319174c503e1e0e08c3006a67335d11959e41e8c
Zhiyuan Lv <zhiyuan.lv@intel.com>
2025-07-23 17:24:48 +0800

    pkvm: x86: Correctly patch call thunks for pkvm
    The call thunks patching with MITIGATION_CALL_DEPTH_TRACKING will patch
    kernel code with an assembly instruction which will use per-cpu
    variable. The patch needs to use pkvm defined variable for pkvm
    hypervisor code.
    
    Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>

--- arch/x86/include/asm/pkvm_image.h
+++ arch/x86/include/asm/pkvm_image.h
@@ -2,6 +2,8 @@
 #ifndef _ASM_X86_PKVM_IMAGE_H
 #define _ASM_X86_PKVM_IMAGE_H
 
+#include <linux/types.h>
+
 #if defined(__PKVM_HYP__)
 /*
  * For the pkvm hypervisor code to use the pkvm hypervisor symbols.
@@ -45,5 +47,19 @@
 	END_PKVM_SECTION
 
 #endif /* LINKER_SCRIPT */
+
+#ifndef __ASSEMBLER__
+
+#ifdef CONFIG_PKVM_X86
+extern char __pkvm_text_start[], __pkvm_text_end[];
+static inline bool is_pkvm_text(void *addr)
+{
+	return addr >= (void *)__pkvm_text_start && addr < (void *)__pkvm_text_end;
+}
+#else
+static inline bool is_pkvm_text(void *addr) { return false; }
+#endif
+
+#endif
 
 #endif /* _ASM_X86_PKVM_IMAGE_H */
