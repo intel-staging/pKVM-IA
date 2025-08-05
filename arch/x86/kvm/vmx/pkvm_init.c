c0bde1bf12cb7b503e57d5bcb16f0b96c31305f8
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-05 12:48:04 +0800

    pKVM: VMX: Deprivilege the host
    As the VMCS and the vmexit handlers are both ready, setup the guest
    RFLAGS/RSP/RIP to deprivilege the linux to non-root mode via the
    vmlaunch.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -450,8 +450,30 @@ static __init int pkvm_host_init_vmx(struct vcpu_vmx *vmx, struct pkvm_hyp *pkvm
 
 static noinline int local_deprivilege_cpu(void)
 {
-	/* TODO */
-	return -EINVAL;
+	int ret;
+
+	asm volatile(
+		"pushfq\n"
+		"popq %%rax\n"
+		"movq %3, %%rdx\n"
+		"vmwrite %%rax, %%rdx\n"
+		"movq %%rsp, %%rax\n"
+		"movq %4, %%rdx\n"
+		"vmwrite %%rax, %%rdx\n"
+		"movq $host_vm_entry_point, %%rax\n"
+		"movq %1, %%rdx\n"
+		"vmwrite %%rax, %%rdx\n"
+		"movl $0, %0\n"
+		"vmlaunch\n"
+		/* vmlaunch failed */
+		"movl %2, %0\n"
+		/* successfully deprivileged */
+		"host_vm_entry_point: nop\n"
+		: "=m"(ret)
+		: "i"(GUEST_RIP), "i"(-EINVAL), "i"(GUEST_RFLAGS), "i"(GUEST_RSP)
+		: "rax", "rdx", "memory");
+
+	return ret;
 }
 
 static __init void pkvm_host_deprivilege_cpu(void *data)
