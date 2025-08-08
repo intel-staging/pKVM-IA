5963b0672060a276619bd5a4663c85c20e7d55db
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-08 10:02:07 +0800

    pkvm: vmx: Setup vmcs config for deprivilege purpose
    Setup the vmcs config with the desired settings for deprivilege purpose.
    These settings are chosen to minimize VM exits for the deprivileged host
    thereby providing the best possible performance.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -11,9 +11,59 @@ static int __init early_pkvm_parse_cmdline(char *buf)
 }
 early_param("kvm-intel.pkvm", early_pkvm_parse_cmdline);
 
+static struct vmcs_config host_vmcs_config;
+
 u64 pkvm_total_reserve_pages(void)
 {
 	return pkvm_vmx_data_pages();
+}
+
+static __init int pkvm_setup_host_vmcs_config(void)
+{
+	struct vmcs_config *vmcs_config = &host_vmcs_config;
+	struct vmx_capability *vmx_cap = &vmx_capability;
+	struct vmcs_config_setting setting = {
+		.cpu_based_vm_exec_ctrl_req =
+			CPU_BASED_USE_MSR_BITMAPS |
+			CPU_BASED_ACTIVATE_SECONDARY_CONTROLS,
+		.cpu_based_vm_exec_ctrl_opt = 0,
+		.secondary_vm_exec_ctrl_req =
+			SECONDARY_EXEC_ENABLE_EPT,
+		.secondary_vm_exec_ctrl_opt =
+			SECONDARY_EXEC_ENABLE_VPID |
+			SECONDARY_EXEC_ENABLE_INVPCID |
+			SECONDARY_EXEC_ENABLE_XSAVES |
+			SECONDARY_EXEC_ENABLE_RDTSCP |
+			SECONDARY_EXEC_ENABLE_USR_WAIT_PAUSE,
+		.tertiary_vm_exec_ctrl_opt = 0,
+		.pin_based_vm_exec_ctrl_req = 0,
+		.pin_based_vm_exec_ctrl_opt = 0,
+		.vmexit_ctrl_req =
+			VM_EXIT_HOST_ADDR_SPACE_SIZE |
+			VM_EXIT_LOAD_IA32_PAT |
+			VM_EXIT_LOAD_IA32_EFER |
+			VM_EXIT_SAVE_IA32_PAT |
+			VM_EXIT_SAVE_IA32_EFER |
+			VM_EXIT_SAVE_DEBUG_CONTROLS,
+		.vmexit_ctrl_opt = 0,
+		.vmentry_ctrl_req =
+			VM_ENTRY_LOAD_DEBUG_CONTROLS |
+			VM_ENTRY_IA32E_MODE |
+			VM_ENTRY_LOAD_IA32_EFER |
+			VM_ENTRY_LOAD_IA32_PAT,
+		.vmentry_ctrl_opt = 0,
+	};
+
+	if (setup_vmcs_config_common(vmcs_config, vmx_cap, &setting))
+		return -EINVAL;
+
+	pr_info("pin_based_exec_ctrl 0x%x\n", vmcs_config->pin_based_exec_ctrl);
+	pr_info("cpu_based_exec_ctrl 0x%x\n", vmcs_config->cpu_based_exec_ctrl);
+	pr_info("cpu_based_2nd_exec_ctrl 0x%x\n", vmcs_config->cpu_based_2nd_exec_ctrl);
+	pr_info("vmexit_ctrl 0x%x\n", vmcs_config->vmexit_ctrl);
+	pr_info("vmentry_ctrl 0x%x\n", vmcs_config->vmentry_ctrl);
+
+	return 0;
 }
 
 static __init int pkvm_setup_host_vm(struct pkvm_hyp *pkvm)
@@ -94,6 +144,12 @@ int __init vmx_pkvm_init(void)
 	if (!pkvm) {
 		pr_err("cannot alloc pkvm_hyp\n");
 		ret = -ENOMEM;
+		goto out;
+	}
+
+	ret = pkvm_setup_host_vmcs_config();
+	if (ret) {
+		pr_err("setup host vmcs config failed\n");
 		goto out;
 	}
 
