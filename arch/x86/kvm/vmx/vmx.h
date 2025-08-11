9810d5557bb21c86d149d13003244951159adcd7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-11 13:03:08 +0800

    pKVM: x86: Add host kvm instance
    Add a global kvm instance for the host VM. As the kvm instance for Intel
    processors is actually pointing to a kvm_vmx structure, reserve the
    corresponding memory pages according to the kvm_vmx structure size. As not
    all elements in the kvm instance will be useful for the host, the kvm
    instance will be initialized on demand.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -744,7 +744,16 @@ int vmx_init(void);
 void vmx_exit(void);
 
 #ifdef CONFIG_PKVM_INTEL
+
+#define PKVM_HOST_KVM_VMX_PAGES		(PAGE_ALIGN(sizeof(struct kvm_vmx)) >> PAGE_SHIFT)
+
+static inline unsigned long pkvm_vmx_data_pages(void)
+{
+	return pkvm_data_pages(PKVM_HOST_KVM_VMX_PAGES);
+}
+
 int __init vmx_pkvm_init(void);
-#endif
+
+#endif /* CONFIG_PKVM_INTEL */
 
 #endif /* __KVM_X86_VMX_H */
