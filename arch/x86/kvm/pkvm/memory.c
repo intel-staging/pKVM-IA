a0a07864c72b6cc48ddd53ffc182bc24f768e37f
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-11 18:01:29 +0800

    pKVM: x86: Reuse kernel direct mapping
    To simplify, the pKVM hypervisor can reuse kernel direct mapping to
    translate addresses. This will require the pKVM hypervisor to have the
    symbol page_offset_base and phys_base. Define these two symbols in the
    pKVM hypervisor code and initialize with the same value used by the
    linux kernel before deprivilege. With this, the pKVM hypervisor code can
    use __va and __pa to translate between VA and PA.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/kvm/pkvm/memory.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/cache.h>
+#include <linux/types.h>
+
+unsigned long page_offset_base __ro_after_init;
+unsigned long phys_base;
--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -22,6 +22,18 @@ u64 pkvm_total_reserve_pages(void)
 	return pkvm_vmx_data_pages();
 }
 
+static __init void pkvm_setup_syms(void)
+{
+	/*
+	 * The pKVM hypervisor has defined the same symbol page_offset_base
+	 * and phys_base with the linux kernel. Initialize with the same value
+	 * used by the linux kernel before deprivilege. With this, the pkvm
+	 * hypervisor code can use __va and __pa to translate between VA and PA.
+	 */
+	pkvm_sym(page_offset_base) = page_offset_base;
+	pkvm_sym(phys_base) = phys_base;
+}
+
 static __init int pkvm_setup_host_vmcs_config(void)
 {
 	struct vmcs_config *vmcs_config = &pkvm_sym(host_vmcs_config);
@@ -517,6 +529,8 @@ int __init vmx_pkvm_init(void)
 		ret = -ENOMEM;
 		goto out;
 	}
+
+	pkvm_setup_syms();
 
 	ret = pkvm_setup_host_vmcs_config();
 	if (ret) {
