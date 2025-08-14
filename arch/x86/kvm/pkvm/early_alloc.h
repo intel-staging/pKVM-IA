4587b96fdb931445c2b4fbbc93454483929c614d
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-14 09:23:14 +0800

    pKVM: x86: Implement a pgtable mm_ops for early alloc
    The pKVM hypervisor will use the early alloc to allocate memory pages for
    its own purpose, e.g., create its own MMU page table. Implement a pgtable
    memory management ops for this purpose. As the early allocated memory
    pages cannot be freed, it is not necessary to support the get_page and
    the put_page callbacks. The page count is also meaningless.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/kvm/pkvm/early_alloc.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_EARLY_ALLOC_H
+#define __PKVM_EARLY_ALLOC_H
+
+#include "pgtable.h"
+
+extern const struct pkvm_pgtable_mm_ops pkvm_early_alloc_mm_ops;
+
+#endif /* __PKVM_EARLY_ALLOC_H */
