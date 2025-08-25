c990b2484657ceb20b62502247ffdcad63573e4a
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-25 13:57:18 +0800

    pKVM: x86: Add hyp mmu map
    Add hyp mmu map support to install vaddr to phys mappings, which is
    implemented via the generic pkvm_pgtable_map api provided by the pkvm
    pgtable infrastructure.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/mmu.h
+++ arch/x86/kvm/pkvm/mmu.h
@@ -3,5 +3,7 @@
 #define __PKVM_X86_MMU_H
 
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages);
+int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
+		     unsigned long size, u64 prot);
 
 #endif /* __PKVM_X86_MMU_H */
