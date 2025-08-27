d509be483cc6087def2c3a18f004a5ba267a1d3b
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-27 14:55:16 +0800

    pKVM: x86: Install mappings to the host mmu
    During the pKVM finalizing, the necessary mappings should be installed
    in the host mmu for the host VM to access the memory and MMIO. Map all
    the memory blocks with read/write/exec permissions, and map the holes
    between memory blocks as MMIO with read/write/exec permissions.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/init_finalize.c
+++ arch/x86/kvm/pkvm/init_finalize.c
@@ -139,6 +139,40 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 	return pkvm_hyp_mmu_switch_to_buddy(hyp_pgt_base, nr_pages);
 }
 
+static int create_host_mmu(host_mmu_init_fn_t host_mmu_init_fn)
+{
+	struct memblock_region *reg;
+	unsigned long phys = 0;
+	unsigned int i;
+	int ret;
+
+	ret = pkvm_host_mmu_init(host_pgt_base, pkvm_host_pgtable_pages(),
+				 host_mmu_init_fn);
+	if (ret)
+		return ret;
+
+	/* Map memory blocks with RWX permissions */
+	for (i = 0; i < pkvm_memblock_nr; i++) {
+		reg = &pkvm_memory[i];
+		ret = pkvm_host_mmu_map((unsigned long)reg->base,
+					(unsigned long)reg->size,
+					true, true, true, false);
+		if (ret)
+			return ret;
+	}
+
+	/* Map holes between memblocks as MMIO with RWX permissions */
+	for (i = 0; i < pkvm_memblock_nr; i++, phys = reg->base + reg->size) {
+		reg = &pkvm_memory[i];
+		ret = pkvm_host_mmu_map(phys, (unsigned long)reg->base - phys,
+					true, true, true, true);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 			   struct pkvm_init_ops *init_ops)
 {
@@ -169,8 +203,7 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 	if (ret)
 		return ret;
 
-	return pkvm_host_mmu_init(host_pgt_base, pkvm_host_pgtable_pages(),
-				  host_mmu_init_fn);
+	return create_host_mmu(host_mmu_init_fn);
 }
 
 int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
