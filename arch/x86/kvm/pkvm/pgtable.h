956dc87b8ffcc9f848b09e1015fd90269a0b2201
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-27 17:58:14 +0800

    pKVM: x86: Add page table map support
    Add pkvm_pgtable_map to install a virt to phys mapping in a pKVM page
    table with the given property bits at the specified memory range. If
    the mapping already exists at a higher level, split the huge mapping
    into smaller ones and install the mapping at the proper level. If the
    mapping already exists at the same level but with different physical
    address or property bits, replaces by the new one and flush the TLB.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pgtable.h
+++ arch/x86/kvm/pkvm/pgtable.h
@@ -123,6 +123,8 @@ int pkvm_pgtable_init(struct pkvm_pgtable *pgt,
 		      const struct pkvm_pgtable_ops *pgt_ops);
 int pkvm_pgtable_walk(struct pkvm_pgtable *pgt, unsigned long vaddr,
 		      unsigned long size, struct pkvm_pgtable_walker *walker);
+int pkvm_pgtable_map(struct pkvm_pgtable *pgt, unsigned long vaddr,
+		     unsigned long phys, unsigned long size, u64 prot);
 
 /*
  * Return the max size of the virtual address space that can be
