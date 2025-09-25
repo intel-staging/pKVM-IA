4ff918a54680528e4248c1eb90bf05f7c584232f
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-25 12:57:56 +0800

    pKVM: x86: Expose PKVM_VMX_VM_SIZE for vmx host
    Expose PKVM_VMX_VM_SIZE to allow the VMX host to know the size of
    a VM instance which is managed by the pKVM hypervisor, so that no need
    to expose struct pkvm_vm which is internal to the pKVM hypervisor.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm_constants.c
+++ arch/x86/kvm/pkvm/pkvm_constants.c
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kbuild.h>
+#include <vmx/vmx.h>
 #include "memory.h"
+#include "pkvm.h"
 
 int main(void)
 {
 	DEFINE(PKVM_VMEMMAP_ENTRY_SIZE, sizeof(struct pkvm_page));
+#ifdef CONFIG_PKVM_INTEL
+	DEFINE(PKVM_VMX_VM_SIZE, PKVM_VM_BASE_SIZE + sizeof(struct kvm_vmx));
+#endif
 	return 0;
 }
