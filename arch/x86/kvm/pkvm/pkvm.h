41f97046d7b0a1f30a02589d0b06d7b217b751ee
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-25 10:21:43 +0800

    pKVM: x86: Introduce struct pkvm_vm
    Introduce struct pkvm_vm to represent a guest VM. The struct kvm should
    be the last element. In cases where struct kvm is wrapped by a vendor
    specific structure, putting it as the last element can safely extend the
    size of pkvm_vm w/o affecting layout compatibility.
    
    If struct kvm is wrapped by a vendor specific structure, it must reside
    at offset 0 of that structure. This ensures that &pkvm_vm->kvm correctly
    resolves to the underlying struct kvm instance. It is the responsibility
    of vendor code to guarantee this layout.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.h
+++ arch/x86/kvm/pkvm/pkvm.h
@@ -8,6 +8,44 @@
 DECLARE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
+/* Represents a guest VM. */
+struct pkvm_vm {
+	/* Point to the kvm structure owned by the host */
+	struct kvm *shared_kvm;
+	/*
+	 * The donated structure size, possibly including a vendor specific
+	 * structure wrapping the kvm structure (see below).
+	 */
+	size_t size;
+	/*
+	 * The struct kvm should be the last element. In cases where struct kvm
+	 * is wrapped by a vendor specific structure, putting it as the last
+	 * element can safely extend the size of pkvm_vm w/o affecting layout
+	 * compatibility.
+	 *
+	 * If struct kvm is wrapped by a vendor specific structure, it must reside
+	 * at offset 0 of that structure. This ensures that &pkvm_vm->kvm correctly
+	 * resolves to the underlying struct kvm instance. It is the responsibility
+	 * of vendor code to guarantee this layout.
+	 */
+	struct kvm kvm;
+};
+
+/* The pkvm_vm structure size w/o struct kvm */
+#define PKVM_VM_BASE_SIZE		offsetof(struct pkvm_vm, kvm)
+
+static inline struct pkvm_vm *to_pkvm(struct kvm *kvm)
+{
+	/*
+	 * Make sure the kvm structure is the last element of pkvm_vm. See
+	 * comments of struct pkvm_vm.
+	 */
+	BUILD_BUG_ON(sizeof(struct pkvm_vm) !=
+		     PKVM_VM_BASE_SIZE + sizeof(struct kvm));
+
+	return container_of(kvm, struct pkvm_vm, kvm);
+}
+
 int pkvm_handle_host_hypercall(unsigned long nr, unsigned long a0,
 			       unsigned long a1, unsigned long a2,
 			       unsigned long a3);
