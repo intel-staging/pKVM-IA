668d05a521c8aa8e970bde8969d63cec70919f6d
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-29 09:57:41 +0800

    KVM: pKVM: Implement check_processor_compatibility operation
    Enable the pKVM VMX host to check processor's compatibility via using
    the check_processor_compatibility PV interface.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kvm_host.h>
+#include <asm/kvm_pkvm.h>
+
+static int pkvm_check_processor_compat(void)
+{
+	return pkvm_hypercall(check_processor_compatibility);
+}
 
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
+
+	.check_processor_compatibility = pkvm_check_processor_compat,
 };
