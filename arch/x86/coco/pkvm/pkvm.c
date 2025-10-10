326283492e24cf84fbdcf6a878d9541cce2591be
Dmytro Maluka <dmaluka@google.com>
2025-10-10 17:05:16 +0000

    x86/pkvm: Use hypercall to start CPUs
    pKVM does not allow starting secondary CPUs of protected guests in the
    traditional way via INIT/SIPI, since the APIC, including INIT/SIPI, is
    emulated by the host and thus is not trusted to control when and at what
    address to start running a CPU. Instead, pKVM requires the guest to use
    its PKVM_GHC_START_CPU hypercall for that.
    
    Luckily kernel already provides an infrastructure for replacing the
    standard method of starting secondary CPUs with a custom one. So
    leverage it.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/coco/pkvm/pkvm.c
+++ arch/x86/coco/pkvm/pkvm.c
@@ -4,6 +4,7 @@
 #include <asm/coco.h>
 #include <asm/pkvm_guest.h>
 #include <asm/pgtable.h>
+#include <asm/apic.h>
 
 DEFINE_STATIC_KEY_FALSE(pkvm_guest_detected);
 EXPORT_SYMBOL(pkvm_guest_detected);
@@ -126,6 +127,11 @@ static void pkvm_mmio_writeq(u64 v, volatile void __iomem *addr)
 	pkvm_virt_mmio(8, true, (unsigned long)addr, &val);
 }
 
+static int pkvm_wakeup_secondary_cpu(u32 apic_id, unsigned long start_ip, unsigned int cpu)
+{
+	return kvm_hypercall2(PKVM_GHC_START_CPU, apic_id, start_ip);
+}
+
 __init void pkvm_guest_init_coco(void)
 {
 	cc_vendor = CC_VENDOR_PKVM;
@@ -156,4 +162,6 @@ __init void pkvm_guest_init_coco(void)
 	pv_ops.mmio.pci_mmcfg_writeb = pkvm_mmio_writeb;
 	pv_ops.mmio.pci_mmcfg_writew = pkvm_mmio_writew;
 	pv_ops.mmio.pci_mmcfg_writel = pkvm_mmio_writel;
+
+	apic_update_callback(wakeup_secondary_cpu, pkvm_wakeup_secondary_cpu);
 }
