b2679a1e03ed7ecd4a08463e7c1aed64244a986c
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-12 20:02:49 +0800

    pKVM: x86: Redefine KVM print macros for the pKVM hypervisor
    The KVM print macros may be included in the code which are leveraged
    by the pKVM hypervisor. Overridden these print macros to use the "pkvm"
    prefixes, and avoid printing current task information which is not
    supported in the pKVM hypervisor.
    
    The symbols enabled by CONFIG_DYNAMIC_DEBUG/_CORE cannot be removed by
    undefine these kernel configs in the undef.h due to the same reason with
    the CONFIG_BUG/CONFIG_GENERIC_BUG. So redefine the kvm print macros in
    non-debug version pKVM as empty to avoid introducing symbols enabled by
    CONFIG_DYNAMIC_DEBUG/_CORE. For debug version pKVM, redefine the kvm
    print macros to just print logs as no valid current pid symbols.
    
    [ Dmytro: Redefine kvm print macros ]
    
    Co-developed-by: Dmytro Maluka <dmaluka@google.com>
    Squash changelog:
      6dd2fdc5ac77 ("pKVM: x86: Move redefinitions of BUG, WARN etc to a separate header")
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/pkvm_redef.h
+++ arch/x86/include/asm/pkvm_redef.h
@@ -4,7 +4,32 @@
 
 #ifdef __PKVM_HYP__
 
-#ifndef CONFIG_PKVM_X86_DEBUG
+#undef kvm_err
+#undef kvm_info
+#undef kvm_debug
+#undef kvm_debug_ratelimited
+#undef kvm_pr_unimpl
+
+#ifdef CONFIG_PKVM_X86_DEBUG
+
+#define kvm_err(fmt, ...) \
+	pr_err("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_info(fmt, ...) \
+	pr_info("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug(fmt, ...) \
+	pr_debug("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug_ratelimited(fmt, ...) \
+	pr_debug_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_pr_unimpl(fmt, ...) \
+	pr_err_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+
+#else /* CONFIG_PKVM_X86_DEBUG */
+
+#define kvm_err(fmt, ...) do {} while(0)
+#define kvm_info(fmt, ...) do {} while(0)
+#define kvm_debug(fmt, ...) do {} while(0)
+#define kvm_debug_ratelimited(fmt, ...) do {} while(0)
+#define kvm_pr_unimpl(fmt, ...) do {} while(0)
 
 #undef WARN_ON
 #undef WARN
