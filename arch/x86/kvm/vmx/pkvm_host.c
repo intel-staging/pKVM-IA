6869eb9746a2131a0d68419694d6198efab666c7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-20 16:51:14 +0800

    KVM: pVMX: Implement event injection operations
    Enable the pKVM VMX host to inject irq/nmi/exception to the guest via
    the event injection PV interfaces. As pVM's exception is emulated by the
    pKVM, there should be no need for the host to inject exception to a pVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -5,6 +5,7 @@
 #include <asm/kvm_pkvm.h>
 #include "pkvm_constants.h"
 #include "posted_intr.h"
+#include "trace.h"
 #include "x86_ops.h"
 #include "vmx.h"
 
@@ -814,6 +815,31 @@ static u32 pkvm_get_interrupt_shadow(struct kvm_vcpu *vcpu)
 	return out.get_interrupt_shadow.data;
 }
 
+static void pkvm_inject_irq(struct kvm_vcpu *vcpu, bool reinjected)
+{
+	trace_kvm_inj_virq(vcpu->arch.interrupt.nr,
+			   vcpu->arch.interrupt.soft, reinjected);
+
+	++vcpu->stat.irq_injections;
+
+	KVM_BUG_ON(pkvm_hypercall(inject_irq), vcpu->kvm);
+}
+
+static void pkvm_inject_nmi(struct kvm_vcpu *vcpu)
+{
+	++vcpu->stat.nmi_injections;
+
+	KVM_BUG_ON(pkvm_hypercall(inject_nmi), vcpu->kvm);
+}
+
+static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
+{
+	if (pkvm_is_protected_vcpu(vcpu))
+		return;
+
+	KVM_BUG_ON(pkvm_hypercall(inject_exception), vcpu->kvm);
+}
+
 static int pkvm_interrupt_allowed(struct kvm_vcpu *vcpu, bool for_injection)
 {
 	return pkvm_hypercall(interrupt_allowed, for_injection);
@@ -905,6 +931,9 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 
 	.set_interrupt_shadow = pkvm_set_interrupt_shadow,
 	.get_interrupt_shadow = pkvm_get_interrupt_shadow,
+	.inject_irq = pkvm_inject_irq,
+	.inject_nmi = pkvm_inject_nmi,
+	.inject_exception = pkvm_inject_exception,
 	.interrupt_allowed = pkvm_interrupt_allowed,
 	.nmi_allowed = pkvm_nmi_allowed,
 	.get_nmi_mask = pkvm_get_nmi_mask,
