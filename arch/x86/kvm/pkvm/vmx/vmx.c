BayLibre/android_kernel_common

ANDROID: pkvm: x86: Switch to using PV MMU instead of emulated EPT
Bug: 391541180
Test: run various VMs

Change-Id: I79fe91b6953e21da0a79268795e3909abeb2d1c2
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/vmx/vmx.c
@@ -5106,8 +5106,7 @@ static int handle_invlpg(struct kvm_vcpu *vcpu)
 static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 {
 	u64 nr, a0, a1, a2, a3;
-	struct shadow_vcpu_state *shadow_vcpu = kvm_vcpu_to_shadow(vcpu);
-	struct pkvm_pgtable *pgstate_pgt = &shadow_vcpu->vm->pgstate_pgt;
+	struct pkvm_vm *pkvm_vm = to_pkvm(vcpu->kvm);
 	int cpl = vmx_get_cpl(vcpu);
 	int ret = -KVM_EPERM;
 
@@ -5124,10 +5123,10 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 
 	switch (nr) {
 	case PKVM_GHC_SHARE_MEM:
-		ret = __pkvm_guest_share_host(pgstate_pgt, a0, a1);
+		ret = __pkvm_guest_share_host(&pkvm_vm->pgt, a0, a1);
 		break;
 	case PKVM_GHC_UNSHARE_MEM:
-		ret = __pkvm_guest_unshare_host(pgstate_pgt, a0, a1);
+		ret = __pkvm_guest_unshare_host(&pkvm_vm->pgt, a0, a1);
 		break;
 	case PKVM_GHC_IOREAD:
 	case PKVM_GHC_IOWRITE:

arch/x86/kvm/pkvm/x86.c
@@ -2711,9 +2711,15 @@ EXPORT_SYMBOL_GPL(kvm_emulate_halt);
 int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 {
 #ifdef __PKVM_HYP__
+	struct pkvm_vm *pkvm_vm = to_pkvm(vcpu->kvm);
+
 	vcpu->arch.last_vmentry_cpu = -1;
 	vcpu->arch.regs_avail = ~0;
 	vcpu->arch.regs_dirty = ~0;
+
+	vcpu->arch.root_mmu.root_role.level = pkvm_vm->pgt.level;
+	vcpu->arch.root_mmu.root.hpa = pkvm_vm->pgt.root_pa;
+	vcpu->arch.mmu = &vcpu->arch.root_mmu;
 
 	vcpu->arch.pat = MSR_IA32_CR_PAT_DEFAULT;
 

arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -52,14 +52,14 @@ struct pkvm_mem_transition {
 	struct pkvm_mem_trans_desc	completer;
 };
 
-static void guest_pgstate_pgt_lock(struct pkvm_pgtable *pgt)
+static void guest_pgt_lock(struct pkvm_pgtable *pgt)
 {
-	pkvm_spin_lock(&pgstate_pgt_to_shadow_vm(pgt)->lock);
+	pkvm_spin_lock(&pgt_to_pkvm(pgt)->pgt_lock);
 }
 
-static void guest_pgstate_pgt_unlock(struct pkvm_pgtable *pgt)
+static void guest_pgt_unlock(struct pkvm_pgtable *pgt)
 {
-	pkvm_spin_unlock(&pgstate_pgt_to_shadow_vm(pgt)->lock);
+	pkvm_spin_unlock(&pgt_to_pkvm(pgt)->pgt_lock);
 }
 
 static u64 pkvm_init_invalid_leaf_owner(pkvm_id owner_id)
@@ -135,10 +135,7 @@ static int __host_check_page_state_range(struct pkvm_pgtable *pgt_override, u64 
 
 static pkvm_id pkvm_guest_id(struct pkvm_pgtable *pgt)
 {
-	struct pkvm_shadow_vm *shadow_vm = pgstate_pgt_to_shadow_vm(pgt);
-
-	/* Using the pkvm_vm handle as guest_id. */
-	return shadow_to_kvm(shadow_vm)->arch.pkvm.pkvm_vm_handle;
+	return pgt_to_kvm(pgt)->arch.pkvm.pkvm_vm_handle;
 }
 
 static pkvm_id __pkvm_owner_id(const struct pkvm_mem_trans_desc *desc)
@@ -764,7 +761,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	if (!PAGE_ALIGNED(size))
 		return -EINVAL;
 
-	guest_pgstate_pgt_lock(guest_pgt);
+	guest_pgt_lock(guest_pgt);
 	host_ept_lock();
 
 	while (size) {
@@ -783,7 +780,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgstate_pgt_unlock(guest_pgt);
+	guest_pgt_unlock(guest_pgt);
 
 
 	return ret;
@@ -1010,7 +1007,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	u64 prot;
 	int ret = 0;
 
-	guest_pgstate_pgt_lock(guest_pgt);
+	guest_pgt_lock(guest_pgt);
 	host_ept_lock();
 
 	while (size) {
@@ -1029,7 +1026,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgstate_pgt_unlock(guest_pgt);
+	guest_pgt_unlock(guest_pgt);
 
 	return ret;
 }

intel-staging/pKVM-IA

Now we are ready to start using PV MMU instead of emulated EPT page
tables shadowed by pKVM. To do that, make the following changes (all in
one commit, to avoid breaking things):
- Set the guest's EPT_POINTER in pKVM to the PV MMU page table instead
  of the shadow EPT.
- Adjust pKVM's guest share/unshare code to use the PV MMU page table.
- Disable using vEPT-specific callbacks pkvm_tlb_remote_flush() and
  pkvm_tlb_remote_flush_with_range().
- Force kvm_memslots_have_rmaps() to return false if enable_pkvm is true
  (not just if tdp_mmu_enabled is true).
- Finally, disable tdp_mmu_enabled in the host to switch to PV MMU.
Test: run various VMs, including KVM unit tests and selftests
Upstream-Task: 402758258

arch/x86/kvm/pkvm/vmx/vmx.c
@@ -5104,8 +5104,7 @@ static int handle_invlpg(struct kvm_vcpu *vcpu)
 static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 {
 	u64 nr, a0, a1, a2, a3;
-	struct shadow_vcpu_state *shadow_vcpu = kvm_vcpu_to_shadow(vcpu);
-	struct pkvm_pgtable *pgstate_pgt = &shadow_vcpu->vm->pgstate_pgt;
+	struct pkvm_vm *pkvm_vm = to_pkvm(vcpu->kvm);
 	int cpl = vmx_get_cpl(vcpu);
 	int ret = -KVM_EPERM;
 
@@ -5122,10 +5121,10 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 
 	switch (nr) {
 	case PKVM_GHC_SHARE_MEM:
-		ret = __pkvm_guest_share_host(pgstate_pgt, a0, a1);
+		ret = __pkvm_guest_share_host(&pkvm_vm->mmu, a0, a1);
 		break;
 	case PKVM_GHC_UNSHARE_MEM:
-		ret = __pkvm_guest_unshare_host(pgstate_pgt, a0, a1);
+		ret = __pkvm_guest_unshare_host(&pkvm_vm->mmu, a0, a1);
 		break;
 	case PKVM_GHC_IOREAD:
 	case PKVM_GHC_IOWRITE:

arch/x86/kvm/pkvm/x86.c
@@ -2709,11 +2709,18 @@ EXPORT_SYMBOL_GPL(kvm_emulate_halt);
 int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 {
 #ifdef __PKVM_HYP__
+	struct pkvm_vm *pkvm_vm = to_pkvm(vcpu->kvm);
+
 	vcpu->arch.last_vmentry_cpu = -1;
 	vcpu->arch.regs_avail = ~0;
 	vcpu->arch.regs_dirty = ~0;
 
 	vcpu->arch.mp_state = KVM_MP_STATE_UNINITIALIZED;
+
+	vcpu->arch.root_mmu.root_role.level = pkvm_vm->mmu.level;
+	vcpu->arch.root_mmu.root.hpa = pkvm_vm->mmu.root_pa;
+	vcpu->arch.mmu = &vcpu->arch.root_mmu;
+
 	vcpu->arch.pat = MSR_IA32_CR_PAT_DEFAULT;
 
 	return kvm_x86_call(vcpu_create)(vcpu);

arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -52,14 +52,14 @@ struct pkvm_mem_transition {
 	struct pkvm_mem_trans_desc	completer;
 };
 
-static void guest_pgstate_pgt_lock(struct pkvm_pgtable *pgt)
+static void guest_mmu_lock(struct pkvm_pgtable *pgt)
 {
-	pkvm_spin_lock(&pgstate_pgt_to_shadow_vm(pgt)->lock);
+	pkvm_spin_lock(&pgt_to_pkvm(pgt)->mmu_lock);
 }
 
-static void guest_pgstate_pgt_unlock(struct pkvm_pgtable *pgt)
+static void guest_mmu_unlock(struct pkvm_pgtable *pgt)
 {
-	pkvm_spin_unlock(&pgstate_pgt_to_shadow_vm(pgt)->lock);
+	pkvm_spin_unlock(&pgt_to_pkvm(pgt)->mmu_lock);
 }
 
 static u64 pkvm_init_invalid_leaf_owner(pkvm_id owner_id)
@@ -135,10 +135,7 @@ static int __host_check_page_state_range(struct pkvm_pgtable *pgt_override, u64 
 
 static pkvm_id pkvm_guest_id(struct pkvm_pgtable *pgt)
 {
-	struct pkvm_shadow_vm *shadow_vm = pgstate_pgt_to_shadow_vm(pgt);
-
-	/* Using the pkvm_vm handle as guest_id. */
-	return shadow_to_kvm(shadow_vm)->arch.pkvm.pkvm_vm_handle;
+	return pgt_to_kvm(pgt)->arch.pkvm.pkvm_vm_handle;
 }
 
 static pkvm_id __pkvm_owner_id(const struct pkvm_mem_trans_desc *desc)
@@ -776,7 +773,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	if (!PAGE_ALIGNED(size))
 		return -EINVAL;
 
-	guest_pgstate_pgt_lock(guest_pgt);
+	guest_mmu_lock(guest_pgt);
 	host_ept_lock();
 
 	while (size) {
@@ -795,7 +792,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgstate_pgt_unlock(guest_pgt);
+	guest_mmu_unlock(guest_pgt);
 
 
 	return ret;
@@ -1022,7 +1019,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	u64 prot;
 	int ret = 0;
 
-	guest_pgstate_pgt_lock(guest_pgt);
+	guest_mmu_lock(guest_pgt);
 	host_ept_lock();
 
 	while (size) {
@@ -1041,7 +1038,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgstate_pgt_unlock(guest_pgt);
+	guest_mmu_unlock(guest_pgt);
 
 	return ret;
 }