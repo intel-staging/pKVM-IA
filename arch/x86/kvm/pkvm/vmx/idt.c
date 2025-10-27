7357b37edf230b5a9510db2b208d1427eb627991
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-27 17:20:49 +0800

    pKVM: VMX: Ensure the host VMCS is loaded when handling NMI
    The pKVM captured NMI should be injected to the host VM. With the
    vcpu_load PV interface, it is possible that a guest VMCS is currently
    loaded when an NMI is captured. To ensure NMI being correctly injected
    to the host VM, load the host VMCS before handling NMI and restore the
    previous VMCS after that.
    
    Squash changelog:
      aea03e80ea42 ("pKVM: VMX: Don't BUG_ON if no loaded VMCS")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/vmx/idt.c
+++ arch/x86/kvm/pkvm/vmx/idt.c
@@ -5,11 +5,20 @@
 #include <vmx/x86_ops.h>
 #include "host_vmx.h"
 #include "idt.h"
+#include "memory.h"
 #include "pkvm.h"
 
 static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 {
 	struct kvm_vcpu *vcpu = this_cpu_read(host_vcpu);
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+	u64 cur_vmcs_pa = vmcs_store();
+	bool is_host_vmcs;
+
+	/* Switch to the host VMCS if the current one is not for host. */
+	is_host_vmcs = (cur_vmcs_pa == __pkvm_pa(vmx->loaded_vmcs->vmcs));
+	if (!is_host_vmcs)
+		vmcs_load(vmx->loaded_vmcs->vmcs);
 
 	/*
 	 * The NMI happens while the pKVM hypervisor is running, but it should
@@ -23,7 +32,11 @@ static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 	 * Request host immediate exit in case the pending NMI has already been
 	 * handled in this host vmexit handling cycle.
 	 */
-	request_host_immediate_exit(to_vmx(vcpu));
+	request_host_immediate_exit(vmx);
+
+	/* Restore to the previous VMCS if it is not for host. */
+	if (!is_host_vmcs && VALID_PAGE(cur_vmcs_pa))
+		vmcs_load(__pkvm_va(cur_vmcs_pa));
 }
 
 void pkvm_vmx_register_excp_handlers(void)
