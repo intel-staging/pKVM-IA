4a6fb05386568440ae49d326e1bd3e568cf48d74
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 15:59:07 +0800

    pKVM: VMX: Handle EXIT_REASON_INVD
    Handle EXIT_REASON_INVD alone in the pKVM hypervisor by emulating the
    INVD instruction as noop.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -6673,8 +6673,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_MSR_WRITE]               = kvm_emulate_wrmsr,
 	[EXIT_REASON_INTERRUPT_WINDOW]        = handle_interrupt_window,
 	[EXIT_REASON_HLT]                     = kvm_emulate_halt,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_INVD]		      = kvm_emulate_invd,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_INVLPG]		      = handle_invlpg,
 	[EXIT_REASON_RDPMC]                   = kvm_emulate_rdpmc,
 	[EXIT_REASON_VMCALL]                  = kvm_emulate_hypercall,
--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -2338,6 +2338,7 @@ int kvm_emulate_wrmsr_imm(struct kvm_vcpu *vcpu, u32 msr, int reg)
 	return __kvm_emulate_wrmsr(vcpu, msr, kvm_register_read(vcpu, reg));
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_emulate_wrmsr_imm);
+#endif /* !__PKVM_HYP__ */
 
 int kvm_emulate_as_nop(struct kvm_vcpu *vcpu)
 {
@@ -2351,6 +2352,7 @@ int kvm_emulate_invd(struct kvm_vcpu *vcpu)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_emulate_invd);
 
+#ifndef __PKVM_HYP__
 fastpath_t handle_fastpath_invd(struct kvm_vcpu *vcpu)
 {
 	if (!kvm_emulate_invd(vcpu))
