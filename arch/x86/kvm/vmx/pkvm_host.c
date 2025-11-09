e94959cc14be3abc6a3fff4b309f6e66b2d468ee
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:59:52 +0800

    KVM: pVMX: Implement vcpu_deliver_sipi_vector operation
    Enable the pKVM VMX host to deliver SIPI vector to guest VMs. Allowing
    the host to do so for pVMs is not secure. So pVMs will use a secure SIPI
    mechanism eventually. Once this is ready, the pKVM host only need to
    deliver SIPI vector to npVMs.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1024,6 +1024,8 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.pi_start_bypass = vmx_pi_start_bypass,
 
 	.apic_init_signal_blocked = pkvm_apic_init_signal_blocked,
+
+	.vcpu_deliver_sipi_vector = kvm_vcpu_deliver_sipi_vector,
 };
 
 bool pkvm_interrupt_blocked(struct kvm_vcpu *vcpu)
