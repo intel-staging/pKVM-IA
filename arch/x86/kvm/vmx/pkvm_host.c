56b23f76794e5a4ab4d5b4c4246a6dc258126500
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:51:31 +0800

    KVM: pVMX: Implement vm_init operation
    Enable the pKVM VMX host to initialize a guest VM using the vm_init PV
    interface. The host allocates memory pages to serve as the backing for
    the pkvm_vm structure, which is private to the pKVM hypervisor, and gets
    the guest VM handle from the vm_init PV interface.
    
    As the pKVM hypervisor can only handle with the contiguous physical
    memory, use kzalloc rather than __vmalloc to allocate the kvm structure
    in the host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
+#include "pkvm_constants.h"
+#include "x86_ops.h"
+#include "vmx.h"
 
 static int pkvm_check_processor_compat(void)
 {
@@ -22,6 +25,40 @@ static void pkvm_disable_virtualization_cpu(void)
 	 */
 }
 
+static int pkvm_vm_init(struct kvm *kvm)
+{
+	void *pkvm_vm;
+	int ret;
+
+	/*
+	 * Some of struct kvm elements are initialized by the vmx_vm_init()
+	 * which can be leveraged by the pKVM host as this initialization is
+	 * simple and no VMX hardware involved.
+	 */
+	ret = vmx_vm_init(kvm);
+	if (ret)
+		return ret;
+
+	pkvm_vm = alloc_pages_exact(PKVM_VMX_VM_SIZE, GFP_KERNEL_ACCOUNT);
+	if (!pkvm_vm)
+		return -ENOMEM;
+
+	ret = pkvm_hypercall(vm_init, __pa(kvm), __pa(pkvm_vm));
+	if (ret < 0)
+		goto free_page;
+
+	kvm->arch.pkvm.handle = ret;
+
+	if (pkvm_is_protected_vm(kvm))
+		kvm->arch.has_protected_state = true;
+
+	return 0;
+
+free_page:
+	free_pages_exact(pkvm_vm, PKVM_VMX_VM_SIZE);
+	return ret;
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
@@ -30,4 +67,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.enable_virtualization_cpu = pkvm_enable_virtualization_cpu,
 	.disable_virtualization_cpu = pkvm_disable_virtualization_cpu,
 	.emergency_disable_virtualization_cpu = pkvm_disable_virtualization_cpu,
+
+	.vm_size = sizeof(struct kvm_vmx),
+	.vm_init = pkvm_vm_init,
 };
