ff38fa7e9d3aa0f82d2614926b904d8db59067e7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:25:06 +0800

    KVM: pKVM: Implement enable_virtualization_cpu operation
    Enable the pKVM VMX host to enable virtualization features on a specific
    CPU via the enable_virtualization_cpu PV interface.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -7,8 +7,15 @@ static int pkvm_check_processor_compat(void)
 	return pkvm_hypercall(check_processor_compatibility);
 }
 
+static int pkvm_enable_virtualization_cpu(void)
+{
+	return pkvm_hypercall(enable_virtualization_cpu);
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
 	.check_processor_compatibility = pkvm_check_processor_compat,
+
+	.enable_virtualization_cpu = pkvm_enable_virtualization_cpu,
 };
