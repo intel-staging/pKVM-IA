844a33f0c7307bf4edfcb2f67831c37d7e9fc153
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:57:15 +0800

    KVM: pVMX: Implement load_eoi_exitmap operation
    Enable the pKVM VMX host to configure the exit map for EOI via the
    corresponding PV interface.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -909,6 +909,15 @@ static void pkvm_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu)
 			   vcpu->kvm);
 }
 
+static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 *eoi_exit_bitmap)
+{
+	if (kvm_vcpu_apicv_active(vcpu))
+		KVM_BUG_ON(pkvm_hypercall(load_eoi_exitmap, eoi_exit_bitmap[0],
+					  eoi_exit_bitmap[1], eoi_exit_bitmap[2],
+					  eoi_exit_bitmap[3]),
+			   vcpu->kvm);
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
@@ -979,6 +988,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.x2apic_icr_is_split = false,
 	.set_virtual_apic_mode = pkvm_set_virtual_apic_mode,
 	.refresh_apicv_exec_ctrl = pkvm_refresh_apicv_exec_ctrl,
+	.load_eoi_exitmap = pkvm_load_eoi_exitmap,
 };
 
 bool pkvm_interrupt_blocked(struct kvm_vcpu *vcpu)
