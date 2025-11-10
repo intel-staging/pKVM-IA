c2f90b4dab6acbb2f774e3a4f2ea503ff904e5d3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:50:51 +0800

    pKVM: VMX: Import the vmenter.S
    Import the vmenter.S to include the VMX asm code as part of the pKVM
    hypervisor, including the dependent symbols.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7559,6 +7559,7 @@ static void vmx_update_hv_timer(struct kvm_vcpu *vcpu, bool force_immediate_exit
 		vmx->loaded_vmcs->hv_timer_soft_disabled = true;
 	}
 }
+#endif /* !__PKVM_HYP__ */
 
 void noinstr vmx_update_host_rsp(struct vcpu_vmx *vmx, unsigned long host_rsp)
 {
@@ -7567,7 +7568,6 @@ void noinstr vmx_update_host_rsp(struct vcpu_vmx *vmx, unsigned long host_rsp)
 		vmcs_writel(HOST_RSP, host_rsp);
 	}
 }
-#endif /* !__PKVM_HYP__ */
 
 void noinstr vmx_spec_ctrl_restore_host(struct vcpu_vmx *vmx,
 					unsigned int flags)
