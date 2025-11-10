52855ad1b77dbf20f4928540eb08cbbe7a74692b
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:32:31 +0800

    pKVM: VMX: Implement vm_destroy operation
    Import vmx_vm_destroy() from KVM to implement vm_destroy operation for
    pKVM. As virtual IPI support is not yet supported, this function just
    does nothing for pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -876,8 +876,8 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.vm_size = sizeof(struct kvm_vmx),
 
 	.vm_init = vt_op(vm_init),
-#ifndef __PKVM_HYP__
 	.vm_destroy = vt_op(vm_destroy),
+#ifndef __PKVM_HYP__
 	.vm_pre_destroy = vt_op_tdx_only(vm_pre_destroy),
 
 	.vcpu_precreate = vt_op(vcpu_precreate),
