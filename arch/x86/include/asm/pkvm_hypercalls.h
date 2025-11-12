1f6c35164f3c6b9b19b63cf8fa8fce254b795897
Dmytro Maluka <dmaluka@google.com>
2025-11-12 10:01:00 +0800

    pKVM: x86: Introduce macro magic for hypercall definitions
    In preparation for implementing vmexit stats for pKVM hypercalls which
    will print hypercall names in a human-readable form, introduce
    pkvm_hypercall.h which, a bit similarly to kvm-x86-ops.h, allows
    implementing multiple definitions of various things based on the
    hypercall names, while avoiding duplicating all those names in multiple
    places in the code.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

---
+++ arch/x86/include/asm/pkvm_hypercalls.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef PKVM_HC
+BUILD_BUG_ON(1)
+#endif
+
+/* Hypercalls used only during pKVM initialization */
+PKVM_HC(init_finalize)
+
+/* pKVM vmexit tracing/profiling */
+PKVM_HC(enable_vmexit_trace)
+PKVM_HC(dump_vmexit_trace)
+
+#undef PKVM_HC
