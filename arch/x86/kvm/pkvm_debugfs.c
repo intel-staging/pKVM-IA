d579822198e9de0ab100f47607108101892f70d3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-13 14:43:01 +0800

    pKVM: x86: trace: Register enable_vmexit_trace debugfs
    Register enable_vmexit_trace debugfs in /sys/kernel/debug/pkvm/, which
    uses the enable_vmexit_trace PV interface to control pKVM's tracing for
    each CPU.
    
    To enable tracing, echo 1 > /sys/kernel/debug/pkvm/enable_vmexit_trace.
    To disable tracing, echo 0 > /sys/kernel/debug/pkvm/enable_vmexit_trace.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm_debugfs.c
+++ arch/x86/kvm/pkvm_debugfs.c
@@ -3,11 +3,68 @@
 
 #include <linux/debugfs.h>
 #include <linux/kvm_host.h>
+#include <asm/kvm_pkvm.h>
+
+static void enable_vmexit_trace_func(void *data)
+{
+	u64 val;
+
+	if (!data)
+		return;
+
+	val = *(u64 *)data;
+	pkvm_hypercall(enable_vmexit_trace, val);
+}
+
+static int enable_vmexit_trace(void *data, u64 val)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		smp_call_function_single(cpu, enable_vmexit_trace_func, &val, true);
+
+	return 0;
+}
+DEFINE_SIMPLE_ATTRIBUTE(enable_vmexit_trace_fops, NULL, enable_vmexit_trace, "%llu\n");
+
+struct debugfs_item {
+	const char *name;
+	const umode_t mode;
+	const struct file_operations *fops;
+	struct dentry *dentry;
+};
+
+struct debugfs_item debugfs_files[] = {
+	{ "enable_vmexit_trace", 0222, &enable_vmexit_trace_fops},
+	{ NULL }
+};
 
 void pkvm_init_debugfs(void)
 {
 	struct dentry *debugfs_dir = debugfs_create_dir("pkvm", NULL);
+	struct debugfs_item *p;
 
-	if (IS_ERR_OR_NULL(debugfs_dir))
+	if (IS_ERR_OR_NULL(debugfs_dir)) {
 		pr_err("failed to add debugfs root\n");
+		return;
+	}
+
+	for (p = debugfs_files; p->name; ++p) {
+		p->dentry = debugfs_create_file(p->name, p->mode,
+						debugfs_dir,
+						NULL, p->fops);
+		if (IS_ERR_OR_NULL(p->dentry)) {
+			pr_err("failed to add debugfs %s\n", p->name);
+			goto out_dir;
+		}
+	}
+
+	return;
+
+out_dir:
+	for (p = debugfs_files; p->dentry; ++p) {
+		debugfs_remove(p->dentry);
+		p->dentry = NULL;
+	}
+	debugfs_remove(debugfs_dir);
 }
