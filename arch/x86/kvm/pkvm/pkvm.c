ebd8f8e0188c08deda42f6cacf757b7262221596
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-24 19:30:46 +0800

    pKVM: x86: Introduce struct pkvm_vcpu
    Introduce struct pkvm_vcpu to represent a guest vCPU. Similar to the
    struct kvm for pkvm_vm,  struct kvm_vcpu should be the last element
    to allow vendor specific extensions via structure embedding. And
    struct kvm_vcpu should also reside at offset 0 if it is wrapped by a
    vendor specific structure.
    
    A global variable kvm_vcpu_sz is introduced to represent the actual
    kvm_vcpu instance size, which is initialized by the size of struct
    kvm_vcpu as the default value. If struct kvm_vcpu is wrapped by a vendor
    code, then kvm_vcpu_sz should also be extended.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -42,6 +42,14 @@ static struct pkvm_vm_ref {
 	struct pkvm_vm *pkvm_vm;
 } pkvm_vms_ref[MAX_PKVM_VMS];
 
+/*
+ * Represents the actual, extended kvm_vcpu structure size. It is initialized as
+ * the size of struct kvm_vcpu. And if the vendor code extends kvm_vcpu instance
+ * via embedding struct kvm_vcpu to its specific structure, this size should also
+ * be extended by the vendor code.
+ */
+size_t kvm_vcpu_sz = sizeof(struct kvm_vcpu);
+
 static int pkvm_enable_virtualization_cpu(void)
 {
 	kvm_user_return_msr_cpu_online();
