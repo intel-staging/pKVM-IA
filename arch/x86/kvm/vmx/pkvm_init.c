3a3d57b21fcb207bd4afbfae0878c860580bfe96
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-01 13:30:37 +0800

    pKVM: x86: Add kernel command line to control relax_cpu_bugs
    Add a kernel command line parameter to control relax_cpu_bugs, which
    determines whether the pKVM hypervisor should run when it cannot
    mitigate all CPU vulnerabilities. By default, relax_cpu_bugs is true to
    allow pKVM to run even when some CPU vulnerabilities remain unmitigated
    to enable pKVM usage for development purposes. For production, this
    parameter should be set to false.
    
    Suggested-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -17,6 +17,11 @@ static int __init early_pkvm_parse_cmdline(char *buf)
 early_param("kvm-intel.pkvm", early_pkvm_parse_cmdline);
 
 static bool relax_cpu_bugs = true;
+static int __init early_pkvm_relax_cpu_bugs_parse_cmdline(char *buf)
+{
+	return kstrtobool(buf, &relax_cpu_bugs);
+}
+early_param("kvm-intel.pkvm_relax_cpu_bugs", early_pkvm_relax_cpu_bugs_parse_cmdline);
 
 static DEFINE_PER_CPU(struct pkvm_pcpu *, pkvm_pcpu);
 static DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
@@ -1244,6 +1249,8 @@ int __init vmx_pkvm_init(void)
 			goto out;
 		}
 		pr_warn("allow pkvm to run with unmitigated CPU bugs\n");
+		pr_warn("to prevent pkvm running on such CPU, ");
+		pr_cont("reboot with kvm-intel.pkvm_relax_cpu_bugs=false\n");
 	}
 
 	pkvm_sym(init_ops) = pkvm_sym(pkvm_vmx_init_ops);
