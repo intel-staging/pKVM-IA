5976cc0fe9ac6c4c17d159fb272bdef7d1c725da
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-02 09:15:50 +0800

    pKVM: x86: Add vm_destroy PV interface
    Add vm_destroy PV interface to destroy a guest VM. This interface
    releases the pkvm_vm handle, and involves the vendor-specific operation
    to destroy vendor resources. It also tears down donated memory pages and
    unshares the host KVM structure.
    
    The physical address and size of the torn-down memory pages are stored
    in the pkvm_memcache structure, which will be returned for the host to
    reclaim.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/pkvm_hypercalls.h
+++ arch/x86/include/asm/pkvm_hypercalls.h
@@ -18,6 +18,7 @@ PKVM_HC(dump_vmexit_trace)
 PKVM_HC(check_processor_compatibility)
 PKVM_HC(enable_virtualization_cpu)
 PKVM_HC(vm_init)
+PKVM_HC_OUT(vm_destroy)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT
