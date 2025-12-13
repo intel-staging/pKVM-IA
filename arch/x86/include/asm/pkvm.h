e449bc3721fd68da730aa2fe0e57a0e08408c335
Vineeth Pillai <vineethrp@google.com>
2025-12-13 09:42:33 -0500

    ANDROID: pkvm: x86: Move memcache definition out of kvm header
    In preparation to using memcache for page donation in pvIOMMU, move the
    pkvm_memcache definition from kvm_host.h to pkvm.h
    
    This is to expose pkvm_memcache to host driver code and also to
    hypercall param structures defined in pkvm.h. Since kvm_host.h includes
    pkvm.h, moving the definitions will not cause issues with kvm_host.h
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I441e9996b2a0d3f91769bf1742a321c1628433ae
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -91,6 +91,72 @@ static inline unsigned long __pkvm_hypercall(unsigned long nr, unsigned long p1,
 
 #ifdef CONFIG_PKVM_INTEL
 
+struct pkvm_memcache {
+	phys_addr_t head;
+	unsigned long nr_pages;
+#define PKVM_MC_ACCOUNT_PGTABLE_PAGES BIT(1)
+	unsigned long flags;
+};
+
+static inline void push_pkvm_memcache(struct pkvm_memcache *mc,
+				      phys_addr_t *p,
+				      phys_addr_t (*to_pa)(void *virt))
+{
+	*p = mc->head;
+	mc->head = to_pa(p);
+	mc->nr_pages++;
+}
+
+static inline void *pop_pkvm_memcache(struct pkvm_memcache *mc,
+				      void *(*to_va)(phys_addr_t phys))
+{
+	phys_addr_t *p = to_va(mc->head);
+
+	if (!mc->nr_pages)
+		return NULL;
+
+	mc->head = *p;
+	mc->nr_pages--;
+
+	return p;
+}
+
+static inline int __topup_pkvm_memcache(struct pkvm_memcache *mc,
+					unsigned long min_pages,
+					void *(*alloc_fn)(void *arg),
+					phys_addr_t (*to_pa)(void *virt),
+					void *arg)
+{
+	while (mc->nr_pages < min_pages) {
+		phys_addr_t *p = alloc_fn(arg);
+
+		if (!p)
+			return -ENOMEM;
+
+		push_pkvm_memcache(mc, p, to_pa);
+	}
+
+	return 0;
+}
+
+static inline void __free_pkvm_memcache(struct pkvm_memcache *mc,
+					void (*free_fn)(void *virt, void *arg),
+					void *(*to_va)(phys_addr_t phys),
+					void *arg)
+{
+	while (mc->nr_pages)
+		free_fn(pop_pkvm_memcache(mc, to_va), arg);
+}
+
+void free_pkvm_memcache(struct pkvm_memcache *mc);
+int topup_pkvm_memcache(struct pkvm_memcache *mc, unsigned long min_pages);
+
+static inline void init_pkvm_mmu_memcache(struct pkvm_memcache *mc)
+{
+	memset(mc, 0, sizeof(*mc));
+	mc->flags = PKVM_MC_ACCOUNT_PGTABLE_PAGES;
+}
+
 /* pviommu Hypercall structures */
 /*
  * With pviommu, iommu driver do not have full control over the iommu hardware and
