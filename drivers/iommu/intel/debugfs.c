a0ec8a3b81d4e853e05ca5e92fe395e9de64302e
Vineeth Pillai <vineethrp@google.com>
2025-12-15 10:21:45 -0500

    ANDROID: iommu/vt-d: Don't access IOMMU page table for FLPT_DEFAULT_DID
    For passthrough mode, pKVM converts the translation to second level and
    installs host ept as the page table. So do not access the page table in
    that case since host ept is not accessible from host.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I77eea121fec1969452123bfbace5a170b6e7d194
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/debugfs.c
+++ drivers/iommu/intel/debugfs.c
@@ -416,6 +416,16 @@ static int domain_translation_struct_show(struct seq_file *m,
 				goto iommu_unlock;
 
 			/*
+			 * Host uses FLPT_DEFAULT_DID as did for passthrough mode. pKVM switches
+			 * it to second level translation and uses host EPT as the page table to
+			 * enforce security guarantees by pKVM. Hoset EPT is not accessible to
+			 * host and hence do not try to walk the page table in that case.
+			 */
+			if (pkvm_pviommu_enabled() &&
+					pasid_get_domain_id(pasid_tbl_entry) == FLPT_DEFAULT_DID)
+				goto iommu_unlock;
+
+			/*
 			 * According to PASID Granular Translation Type(PGTT),
 			 * get the page table pointer.
 			 */
@@ -435,6 +445,16 @@ static int domain_translation_struct_show(struct seq_file *m,
 			}
 			pgd &= VTD_PAGE_MASK;
 		} else { /* legacy mode */
+			/*
+			 * Host uses FLPT_DEFAULT_DID as did for passthrough mode. pKVM switches
+			 * it to second level translation and uses host EPT as the page table to
+			 * enforce security guarantees by pKVM. Hoset EPT is not accessible to
+			 * host and hence do not try to walk the page table in that case.
+			 */
+			if (pkvm_pviommu_enabled() &&
+					context_domain_id(context) == FLPT_DEFAULT_DID)
+				goto iommu_unlock;
+
 			pgd = context->lo & VTD_PAGE_MASK;
 			agaw = context->hi & 7;
 		}
