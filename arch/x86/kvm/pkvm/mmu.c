68d6bd7e23723b10f766ebf8c84eea33524ddcb7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-16 09:17:10 +0800

    pKVM: x86: Clear memory pages before donate to host
    To make sure no sensitive data is leaked to the host after donating
    the memory pages used by the pKVM hypervisor, clear the contents in
    such memory pages if it is necessary. Also flush the cpu cache to
    prevent the host from getting the previous contents in RAM via
    non-coherent DMA.
    
    Suggested-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -445,6 +445,7 @@ unlock:
  * pkvm_hyp_donate_host() - Donate memory pages from hypervisor to host.
  * @phys:	Physical address of the memory region to donate.
  * @size:	Size of the memory region to donate.
+ * @clear:	If true, clear the memory region before donating.
  *
  * The donation transfers ownership of the memory pages in range
  * [@phys, @phys + @size) from the hypervisor to the host, thus allowing the
@@ -456,16 +457,20 @@ unlock:
  * updated to PKVM_PAGE_OWNED to indicate the ownership has been transferred to
  * the host.
  */
-void pkvm_hyp_donate_host(unsigned long phys, unsigned long size)
+void pkvm_hyp_donate_host(unsigned long phys, unsigned long size, bool clear)
 {
 	u64 prot = host_mmu.pgt_ops->calc_pte_perm(true, true, true) |
 		   host_mmu.pgt_ops->calc_pte_memtype(false);
+	void *va = __pkvm_va(phys);
 	int ret;
 
 	if (!PAGE_ALIGNED(phys) || !PAGE_ALIGNED(size) || size == 0) {
 		ret = -EINVAL;
 		goto out;
 	}
+
+	if (clear)
+		pkvm_clear_memory(va, size);
 
 	pkvm_host_mmu_lock();
 
