905a2e17892b0c8824baa73be0881080ca339001
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-24 10:40:37 +0800

    pKVM: x86: Add set_cr0 PV interface
    Add set_cr0 PV interface to allow the host to set guest CR0 via the
    vendor-specific set_cr0 operation. For a pVM, this PV interface allows
    the host to set the CR0 before the pVM has started running, but doesn't
    allow to modify it if the pVM is already running.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/pkvm_hypercalls.h
+++ arch/x86/include/asm/pkvm_hypercalls.h
@@ -30,6 +30,7 @@ PKVM_HC(set_msr)
 PKVM_HC_OUT(get_msr)
 PKVM_HC_OUT(cache_reg)
 PKVM_HC(set_cr4)
+PKVM_HC(set_cr0)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT
--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -695,6 +695,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__set_msr:
 	case __pkvm__get_msr:
 	case __pkvm__set_cr4:
+	case __pkvm__set_cr0:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -868,6 +869,17 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__set_cr4:
 		kvm_x86_call(set_cr4)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__set_cr0:
+		kvm_x86_call(set_cr0)(vcpu, pkvm_hc_input1(hvcpu));
+		/*
+		 * EFER will be updated if the vCPU enters to or exits from the
+		 * long mode. Update the EFER for the host unconditionally. As
+		 * the updating is just one line code which is simpler and has
+		 * smaller overhead comparing with the case of doing the check
+		 * first.
+		 */
+		to_pkvm_vcpu(vcpu)->shared_vcpu->arch.efer = vcpu->arch.efer;
 		break;
 	default:
 		ret = -EINVAL;
