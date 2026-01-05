BayLibre/android_kernel_common

ANDROID: pkvm: x86: Drop support for hardware requiring write buffer flush
pkvm is not expected to be supported on ancient hardware that requires
write buffer flushing. So explicitly remove the support for simplicity.

Bug: 391542119
Upstream-Task: 402758258
Test: Build and Boot

Change-Id: I91d15b3e2e66042bd16c36613d2aa402ef27d09f
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -241,6 +241,16 @@ static __init int check_and_init_iommu(struct pkvm_hyp *pkvm)
 		ecap = readq(drhd->iommu->reg + DMAR_ECAP_REG);
 
 		/*
+		 * Since pkvm is not expected to be supported on ancient hardware which
+		 * requires write buffer flushing, require cap_rwbuf=0 for simplicity.
+		 */
+		if (cap_rwbf(cap)) {
+			pr_warn("pkvm: drhd reg_base 0x%llx: CAP.RWBUF=1 is not supported\n",
+					drhd->reg_base_addr);
+			return -EINVAL;
+		}
+
+		/*
 		 * pkvm expects Queued Invalidation support for simplicity and efficiency.
 		 */
 		if (!ecap_qis(ecap)) {

intel-staging/pKVM-IA

arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -243,6 +243,16 @@ static __init int check_and_init_iommu(struct pkvm_hyp *pkvm)
 		ecap = readq(drhd->iommu->reg + DMAR_ECAP_REG);
 
 		/*
+		 * Since pkvm is not expected to be supported on ancient hardware which
+		 * requires write buffer flushing, require cap_rwbf=0 for simplicity.
+		 */
+		if (cap_rwbf(cap)) {
+			pr_warn("pkvm: drhd reg_base 0x%llx: CAP.RWBF=1 is not supported\n",
+					drhd->reg_base_addr);
+			return -EINVAL;
+		}
+
+		/*
 		 * pkvm expects Queued Invalidation support for simplicity and efficiency.
 		 */
 		if (!ecap_qis(ecap)) {