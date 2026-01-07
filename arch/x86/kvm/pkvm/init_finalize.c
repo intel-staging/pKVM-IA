cxdong/pub-pKVM-IA

commit 61b41408baa0855fc97d67c2fa0f99856b3c900a

pKVM: x86: Sort the exception table exception
Sort the exception table section so that to make the exception table
searching faster with the binary search.

The exception table is in the pKVM's rodata section. Once switch to the
pKVM's MMU, the rodata section is mapped with read-only thus sorting the
exception table cannot be done as it requires read-write permission. So
sorting the exception table with the host MMU which still maps the pKVM's
rodata section with read-write permission before switching to the pKVM's
MMU.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/init_finalize.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kvm_host.h>
+#include <linux/extable.h>
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
 #include "init_finalize.h"
@@ -218,6 +219,17 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 
 	if (!PAGE_ALIGNED(mem_base) || !mem_size)
 		return -EINVAL;
+
+	/*
+	 * The exception table is in the pKVM's rodata section. Once switch
+	 * to the pKVM's MMU, the rodata section is mapped with read-only thus
+	 * sorting the exception table cannot be done as it requires read-write
+	 * permission. So sorting the exception table with the host MMU which
+	 * still maps the pKVM's rodata section with read-write permission
+	 * before switching to the pKVM's MMU.
+	 */
+	if (&__stop___ex_table > &__start___ex_table)
+		sort_extable(__start___ex_table, __stop___ex_table);
 
 	ret = divide_memory_pool(mem_base, mem_size);
 	if (ret)

arch/x86/include/asm/pkvm_image.h
@@ -48,6 +48,9 @@ extern char pkvm_sym(text_start)[], pkvm_sym(text_end)[];
 extern char pkvm_sym(rodata_start)[], pkvm_sym(rodata_end)[];
 extern char pkvm_sym(data_start)[], pkvm_sym(data_end)[];
 extern char pkvm_sym(bss_start)[], pkvm_sym(bss_end)[];
+struct exception_table_entry;
+extern struct exception_table_entry pkvm_sym(__start___ex_table)[];
+extern struct exception_table_entry pkvm_sym(__stop___ex_table)[];
 static inline bool is_pkvm_text(void *addr)
 {
 	return addr >= (void *)pkvm_sym(text_start) && addr < (void *)pkvm_sym(text_end);

intel-staging/pKVM-IA

commit 158741fcf4c637028432902d8048503eac739c90

SQUASHME: pKVM: x86: Sort the exception table before deprvilege
The pKVM hypervisor's IDT will be programmed into VMCS before
deprivileging the CPU. Once deprivileging is done and the CPU
enters to the root mode, the pKVM's exception handlers should be
functional. So before that, sort pKVM's exception table to make sure the
exception fixup working as expected.

Fixes: 61b41408baa0 ("pKVM: x86: Sort the exception table exception")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/init_finalize.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kvm_host.h>
-#include <linux/extable.h>
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
 #include "fpu.h"
@@ -226,17 +225,6 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 
 	if (!PAGE_ALIGNED(mem_base) || !mem_size)
 		return -EINVAL;
-
-	/*
-	 * The exception table is in the pKVM's rodata section. Once switch
-	 * to the pKVM's MMU, the rodata section is mapped with read-only thus
-	 * sorting the exception table cannot be done as it requires read-write
-	 * permission. So sorting the exception table with the host MMU which
-	 * still maps the pKVM's rodata section with read-write permission
-	 * before switching to the pKVM's MMU.
-	 */
-	if (&__stop___ex_table > &__start___ex_table)
-		sort_extable(__start___ex_table, __stop___ex_table);
 
 	ret = divide_memory_pool(mem_base, mem_size);
 	if (ret)

arch/x86/kvm/vmx/pkvm_init.c
@@ -3,6 +3,7 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/extable.h>
 #include <asm/pkvm_image.h>
 #include "pkvm_constants.h"
 #include "vmx.h"
@@ -1025,6 +1026,16 @@ static __init int pkvm_host_deprivilege_cpus(struct pkvm_hyp *pkvm)
 	int cpu, ret = 0;
 
 	pkvm_sym(pkvm_vmx_register_excp_handlers)();
+
+	/*
+	 * The pKVM hypervisor's IDT will be programmed into VMCS before
+	 * deprivileging the CPU. Once deprivileging is done and the CPU
+	 * enters to the root mode, the pKVM's exception handlers should be
+	 * functional. So before that, sort pKVM's exception table to make
+	 * sure the exception fixup working as expected.
+	 */
+	if (&pkvm_sym(__stop___ex_table) > &pkvm_sym(__start___ex_table))
+		sort_extable(pkvm_sym(__start___ex_table), pkvm_sym(__stop___ex_table));
 
 	for_each_possible_cpu(cpu) {
 		ret = smp_call_function_single(cpu, pkvm_host_deprivilege_cpu, &p, 1);