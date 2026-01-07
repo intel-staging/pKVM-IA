cxdong/pub-pKVM-IA

commit 1604e0800c5b5e5f37d0eaa03498ef8554f5a68d

pKVM: vmx: Enforce PMU isolation
Currently pkvm doesn't touch any PMU register, implying that PMU is
fully controlled by the host which could gain insight into the
activities of pKVM or pVM. The worse - the use of PEBS could lead
to arbitrary memory corruption in pVM on platforms where PEBS
buffer is based on virtual address.

There are quite some PMU related MSRs. Fortunately they are all
controlled by a global control MSR. Ensure it's cleared to ZERO
to disable all PMU activities upon VM-exit and restored to the host
value upon VM-enter.

To avoid the overhead of reading the MSR in every exit, intercept
host write to MSR_CORE_PERF_GLOBAL_CTRL and enable VMCS control
fields only when it's set to a non-ZERO value.

With this change the host can only profile itself. To enable the
host profiling npVM, additional logic is required to pass info
from the host about which PMU MSRs should be switched when entering/
exiting npVM based on policy in host perf core. pkvm-high also
need hook to the kvm perf callbacks for acquiring guest IP and
guest state (user vs. kernel) from pKVM.

Signed-off-by: Kevin Tian <kevin.tian@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -142,11 +142,45 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
 	int ret = X86EMUL_CONTINUE;
 	u32 low, high;
+	u64 val;
 
 	low = vcpu->arch.regs[VCPU_REGS_RAX];
 	high = vcpu->arch.regs[VCPU_REGS_RDX];
+	val = low | ((u64)high << 32);
 
 	switch (msr) {
+	case MSR_CORE_PERF_GLOBAL_CTRL: {
+		struct kvm_pmu *pmu = vcpu_to_pmu(vcpu);
+		struct vcpu_vmx *vmx = to_vmx(vcpu);
+
+		if (pmu->global_ctrl == val)
+			break;
+
+		/*
+		 * PMU is owned by the host. But the host must be prevented
+		 * from profiling pKVM or pVM so the global ctrl MSR is kept
+		 * as ZERO (disabled) outside of the host context.
+		 *
+		 * Capture the value written by host. If it's non-zero then
+		 * update the VMCS guest field and rely on VM entry/exit
+		 * control to switch the MSR value. The VMCS host field is
+		 * fixed to ZERO.
+		 */
+		pmu->global_ctrl = val;
+		if (val) {
+			vmcs_write64(GUEST_IA32_PERF_GLOBAL_CTRL, val);
+			vm_entry_controls_setbit(vmx,
+					VM_ENTRY_LOAD_IA32_PERF_GLOBAL_CTRL);
+			vm_exit_controls_setbit(vmx,
+					VM_EXIT_LOAD_IA32_PERF_GLOBAL_CTRL);
+		} else {
+			vm_entry_controls_clearbit(vmx,
+					VM_ENTRY_LOAD_IA32_PERF_GLOBAL_CTRL);
+			vm_exit_controls_clearbit(vmx,
+					VM_EXIT_LOAD_IA32_PERF_GLOBAL_CTRL);
+		}
+		break;
+	}
 	default:
 		/*
 		 * The MSRs intercepted by the writing bitmap should be

arch/x86/kvm/vmx/pkvm_init.c
@@ -29,7 +29,9 @@ static struct gdt_page pkvm_gdt_page = {
 	},
 };
 
-static unsigned int intercept_w_msrs[] = {};
+static unsigned int intercept_w_msrs[] = {
+	MSR_CORE_PERF_GLOBAL_CTRL,
+};
 
 u64 pkvm_total_reserve_pages(void)
 {
@@ -98,13 +100,15 @@ static __init int pkvm_setup_host_vmcs_config(void)
 			VM_EXIT_LOAD_IA32_EFER |
 			VM_EXIT_SAVE_IA32_PAT |
 			VM_EXIT_SAVE_IA32_EFER |
-			VM_EXIT_SAVE_DEBUG_CONTROLS,
+			VM_EXIT_SAVE_DEBUG_CONTROLS |
+			VM_EXIT_LOAD_IA32_PERF_GLOBAL_CTRL,
 		.vmexit_ctrl_opt = 0,
 		.vmentry_ctrl_req =
 			VM_ENTRY_LOAD_DEBUG_CONTROLS |
 			VM_ENTRY_IA32E_MODE |
 			VM_ENTRY_LOAD_IA32_EFER |
-			VM_ENTRY_LOAD_IA32_PAT,
+			VM_ENTRY_LOAD_IA32_PAT |
+			VM_ENTRY_LOAD_IA32_PERF_GLOBAL_CTRL,
 		.vmentry_ctrl_opt = 0,
 	};
 
@@ -338,7 +342,7 @@ static inline u32 get_ar(u16 sel)
 		vmcs_write32(GUEST_##SEG##_LIMIT, limit);	\
 	} while (0)
 
-static __init void init_guest_state_area_from_native(void)
+static __init void init_guest_state_area_from_native(struct vcpu_vmx *vmx)
 {
 	int cpu = smp_processor_id();
 	struct desc_ptr dt;
@@ -402,11 +406,18 @@ static __init void init_guest_state_area_from_native(void)
 
 	rdmsrq(MSR_IA32_CR_PAT, msrq);
 	vmcs_write64(GUEST_IA32_PAT, msrq);
+
+	if (!rdmsrq_safe(MSR_CORE_PERF_GLOBAL_CTRL, &msrq)) {
+		struct kvm_pmu *pmu = vcpu_to_pmu(&vmx->vcpu);
+
+		pmu->global_ctrl = msrq;
+		vmcs_write64(GUEST_IA32_PERF_GLOBAL_CTRL, msrq);
+	}
 }
 
-static __init void init_guest_state_area(void)
+static __init void init_guest_state_area(struct vcpu_vmx *vmx)
 {
-	init_guest_state_area_from_native();
+	init_guest_state_area_from_native(vmx);
 
 	/*Guest non register state*/
 	vmcs_write32(GUEST_ACTIVITY_STATE, GUEST_ACTIVITY_ACTIVE);
@@ -457,6 +468,8 @@ static __init void init_host_state_area(struct vcpu_vmx *vmx, struct pkvm_hyp *p
 
 	rdmsrq(MSR_IA32_CR_PAT, msrq);
 	vmcs_write64(HOST_IA32_PAT, msrq);
+
+	vmcs_write64(HOST_IA32_PERF_GLOBAL_CTRL, 0);
 
 	/*
 	 * [pcpu->stack, pcpu->stack + PKVM_STACK_SIZE) is per cpu stack.
@@ -539,13 +552,27 @@ static __init void init_execution_control(struct vcpu_vmx *vmx)
 
 static __init void init_vmexit_control(struct vcpu_vmx *vmx)
 {
-	vm_exit_controls_set(vmx, pkvm_sym(host_vmcs_config).vmexit_ctrl);
+	u32 vmexit_ctrl = pkvm_sym(host_vmcs_config).vmexit_ctrl;
+	struct kvm_pmu *pmu = vcpu_to_pmu(&vmx->vcpu);
+
+	/* No need to switch if PMU is not enabled */
+	if (!pmu->global_ctrl)
+		vmexit_ctrl &= ~VM_EXIT_LOAD_IA32_PERF_GLOBAL_CTRL;
+
+	vm_exit_controls_set(vmx, vmexit_ctrl);
 	vmcs_write32(VM_EXIT_MSR_STORE_COUNT, 0);
 }
 
 static __init void init_vmentry_control(struct vcpu_vmx *vmx)
 {
-	vm_entry_controls_set(vmx, pkvm_sym(host_vmcs_config).vmentry_ctrl);
+	u32 vmentry_ctrl = pkvm_sym(host_vmcs_config).vmentry_ctrl;
+	struct kvm_pmu *pmu = vcpu_to_pmu(&vmx->vcpu);
+
+	/* No need to switch if PMU is not enabled */
+	if (!pmu->global_ctrl)
+		vmentry_ctrl &= ~VM_ENTRY_LOAD_IA32_PERF_GLOBAL_CTRL;
+
+	vm_entry_controls_set(vmx, vmentry_ctrl);
 	vmcs_write32(VM_ENTRY_INTR_INFO_FIELD, 0);
 	vmcs_write32(VM_ENTRY_MSR_LOAD_COUNT, 0);
 	vmcs_write32(VM_ENTRY_INTR_INFO_FIELD, 0);
@@ -557,7 +584,7 @@ static __init int pkvm_host_init_vmx(struct vcpu_vmx *vmx, struct pkvm_hyp *pkvm
 	vmcs_load(vmx->loaded_vmcs->vmcs);
 	vmx->loaded_vmcs->cpu = smp_processor_id();
 
-	init_guest_state_area();
+	init_guest_state_area(vmx);
 	init_host_state_area(vmx, pkvm);
 	init_execution_control(vmx);
 	init_vmexit_control(vmx);

intel-staging/pKVM-IA

commit 64f6e5f8f8c4ba666c3d79afe5971296a7815d6e

SQUASHME: pKVM: x86: Fix host MSR_CORE_PERF_GLOBAL_CTRL emulation
Writing MSR_CORE_PERF_GLOBAL_CTRL from the host is always intercepted.
Before emulating, should check if this MSR is implemented by the
hardware or not. If it is not, should inject #GP.

Fixes: 1604e0800c5b ("pKVM: vmx: Enforce PMU isolation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -149,6 +149,11 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 		struct kvm_pmu *pmu = vcpu_to_pmu(vcpu);
 		struct vcpu_vmx *vmx = to_vmx(vcpu);
 
+		if (!kvm_pmu_has_perf_global_ctrl(pmu)) {
+			ret = X86EMUL_UNHANDLEABLE;
+			break;
+		}
+
 		if (pmu->global_ctrl == val)
 			break;
 

arch/x86/kvm/vmx/pkvm_init.c
@@ -714,7 +714,11 @@ static __init void init_guest_state_area_from_native(struct vcpu_vmx *vmx)
 
 	if (!rdmsrq_safe(MSR_CORE_PERF_GLOBAL_CTRL, &msrq)) {
 		struct kvm_pmu *pmu = vcpu_to_pmu(&vmx->vcpu);
+		union cpuid10_eax eax = {
+			.full = native_cpuid_eax(10),
+		};
 
+		pmu->version = eax.split.version_id;
 		pmu->global_ctrl = msrq;
 		vmcs_write64(GUEST_IA32_PERF_GLOBAL_CTRL, msrq);
 	}