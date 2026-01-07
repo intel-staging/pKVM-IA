cxdong/pub-pKVM-IA

commit 71cb278042b55e5609446557cc06376d3ca66860

pKVM: x86: Introduce infrastructure to intercept host MSR writing
Introduce intercept_w_msrs[] to contain the MSR which should be
intercepted by the pKVM hypervisor when the host is writing. The
intercepted MSR will be emulated by the pKVM hypervisor specifically
by the wrmsr vmexit handler.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -140,24 +140,29 @@ static int handle_read_msr(struct kvm_vcpu *vcpu)
 static int handle_write_msr(struct kvm_vcpu *vcpu)
 {
 	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	int ret = X86EMUL_CONTINUE;
 	u32 low, high;
-
-	/*
-	 * The MSR writing bitmap doesn't intercept any MSR. If the vmexit is
-	 * caused by such MSR in the range of the bitmap, it should be a code
-	 * bug.
-	 */
-	BUG_ON(is_msr_in_bitmap_range(msr));
 
 	low = vcpu->arch.regs[VCPU_REGS_RAX];
 	high = vcpu->arch.regs[VCPU_REGS_RDX];
 
-	if (wrmsr_safe(msr, low, high)) {
-		kvm_inject_gp(vcpu, 0);
-		return X86EMUL_UNHANDLEABLE;
+	switch (msr) {
+	default:
+		/*
+		 * The MSRs intercepted by the writing bitmap should be
+		 * emulated by the switch cases. Otherwise it should be a code
+		 * bug.
+		 */
+		BUG_ON(is_msr_in_bitmap_range(msr));
+
+		if (wrmsr_safe(msr, low, high)) {
+			kvm_inject_gp(vcpu, 0);
+			ret = X86EMUL_UNHANDLEABLE;
+		}
+		break;
 	}
 
-	return X86EMUL_CONTINUE;
+	return ret;
 }
 
 static void handle_preemption_timer(struct kvm_vcpu *vcpu)

arch/x86/kvm/vmx/pkvm_init.c
@@ -29,6 +29,8 @@ static struct gdt_page pkvm_gdt_page = {
 	},
 };
 
+static unsigned int intercept_w_msrs[] = {};
+
 u64 pkvm_total_reserve_pages(void)
 {
 	u64 total = pkvm_vmx_data_pages();
@@ -270,6 +272,10 @@ static __init int pkvm_setup_host_vcpu(struct pkvm_hyp *pkvm, int cpu)
 		pr_err("no msr_bitmap page for CPU%d\n", cpu);
 		return -ENOMEM;
 	}
+
+	/* Set msr bitmap to intercept some MSR writing */
+	for (int i = 0; i < ARRAY_SIZE(intercept_w_msrs); i++)
+		vmx_set_msr_bitmap_write(vmx->vmcs01.msr_bitmap, intercept_w_msrs[i]);
 
 	vmx->vcpu.cpu = cpu;
 	vmx->vcpu.vcpu_id = kvm->created_vcpus;

intel-staging/pKVM-IA

commit faefe24e213fa5457824ad9194bf85a049c66b01

SQUASHME: pKVM: x86: Refactor handle_write_msr a bit
Refactor handle_write_msr to check and inject #GP out of the switch
so that the #GP injection can be leveraged not only the default case
but also the other cases easily.

Fixes: 71cb278042b5 ("pKVM: x86: Introduce infrastructure to intercept host MSR writing")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -190,12 +190,14 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 		 */
 		BUG_ON(is_msr_in_bitmap_range(msr));
 
-		if (wrmsr_safe(msr, low, high)) {
-			kvm_inject_gp(vcpu, 0);
+		if (wrmsr_safe(msr, low, high))
 			ret = X86EMUL_UNHANDLEABLE;
-		}
+
 		break;
 	}
+
+	if (ret == X86EMUL_UNHANDLEABLE)
+		kvm_inject_gp(vcpu, 0);
 
 	return ret;
 }