df9a4373aebd800eaf5187d7ddddf39ddaff926b
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-08 12:39:07 +0800

    pKVM: x86: Prevent pKVM to run if CPU has unsupported bugs
    Enhance CPU bugs mitigations for pKVM. Instead of attempting to mitigate
    CPU bugs across various CPU generations, focus on ADL and PTL CPU bugs
    (as PTL CPU doesn't have any newer bugs than ADL, mitigating ADL also
     mitigates PTL) and implement mitigations in the pKVM hypervisor.
    
    At the same time, to be able to run pKVM on other CPUs as well (for
    development purpose), introduce a global parameter relax_cpu_bugs, set to
    true by default. This allows pKVM to run even when unsupported CPU bugs
    exist, enabling pKVM usage on older CPU generations (e.g. RKL) with
    unmitigated CPU bugs.
    
    Suggested-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -99,6 +99,7 @@ extern u64 pkvm_sym(sme_me_mask);
 #endif
 extern struct cpumask pkvm_sym(__cpu_possible_mask);
 extern unsigned int pkvm_sym(nr_cpu_ids);
+DECLARE_STATIC_KEY_FALSE(pkvm_sym(switch_vcpu_ibpb));
 
 u64 pkvm_total_reserve_pages(void);
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
