f247aee2665a48ca897583dd32f6995658f9b131
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 22:06:10 +0800

    pKVM: x86: Add __cpu_possible_mask and nr_cpu_ids symbols
    Add __cpu_possible_mask and nr_cpu_ids symbols to allow the pKVM
    hypervisor to use for_each_possible_cpu.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -114,6 +114,8 @@ extern pteval_t pkvm_sym(__default_kernel_pte_mask);
 extern u64 pkvm_sym(sme_me_mask);
 #endif
 extern struct pkvm_init_ops *pkvm_sym(init_ops);
+extern struct cpumask pkvm_sym(__cpu_possible_mask);
+extern unsigned int pkvm_sym(nr_cpu_ids);
 
 u64 pkvm_total_reserve_pages(void);
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
