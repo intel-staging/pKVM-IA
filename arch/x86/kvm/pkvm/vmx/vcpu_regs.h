cxdong/pub-pKVM-IA

commit 321aa36f10507ee1c9716a69b7cbf8a8c00d01e4

pKVM: VMX: Setup the RSP and RIP fields for the pKVM hypervisor
Each pkvm_pcpu has its own stack memory which is defined as 16K. The top
16 bytes are reserved to store parameters as below for the pkvm
hypervisor to use after the deprivileged host vmexit to the root mode.
And the RSP starts from stack top - 16 is:
 ------------ Stack layout ----------
 stack_top:
 stack_resv + 8:     struct vcpu_vmx *vmx
 stack_resv + 0:     pointer to vcpu->arch.regs
 stack_resv:         VMCS.HOST_RSP for PCPU
                     .........
                     .........
 stack_bottom:

The RIP is set to pkvm_host_vmexit_entry which is the first function to
run after vmexit. It will save all the GPRs to vcpu->arch.regs and
handle vmexit reasons (implemented by the following patch). Then restore
the GPRs and vmresume to the deprivileged host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmentry.S
@@ -1,0 +1,100 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/linkage.h>
+#include <asm/kvm_vcpu_regs.h>
+#include <asm/frame.h>
+#include <asm/asm.h>
+#include <asm/bitsperlong.h>
+#include <asm/unwind_hints.h>
+#include <asm/nospec-branch.h>
+
+#define WORD_SIZE (BITS_PER_LONG / 8)
+
+#define VCPU_RAX	(__VCPU_REGS_RAX * WORD_SIZE)
+#define VCPU_RCX	(__VCPU_REGS_RCX * WORD_SIZE)
+#define VCPU_RDX	(__VCPU_REGS_RDX * WORD_SIZE)
+#define VCPU_RBX	(__VCPU_REGS_RBX * WORD_SIZE)
+#define VCPU_RBP	(__VCPU_REGS_RBP * WORD_SIZE)
+#define VCPU_RSI	(__VCPU_REGS_RSI * WORD_SIZE)
+#define VCPU_RDI	(__VCPU_REGS_RDI * WORD_SIZE)
+
+#define VCPU_R8		(__VCPU_REGS_R8  * WORD_SIZE)
+#define VCPU_R9		(__VCPU_REGS_R9  * WORD_SIZE)
+#define VCPU_R10	(__VCPU_REGS_R10 * WORD_SIZE)
+#define VCPU_R11	(__VCPU_REGS_R11 * WORD_SIZE)
+#define VCPU_R12	(__VCPU_REGS_R12 * WORD_SIZE)
+#define VCPU_R13	(__VCPU_REGS_R13 * WORD_SIZE)
+#define VCPU_R14	(__VCPU_REGS_R14 * WORD_SIZE)
+#define VCPU_R15	(__VCPU_REGS_R15 * WORD_SIZE)
+
+/*
+ * pkvm_host_vmexit_entry - Handle a VM-Exit event from the deprivileged
+ * host in pkvm.
+ *
+ * The pKVM stack starts from fixed address, which is pkvm_sp_top
+ * configured in VMCS.HOST_RSP.
+ *
+ * pkvm_sp_top = pcpu->stack + PKVM_STACK_SIZE - PKVM_STACK_TOP_RESV
+ * pkvm_sp_top[0] --> pointer to vcpu->arch.regs
+ * pkvm_sp_top[1] --> struct vcpu_vmx *vmx
+ *
+ */
+SYM_FUNC_START(pkvm_host_vmexit_entry)
+	ENDBR
+	push %_ASM_BX
+	mov WORD_SIZE*1(%_ASM_SP), %_ASM_BX
+
+	mov %_ASM_AX, VCPU_RAX(%_ASM_BX)
+	mov %_ASM_CX, VCPU_RCX(%_ASM_BX)
+	mov %_ASM_DX, VCPU_RDX(%_ASM_BX)
+	mov %_ASM_SI, VCPU_RSI(%_ASM_BX)
+	mov %_ASM_DI, VCPU_RDI(%_ASM_BX)
+	mov %_ASM_BP, VCPU_RBP(%_ASM_BX)
+	mov %r8 , VCPU_R8(%_ASM_BX)
+	mov %r9 , VCPU_R9(%_ASM_BX)
+	mov %r10, VCPU_R10(%_ASM_BX)
+	mov %r11, VCPU_R11(%_ASM_BX)
+	mov %r12, VCPU_R12(%_ASM_BX)
+	mov %r13, VCPU_R13(%_ASM_BX)
+	mov %r14, VCPU_R14(%_ASM_BX)
+	mov %r15, VCPU_R15(%_ASM_BX)
+	pop VCPU_RBX(%_ASM_BX)
+
+	/* RDI: point to vcpu_vmx */
+	mov WORD_SIZE*1(%_ASM_SP), %_ASM_DI
+
+	/* pKVM never enables interrupts */
+	call pkvm_host_vmexit_main
+
+	/*
+	 * Restore registers to reflect the vm-exit handling/emulation result
+	 * before vmresume.
+	 */
+	mov VCPU_RAX(%_ASM_BX), %_ASM_AX
+	mov VCPU_RCX(%_ASM_BX), %_ASM_CX
+	mov VCPU_RDX(%_ASM_BX), %_ASM_DX
+	mov VCPU_RSI(%_ASM_BX), %_ASM_SI
+	mov VCPU_RDI(%_ASM_BX), %_ASM_DI
+	mov VCPU_RBP(%_ASM_BX), %_ASM_BP
+	mov VCPU_R8(%_ASM_BX),  %r8
+	mov VCPU_R9(%_ASM_BX),  %r9
+	mov VCPU_R10(%_ASM_BX), %r10
+	mov VCPU_R11(%_ASM_BX), %r11
+	mov VCPU_R12(%_ASM_BX), %r12
+	mov VCPU_R13(%_ASM_BX), %r13
+	mov VCPU_R14(%_ASM_BX), %r14
+	mov VCPU_R15(%_ASM_BX), %r15
+	mov VCPU_RBX(%_ASM_BX), %_ASM_BX
+
+	/*
+	 * The first time of entering guest is in local_deprivilege_cpu() which
+	 * requires vmlaunch. This code path is called after that, hence only
+	 * vmresume required.
+	 */
+	vmresume
+
+	/* should not come here */
+	mov $0x4400, %_ASM_CX
+	vmread %_ASM_CX, %_ASM_AX
+failed:
+	jmp failed			/* should never come here */
+SYM_FUNC_END(pkvm_host_vmexit_entry)

arch/x86/include/asm/kvm_pkvm.h
@@ -7,6 +7,8 @@
 
 #define PKVM_MEMBLOCK_REGIONS		128
 #define PKVM_STACK_SIZE			SZ_16K
+/* Size of reserved space for private parameter in pKVM stack */
+#define PKVM_STACK_TOP_RESV		16
 
 struct pkvm_pcpu {
 	u8 stack[PKVM_STACK_SIZE] __aligned(16);
@@ -34,6 +36,11 @@ static inline unsigned long pkvm_data_pages(unsigned long extra_global,
 	unsigned long percpu_pages = PKVM_PCPU_PAGES + extra_percpu;
 
 	return global_pages + percpu_pages * num_possible_cpus();
+}
+
+static inline unsigned long get_host_stack_top(struct pkvm_pcpu *pcpu)
+{
+	return (unsigned long) &pcpu->stack[sizeof(pcpu->stack)];
 }
 
 #endif /* CONFIG_PKVM_X86 */

arch/x86/kvm/pkvm/Makefile
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
 
-pkvm-hyp-obj			:= early_alloc.o
+ccflags-y += -I $(srctree)/arch/x86/kvm
 
-obj-$(CONFIG_PKVM_X86)		+= $(pkvm-hyp-obj)
+pkvm-hyp-y			:= early_alloc.o
+
+pkvm-hyp-$(CONFIG_PKVM_INTEL)	+= vmx/host_vmentry.o vmx/host_vmx.o
+
+obj-$(CONFIG_PKVM_X86)		+= pkvm-hyp.o

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -1,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "host_vmx.h"
+
+void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
+{
+	/* TODO */
+}

arch/x86/kvm/pkvm/vmx/host_vmx.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_VMX_HOST_VMX_H
+#define __PKVM_VMX_HOST_VMX_H
+
+#include <vmx/vmx.h>
+
+void pkvm_host_vmexit_main(struct vcpu_vmx *vmx);
+
+#endif /* __PKVM_VMX_HOST_VMX_H */

arch/x86/kvm/vmx/pkvm_init.c
@@ -276,9 +276,10 @@ static __init void init_guest_state_area(void)
 	vmcs_write64(VMCS_LINK_POINTER, -1ull);
 }
 
-static __init void init_host_state_area(void)
+static __init void init_host_state_area(struct vcpu_vmx *vmx, struct pkvm_hyp *pkvm)
 {
 	int cpu = smp_processor_id();
+	unsigned long host_rsp;
 	struct desc_ptr dt;
 	u16 selector;
 	u64 msrq;
@@ -331,6 +332,31 @@ static __init void init_host_state_area(void)
 
 	rdmsrq(MSR_IA32_CR_PAT, msrq);
 	vmcs_write64(HOST_IA32_PAT, msrq);
+
+	/*
+	 * [pcpu->stack, pcpu->stack + PKVM_STACK_SIZE) is per cpu stack.
+	 * It is used as stack when the pcpu enters pKVM, i.e. HOST stack from
+	 * VMX point of view.
+	 *
+	 * Within the top of stack, a small region starting from stack_resv
+	 * is reserved  to store private paremeters,
+	 *
+	 * ------------ Stack layout ----------
+	 * stack_top:
+	 * stack_resv + 8:	struct vcpu_vmx *vmx
+	 * stack_resv + 0:	pointer to vcpu->arch.regs
+	 * stack_resv:		(stack_top - PKVM_STACK_TOP_RESV) = VMCS.HOST_RSP for PCPU
+	 *			.........
+	 *			.........
+	 * stack_bottom:
+	 */
+	host_rsp = get_host_stack_top(pkvm->pcpus[cpu]) - PKVM_STACK_TOP_RESV;
+
+	vmcs_writel(HOST_RSP, host_rsp);
+	*((struct vcpu_vmx **) (host_rsp + 8)) = vmx;
+	*((unsigned long **) host_rsp) = vmx->vcpu.arch.regs;
+
+	vmcs_writel(HOST_RIP, (unsigned long)pkvm_host_vmexit_entry);
 }
 
 static __init void init_execution_control(struct vcpu_vmx *vmx)
@@ -396,14 +422,14 @@ static __init void init_vmentry_control(struct vcpu_vmx *vmx)
 	vmcs_write32(VM_ENTRY_INTR_INFO_FIELD, 0);
 }
 
-static __init int pkvm_host_init_vmx(struct vcpu_vmx *vmx)
+static __init int pkvm_host_init_vmx(struct vcpu_vmx *vmx, struct pkvm_hyp *pkvm)
 {
 	vmx->loaded_vmcs = &vmx->vmcs01;
 	vmcs_load(vmx->loaded_vmcs->vmcs);
 	vmx->loaded_vmcs->cpu = smp_processor_id();
 
 	init_guest_state_area();
-	init_host_state_area();
+	init_host_state_area(vmx, pkvm);
 	init_execution_control(vmx);
 	init_vmexit_control(vmx);
 	init_vmentry_control(vmx);
@@ -434,7 +460,7 @@ static __init void pkvm_host_deprivilege_cpu(void *data)
 		goto done;
 	}
 
-	ret = pkvm_host_init_vmx(to_vmx(vcpu));
+	ret = pkvm_host_init_vmx(to_vmx(vcpu), p->pkvm);
 	if (ret) {
 		pr_err("CPU%d init vmx failed, ret %d\n", cpu, ret);
 		goto vmxoff;

arch/x86/kvm/vmx/vmx.h
@@ -776,6 +776,7 @@ static inline unsigned long pkvm_vmx_data_pages(void)
 }
 
 int __init vmx_pkvm_init(void);
+void pkvm_host_vmexit_entry(void);
 
 #endif /* CONFIG_PKVM_INTEL */
 

intel-staging/pKVM-IA

commit fcb594bd17eb0f8c2a43f78eab5003e1d04a26ec

SQUASHME: pKVM: VMX: Move vCPU register macros to header
These macros will be used by reprivilege code as well and hence, move it
to a common header file.

Fixes: 321aa36f1050 (pKVM: VMX: Setup the RSP and RIP fields for the pKVM hypervisor)

Signed-off-by: Vineeth Pillai (Google) <vineeth@bitbyteword.org>

arch/x86/kvm/pkvm/vmx/host_vmentry.S
@@ -3,30 +3,11 @@
 #include <asm/kvm_vcpu_regs.h>
 #include <asm/frame.h>
 #include <asm/asm.h>
-#include <asm/bitsperlong.h>
 #include <asm/unwind_hints.h>
 #include <asm/nospec-branch.h>
 #include "kvm-asm-offsets.h"
 #include "run_flags.h"
-
-#define WORD_SIZE (BITS_PER_LONG / 8)
-
-#define VCPU_RAX	(__VCPU_REGS_RAX * WORD_SIZE)
-#define VCPU_RCX	(__VCPU_REGS_RCX * WORD_SIZE)
-#define VCPU_RDX	(__VCPU_REGS_RDX * WORD_SIZE)
-#define VCPU_RBX	(__VCPU_REGS_RBX * WORD_SIZE)
-#define VCPU_RBP	(__VCPU_REGS_RBP * WORD_SIZE)
-#define VCPU_RSI	(__VCPU_REGS_RSI * WORD_SIZE)
-#define VCPU_RDI	(__VCPU_REGS_RDI * WORD_SIZE)
-
-#define VCPU_R8		(__VCPU_REGS_R8  * WORD_SIZE)
-#define VCPU_R9		(__VCPU_REGS_R9  * WORD_SIZE)
-#define VCPU_R10	(__VCPU_REGS_R10 * WORD_SIZE)
-#define VCPU_R11	(__VCPU_REGS_R11 * WORD_SIZE)
-#define VCPU_R12	(__VCPU_REGS_R12 * WORD_SIZE)
-#define VCPU_R13	(__VCPU_REGS_R13 * WORD_SIZE)
-#define VCPU_R14	(__VCPU_REGS_R14 * WORD_SIZE)
-#define VCPU_R15	(__VCPU_REGS_R15 * WORD_SIZE)
+#include "vcpu_regs.h"
 
 /*
  * pkvm_host_vmexit_entry - Handle a VM-Exit event from the deprivileged

arch/x86/kvm/pkvm/vmx/vcpu_regs.h
@@ -1,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_VMX_VCPU_REGS_H
+#define __PKVM_VMX_VCPU_REGS_H
+
+#include <asm/bitsperlong.h>
+
+#define WORD_SIZE (BITS_PER_LONG / 8)
+
+#define VCPU_RAX	(__VCPU_REGS_RAX * WORD_SIZE)
+#define VCPU_RCX	(__VCPU_REGS_RCX * WORD_SIZE)
+#define VCPU_RDX	(__VCPU_REGS_RDX * WORD_SIZE)
+#define VCPU_RBX	(__VCPU_REGS_RBX * WORD_SIZE)
+#define VCPU_RBP	(__VCPU_REGS_RBP * WORD_SIZE)
+#define VCPU_RSI	(__VCPU_REGS_RSI * WORD_SIZE)
+#define VCPU_RDI	(__VCPU_REGS_RDI * WORD_SIZE)
+
+#define VCPU_R8		(__VCPU_REGS_R8  * WORD_SIZE)
+#define VCPU_R9		(__VCPU_REGS_R9  * WORD_SIZE)
+#define VCPU_R10	(__VCPU_REGS_R10 * WORD_SIZE)
+#define VCPU_R11	(__VCPU_REGS_R11 * WORD_SIZE)
+#define VCPU_R12	(__VCPU_REGS_R12 * WORD_SIZE)
+#define VCPU_R13	(__VCPU_REGS_R13 * WORD_SIZE)
+#define VCPU_R14	(__VCPU_REGS_R14 * WORD_SIZE)
+#define VCPU_R15	(__VCPU_REGS_R15 * WORD_SIZE)
+
+#endif /* __PKVM_VMX_VCPU_REGS_H */
+