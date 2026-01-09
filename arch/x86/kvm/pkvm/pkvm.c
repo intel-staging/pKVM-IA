9c8bc64e5c5316c1d1ed34b50adfd846fe727ce2
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 16:08:42 +0800

    pKVM: x86: Return hypercall outputs to the host
    Some host hypercall may have outputs to the host. These outputs will be
    returned to the host by writing them into the corresponding host vcpu
    registers.
    
    Squash changelog:
      332ec5d1327f ("pKVM: x86: prevent hypervisor stack leaking")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -150,9 +150,12 @@ unshare:
 
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 {
+	enum pkvm_hc hc = pkvm_hc(vcpu);
+	/* Zero 'out' to prevent leaking stack data on error */
+	union pkvm_hc_data out = {0};
 	int ret = 0;
 
-	switch (pkvm_hc(vcpu)) {
+	switch (hc) {
 	case __pkvm__init:
 		ret = pkvm_init((struct pkvm_mem_info *)pkvm_hc_input1(vcpu),
 				pkvm_hc_input2(vcpu));
@@ -181,6 +184,8 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = -EINVAL;
 		break;
 	}
+
+	pkvm_hc_set_output(vcpu, hc, &out);
 
 	pkvm_hc_set_ret(vcpu, ret);
 }
