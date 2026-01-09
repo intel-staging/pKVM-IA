ffcaa9400741b9aba448fad8f3b0c10cb733fff0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 11:21:57 +0800

    pKVM: x86: Import kernel libraries for extable
    Import the kernel extable, bsearch and sort libraries to make the pKVM
    hypervisor having the capability to search the exception table section.
    
    Squash changelog:
      ed7c9166d212 ("pKVM: x86: Fix missing return value in __cond_resched stub")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/kern_stub.c
+++ arch/x86/kvm/pkvm/kern_stub.c
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
 #include "kern_stub.h"
 
 void warn_thunk_thunk(void) {}
+
+#ifndef CONFIG_PREEMPTION
+int __cond_resched(void) { return 0; }
+#endif
+
+#ifdef CONFIG_DEBUG_ATOMIC_SLEEP
+void __might_resched(const char *file, int line, unsigned int offsets) {}
+#endif
