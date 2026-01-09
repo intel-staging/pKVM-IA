fd38edd99c124d13cb2eb03452da67fee01ccaa0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 12:20:10 +0800

    pKVM: x86: Define pKVM's own address translation helpers
    Although the pKVM hypervisor use the same direct mapping with the linux
    kernel, define pKVM's owner address translation helpers __pkvm_pa and
    __pkvm_va which points to the __pa and __va macro. The pKVM hypervisor
    code can switch to use the its own helpers, so that it will be easier
    for the pKVM hypervisor to use a different mapping in the future if it
    is possible.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/kvm/pkvm/memory.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __PKVM_X86_MEMORY_H
+#define __PKVM_X86_MEMORY_H
+
+#include <asm/page.h>
+
+#define __pkvm_pa		__pa
+#define __pkvm_va		__va
+
+#endif /* __PKVM_X86_MEMORY_H */
