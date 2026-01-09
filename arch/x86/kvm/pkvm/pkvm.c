9c67ccfeadbb6af5aa0d6dbc6c4051851f9e4ed5
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 14:49:29 +0800

    pKVM: x86: Introduce pkvm_vm handle
    Introduce pkvm_vm handle to identify a guest VM instance created by
    the pKVM hypervisor. The handle is allocated during VM initialization
    from a global pkvm_vms_bitmap, which is protected by a lock to ensure
    safe concurrent access. And the handle is returned to the host via the
    vm_init PV interface.
    
    For simplicity, pkvm_vms_bitmap is statically defined to support up to
    64 guest VMs.
    
    Squash changelog:
      bb87706af534 ("pKVM: x86: Fix publish-before-initialize of VM handle")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
 #include <asm/fpu/xcr.h>
+#include <asm/pkvm_spinlock.h>
+#include "debug.h"
 #include "init.h"
 #include "lapic.h"
 #include "mem_protect.h"
@@ -26,6 +28,69 @@ bool tdp_enabled = true;
 struct pkvm_hyp *pkvm_hyp;
 DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
+
+/* The maximum number of VMs under pkvm. */
+#define MAX_PKVM_VMS		64
+
+static DECLARE_BITMAP(pkvm_vms_bitmap, MAX_PKVM_VMS);
+static DEFINE_PKVM_SPINLOCK(pkvm_vms_lock);
+static struct pkvm_vm_ref {
+	/* Reference counter to indicate if pkvm_vm is in use */
+	atomic_t refcount;
+	/* Point to pkvm_vm in pkvm */
+	struct pkvm_vm *pkvm_vm;
+} pkvm_vms_ref[MAX_PKVM_VMS];
+
+static int allocate_pkvm_vm_handle(struct pkvm_vm *pkvm_vm)
+{
+	struct pkvm_vm_ref *pkvm_vm_ref;
+	int idx;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	idx = find_first_zero_bit(pkvm_vms_bitmap, MAX_PKVM_VMS);
+	if (idx == MAX_PKVM_VMS) {
+		pkvm_spin_unlock(&pkvm_vms_lock);
+		return -ENOMEM;
+	}
+	__set_bit(idx, pkvm_vms_bitmap);
+
+	pkvm_vm->kvm.arch.pkvm.handle = idx;
+	pkvm_vm_ref = &pkvm_vms_ref[idx];
+	pkvm_vm_ref->pkvm_vm = pkvm_vm;
+	atomic_set(&pkvm_vm_ref->refcount, 1);
+
+	pkvm_spin_unlock(&pkvm_vms_lock);
+
+	return idx;
+}
+
+static struct pkvm_vm *free_pkvm_vm_handle(int handle)
+{
+	struct pkvm_vm_ref *pkvm_vm_ref;
+	struct pkvm_vm *pkvm_vm = NULL;
+	int idx = handle;
+
+	if (idx < 0 || idx >= MAX_PKVM_VMS)
+		return NULL;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	pkvm_vm_ref = &pkvm_vms_ref[idx];
+	if (atomic_cmpxchg(&pkvm_vm_ref->refcount, 1, 0) != 1) {
+		pkvm_err("VM%d is busy, refcount %d\n", handle,
+			 atomic_read(&pkvm_vm_ref->refcount));
+		goto out;
+	}
+
+	pkvm_vm = pkvm_vm_ref->pkvm_vm;
+	pkvm_vm_ref->pkvm_vm = NULL;
+
+	__clear_bit(idx, pkvm_vms_bitmap);
+out:
+	pkvm_spin_unlock(&pkvm_vms_lock);
+	return pkvm_vm;
+}
 
 static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa)
 {
@@ -64,12 +129,18 @@ static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa)
 					     pkvm_vm->shared_kvm->arch.disabled_quirks) &
 					    kvm_caps.supported_quirks;
 
-	ret = kvm_x86_call(vm_init)(kvm);
-	if (ret)
+	ret = allocate_pkvm_vm_handle(pkvm_vm);
+	if (ret < 0)
 		goto undonate;
 
-	return 0;
+	ret = kvm_x86_call(vm_init)(kvm);
+	if (ret)
+		goto free_handle;
 
+	return kvm->arch.pkvm.handle;
+
+free_handle:
+	free_pkvm_vm_handle(kvm->arch.pkvm.handle);
 undonate:
 	pkvm_hyp_donate_host(__pkvm_pa(pkvm_vm), size, false);
 unshare:
