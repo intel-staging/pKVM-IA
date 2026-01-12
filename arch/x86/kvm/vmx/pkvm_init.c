0d04a28ad50a2092f816c88e0c5f128439d69127
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-12 12:21:24 +0800

    Revert "REVERTME: pkvm: x86: Temporarily fail pkvm initialization"
    This reverts commit 4ae44a0bc10c521dd789b8716cf860d10d985b23.
    
    Now pvVMCS is ready and the host is able to create VMs on top of the
    pKVM hypervisor.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -1173,18 +1173,6 @@ static __init int pkvm_hyp_init(void)
 		}
 	}
 
-	/*
-	 * XXX: Revert
-	 * Temporarily fail pkvm initialization until pVMCS is fully merged.
-	 * pKVM doesn't serve any real purpose until we have pVMCS ready and
-	 * this failure helps us test reprivilege logic. This also enables
-	 * host to boot normally with KVM enabled and thereby not breaking
-	 * any virtualization functionality.
-	 */
-	if (!ret || !init_ret) {
-		pr_err("Explicitly triggering pkvm initialization failure!\n");
-		ret = -EFAULT;
-	}
 	return ret ? ret : init_ret;
 }
 
