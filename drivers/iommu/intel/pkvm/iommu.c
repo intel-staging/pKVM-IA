51d9ef5fec516bc0146ead6c9daddcc85a096db0
Vineeth Pillai <vineethrp@google.com>
2026-01-15 06:35:30 -0500

    iommu/vt-d: pKVM: Driver stub and vendor hooks for IOMMU
    Introduce a driver source layout for pKVM's intel IOMMU. The driver code
    resides in pkvm folder inside intel IOMMU folder and has its own sub
    Makefile. pKVM includes this Makefile to build the pKVM driver for intel
    IOMMU.
    
    Also implement the vendor hooks for initializing IOMMU during pKVM
    initialization. Add a stub callback for intel IOMMU.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

---
+++ drivers/iommu/intel/pkvm/iommu.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright © 2026 Google.
+ */
+#include "pkvm_iommu.h"
+
+int pkvm_intel_iommu_init(void)
+{
+	/* TODO: Implement pKVM IOMMU initialization logic */
+	return 0;
+}
