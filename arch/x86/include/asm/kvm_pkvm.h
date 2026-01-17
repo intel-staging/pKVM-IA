f7d18d927cb8618488a7ab7374681b7c60e04ff0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-17 09:56:49 +0800

    pKVM: x86: Introduce a new vm type
    Introduce a new vm type, KVM_X86_PKVM_PROTECTED_VM, to represent VMs
    whose CPU and memory states are isolated from the host and other guests
    by the pKVM hypervisor. Add helper functions pkvm_is_protected_vm() and
    pkvm_is_protected_vcpu() to determine whether a VM or vCPU is protected
    by pKVM.
    
    [ Chuanxiao: rewrite commit message ]
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -239,6 +239,21 @@ static inline unsigned long pkvm_host_pgtable_pages(void)
 	       __pkvm_pgtable_max_pages(SZ_2G >> PAGE_SHIFT);
 }
 
+static inline bool pkvm_is_protected_vm(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_PKVM_PROTECTED_VM;
+}
+
+static inline bool pkvm_is_protected_vcpu(struct kvm_vcpu *vcpu)
+{
+	return pkvm_is_protected_vm(vcpu->kvm);
+}
+
+#else /* !CONFIG_PKVM_X86 */
+
+static inline bool pkvm_is_protected_vm(struct kvm *kvm) { return false; }
+static inline bool pkvm_is_protected_vcpu(struct kvm_vcpu *vcpu) { return false; }
+
 #endif /* CONFIG_PKVM_X86 */
 
 #endif /* _ASM_X86_KVM_PKVM_H */
