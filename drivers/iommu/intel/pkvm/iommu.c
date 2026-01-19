1140ca93d949fbcd3198187b33bc4434b4a3bdcf
Vineeth Pillai <vineethrp@google.com>
2026-01-19 18:36:49 -0500

    iommu/vt-d: pKVM: Hypercall for IOMMU MMIO access
    Implement hypercall for accessing IOMMU registers so that host doesn't
    bypass pKVM security enforcement.
    
    This patch implements the hypercall as a trap and passthrough mechanism.
    Later patches implements logic to enforce security guarantees.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/iommu.c
+++ drivers/iommu/intel/pkvm/iommu.c
@@ -22,6 +22,120 @@ unsigned int iommu_pglvl_mask = IOMMU_PGT_4LEVEL | IOMMU_PGT_5LEVEL;
 static struct intel_iommu iommus[PKVM_MAX_IOMMU_NUM];
 static int nr_iommus;
 
+static struct intel_iommu *iommu_from_phys(unsigned long phys)
+{
+	int i;
+
+	for (i = 0; i < nr_iommus; i++) {
+		struct intel_iommu *iommu = &iommus[i];
+
+		if (phys >= iommu->reg_phys && phys < (iommu->reg_phys + iommu->reg_size))
+			return iommu;
+	}
+
+	return NULL;
+}
+
+static int iommu_direct_mmio_read(struct intel_iommu *iommu, u64 phys,
+				  int len, u64 *val)
+{
+	unsigned long offset = phys - iommu->reg_phys;
+	void *reg = iommu->reg + offset;
+
+	switch (len) {
+	case 4:
+		*val = readl(reg);
+		break;
+	case 8:
+		*val = readq(reg);
+		break;
+	default:
+		pkvm_err("%s: unsupported len %d\n", __func__, len);
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static int iommu_direct_mmio_write(struct intel_iommu *iommu, u64 phys,
+				   int len, u64 val)
+{
+	unsigned long offset = phys - iommu->reg_phys;
+	void *reg = iommu->reg + offset;
+
+	switch (len) {
+	case 4:
+		writel((u32)val, reg);
+		break;
+	case 8:
+		writeq(val, reg);
+		break;
+	default:
+		pkvm_err("%s: unsupported len %d\n", __func__, len);
+		return -EINVAL;
+	}
+	return 0;
+}
+
+int pkvm_iommu_mmio_read(u64 phys, int len, u64 *val)
+{
+	struct intel_iommu *iommu = iommu_from_phys(phys);
+	unsigned long offset;
+	int ret = 0;
+
+	if (!iommu)
+		return -EINVAL;
+
+	pkvm_spin_lock(&iommu->lock);
+	offset = phys - iommu->reg_phys;
+
+	switch (offset) {
+	case DMAR_GCMD_REG:
+		ret = -EINVAL;
+		break;
+	case DMAR_CAP_REG:
+		*val = iommu->cap;
+		break;
+	case DMAR_ECAP_REG:
+		*val = iommu->ecap;
+		break;
+	default:
+		/* Not emulated MMIO can directly go to hardware */
+		ret = iommu_direct_mmio_read(iommu, phys, len, val);
+	}
+
+	pkvm_spin_unlock(&iommu->lock);
+	return ret;
+}
+
+int pkvm_iommu_mmio_write(u64 phys, int len, u64 val)
+{
+	struct intel_iommu *iommu = iommu_from_phys(phys);
+	unsigned long offset;
+	int ret = 0;
+
+	if (!iommu)
+		return -EINVAL;
+
+	pkvm_spin_lock(&iommu->lock);
+	offset = phys - iommu->reg_phys;
+
+	switch (offset) {
+	case DMAR_CAP_REG:
+		fallthrough;
+	case DMAR_ECAP_REG:
+		fallthrough;
+	case DMAR_GSTS_REG:
+		ret = -EINVAL;
+		break;
+	default:
+		/* Not emulated MMIO can directly go to hardware */
+		ret = iommu_direct_mmio_write(iommu, phys, len, val);
+	}
+
+	pkvm_spin_unlock(&iommu->lock);
+	return ret;
+}
+
 int __init prepare_iommu(struct intel_iommu_info *info)
 {
 	struct intel_iommu *iommu;
