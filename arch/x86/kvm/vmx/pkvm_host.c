cxdong/pub-pKVM-IA

commit 62e9247018dbc859a63630904fe39f13bf0ea607

KVM: pVMX: Implement idt/gdt related operations
Enable the pKVM VMX host to access guest IDT and GDT via the idt/gdt PV
interfaces. Skipping these PV interfaces once the guest state is
marked as protected as the future accessing guest IDT and GDT is no
longer permitted by the pKVM hypervisor.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -618,6 +618,56 @@ static int pkvm_set_efer(struct kvm_vcpu *vcpu, u64 efer)
 	return ret;
 }
 
+static void pkvm_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
+{
+	union pkvm_hc_data data;
+
+	if (vcpu->arch.guest_state_protected ||
+	    KVM_BUG_ON(pkvm_hypercall_out(get_idt, &data), vcpu->kvm)) {
+		memset(dt, 0, sizeof(*dt));
+		return;
+	}
+
+	*dt = data.get_idt.desc;
+}
+
+static void pkvm_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
+{
+	union pkvm_hc_data data = {
+		.set_gdt.desc = *dt,
+	};
+
+	if (vcpu->arch.guest_state_protected)
+		return;
+
+	KVM_BUG_ON(pkvm_hypercall_in(set_idt, &data), vcpu->kvm);
+}
+
+static void pkvm_get_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
+{
+	union pkvm_hc_data data;
+
+	if (vcpu->arch.guest_state_protected ||
+	    KVM_BUG_ON(pkvm_hypercall_out(get_gdt, &data), vcpu->kvm)) {
+		memset(dt, 0, sizeof(*dt));
+		return;
+	}
+
+	*dt = data.get_gdt.desc;
+}
+
+static void pkvm_set_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
+{
+	union pkvm_hc_data data = {
+		.set_gdt.desc = *dt,
+	};
+
+	if (vcpu->arch.guest_state_protected)
+		return;
+
+	KVM_BUG_ON(pkvm_hypercall_in(set_gdt, &data), vcpu->kvm);
+}
+
 static void pkvm_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
 {
 	if (!pkvm_is_protected_vcpu(vcpu))
@@ -739,6 +789,10 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.is_valid_cr4 = pkvm_is_valid_cr4,
 	.set_cr4 = pkvm_set_cr4,
 	.set_efer = pkvm_set_efer,
+	.get_idt = pkvm_get_idt,
+	.set_idt = pkvm_set_idt,
+	.get_gdt = pkvm_get_gdt,
+	.set_gdt = pkvm_set_gdt,
 	.set_dr7 = pkvm_set_dr7,
 	.cache_reg = pkvm_cache_reg,
 	.get_rflags = pkvm_get_rflags,

intel-staging/pKVM-IA

commit 3803ec8fc582d852f8b5216e8173e0ec91b1f299

SQUASHME: KVM: pVMX: fix pkvm_set_idt PV interface
Fix pkvm_set_idt which accidentally used set_gdt instead of set_idt field.

Fixes: 62e9247018db ("KVM: pVMX: Implement idt/gdt related operations")

Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -635,7 +635,7 @@ static void pkvm_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
 static void pkvm_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
 {
 	union pkvm_hc_data data = {
-		.set_gdt.desc = *dt,
+		.set_idt.desc = *dt,
 	};
 
 	if (vcpu->arch.guest_state_protected)