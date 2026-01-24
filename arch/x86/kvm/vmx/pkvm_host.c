a0039ca7456fb30bf6f1ba5c03b8314f829f4107
Dmytro Maluka <dmaluka@google.com>
2026-01-24 21:40:45 +0000

    KVM: pVMX: Finalize pVM before running it
    Modify the host-side `vcpu_pre_run` hook to automatically invoke the
    `vm_finalize` hypercall just before the primary vCPU (aka BSP i.e.
    Bootstrap Processor) of a protected VM is run for the first time.
    
    This completes the handshake between the host VMM and the pKVM hypervisor,
    ensuring that the `pvmfw` configuration is committed and validated
    before guest execution begins.
    
    [ Grzegorz: carve-out part of [1] and prepare commit log. Also squash [2]
      leftovers.
      [1] ANDROID: pkvm: x86: Refactor pvmfw handling as a part of new PV code
          https://android-review.googlesource.com/c/kernel/common/+/3632018
      [2] ANDROID: pkvm: x86: Properly return error in pvmfw address set ioctl
          https://android-review.googlesource.com/c/kernel/common/+/3632020 ]
    
    Squash changelog:
      1eda2a14c1c0 ("KVM: pVMX: Check finalized state before vm_finalize hypercall")
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1315,6 +1315,19 @@ static void pkvm_flush_tlb_guest(struct kvm_vcpu *vcpu)
 
 static int pkvm_vcpu_pre_run(struct kvm_vcpu *vcpu)
 {
+	struct kvm_pkvm_vm *pkvm = &vcpu->kvm->arch.pkvm;
+	int ret = 0;
+
+	if (unlikely(pkvm_is_protected_vcpu(vcpu) && !kvm_vcpu_has_run(vcpu) &&
+		     kvm_vcpu_is_reset_bsp(vcpu))) {
+		mutex_lock(&pkvm->finalized_lock);
+		if (!pkvm->finalized)
+			ret = pkvm_hypercall(vm_finalize, pkvm->handle);
+		mutex_unlock(&pkvm->finalized_lock);
+		if (ret < 0)
+			return ret;
+	}
+
 	return 1;
 }
 
