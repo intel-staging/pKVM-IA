4b1acd5dad0ab0f7c26e3a318079a0711ceea99d
Vineeth Pillai <vineethrp@google.com>
2026-01-26 21:00:13 -0500

    pKVM: VMX: Do not handle ept violation on IOMMU MMIO access
    If host is trying to access IOMMU MMIO after it has been unmapped from
    EPT, we should not be mapping it back as it enables host to access
    IOMMU MMIO space and bypass pKVM.
    
    Do not handle the EPT violation in that case which would inject #GP into
    the host.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/pkvm/vmx/ept.c
+++ arch/x86/kvm/pkvm/vmx/ept.c
@@ -426,7 +426,8 @@ void pkvm_handle_host_ept_violation(struct kvm_vcpu *vcpu)
 	 * EPT when initialize, except for the MMIO in the high-end address.
 	 * Handle the MMIO only.
 	 */
-	if (pkvm_find_addr_range(gpa, &range) || is_pvmfw(gpa)) {
+	if (pkvm_find_addr_range(gpa, &range) || is_pvmfw(gpa) ||
+	    is_iommu_mmio(gpa)) {
 		pkvm_err_ratelimited("Host access to protected memory at 0x%lx\n", gpa);
 		goto failed;
 	}
@@ -459,7 +460,8 @@ void pkvm_handle_host_ept_violation(struct kvm_vcpu *vcpu)
 		cur.start = ALIGN_DOWN(gpa, size);
 		cur.end = cur.start + size - 1;
 
-		if (range_contains(&range, &cur) && !overlaps_pvmfw(cur.start, size)) {
+		if (range_contains(&range, &cur) && !overlaps_pvmfw(cur.start, size) &&
+		    !overlaps_iommu_mmio(cur.start, size)) {
 			/*
 			 * TODO: In case the host mmu free pages are not
 			 * enough, -ENOMEM will be returned. This could be
