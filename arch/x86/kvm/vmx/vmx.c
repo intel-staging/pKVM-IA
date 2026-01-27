4c8e796dda59ee91e044892f7fc1f9e8440a674f
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-27 13:01:02 +0800

    pKVM: VMX: Sync vCPU state for EXIT_REASON_MSR_READ
    Before returning to the host with the EXIT_REASON_MSR_READ, the vCPU RCX
    and exit instruction length will be shared with the host. And Before
    entering the guest again, update the vCPU RAX/RDX with the host emulated
    result and skip the rdmsr instruction.
    
    Specifically, all the npVM's vCPU registers are synced unconditionally
    thus only need to share the exit instruction length before returning
    back to the host so that the host can skip the rdmsr instruction. For
    the pVM, no need to share the exit instruction length as the rdmsr
    instruction is skipped by the pKVM hypervisor rather than the host.
    
    Squash changelog:
      15624f908e09 ("pKVM: VMX: Fix register leak and injection for MSR exits")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -9632,6 +9632,19 @@ static void update_protected_vcpu_state(struct kvm_vcpu *vcpu,
 		WARN_ON_ONCE(kvm_skip_emulated_instruction(vcpu) != 1);
 		break;
 	}
+	case EXIT_REASON_MSR_READ:
+		if (!pkvm_host_has_emulated_msr(vcpu->kvm, kvm_rcx_read(vcpu)))
+			to_pkvm_vcpu(vcpu)->host_emulated_msr_err = 1;
+
+		if (!to_pkvm_vcpu(vcpu)->host_emulated_msr_err) {
+			kvm_rax_write(vcpu, (u32)shared_vcpu->arch.regs[VCPU_REGS_RAX]);
+			kvm_rdx_write(vcpu, (u32)shared_vcpu->arch.regs[VCPU_REGS_RDX]);
+		}
+
+		WARN_ON_ONCE(kvm_complete_insn_gp(vcpu,
+				to_pkvm_vcpu(vcpu)->host_emulated_msr_err) != 1);
+		to_pkvm_vcpu(vcpu)->host_emulated_msr_err = 0;
+		break;
 	default:
 		break;
 	}
@@ -9684,7 +9697,8 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 		break;
 	}
 	case EXIT_REASON_IO_INSTRUCTION:
-		/* For the host to skip the emulated IO instruction. */
+	case EXIT_REASON_MSR_READ:
+		/* For the host to skip the instruction for certain exit reasons */
 		shared_vcpu->arch.event_exit_inst_len = vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
 		break;
 	default:
@@ -9711,6 +9725,9 @@ static void share_protected_vcpu_state(struct kvm_vcpu *vcpu,
 		}
 		break;
 	}
+	case EXIT_REASON_MSR_READ:
+		shared_vcpu->arch.regs[VCPU_REGS_RCX] = (u32)kvm_rcx_read(vcpu);
+		break;
 	default:
 		break;
 	}
