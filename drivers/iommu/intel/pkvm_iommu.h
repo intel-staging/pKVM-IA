b4fcc9bc22cde5a43a62f552b43b26f5b5758cdc
Vineeth Pillai <vineethrp@google.com>
2026-01-29 11:15:35 -0500

    iommu/vt-d: pKVM: Pasid table hypercall for second level translation
    Implement hypercalls for setting pasid table second level translation
    details and host driver changes to use the hypercall.
    
    Passthrough mode needs special attention as it allows devices to
    essentially bypass IOMMU and access whole of system memory. So pKVM
    converts this to a second stage translated DMA mode with host ept as the
    page table.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm_iommu.h
+++ drivers/iommu/intel/pkvm_iommu.h
@@ -121,6 +121,8 @@ int pkvm_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info 
 			   u8 bus, u8 devfn);
 int pkvm_pasid_setup_fl(struct device_domain_info *info, phys_addr_t fsptptr,
 			u32 pasid, u16 did, u16 old_did, int flags);
+int pkvm_pasid_setup_sl(struct device_domain_info *info, phys_addr_t ssptptr,
+			u32 pasid, u16 did, u16 old_did);
 #else /* __PKVM_HYP__ */
 /*
  * dev_iommu_priv_get is called from quite a few places in code re-used by
@@ -174,6 +176,7 @@ int pkvm_iommu_clear_ce(struct clear_ce_data *data);
 int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out);
 int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *in, struct set_sm_ce_data *out);
 int pkvm_iommu_pasid_setup_fl(struct pasid_setup_fl_data *in, struct pasid_setup_fl_data *out);
+int pkvm_iommu_pasid_setup_sl(struct pasid_setup_sl_data *in, struct pasid_setup_sl_data *out);
 #endif /* !__PKVM_HYP__ */
 #else /* !CONFIG_PKVM_INTEL */
 static inline int pkvm_qi_submit_sync(struct intel_iommu *iommu,
@@ -202,6 +205,12 @@ static inline int pkvm_pasid_table_setup(struct intel_iommu *iommu,
 static inline int pkvm_pasid_setup_fl(struct device_domain_info *info,
 				      phys_addr_t fsptptr, u32 pasid,
 				      u16 did, u16 old_did, int flags)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_pasid_setup_sl(struct device_domain_info *info,
+				      phys_addr_t ssptptr, u32 pasid,
+				      u16 did, u16 old_did)
 {
 	return -EOPNOTSUPP;
 }
