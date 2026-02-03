cxdong/pub-pKVM-IA

commit 27b430f8800910fb13d47c343a921690fa5603f9

pKVM: VMX: Handle EXIT_REASON_TASK_SWITCH
The EXIT_REASON_TASK_SWITCH vmexit is handled differently for the pVM
and npVM. For the pVM, injecting #GP as the pKVM hypervisor doesn't
support instruction emulation and the host cannot emulate task switch.
For the npVM, the vmexit is handled cooperatively by the pKVM hypervisor
and the host. The pKVM clears the event injection if the task switch is
caused by the task gate event and skips the instruction if the task
switch is caused by instructions. Then returns back to the host to
emulate the task switch.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -6215,6 +6215,7 @@ static int handle_apic_write(struct kvm_vcpu *vcpu)
 	kvm_apic_write_nodecode(vcpu, offset);
 	return 1;
 }
+#endif /* !__PKVM_HYP__ */
 
 static int handle_task_switch(struct kvm_vcpu *vcpu)
 {
@@ -6224,6 +6225,18 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 	u32 error_code = 0;
 	u16 tss_selector;
 	int reason, type, idt_v, idt_index;
+
+#ifdef __PKVM_HYP__
+	/*
+	 * The pKVM hypervisor doesn't have instruction emulation thus cannot
+	 * emulate the task switch. So handle the EXIT_REASON_TASK_SWITCH for
+	 * the pVM by injecting #GP.
+	 */
+	if (pkvm_is_protected_vcpu(vcpu)) {
+		kvm_queue_exception(vcpu, GP_VECTOR);
+		return 1;
+	}
+#endif
 
 	idt_v = (vmx->idt_vectoring_info & VECTORING_INFO_VALID_MASK);
 	idt_index = (vmx->idt_vectoring_info & VECTORING_INFO_VECTOR_MASK);
@@ -6264,6 +6277,7 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 		       type != INTR_TYPE_NMI_INTR))
 		WARN_ON(!skip_emulated_instruction(vcpu));
 
+#ifndef __PKVM_HYP__
 	/*
 	 * TODO: What about debug traps on tss switch?
 	 *       Are we supposed to inject them and update dr6?
@@ -6271,8 +6285,16 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 	return kvm_task_switch(vcpu, tss_selector,
 			       type == INTR_TYPE_SOFT_INTR ? idt_index : -1,
 			       reason, has_error_code, error_code);
+#else
+	/*
+	 * Switch to the host and let the host to emulate the task switch
+	 * for the npVMs.
+	 */
+	return 0;
+#endif
 }
 
+#ifndef __PKVM_HYP__
 static int handle_ept_violation(struct kvm_vcpu *vcpu)
 {
 	unsigned long exit_qualification = vmx_get_exit_qual(vcpu);
@@ -6724,8 +6746,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_WBINVD]                  = kvm_emulate_wbinvd,
 #endif
 	[EXIT_REASON_XSETBV]                  = kvm_emulate_xsetbv,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_TASK_SWITCH]             = handle_task_switch,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_MCE_DURING_VMENTRY]      = handle_machine_check,
 	[EXIT_REASON_GDTR_IDTR]		      = handle_desc,
 	[EXIT_REASON_LDTR_TR]		      = handle_desc,

arch/x86/kvm/vmx/pkvm_host.c
@@ -366,6 +366,56 @@ static int handle_apic_eoi_induced(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+static int handle_task_switch(struct kvm_vcpu *vcpu)
+{
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+	unsigned long exit_qualification;
+	bool has_error_code = false;
+	u32 error_code = 0;
+	u16 tss_selector;
+	int reason, type, idt_v, idt_index;
+
+	idt_v = (vmx->idt_vectoring_info & VECTORING_INFO_VALID_MASK);
+	idt_index = (vmx->idt_vectoring_info & VECTORING_INFO_VECTOR_MASK);
+	type = (vmx->idt_vectoring_info & VECTORING_INFO_TYPE_MASK);
+
+	exit_qualification = vmx_get_exit_qual(vcpu);
+
+	reason = (u32)exit_qualification >> 30;
+	if (reason == TASK_SWITCH_GATE && idt_v) {
+		switch (type) {
+		case INTR_TYPE_NMI_INTR:
+			vcpu->arch.nmi_injected = false;
+			break;
+		case INTR_TYPE_EXT_INTR:
+		case INTR_TYPE_SOFT_INTR:
+			kvm_clear_interrupt_queue(vcpu);
+			break;
+		case INTR_TYPE_HARD_EXCEPTION:
+			if (vmx->idt_vectoring_info &
+			    VECTORING_INFO_DELIVER_CODE_MASK) {
+				has_error_code = true;
+				error_code = vmx->error_code;
+			}
+			fallthrough;
+		case INTR_TYPE_SOFT_EXCEPTION:
+			kvm_clear_exception_queue(vcpu);
+			break;
+		default:
+			break;
+		}
+	}
+	tss_selector = exit_qualification;
+
+	/*
+	 * TODO: What about debug traps on tss switch?
+	 *       Are we supposed to inject them and update dr6?
+	 */
+	return kvm_task_switch(vcpu, tss_selector,
+			       type == INTR_TYPE_SOFT_INTR ? idt_index : -1,
+			       reason, has_error_code, error_code);
+}
+
 /*
  * The exit handlers return 1 if the exit was handled fully and guest execution
  * may resume.  Otherwise they set the kvm_run parameter to indicate what needs
@@ -387,6 +437,7 @@ static int (*pkvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_APIC_WRITE]              = handle_apic_write,
 	[EXIT_REASON_EOI_INDUCED]             = handle_apic_eoi_induced,
 	[EXIT_REASON_WBINVD]                  = kvm_emulate_wbinvd,
+	[EXIT_REASON_TASK_SWITCH]             = handle_task_switch,
 };
 
 static const int pkvm_vmx_max_exit_handlers = ARRAY_SIZE(pkvm_vmx_exit_handlers);

intel-staging/pKVM-IA

commit 607a69dafaccc9bbdb82a1ef0aa439c26dea6d24

SQUASHME: pKVM: VMX: Fix unused-but-set-variable warning in handle_task_switch
Fix the unused-but-set-variable compiling warnings in
handle_task_switch.

Fixes: 27b430f88009 ("pKVM: VMX: Handle EXIT_REASON_TASK_SWITCH")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -6235,10 +6235,14 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
 	unsigned long exit_qualification;
+#ifndef __PKVM_HYP__
 	bool has_error_code = false;
 	u32 error_code = 0;
 	u16 tss_selector;
 	int reason, type, idt_v, idt_index;
+#else
+	int reason, type, idt_v;
+#endif
 
 #ifdef __PKVM_HYP__
 	/*
@@ -6253,7 +6257,9 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 #endif
 
 	idt_v = (vmx->idt_vectoring_info & VECTORING_INFO_VALID_MASK);
+#ifndef __PKVM_HYP__
 	idt_index = (vmx->idt_vectoring_info & VECTORING_INFO_VECTOR_MASK);
+#endif
 	type = (vmx->idt_vectoring_info & VECTORING_INFO_TYPE_MASK);
 
 	exit_qualification = vmx_get_exit_qual(vcpu);
@@ -6270,6 +6276,7 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 			kvm_clear_interrupt_queue(vcpu);
 			break;
 		case INTR_TYPE_HARD_EXCEPTION:
+#ifndef __PKVM_HYP__
 			if (vmx->idt_vectoring_info &
 			    VECTORING_INFO_DELIVER_CODE_MASK) {
 				has_error_code = true;
@@ -6277,6 +6284,7 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 					vmcs_read32(IDT_VECTORING_ERROR_CODE);
 			}
 			fallthrough;
+#endif
 		case INTR_TYPE_SOFT_EXCEPTION:
 			kvm_clear_exception_queue(vcpu);
 			break;
@@ -6284,7 +6292,9 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 			break;
 		}
 	}
+#ifndef __PKVM_HYP__
 	tss_selector = exit_qualification;
+#endif
 
 	if (!idt_v || (type != INTR_TYPE_HARD_EXCEPTION &&
 		       type != INTR_TYPE_EXT_INTR &&