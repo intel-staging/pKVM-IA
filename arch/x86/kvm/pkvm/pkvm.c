aa67dda50d79df1598806ac9f6024c86da73cccb
Chenyi Qiang <chenyi.qiang@intel.com>
2026-02-04 10:43:30 +0800

    SQUASHME: pKVM: x86: Use pkvm_is_protected_vm() before vcpu is initialized.
    Since vcpu->kvm is initialized after this check,
    pkvm_is_protected_vcpu() is not functional at this stage.
    
    Fixes: 6fdd05cd1022 ("pKVM: VMX: Synchronize host disabled_exits to pKVM in npVMs")
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -391,7 +391,7 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 		kvm->arch.notify_window = pkvm_vm->shared_kvm->arch.notify_window;
 		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
 	}
-	if (!pkvm_is_protected_vcpu(vcpu))
+	if (!pkvm_is_protected_vm(kvm))
 		kvm->arch.disabled_exits = pkvm_vm->shared_kvm->arch.disabled_exits;
 
 	pkvm_spin_unlock(&pkvm_vm->lock);
