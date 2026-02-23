cxdong/pub-pKVM-IA

commit b0738f54947b36214a9e42ae59a115c85f578aa4

pKVM: x86: Return hypercall outputs to the host
Some host hypercall may have outputs to the host. These outputs will be
returned to the host by writing them into the corresponding host vcpu
registers.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -158,9 +158,11 @@ unshare:
 
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 {
+	enum pkvm_hc hc = pkvm_hc(vcpu);
+	union pkvm_hc_data out;
 	int ret = 0;
 
-	switch (pkvm_hc(vcpu)) {
+	switch (hc) {
 	case __pkvm__init:
 		ret = pkvm_init((struct pkvm_mem_info *)pkvm_hc_input1(vcpu),
 				pkvm_hc_input2(vcpu));
@@ -192,6 +194,8 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = -EINVAL;
 		break;
 	}
+
+	pkvm_hc_set_output(vcpu, hc, &out);
 
 	pkvm_hc_set_ret(vcpu, ret);
 }

arch/x86/include/asm/kvm_pkvm.h
@@ -87,6 +87,16 @@ enum {
 	#include <asm/pkvm_hypercalls.h>
 };
 
+static inline int pkvm_hc_output_num(enum pkvm_hc hc)
+{
+	switch (hc) {
+	#define PKVM_HC(f) case TO_PKVM_HC(f): return PKVM_HC_OUTPUT_NUM(f);
+	#include <asm/pkvm_hypercalls.h>
+	default:
+		return 0;
+	}
+}
+
 #define PKVM_HC_IN_0()
 #define PKVM_HC_IN_1(a1)		, "b"(a1)
 #define PKVM_HC_IN_2(a1, a2)		PKVM_HC_IN_1(a1), "c"(a2)
@@ -154,6 +164,41 @@ DEFINE_PKVM_HC_INPUT(4, RSI)
 static inline void pkvm_hc_set_ret(struct kvm_vcpu *vcpu, int ret)
 {
 	vcpu->arch.regs[VCPU_REGS_RAX] = ret;
+}
+
+#define DEFINE_PKVM_HC_OUTPUT(n, reg)							\
+static inline void pkvm_hc_set_output##n(struct kvm_vcpu *vcpu, union pkvm_hc_data *p)	\
+{											\
+	BUILD_BUG_ON(n == 0 || n > PKVM_HC_DATA_MAX_NUM);				\
+	vcpu->arch.regs[VCPU_REGS_##reg] = p->raw.data[n - 1];				\
+}
+
+DEFINE_PKVM_HC_OUTPUT(1, RBX)
+DEFINE_PKVM_HC_OUTPUT(2, RCX)
+DEFINE_PKVM_HC_OUTPUT(3, RDX)
+DEFINE_PKVM_HC_OUTPUT(4, RSI)
+
+static inline void pkvm_hc_set_output(struct kvm_vcpu *vcpu, enum pkvm_hc hc,
+				      union pkvm_hc_data *out)
+{
+	switch (pkvm_hc_output_num(hc)) {
+	case 4:
+		pkvm_hc_set_output4(vcpu, out);
+		fallthrough;
+	case 3:
+		pkvm_hc_set_output3(vcpu, out);
+		fallthrough;
+	case 2:
+		pkvm_hc_set_output2(vcpu, out);
+		fallthrough;
+	case 1:
+		pkvm_hc_set_output1(vcpu, out);
+		fallthrough;
+	case 0:
+		break;
+	default:
+		BUG();
+	}
 }
 
 extern unsigned long pkvm_sym(page_offset_base);

intel-staging/pKVM-IA

commit b514a3b2a6f6f19d38d5e2c62de59eb0886f99bf

pKVM: x86: prevent hypervisor stack leaking
When pKVM handles host hypercalls it allocates an output structure on
the stack, fills it accordingly during hypercall handling and finally
use pkvm_hc_set_output() to map the data to the proper host vCPU
registers so the host can read it back.

However, during hypercalls handling, some errors may occur that prevent
the output data from being set. Because the pkvm_handle_host_hypercall()
unconditionally copies the 'out' structure to the vCPU registers,
uninitialized hyp stack (which potentially has sensitive data) can be
leaked to the host.

To prevent described hypervisor stack leaking, zero initialize
pkvm_hc_data 'out'.

Fixes: b0738f54947b ("pKVM: x86: Return hypercall outputs to the host")

Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1803,7 +1803,8 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 {
 	enum pkvm_hc hc = pkvm_hc(vcpu);
-	union pkvm_hc_data in, out;
+	/* Zero 'out' to prevent leaking stack data on error */
+	union pkvm_hc_data in, out = {0};
 	int ret = 0;
 
 	pkvm_hc_get_input(vcpu, hc, &in);