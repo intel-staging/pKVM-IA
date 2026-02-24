cxdong/pub-pKVM-IA

commit 58f48d1cb644b1de7b5c496191cc9d96b1f198b4

KVM: pVMX: Add new fpstate memory for xfd
Add new fpstate memory via the vcpu_add_fpstate PV interface if the xfd
feature is enabled for pVM. As the pVM's FPU state is managed by the
pKVM hypervisor while the npVM's FPU state is managed by the host,
re-allocating is only necessary for the pVM, and should be done before
adding the new cpuid entries to the pKVM hypervisor.

If the new fpstate memory is added successfully, free the old one.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -910,6 +910,28 @@ static void pkvm_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
 	KVM_BUG_ON(pkvm_hypercall(hwapic_isr_update, max_isr), vcpu->kvm);
 }
 
+static int pkvm_vcpu_realloc_fpstate(struct kvm_vcpu *vcpu)
+{
+	union pkvm_hc_data out;
+	size_t fpsize;
+	void *fps;
+	int ret;
+
+	fpsize = PAGE_ALIGN(vcpu->arch.guest_fpu.fpstate->size +
+			    ALIGN(offsetof(struct fpstate, regs), 64));
+	fps = alloc_pages_exact(fpsize, GFP_KERNEL_ACCOUNT);
+	if (!fps)
+		return -ENOMEM;
+
+	ret = pkvm_hypercall_out(vcpu_add_fpstate, &out, __pa(fps), fpsize);
+	if (KVM_BUG_ON(ret, vcpu->kvm))
+		free_pages_exact(fps, fpsize);
+	else
+		kvm_free_pkvm_memcache(&out.vcpu_add_fpstate.memcache);
+
+	return ret;
+}
+
 static void pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 {
 	struct kvm_cpuid_entry2 *e2 = vcpu->arch.cpuid_entries;
@@ -919,6 +941,19 @@ static void pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 	size_t size;
 
 	if (vcpu->arch.guest_state_protected || !e2 || !nent)
+		return;
+
+	/*
+	 * With exposing the FPU dynamic feature via the cpuid, the fpstate
+	 * allocated when creating the vcpu may not be sufficient for the
+	 * guest. As the pVM's FPU state is managed by the pKVM hypervisor
+	 * while the npVM's FPU state is managed by the host, re-allocating the
+	 * fpstate is only necessary for the pVM, and should be done before
+	 * adding the new cpuid entries to the pKVM hypervisor.
+	 */
+	if ((vcpu->arch.guest_fpu.xfeatures & XFEATURE_MASK_USER_DYNAMIC) &&
+	    pkvm_is_protected_vcpu(vcpu) &&
+	    pkvm_vcpu_realloc_fpstate(vcpu))
 		return;
 
 	size = sizeof(struct kvm_cpuid_entry2) * nent;

intel-staging/pKVM-IA

commit c78333a527f0d03587e8732f77f8863332a2cbdb

KVM: pVMX: Use fpu_user_cfg.max_size to compute fpsize
Since pKVM enforces the host's cpuids, calculate fpsize based on the
value that the hardware supports instead of that of the vCPU reported.

Without this, pKVM fails to boot a VM on a machine with Intel AMX, as
crosvm does not request that feature.

fixes: 58f48d1cb644b1de7b5c496191cc9d96b1f198b4 ("KVM: pVMX: Add new fpstate
memory for xfd")

Signed-off-by: Masanori Misono <m.misono760@gmail.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -1629,7 +1629,7 @@ static int pkvm_vcpu_realloc_fpstate(struct kvm_vcpu *vcpu)
 	void *fps;
 	int ret;
 
-	fpsize = PAGE_ALIGN(vcpu->arch.guest_fpu.fpstate->size +
+	fpsize = PAGE_ALIGN(fpu_user_cfg.max_size +
 			    ALIGN(offsetof(struct fpstate, regs), 64));
 	fps = alloc_pages_exact(fpsize, GFP_KERNEL_ACCOUNT);
 	if (!fps)
@@ -1663,7 +1663,7 @@ static void pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 	 * fpstate is only necessary for the pVM, and should be done before
 	 * adding the new cpuid entries to the pKVM hypervisor.
 	 */
-	if ((vcpu->arch.guest_fpu.xfeatures & XFEATURE_MASK_USER_DYNAMIC) &&
+	if ((fpu_user_cfg.max_features & XFEATURE_MASK_USER_DYNAMIC) &&
 	    pkvm_is_protected_vcpu(vcpu) &&
 	    pkvm_vcpu_realloc_fpstate(vcpu))
 		return;