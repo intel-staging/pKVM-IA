cxdong/pub-pKVM-IA

commit b7f4d372ff78643323cb2ad27ddd225980e24c8b

pKVM: x86: Add sync_pir_to_irr PV interface
Add sync_pir_to_irr PV interface for the host to sync the requested
pir to the guest irr via the vendor-specific operation. This PV
interface is always accessible to the host as it is part of guest irq
emulation.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -666,6 +666,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__refresh_apicv_exec_ctrl:
 	case __pkvm__load_eoi_exitmap:
 	case __pkvm__hwapic_isr_update:
+	case __pkvm__sync_pir_to_irr:
 		/*
 		 * The host is responsible for running vCPU, injecting
 		 * interrupts, emulating lapic etc. Always allow the related PV
@@ -941,6 +942,12 @@ static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
 	kvm_x86_call(load_eoi_exitmap)(vcpu, eoi_exit_bitmap);
 }
 
+static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
+{
+	to_pkvm_vcpu(vcpu)->max_irr = pir;
+	kvm_x86_call(sync_pir_to_irr)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1095,6 +1102,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__hwapic_isr_update:
 		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__sync_pir_to_irr:
+		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -68,6 +68,7 @@ PKVM_HC(set_virtual_apic_mode)
 PKVM_HC(refresh_apicv_exec_ctrl)
 PKVM_HC(load_eoi_exitmap)
 PKVM_HC(hwapic_isr_update)
+PKVM_HC(sync_pir_to_irr)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

intel-staging/pKVM-IA

commit aa7ffd0109dcf59f83436de828d1e92c7c5cb9ab

pKVM: x86: Validate the pir value for the pVM
Validate the passed in pir value from the host for the same security
reason with the PV interface __pkvm__inject_irq, to make sure it is not
an exception vector for the pVM as the pVM's exceptions are emulated and
injected by the pKVM itself, and the host is not allowed to inject
exceptions to the pVM.

Fixes: b7f4d372ff786 ("pKVM: x86: Add sync_pir_to_irr PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1148,10 +1148,22 @@ static int pkvm_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
 	return 0;
 }
 
-static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
+static int pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
 {
+	/*
+	 * Validate the passed in pir value from the host to make sure it is not
+	 * an exception vector for the pVM for the same security reason with the
+	 * PV interface __pkvm__inject_irq. See comments in the function
+	 * pkvm_inject_irq.
+	 *
+	 * The value -1 is allowed as it represents no interrupt.
+	 */
+	if (pkvm_is_protected_vcpu(vcpu) && pir != -1 && pir < 32)
+		return -EPERM;
+
 	to_pkvm_vcpu(vcpu)->max_irr = pir;
 	kvm_x86_call(sync_pir_to_irr)(vcpu);
+	return 0;
 }
 
 static int pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu,
@@ -1808,7 +1820,7 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		ret = pkvm_hwapic_isr_update(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__sync_pir_to_irr:
-		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
+		ret = pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__vcpu_after_set_cpuid:
 		ret = pkvm_vcpu_after_set_cpuid(vcpu, pkvm_host_gpa_to_phys(pkvm_hc_input1(hvcpu)),