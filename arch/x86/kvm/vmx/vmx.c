26e3b9762c0365fc7da1a861d7bf83d9d0f12299
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-03-04 10:29:25 +0800

    pKVM: VMX: Align vmx_sync_pir_to_irr with the host KVM implementation
    The vmx_sync_pir_to_irr was modified for the pKVM hypervisor only set
    RVI with the given max_irr from the host for simplify. Now to support
    protecting apic page, the pKVM hypervisor needs to find the highest IRR
    by its own from the vIRR, which matches the host KVM implementation in
    the host KVM/VMX code. Refactor the vmx_sync_pir_to_irr() for this
    purpose.
    
    The minor change is that, the PID is still owned by the host KVM thus
    the pKVM needs to look at the host owned PID to get the PIR. Another
    thing is that the host KVM still cannot access the VMCS to set RVI. As
    the pKVM hypervisor is able to set the RVI in the vcpu run loop via
    calling sync_pir_to_irr callback, the host KVM doesn't need to use PV
    interface to do so. So if pKVM is enabled from the host side, just
    return the max_irr rather than setting the RVI.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7587,6 +7587,9 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 {
 #ifndef __PKVM_HYP__
 	struct vcpu_vt *vt = to_vt(vcpu);
+#else
+	struct vcpu_vt *vt = to_vt(to_pkvm_vcpu(vcpu)->shared_vcpu);
+#endif
 	int max_irr;
 	bool got_posted_interrupt;
 
@@ -7623,22 +7626,16 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 	 * a VM-Exit and the subsequent entry will call sync_pir_to_irr.
 	 */
 	if (!is_guest_mode(vcpu) && kvm_vcpu_apicv_active(vcpu)) {
-#ifdef CONFIG_PKVM_INTEL
-		if (!enable_pkvm)
-			vmx_set_rvi(max_irr);
-		else if (max_irr != -1)
-			KVM_BUG_ON(pkvm_hypercall(sync_pir_to_irr, max_irr), vcpu->kvm);
-#else
-		vmx_set_rvi(max_irr);
+#ifndef __PKVM_HYP__
+		if (enable_pkvm)
+			/* RVI will be updated by the pKVM before entering the guest. */
+			return max_irr;
 #endif
+		vmx_set_rvi(max_irr);
 	} else if (got_posted_interrupt) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 	}
-#else
-	int max_irr = to_pkvm_vcpu(vcpu)->max_irr;
 
-	vmx_set_rvi(max_irr);
-#endif
 	return max_irr;
 }
 
