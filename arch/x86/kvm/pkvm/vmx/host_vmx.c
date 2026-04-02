cxdong/pub-pKVM-IA

commit 5ffe84fbdf8266a3bf5577e205a74e07f4c6180e

pKVM: VMX: Introduce a fixup mechanism for the host vCPU
Introduce a fixup mechanism to do the fixup for the host vCPU before
VM-Entry. This will be used by the next patch to fixup the host VMCS
configuration which has to be done after deprivileging. The per-cpu
host_vcpu_fixup flag is initialized as true to make the fixup function
run before the first VM-Resume to fix any thing that cannot be done by
the deprivileging. This per-cpu flag will be set to false to prevent the
fixup function from running again at the subsequent VM-Resumes. Setting
this flag to true again will re-trigger the fixup.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmentry.S
@@ -79,6 +79,10 @@ SYM_FUNC_START(pkvm_host_vmexit_entry)
 	wrmsr
 
 .Lspec_ctrl_done:
+	cmpb $1, PER_CPU_VAR(host_vcpu_fixup)
+	je .Lfixup
+
+.Lresume:
 	/*
 	 * Restore registers to reflect the vm-exit handling/emulation result
 	 * before vmresume.
@@ -113,5 +117,9 @@ SYM_FUNC_START(pkvm_host_vmexit_entry)
 	vmread %_ASM_CX, %_ASM_AX
 failed:
 	jmp failed			/* should never come here */
+.Lfixup:
+	mov WORD_SIZE*1(%_ASM_SP), %_ASM_DI
+	call pkvm_host_vmx_fixup
+	jmp .Lresume
 SYM_FUNC_END(pkvm_host_vmexit_entry)
 STACK_FRAME_NON_STANDARD(pkvm_host_vmexit_entry);

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -400,3 +400,8 @@ handle_events:
 
 	pkvm_trace_vmexit_end(vcpu, vt->exit_reason.basic);
 }
+
+void pkvm_host_vmx_fixup(struct vcpu_vmx *vmx)
+{
+	this_cpu_write(host_vcpu_fixup, false);
+}

arch/x86/kvm/pkvm/vmx/host_vmx.h
@@ -14,4 +14,6 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx);
 
 void pkvm_vmx_reprivilege_cpu(unsigned long *vcpu_regs);
 
+void pkvm_host_vmx_fixup(struct vcpu_vmx *vmx);
+
 #endif /* __PKVM_VMX_HOST_VMX_H */

arch/x86/kvm/pkvm/cpu.c
@@ -72,6 +72,7 @@ int pkvm_setup_per_cpu(int cpu, unsigned long base)
 	per_cpu(cpu_number, cpu) = cpu;
 	per_cpu(phys_cpu, cpu) = pcpu;
 	per_cpu(host_vcpu, cpu) = vcpu;
+	per_cpu(host_vcpu_fixup, cpu) = true;
 
 	task = &per_cpu_ptr(&pkvm_task, cpu)->task;
 	task->group_leader = task;

arch/x86/kvm/pkvm/pkvm.c
@@ -32,6 +32,7 @@ bool tdp_enabled = true;
 struct pkvm_hyp *pkvm_hyp;
 DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
+DEFINE_PER_CPU(bool, host_vcpu_fixup);
 /*
  * similarly pmu.c is not compiled. define kvm_mmu_cap here for the use
  * in cpuid.c

arch/x86/kvm/pkvm/pkvm.h
@@ -11,6 +11,7 @@
 
 DECLARE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
+DECLARE_PER_CPU(bool, host_vcpu_fixup);
 extern size_t kvm_vcpu_sz;
 
 /* Represents a guest vCPU. */

intel-staging/pKVM-IA

commit 1c6da7c06177ea769bac9dad30ab7ca799b137c8

pKVM: VMX: Do fixup before switching spec ctrl
To avoid RSB underflow attacks and any other nastiness, there must not
be any returns or indirect branches between the code of switching to the
host's spec ctrl and vmentry. To fix this, the fixup function should be
called before switching to the host's sepc ctrl.

To have a better code readability, calling fixup function at the end of
the pkvm_host_vmexit_main() with an unlikely if conditional as fixup
function is mostly unneeded. Comparing with directly calling it in the
asm code, the assembly code of C may have one more instruction but is
easier to read. With this, change the fixup function as static.

Fixes: 5ffe84fbdf82 ("pKVM: VMX: Introduce a fixup mechanism for the host vCPU")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmentry.S
@@ -79,10 +79,6 @@ SYM_FUNC_START(pkvm_host_vmexit_entry)
 	wrmsr
 
 .Lspec_ctrl_done:
-	cmpb $1, PER_CPU_VAR(host_vcpu_fixup)
-	je .Lfixup
-
-.Lresume:
 	/*
 	 * Restore registers to reflect the vm-exit handling/emulation result
 	 * before vmresume.
@@ -117,9 +113,5 @@ SYM_FUNC_START(pkvm_host_vmexit_entry)
 	vmread %_ASM_CX, %_ASM_AX
 failed:
 	jmp failed			/* should never come here */
-.Lfixup:
-	mov WORD_SIZE*1(%_ASM_SP), %_ASM_DI
-	call pkvm_host_vmx_fixup
-	jmp .Lresume
 SYM_FUNC_END(pkvm_host_vmexit_entry)
 STACK_FRAME_NON_STANDARD(pkvm_host_vmexit_entry);

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -299,6 +299,25 @@ static inline void set_vcpu_mode(struct kvm_vcpu *vcpu, int mode)
 	smp_wmb();
 }
 
+static void fixup_host_vmx(struct vcpu_vmx *vmx)
+{
+	if (boot_cpu_has(X86_FEATURE_INTEL_PT)) {
+		/*
+		 * The VM_ENTRY_LOAD_IA32_RTIT_CTL bit may be cleared due to the
+		 * MSR_IA32_RTIT_CTL TRACEEN bit is set before deprivileging. See
+		 * comments in init_vmentry_control in pkvm_init.c.
+		 *
+		 * Ensure this bit is set after the host exits to the root mode.
+		 * This can be done safely as VM_EXIT_CLEAR_IA32_RTIT_CTL is
+		 * guaranteed to be set which causes the MSR_IA32_RTIT_CTL is 0.
+		 */
+		if (!(vm_entry_controls_get(vmx) & VM_ENTRY_LOAD_IA32_RTIT_CTL))
+			vm_entry_controls_setbit(vmx, VM_ENTRY_LOAD_IA32_RTIT_CTL);
+	}
+
+	this_cpu_write(host_vcpu_fixup, false);
+}
+
 void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 {
 	struct kvm_vcpu *vcpu = &vmx->vcpu;
@@ -398,24 +417,8 @@ handle_events:
 	if (vcpu->arch.cr2 != native_read_cr2())
 		native_write_cr2(vcpu->arch.cr2);
 
+	if (unlikely(this_cpu_read(host_vcpu_fixup)))
+		fixup_host_vmx(vmx);
+
 	pkvm_trace_vmexit_end(vcpu, vt->exit_reason.basic);
-}
-
-void pkvm_host_vmx_fixup(struct vcpu_vmx *vmx)
-{
-	if (boot_cpu_has(X86_FEATURE_INTEL_PT)) {
-		/*
-		 * The VM_ENTRY_LOAD_IA32_RTIT_CTL bit may be cleared due to the
-		 * MSR_IA32_RTIT_CTL TRACEEN bit is set before deprivileging. See
-		 * comments in init_vmentry_control in pkvm_init.c.
-		 *
-		 * Ensure this bit is set after the host exits to the root mode.
-		 * This can be done safely as VM_EXIT_CLEAR_IA32_RTIT_CTL is
-		 * guaranteed to be set which causes the MSR_IA32_RTIT_CTL is 0.
-		 */
-		if (!(vm_entry_controls_get(vmx) & VM_ENTRY_LOAD_IA32_RTIT_CTL))
-			vm_entry_controls_setbit(vmx, VM_ENTRY_LOAD_IA32_RTIT_CTL);
-	}
-
-	this_cpu_write(host_vcpu_fixup, false);
 }

arch/x86/kvm/pkvm/vmx/host_vmx.h
@@ -14,6 +14,4 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx);
 
 void pkvm_vmx_reprivilege_cpu(unsigned long *vcpu_regs);
 
-void pkvm_host_vmx_fixup(struct vcpu_vmx *vmx);
-
 #endif /* __PKVM_VMX_HOST_VMX_H */