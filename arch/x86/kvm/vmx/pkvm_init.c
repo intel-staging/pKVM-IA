cxdong/pub-pKVM-IA

commit b4739a795e34c9ec671f09fd9b3cf97a8c22f0ef

pKVM: VMX: Enforce MPX isolation
The MPX is deprecated in the newer Intel CPU, e.g., PTL. In case the
pKVM hypervisor runs on some older Intel CPU which has MPX, enable the
MPX VM-Exit controls to guarantee the MSR_IA32_BNDCFGS will be cleared
for the pKVM hypervisor to prevent the host from unexpectedly enabling
the MPX for the pKVM without properly programming the bounds registers.

No need to enable VM-Entry control to load IA32_BNDCFGS for the
deprivileged host as the linux kernel will not use the MPX even the CPU
supports.

Statically enforce the VM-Exit control bit introduces overhead to clear
the MSR_IA32_BNDCFGS on every VM-Exit. But as the pKVM will be most
likely run on the newer Intel CPU which doesn't have the MPX, it is not
necessary to dynamically toggle it by intercepting the MSR_IA32_BNDCFGS
write to simplify the code.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_init.c
@@ -222,6 +222,20 @@ static __init int pkvm_setup_host_vmcs_config(void)
 		setting.vmentry_ctrl_req |= VM_ENTRY_LOAD_CET_STATE;
 	}
 
+	if (boot_cpu_has(X86_FEATURE_MPX)) {
+		/*
+		 * The MPX is deprecated in the newer Intel CPU, e.g., PTL. In
+		 * case the pKVM hypervisor runs on some older Intel CPU which
+		 * has the MPX, enable the MPX vmexit control to guarantee the
+		 * MSR_IA32_BNDCFGS will be cleared for the pKVM hypervisor.
+		 *
+		 * No need to enable vmentry control to load IA32_BNDCFGS for
+		 * the deprivileged host as the linux kernel will not use the
+		 * MPX even if the CPU supports it.
+		 */
+		setting.vmexit_ctrl_req |= VM_EXIT_CLEAR_BNDCFGS;
+	}
+
 	if (setup_vmcs_config_common(vmcs_config, vmx_cap, &setting))
 		return -EINVAL;
 

intel-staging/pKVM-IA

commit c5599f2b043684192f99ef42e8d1d81fa123605e

pKVM: VMX: Set VM_ENTRY_LOAD_BNDCFGS to satisfy VMCS pair checks
From the security/function point of view, there is no need to enable
VM-Entry control to load IA32_BNDCFGS for the deprivileged host as the
linux kernel will not use the MPX even if the CPU supports it.

But as the code setup the VMCS config via reusing KVM's
setup_vmcs_config_common() which checks if MPX VM-Entry and VM-Exit
configs are in pair of not, it still needs to set VM_ENTRY_LOAD_BNDCFGS
to pass this check.

Fixes: b4739a795e34 ("pKVM: VMX: Enforce MPX isolation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_init.c
@@ -229,11 +229,17 @@ static __init int pkvm_setup_host_vmcs_config(void)
 		 * has the MPX, enable the MPX vmexit control to guarantee the
 		 * MSR_IA32_BNDCFGS will be cleared for the pKVM hypervisor.
 		 *
-		 * No need to enable vmentry control to load IA32_BNDCFGS for
-		 * the deprivileged host as the linux kernel will not use the
-		 * MPX even if the CPU supports it.
+		 * From the security/function point of view, there is no need to
+		 * enable VM-Entry control to load IA32_BNDCFGS for the
+		 * deprivileged host as the linux kernel will not use the MPX
+		 * even if the CPU supports it. But as the code setup the VMCS
+		 * config via reusing KVM's setup_vmcs_config_common() which
+		 * checks if MPX VM-Entry and VM-Exit configs are in pair of
+		 * not, it still needs to set VM_ENTRY_LOAD_BNDCFGS to pass
+		 * this check.
 		 */
 		setting.vmexit_ctrl_req |= VM_EXIT_CLEAR_BNDCFGS;
+		setting.vmentry_ctrl_req |= VM_ENTRY_LOAD_BNDCFGS;
 	}
 
 	if (setup_vmcs_config_common(vmcs_config, vmx_cap, &setting))