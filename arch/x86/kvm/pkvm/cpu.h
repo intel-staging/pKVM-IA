cxdong/pub-pKVM-IA

commit e712a431031381edfc4a60b6f96eb2036b53a8bf

pKVM: x86: Initialize the percpu fpu
Initialize the percpu current_task fpu with the initial fpstate, which is
used to restore the FPU to the initial state when switches from pVMs to
the host VM, so that to isolation pVM's FPU from host. The permission is
set to the maximum features to allow the guest to use all the supported
features.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/fpu.c
@@ -1,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/sched.h>
+#include <asm/cpufeature.h>
+#include <asm/current.h>
+#include <asm/fpu/types.h>
+#include "internal.h"
+#include "fpu.h"
+
+/*
+ * percpu_fpstate is used to reset the FPU hardware to its initial state
+ * when switching from pVM to host, preventing the host from accessing
+ * pVM's FPU contents.
+ *
+ * To achieve this, it sets percpu_fpstate.regs.xsave.header.xfeatures to 0
+ * (which represents the XSTATE_BV field in the XSAVE header), ensuring the
+ * XRSTOR(S) instruction loads the default FPU state. Since it doesn't need
+ * full-sized memory pages for all FPU registers, a static allocation of
+ * percpu_fpstate is sufficient.
+ */
+static DEFINE_PER_CPU(struct fpstate, percpu_fpstate);
+
+void pkvm_init_percpu_fpu(void)
+{
+	struct fpu *fpu = x86_task_fpu(current);
+
+	/*
+	 * Set the current fpstate pointer to the percpu_fpstate, which is used
+	 * to restore the FPU to the initial state before switching from a pVM
+	 * to the host.
+	 */
+	fpu->fpstate = this_cpu_ptr(&percpu_fpstate);
+	fpstate_init_user(fpu->fpstate);
+
+	/* The perm is initialized with the maximum features */
+	fpu->perm.__state_perm		= fpu_kernel_cfg.max_features;
+	fpu->perm.__state_size		= fpu_kernel_cfg.max_size;
+}

arch/x86/kernel/fpu/core.c
@@ -507,6 +507,7 @@ static inline unsigned int init_fpstate_copy_size(void)
 	/* XSAVE(S) just needs the legacy and the xstate header part */
 	return sizeof(init_fpstate.regs.xsave);
 }
+#endif /* !__PKVM_HYP__ */
 
 static inline void fpstate_init_fxstate(struct fpstate *fpstate)
 {
@@ -545,6 +546,7 @@ void fpstate_init_user(struct fpstate *fpstate)
 		fpstate_init_fstate(fpstate);
 }
 
+#ifndef __PKVM_HYP__
 static void __fpstate_reset(struct fpstate *fpstate)
 {
 	/*

arch/x86/kvm/pkvm/Makefile
@@ -14,7 +14,7 @@ cmd_deps			:= $(src)/compiling_cmds.sh
 
 pkvm-hyp-y			:= early_alloc.o pkvm.o memory.o cpu.o \
 				   idt.o entry.o kern_stub.o init.o pgtable.o \
-				   mmu.o page_alloc.o lapic.o trace.o
+				   mmu.o page_alloc.o lapic.o trace.o fpu.o
 pkvm-lib			:= lib
 pkvm-hyp-y			+= $(pkvm-lib)/find_bit.o
 
@@ -48,6 +48,8 @@ pkvm-hyp-y			+= $(fpu)/core.o
 
 pkvm-obj 			:= $(patsubst %.o,%.pkvm.o,$(pkvm-hyp-y))
 obj-$(CONFIG_PKVM_X86)		+= pkvm.o
+
+CFLAGS_fpu.pkvm.o		+= -iquote $(srctree)/arch/x86/kernel/fpu
 
 AFLAGS_vmx/host_vmentry.pkvm.o	+= -iquote $(srctree)/arch/x86/kvm/vmx
 AFLAGS_vmx/host_vmentry.pkvm.o	+= -iquote $(obj)/

arch/x86/kvm/pkvm/fpu.h
@@ -1,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_FPU_H
+#define __PKVM_X86_FPU_H
+
+void pkvm_init_percpu_fpu(void);
+
+#endif /* __PKVM_X86_FPU_H */

arch/x86/kvm/pkvm/init.c
@@ -2,6 +2,7 @@
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
+#include "fpu.h"
 #include "init.h"
 #include "lapic.h"
 #include "memory.h"
@@ -283,6 +284,8 @@ int pkvm_init(struct pkvm_mem_info infos[], int nr_infos)
 	ret = pkvm_host_mmu_finalize(host_mmu_finalize_fn);
 	if (ret)
 		return ret;
+
+	pkvm_init_percpu_fpu();
 
 	pkvm_vcpu_perf_init(this_cpu_read(host_vcpu));
 

arch/x86/kvm/pkvm/undef.h
@@ -22,6 +22,8 @@
 #undef CONFIG_PREEMPT_COUNT
 #undef CONFIG_X86_SGX_KVM
 #undef CONFIG_USE_X86_SEG_SUPPORT
+#undef CONFIG_MATH_EMULATION
+#undef CONFIG_X86_DEBUG_FPU
 
 #define NOTRACE
 

intel-staging/pKVM-IA

commit 181a7be62c88f42dce520589f859af9542bd7c69

pKVM: x86: Ensure CR0/CR4 won't cause exceptions for XSAVE/XRSTOR
The XSAVE/XRSTOR instructions will be used to save/restore the FPU state
by the pKVM hypervisor if XSAVE feature is supported. According to the
SDM, the exceptions will happen if CR4.OSXSAVE == 0 or CR0.TS == 1. The
pKVM's CR4/CR0 are configured with the same value as the host before
deprivileging, so most likely the CR4/CR0 are correctly configured. But
still ensure this in case the host is not configured properly.

Move the __pkvm_write_cr0/3/4 helpers from VMX specific code to the x86
generic code so that the x86 generic pKVM code can also use them easily.

Fixes: e712a4310313 ("pKVM: x86: Initialize the percpu fpu")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/cpu.h
@@ -1,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_CPU_H
+#define __PKVM_X86_CPU_H
+
+#define PKVM_WRITE_CR(crnum, val) \
+static inline void __pkvm_write_cr##crnum(unsigned long val) \
+{							\
+	asm volatile("mov %0,%%cr" #crnum : "+r" (val) : : "memory"); \
+}
+
+PKVM_WRITE_CR(0, val)
+PKVM_WRITE_CR(3, val)
+PKVM_WRITE_CR(4, val)
+
+#endif /* __PKVM_X86_CPU_H */

arch/x86/kvm/pkvm/fpu.c
@@ -2,6 +2,7 @@
 #include <linux/sched.h>
 #include <asm/cpufeature.h>
 #include <asm/current.h>
+#include "cpu.h"
 #include "internal.h"
 #include "fpu.h"
 #include "xstate.h"
@@ -22,6 +23,28 @@ static DEFINE_PER_CPU(struct fpstate, percpu_fpstate);
 void pkvm_init_percpu_fpu(void)
 {
 	struct fpu *fpu = x86_task_fpu(current);
+
+	if (boot_cpu_has(X86_FEATURE_XSAVE)) {
+		unsigned long cr4 = native_read_cr4();
+		unsigned long cr0 = native_read_cr0();
+
+		/*
+		 * According to SDM Vol. 1 Operation Of XSAVE/XRSTOR:
+		 * 1) If the XSAVE feature set is not enabled (CR4.OSXSAVE = 0), an
+		 * invalid-opcode exception (#UD) occurs.
+		 * 2) If CR0.TS[bit 3] is 1, a device-not-available exception (#NM)
+		 * occurs.
+		 * The pKVM's CR4/CR0 are configured with the same value as the host
+		 * before deprivileging, so most likely CR4.OSXSAVE is already set and
+		 * CR0.TS is clear. But still ensure this in case the host is not
+		 * configured properly.
+		 */
+		if (!(cr4 & X86_CR4_OSXSAVE))
+			__pkvm_write_cr4(cr4 | X86_CR4_OSXSAVE);
+
+		if (cr0 & X86_CR0_TS)
+			__pkvm_write_cr0(cr0 & ~X86_CR0_TS);
+	}
 
 	/*
 	 * Set the current fpstate pointer to the percpu_fpstate, which is used

arch/x86/kvm/pkvm/vmx/host_repriv.c
@@ -6,6 +6,7 @@
 #include <vmx/vmx.h>
 #include "host_vmx.h"
 #include "vcpu_regs.h"
+#include "pkvm/cpu.h"
 
 struct host_cpu_state {
 	unsigned long cr0, cr3, cr4;
@@ -53,16 +54,6 @@ static inline void read_host_cpu_state(struct host_cpu_state *hcs)
 	hcs->cr3 = vmcs_readl(GUEST_CR3);
 	hcs->cr4 = vmcs_readl(GUEST_CR4);
 }
-
-#define PKVM_WRITE_CR(crnum, val) \
-static inline void __pkvm_write_cr##crnum(unsigned long val) \
-{							\
-	asm volatile("mov %0,%%cr" #crnum : "+r" (val) : : "memory"); \
-}
-
-PKVM_WRITE_CR(0, val)
-PKVM_WRITE_CR(3, val)
-PKVM_WRITE_CR(4, val)
 
 /*
  * Restores register state from memory pointed by rdi