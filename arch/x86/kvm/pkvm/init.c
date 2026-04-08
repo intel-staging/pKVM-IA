7f23f95a3ee593d8ad9eceb6d1473ee51a82936f
Grzegorz Jaszczyk <jaszczyk@google.com>
2026-04-08 22:00:46 +0000

    pKVM: x86: Map ramoops memory into hypervisor page table
    Once the ramoops base address is discovered, it must be mapped into the
    hypervisor's private page tables to allow the panic handler to write to it
    after deprivileging.
    
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/init.c
+++ arch/x86/kvm/pkvm/init.c
@@ -172,6 +172,14 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 			return ret;
 	}
 
+	if (pkvm_ramoops_console_size) {
+		ret = pkvm_hyp_mmu_map((unsigned long)__pkvm_va(pkvm_ramoops_console_pa),
+				       pkvm_ramoops_console_pa, pkvm_ramoops_console_size,
+				       (u64)pgprot_val(PAGE_KERNEL));
+		if (ret)
+			return ret;
+	}
+
 	/* Load pKVM hypervisor's MMU to use pKVM vmemmap */
 	pkvm_hyp_mmu_load();
 
