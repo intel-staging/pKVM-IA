180512c15698e532657e45470b113447e544af88
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-04-24 09:35:58 +0800

    pKVM: VMX: Unmap pKVM memory from the host kernel direct mapping
    After all CPUs are deprivileged, the physical memory region reserved for
    the pKVM must no longer be accessible to the host kernel. While the pKVM
    removes these pages from the host EPT to prevent EPT-level access, the
    host kernel's direct mapping (via __va()) still has valid PTEs pointing
    to these pages.
    
    Call set_memory_np() to clear the present bit in those PTEs. Any
    subsequent host kernel access to the pKVM memory region through the
    direct mapping will take a page fault by its own rather than causing an
    EPT violation to the pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -7,6 +7,7 @@
 #include <asm/e820/api.h>
 #include <asm/pkvm_image.h>
 #include <asm/setup.h>
+#include <asm/set_memory.h>
 #include "pkvm_constants.h"
 #include "vmx.h"
 #include "pkvm_iommu.h"
@@ -1506,6 +1507,24 @@ int __init vmx_pkvm_init(void)
 		static_branch_disable(&pkvm_enabled_key);
 		goto repriv_cpus;
 	}
+
+	/*
+	 * After host deprivileging succeed, un-present the kernel direct
+	 * mappings for the memory pages which are reserved from the memblock
+	 * for the pKVM as they are not accessible to the host kernel until the
+	 * platform is power cycled. This can avoid unnecessary EPT violation
+	 * vmexit for the usage of load_unaligned_zeropad().
+	 *
+	 * Note: The host memory pages donated to the pKVM are still mapped in
+	 * the host's MMU. Those pages are not un-presented right now because
+	 * they are sparse allocated from the linux, un-presenting from the
+	 * kernel direct mapping may split a huge PTE into smaller ones which
+	 * may slightly impact the host's performance. Without unpresenting for
+	 * those pages, the usage of load_unaligned_zeropad() can be supported
+	 * via injecting #PF by the pKVM.
+	 */
+	WARN_ON(set_memory_np((unsigned long)__va(pkvm_mem_base),
+			      pkvm_mem_size >> PAGE_SHIFT));
 
 	pkvm_hypercall(init_finalize);
 
