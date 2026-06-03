cxdong/pub-pKVM-IA

commit 37424ba6f15cbe43559b880857fe332da6875ef5

pKVM: VMX: Share vCPU state for EXIT_REASON_IO_INSTRUCTION
Share the necessary vCPU state for the host to emulate the IO
instructions. For the npVM, sharing the exit instruction length for the
host to skip the IO instruction. No need to share the RAX as this is
already shared. For the pVM, sharing the RAX with the host, and no need
to share the exit instruction length as the host will not skip
instruction for the pVM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -9614,7 +9614,24 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 				vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
 		}
 		break;
+	case EXIT_REASON_IO_INSTRUCTION:
+		/* For the host to skip the emulated IO instruction. */
+		shared_vcpu->arch.event_exit_inst_len = vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
+		break;
 	}
+	default:
+		break;
+	}
+}
+
+static void share_protected_vcpu_state(struct kvm_vcpu *vcpu,
+				       struct kvm_vcpu *shared_vcpu)
+{
+	switch (vmx_get_exit_reason(vcpu).basic) {
+	case EXIT_REASON_IO_INSTRUCTION:
+		/* IO output/Input data */
+		shared_vcpu->arch.regs[VCPU_REGS_RAX] = kvm_rax_read(vcpu);
+		break;
 	default:
 		break;
 	}
@@ -9669,6 +9686,8 @@ static void pkvm_vmx_share_vcpu_state_with_host(struct kvm_vcpu *vcpu)
 	    !vmx->fail) {
 		if (!pkvm_is_protected_vcpu(vcpu))
 			share_nonprotected_vcpu_state(vcpu, shared_vcpu);
+		else
+			share_protected_vcpu_state(vcpu, shared_vcpu);
 	}
 }
 

intel-staging/pKVM-IA

commit 630c0faeb9491c818c8e6506271c72c19f3f1a50

pKVM: x86: Fix switch-case structure in share_nonprotected_vcpu_state
Correct the block structure in share_nonprotected_vcpu_state by properly
closing the EXIT_REASON_EXCEPTION_NMI case. Previously, a misplaced
brace caused subsequent exit reason cases to be logically nested
incorrectly, which could lead to undefined behavior during non-protected
vCPU state synchronization.

Fixes: 37424ba6f15c ("pKVM: VMX: Share vCPU state for EXIT_REASON_IO_INSTRUCTION")
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/vmx/vmx.c
@@ -9892,6 +9892,7 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 				vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
 		}
 		break;
+	}
 	case EXIT_REASON_EPT_VIOLATION:
 		to_vmx(shared_vcpu)->exit_gpa = vmcs_read64(GUEST_PHYSICAL_ADDRESS);
 		fallthrough;
@@ -9907,7 +9908,6 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 		to_vmx(shared_vcpu)->instr_info = vmcs_read32(VMX_INSTRUCTION_INFO);
 		shared_vcpu->arch.event_exit_inst_len = vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
 		break;
-	}
 	default:
 		break;
 	}