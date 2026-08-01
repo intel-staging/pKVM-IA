cxdong/pub-pKVM-IA

commit fde0edcd148ca53c4720f980b40f48f2d158a045

iommu/vt-d: pKVM: Pasid table hypercall for first level translation
Implement hypercalls for setting pasid table first level translation
details and host driver changes to use the hypercall.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -226,3 +226,81 @@ int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *in, struct set_sm_ce_data *out)
 	*out = *in;
 	return ret;
 }
+
+static int __get_pasid_table(struct intel_iommu *iommu, u8 bus, u8 devfn, struct pasid_table *table)
+{
+	struct context_entry *context = iommu_context_addr(iommu, bus, devfn, false);
+	u32 pds;
+
+	if (!context || !context_present(context)) {
+		pkvm_err("%s: pasid directory table not found: device[%x:%x]\n",
+			 __func__, bus, devfn);
+		return -EINVAL;
+	}
+
+	pds = get_pasid_dir_size(context);
+	table->table = __pkvm_va(context->lo & VTD_PAGE_MASK);
+	table->max_pasid = pds << PASID_PDE_SHIFT;
+
+	return 0;
+}
+
+static int iommu_pasid_setup_fl(struct pasid_setup_fl_data *data)
+{
+	struct intel_iommu *iommu = iommu_from_phys(data->phys);
+	u16 bdf = PCI_DEVID(data->bus, data->devfn);
+	struct device_domain_info info = { 0 };
+	struct pkvm_device dev = { .info = &info };
+	struct pasid_table table = { 0 };
+	u64 fsptptr;
+	int ret;
+
+	if (!iommu)
+		return -EINVAL;
+
+	if (data->ats_qdep > PCI_ATS_MAX_QDEP)
+		return -EINVAL;
+
+	if (is_dev_in_satc(bdf)) {
+		if (ecap_dit(iommu->ecap))
+			info.pfsid = bdf;
+	} else if (data->ats_supported || data->ats_enabled) {
+		return -EPERM;
+	}
+
+	ret = __get_pasid_table(iommu, data->bus, data->devfn, &table);
+	if (ret)
+		return ret;
+
+	fsptptr = pkvm_host_gpa_to_phys(data->fsptptr_gpa);
+	info.bus = data->bus;
+	info.devfn = data->devfn;
+	info.ats_qdep = data->ats_qdep;
+	info.ats_enabled = data->ats_enabled;
+	info.ats_supported = data->ats_supported;
+	info.pasid_table = &table;
+	info.iommu = iommu;
+
+	ret = accept_page_donation(iommu, &data->donation_page_gpa);
+	if (ret)
+		return ret;
+
+	pkvm_dbg("%s: dev[%x:%x], pasid: %x, fsptptr_gpa: %llx, did: %d, old_did: %d\n", __func__,
+		 data->bus, data->devfn, data->pasid, data->fsptptr_gpa, data->did, data->old_did);
+	if (!data->old_did) {
+		return intel_pasid_setup_first_level(iommu, &dev, fsptptr,
+						     data->pasid, data->did,
+						     data->flags);
+	}
+	return intel_pasid_replace_first_level(iommu, &dev, fsptptr,
+					       data->pasid, data->did,
+					       data->old_did, data->flags);
+}
+
+int pkvm_iommu_pasid_setup_fl(struct pasid_setup_fl_data *in, struct pasid_setup_fl_data *out)
+{
+	int ret = iommu_pasid_setup_fl(in);
+
+	*out = *in;
+	return ret;
+}

arch/x86/include/asm/kvm_pkvm.h
@@ -87,6 +87,21 @@ struct set_sm_ce_data {
 	u8 pasid_supported: 3;
 	u8 pasid_enabled: 1;
 };
+
+struct pasid_setup_fl_data {
+	u64 phys;
+	u64 fsptptr_gpa;
+	u64 donation_page_gpa;
+	u32 pasid;
+	u32 flags;
+	u16 did;
+	u16 old_did; /* replace_fl */
+	u8 bus;
+	u8 devfn;
+	u8 ats_qdep;
+	u8 ats_enabled: 1;
+	u8 ats_supported: 1;
+};
 #endif
 
 #define TO_PKVM_HC(f)		CONCATENATE(__pkvm__, f)
@@ -178,6 +193,10 @@ union pkvm_hc_data {
 		struct set_sm_ce_data in;
 		struct set_sm_ce_data out;
 	} iommu_set_sm_ce;
+	union {
+		struct pasid_setup_fl_data in;
+		struct pasid_setup_fl_data out;
+	} iommu_pasid_setup_fl;
 #endif
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];

arch/x86/include/asm/pkvm_hypercalls.h
@@ -97,6 +97,7 @@ PKVM_HC(iommu_qi_submit)
 PKVM_HC_IN(iommu_clear_ce)
 PKVM_HC_INOUT(iommu_set_lm_ce)
 PKVM_HC_INOUT(iommu_set_sm_ce)
+PKVM_HC_INOUT(iommu_pasid_setup_fl)
 #endif
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -1908,6 +1908,10 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = pkvm_iommu_set_sm_ce(&in.iommu_set_sm_ce.in,
 					   &out.iommu_set_sm_ce.out);
 		break;
+	case __pkvm__iommu_pasid_setup_fl:
+		ret = pkvm_iommu_pasid_setup_fl(&in.iommu_pasid_setup_fl.in,
+						&out.iommu_pasid_setup_fl.out);
+		break;
 #endif
 	default:
 		ret = pkvm_vcpu_handle_host_hypercall(vcpu, hc, &in, &out);

drivers/iommu/intel/pasid.c
@@ -454,6 +454,23 @@ int intel_pasid_setup_first_level(struct intel_iommu *iommu, struct pkvm_device 
 		return -EINVAL;
 	}
 
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		struct device_domain_info *info = dev_iommu_priv_get(dev);
+		int ret;
+
+		if (!info || !info->pasid_table)
+			return -ENODEV;
+
+		ret = pkvm_pasid_setup_fl(info, fsptptr, pasid, did, 0, flags);
+		if (ret)
+			pr_err("%s: iommu%d: pkvm_pasid_setup_fl failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+
+		return ret;
+	}
+#endif
+
 	spin_lock(&iommu->lock);
 	pte = intel_pasid_get_entry(dev, pasid);
 	if (IS_ERR(pte)) {
@@ -497,6 +514,23 @@ int intel_pasid_replace_first_level(struct intel_iommu *iommu,
 		       iommu->seq_id);
 		return -EINVAL;
 	}
+
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		struct device_domain_info *info = dev_iommu_priv_get(dev);
+		int ret;
+
+		if (!info || !info->pasid_table)
+			return -ENODEV;
+
+		ret = pkvm_pasid_setup_fl(info, fsptptr, pasid, did, old_did, flags);
+		if (ret)
+			pr_err("%s: iommu%d: pkvm_pasid_setup_fl failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+
+		return ret;
+	}
+#endif
 
 	pasid_pte_config_first_level(iommu, &new_pte, fsptptr, did, flags);
 

drivers/iommu/intel/pkvm_iommu.c
@@ -227,3 +227,44 @@ int pkvm_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info 
 
 	return ret;
 }
+
+int pkvm_pasid_setup_fl(struct device_domain_info *info, phys_addr_t fsptptr,
+		      u32 pasid, u16 did, u16 old_did, int flags)
+{
+	union pkvm_hc_data d = { 0 };
+	struct pasid_setup_fl_data *data = &d.iommu_pasid_setup_fl.in;
+	struct intel_iommu *iommu = info->iommu;
+	int ret;
+
+	data->phys = iommu->reg_phys;
+	data->fsptptr_gpa = fsptptr;
+	data->pasid = pasid;
+	data->flags = flags;
+	data->did = did;
+	data->old_did = old_did;
+	data->bus = info->bus;
+	data->devfn = info->devfn;
+	data->ats_qdep = info->ats_qdep;
+	data->ats_enabled = info->ats_enabled;
+	data->ats_supported = info->ats_supported;
+
+	spin_lock(&iommu->lock);
+	ret = pkvm_hypercall_inout(iommu_pasid_setup_fl, &d, &d);
+	if (ret == -ENOMEM) {
+		void *donation_page = iommu_alloc_pages_node_sz(iommu->node, GFP_ATOMIC, SZ_4K);
+
+		if (!donation_page) {
+			pr_err("iommu%d: failed to allocate pasid table page\n", iommu->seq_id);
+			spin_unlock(&iommu->lock);
+			return -ENOMEM;
+		}
+		data->donation_page_gpa = virt_to_phys(donation_page);
+		ret = pkvm_hypercall_inout(iommu_pasid_setup_fl, &d, &d);
+
+		if (data->donation_page_gpa)
+			iommu_free_pages(phys_to_virt(data->donation_page_gpa));
+	}
+	spin_unlock(&iommu->lock);
+
+	return ret;
+}

drivers/iommu/intel/pkvm_iommu.h
@@ -119,6 +119,8 @@ int pkvm_context_mapping(struct intel_iommu *iommu, struct device_domain_info *i
 			 u8 bus, u8 devfn, u64 pgd_gpa, u16 did);
 int pkvm_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info *info,
 			   u8 bus, u8 devfn);
+int pkvm_pasid_setup_fl(struct device_domain_info *info, phys_addr_t fsptptr,
+			u32 pasid, u16 did, u16 old_did, int flags);
 #else /* __PKVM_HYP__ */
 /*
  * dev_iommu_priv_get is called from quite a few places in code re-used by
@@ -171,6 +173,7 @@ int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options);
 int pkvm_iommu_clear_ce(struct clear_ce_data *data);
 int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out);
 int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *in, struct set_sm_ce_data *out);
+int pkvm_iommu_pasid_setup_fl(struct pasid_setup_fl_data *in, struct pasid_setup_fl_data *out);
 #endif /* !__PKVM_HYP__ */
 #else /* !CONFIG_PKVM_INTEL */
 static inline int pkvm_qi_submit_sync(struct intel_iommu *iommu,
@@ -193,6 +196,12 @@ static inline int pkvm_context_mapping(struct intel_iommu *iommu,
 static inline int pkvm_pasid_table_setup(struct intel_iommu *iommu,
 					 struct device_domain_info *info,
 					 u8 bus, u8 devfn)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_pasid_setup_fl(struct device_domain_info *info,
+				      phys_addr_t fsptptr, u32 pasid,
+				      u16 did, u16 old_did, int flags)
 {
 	return -EOPNOTSUPP;
 }

intel-staging/pKVM-IA

commit ad734787541c6e6ffbfa0fa6323386bb29ea9613

iommu/vt-d: pKVM: Block first-level PASID setup for default domain
In scalable mode, first-level translation (PGTT=FL_ONLY) translates
device IOVA directly through a first-level page table without second-stage
confinement. During first-level PASID setup (iommu_pasid_setup_fl),
the downstream domain lookup is bypassed and the cache tag is assigned to
pt_domain when did == FLPT_DEFAULT_DID.

A compromised host could pass did == FLPT_DEFAULT_DID along with an
un-donated host physical address as fsptptr_gpa. The hypervisor would
install this host-authored page as the live FLPTPTR without domain
ownership validation, allowing the host to program device DMA to read
and write arbitrary physical memory (including hypervisor and protected VM
memory).

Since pKVM does not support first-level translation (SVA) for the host
default domain, block first-level PASID setup requests when
did == FLPT_DEFAULT_DID by returning -EPERM.

Fixes: fde0edcd148c ("iommu/vt-d: pKVM: Pasid table hypercall for first level translation")
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -264,6 +264,11 @@ static int iommu_pasid_setup_fl(struct pasid_setup_fl_data *data)
 		return -EPERM;
 	}
 
+	if (data->did == FLPT_DEFAULT_DID) {
+		pkvm_err("%s: First-level setup not allowed for default domain\n", __func__);
+		return -EPERM;
+	}
+
 	ret = __get_pasid_table(iommu, data->bus, data->devfn, &table);
 	if (ret)
 		return ret;