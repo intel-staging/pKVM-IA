cxdong/pub-pKVM-IA

commit edc3db0f8b44a61f923e01d27893c14bb1eda55c

iommu/vt-d: pKVM: Legacy mode context update hypercalls
Implement hypercall for setting and clearing contex table entries. Host
donates pages for context table creation.

The donation logic can be summarized as follows:
- struct intel_iommu maintains a one page cache for translation
  structure.
- Host tries the hypercall without donation first time. If it fails with
  ENOMEM, it allocates a page and retries the hypercall by passing in
  the donated page in hypercall IN argument(data->ts_page_gpa)
- if the translation page cache in pKVM is empty, it accepts the
  donation irrespective of whether the page will be used during the
  hypercall. It sets the ts_page_gpa of OUT argument to 0.
- On return from the hypercall, host checks if data->ts_page_gpa and
  frees the page if its not 0.

IOMMU's passthrough mode needs special handling. It by-passes the IOMMU
security and allows devices to access memory directly. When pKVM is
enabled, convert passthrough to DMA mode with host ept as the second
stage page table.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -4,7 +4,9 @@
  *
  */
 #include <asm/kvm_pkvm.h>
+#include <linux/pci.h>
 #include "pkvm/mmu.h"
+#include "pkvm/vmx/ept.h"
 #include "pkvm/memory.h"
 #include "pkvm/pkvm.h"
 #include "pkvm/debug.h"
@@ -27,4 +29,126 @@ int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options)
 	 */
 	return qi_submit_sync(iommu, pkvm_host_gpa_to_virt(desc_gpa),
 			      count, options);
+}
+
+int pkvm_iommu_clear_ce(struct clear_ce_data *data)
+{
+	struct intel_iommu *iommu = iommu_from_phys(data->phys);
+	u16 bdf = PCI_DEVID(data->bus, data->devfn);
+	struct device_domain_info info = { 0 };
+
+	if (!iommu)
+		return -EINVAL;
+
+	if (data->ats_qdep > PCI_ATS_MAX_QDEP)
+		return -EINVAL;
+
+	if (is_dev_in_satc(bdf)) {
+		/*
+		 * Device is in SATC and optimistically assuming that a well crafted SATC
+		 * would contain only physical functions, its safe to set pfsid = bdf.
+		 * TODO: We should probably be verifying SATC for existence of only
+		 * physical functions during pkvm initialization.
+		 */
+		if (ecap_dit(iommu->ecap))
+			info.pfsid = bdf;
+	} else if (data->ats_enabled || data->ats_supported) {
+		return -EPERM;
+	}
+
+	info.iommu = iommu;
+	info.bus = data->bus;
+	info.devfn = data->devfn;
+	info.ats_qdep = data->ats_qdep;
+	info.ats_supported = data->ats_supported;
+	info.ats_enabled = data->ats_enabled;
+
+	pkvm_dbg("%s: dev[%x:%x], ats_qdep: %d\n",
+		 __func__, data->bus, data->devfn, data->ats_qdep);
+	domain_context_clear_one(&info, data->bus, data->devfn);
+
+	return 0;
+}
+
+static int accept_page_donation(struct intel_iommu *iommu, u64 *donation_page_gpa)
+{
+	pkvm_spin_lock(&iommu->lock);
+	if (*donation_page_gpa && !iommu->donation_page) {
+		u64 donation_page_pa = pkvm_host_gpa_to_phys(*donation_page_gpa);
+
+		int ret = pkvm_host_donate_hyp_share_ro(donation_page_pa,
+							VTD_PAGE_SIZE, true);
+
+		if (ret) {
+			pkvm_spin_unlock(&iommu->lock);
+			pkvm_err("iommu%d: failed to write protect donated page(err=%d)!\n",
+				 iommu->seq_id, ret);
+			return ret;
+		}
+		iommu->donation_page = __pkvm_va(donation_page_pa);
+		*donation_page_gpa = 0ULL;
+	}
+	pkvm_spin_unlock(&iommu->lock);
+
+	return 0;
+}
+
+static int iommu_set_lm_ce(struct set_lm_ce_data *data)
+{
+	struct intel_iommu *iommu = iommu_from_phys(data->phys);
+	u16 bdf = PCI_DEVID(data->bus, data->devfn);
+	struct device_domain_info info = { 0 };
+	struct dmar_domain domain = { 0 };
+	int ret;
+
+	if (!iommu)
+		return -EINVAL;
+
+	if (data->ats_qdep > PCI_ATS_MAX_QDEP)
+		return -EINVAL;
+
+	if (is_dev_in_satc(bdf)) {
+		if (ecap_dit(iommu->ecap))
+			info.pfsid = bdf;
+	} else if (data->ats_enabled || data->ats_supported) {
+		return -EPERM;
+	}
+
+	info.bus = data->bus;
+	info.devfn = data->devfn;
+	info.iommu = iommu;
+	info.ats_qdep = data->ats_qdep;
+	info.ats_supported = data->ats_supported;
+	info.ats_enabled = data->ats_enabled;
+	if (data->did == FLPT_DEFAULT_DID) {
+		/*
+		 * Passthrough will break pkvm security guarantees as
+		 * device would be able to access the whole physical
+		 * memory range. Use Second stage translation with host ept
+		 * as second stage pagetable so as to limit device access
+		 * to host memory.
+		 */
+		domain.pgd = __pkvm_va(pkvm_host_ept_root());
+		domain.agaw = level_to_agaw(pkvm_host_ept_level());
+	} else {
+		domain.pgd = pkvm_host_gpa_to_virt(data->pgd_gpa);
+		domain.agaw = iommu->agaw;
+	}
+
+	ret = accept_page_donation(iommu, &data->donation_page_gpa);
+	if (ret)
+		return ret;
+
+	pkvm_dbg("%s: dev[%x:%x], did: %d, pgd: %p, agaw: %d\n", __func__,
+		 data->bus, data->devfn, data->did, domain.pgd, domain.agaw);
+	return domain_context_mapping_one(&domain, iommu, &info, data->did,
+					  data->bus, data->devfn);
+}
+
+int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out)
+{
+	int ret = iommu_set_lm_ce(in);
+
+	*out = *in;
+	return ret;
 }

arch/x86/include/asm/kvm_pkvm.h
@@ -52,6 +52,29 @@ struct pkvm_mem_info {
 	u64 prot;
 };
 
+#ifdef CONFIG_PKVM_INTEL
+struct clear_ce_data {
+	u64 phys;
+	u8 bus;
+	u8 devfn;
+	u8 ats_qdep;
+	u8 ats_enabled: 1;
+	u8 ats_supported: 1;
+};
+
+struct set_lm_ce_data {
+	u64 phys;
+	u64 pgd_gpa;
+	u64 donation_page_gpa;
+	u16 did;
+	u8 bus;
+	u8 devfn;
+	u8 ats_qdep;
+	u8 ats_enabled: 1;
+	u8 ats_supported: 1;
+};
+#endif
+
 #define TO_PKVM_HC(f)		CONCATENATE(__pkvm__, f)
 
 enum pkvm_hc {
@@ -130,6 +153,13 @@ union pkvm_hc_data {
 	struct {
 		u64 val;
 	} iommu_mmio_read;
+	struct {
+		struct clear_ce_data data;
+	} iommu_clear_ce;
+	union {
+		struct set_lm_ce_data in;
+		struct set_lm_ce_data out;
+	} iommu_set_lm_ce;
 #endif
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];

arch/x86/include/asm/pkvm_hypercalls.h
@@ -94,6 +94,8 @@ PKVM_HC(vm_mmu_age)
 PKVM_HC_OUT(iommu_mmio_read)
 PKVM_HC(iommu_mmio_write)
 PKVM_HC(iommu_qi_submit)
+PKVM_HC_IN(iommu_clear_ce)
+PKVM_HC_INOUT(iommu_set_lm_ce)
 #endif
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -1897,6 +1897,13 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 					   pkvm_hc_input3(vcpu),
 					   pkvm_hc_input4(vcpu));
 		break;
+	case __pkvm__iommu_clear_ce:
+		ret = pkvm_iommu_clear_ce(&in.iommu_clear_ce.data);
+		break;
+	case __pkvm__iommu_set_lm_ce:
+		ret = pkvm_iommu_set_lm_ce(&in.iommu_set_lm_ce.in,
+					   &out.iommu_set_lm_ce.out);
+		break;
 #endif
 	default:
 		ret = pkvm_vcpu_handle_host_hypercall(vcpu, hc, &in, &out);

drivers/iommu/intel/iommu.c
@@ -1494,6 +1494,15 @@ int domain_context_mapping_one(struct dmar_domain *domain,
 
 	pr_debug("Set context mapping for %02x:%02x.%d\n",
 		bus, PCI_SLOT(devfn), PCI_FUNC(devfn));
+
+	if (pkvm_enabled()) {
+		ret = pkvm_context_mapping(iommu, info, bus, devfn,
+					   virt_to_phys(domain->pgd), did);
+		if (ret)
+			pr_err("%s: iommu%d: pkvm_context_mapping failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+		return ret;
+	}
 #endif
 
 	spin_lock(&iommu->lock);
@@ -1743,6 +1752,17 @@ void domain_context_clear_one(struct device_domain_info *info, u8 bus, u8 devfn)
 	struct intel_iommu *iommu = info->iommu;
 	struct context_entry *context;
 	u16 did;
+
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		int ret = pkvm_context_clear(iommu->reg_phys, bus, devfn, info);
+
+		if (ret)
+			pr_err("%s: iommu%d: pkvm_context_clear failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+		return;
+	}
+#endif
 
 	spin_lock(&iommu->lock);
 	context = iommu_context_addr(iommu, bus, devfn, 0);
@@ -4435,6 +4455,24 @@ static int context_setup_pass_through(struct device *dev, u8 bus, u8 devfn)
 	struct device_domain_info *info = dev_iommu_priv_get(dev);
 	struct intel_iommu *iommu = info->iommu;
 	struct context_entry *context;
+
+	if (pkvm_enabled()) {
+		int ret = pkvm_context_mapping(iommu, info, bus, devfn, 0, FLPT_DEFAULT_DID);
+		if (ret) {
+			pr_err("%s: iommu%d: pkvm_context_mapping failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+		} else if (!dev_is_real_dma_subdevice(dev)) {
+			/*
+			 * pKVM uses second stage translation for passthrough
+			 * and hence need to enable ATS in pci config space.
+			 * NOTE: pKVM supports ATS only if the device is in
+			 * SATC and pKVM validates this before enabling it
+			 * during context/pasid table updates.
+			 */
+			iommu_enable_pci_ats(info);
+		}
+		return ret;
+	}
 
 	spin_lock(&iommu->lock);
 	context = iommu_context_addr(iommu, bus, devfn, 1);

drivers/iommu/intel/iommu.h
@@ -1053,6 +1053,11 @@ static inline int agaw_to_level(int agaw)
 	return agaw + 2;
 }
 
+static inline u16 level_to_agaw(int level)
+{
+	return (level == 3) ? 1 : (level == 4) ? 2 : 3;
+}
+
 static inline int agaw_to_width(int agaw)
 {
 	return min_t(int, 30 + agaw * LEVEL_STRIDE, MAX_AGAW_WIDTH);

drivers/iommu/intel/pkvm_iommu.c
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include "iommu.h"
+#include "../iommu-pages.h"
 
 int __init pkvm_host_prepare_iommu(void)
 {
@@ -127,5 +128,61 @@ int pkvm_qi_submit_sync(struct intel_iommu *iommu, struct qi_desc *desc,
 	ret = pkvm_hypercall(iommu_qi_submit, iommu->reg_phys,
 			     virt_to_phys(desc_ptr), count, options);
 	kfree(desc_ptr);
+	return ret;
+}
+
+int pkvm_context_clear(u64 phys, u8 bus, u8 devfn, struct device_domain_info *info)
+{
+	union pkvm_hc_data d = { 0 };
+	struct clear_ce_data *data = &d.iommu_clear_ce.data;
+
+	data->phys = phys;
+	data->bus = bus;
+	data->devfn = devfn;
+	data->ats_qdep = info->ats_qdep;
+	data->ats_supported = info->ats_supported;
+	data->ats_enabled = info->ats_enabled;
+
+	return pkvm_hypercall_in(iommu_clear_ce, &d);
+}
+
+int pkvm_context_mapping(struct intel_iommu *iommu, struct device_domain_info *info,
+			 u8 bus, u8 devfn, u64 pgd_gpa, u16 did)
+{
+	union pkvm_hc_data d = { 0 };
+	struct set_lm_ce_data *data = &d.iommu_set_lm_ce.in;
+	int ret;
+
+	data->phys = iommu->reg_phys;
+	data->pgd_gpa = pgd_gpa;
+	data->did = did;
+	data->bus = bus;
+	data->devfn = devfn;
+	data->ats_qdep = info->ats_qdep;
+	data->ats_supported = info->ats_supported;
+	data->ats_enabled = info->ats_enabled;
+
+	spin_lock(&iommu->lock);
+	ret = pkvm_hypercall_inout(iommu_set_lm_ce, &d, &d);
+	if (ret == -ENOMEM) {
+		void *donation_page = iommu_alloc_pages_node_sz(iommu->node, GFP_ATOMIC, SZ_4K);
+
+		if (!donation_page) {
+			pr_err("iommu%d: failed to allocate context page\n", iommu->seq_id);
+			spin_unlock(&iommu->lock);
+			return -ENOMEM;
+		}
+		data->donation_page_gpa = virt_to_phys(donation_page);
+		ret = pkvm_hypercall_inout(iommu_set_lm_ce, &d, &d);
+
+		/*
+		 * If the hypervisor used donation_gpa, it will be set to 0.
+		 * Free the page if hypervisor didn't use the page.
+		 */
+		if (data->donation_page_gpa)
+			iommu_free_pages(phys_to_virt(data->donation_page_gpa));
+	}
+	spin_unlock(&iommu->lock);
+
 	return ret;
 }

drivers/iommu/intel/pkvm_iommu.h
@@ -28,6 +28,7 @@ struct intel_iommu_info {
 
 struct qi_desc;
 struct intel_iommu;
+struct device_domain_info;
 
 #ifdef CONFIG_PKVM_INTEL
 extern u16 pkvm_sym(satc_devs)[];
@@ -113,6 +114,9 @@ int __init pkvm_host_init_iommu(void);
 
 int pkvm_qi_submit_sync(struct intel_iommu *iommu, struct qi_desc *desc,
 			unsigned int count, unsigned long options);
+int pkvm_context_clear(u64 phys, u8 bus, u8 devfn, struct device_domain_info *info);
+int pkvm_context_mapping(struct intel_iommu *iommu, struct device_domain_info *info,
+			 u8 bus, u8 devfn, u64 pgd_gpa, u16 did);
 #else /* __PKVM_HYP__ */
 static inline bool iommu_supports_2m_page(void)
 {
@@ -144,11 +148,24 @@ int pkvm_intel_iommu_init(void);
 int pkvm_iommu_mmio_read(u64 phys, int len, u64 *val);
 int pkvm_iommu_mmio_write(u64 phys, int len, u64 val);
 int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options);
+int pkvm_iommu_clear_ce(struct clear_ce_data *data);
+int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out);
 #endif /* !__PKVM_HYP__ */
 #else /* !CONFIG_PKVM_INTEL */
 static inline int pkvm_qi_submit_sync(struct intel_iommu *iommu,
 				      struct qi_desc *desc, unsigned int count,
 				      unsigned long options)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_context_clear(u64 phys, u8 bus, u8 devfn,
+				     struct device_domain_info *info)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_context_mapping(struct intel_iommu *iommu,
+				       struct device_domain_info *info,
+				       u8 bus, u8 devfn, u64 pgd_gpa, u16 did)
 {
 	return -EOPNOTSUPP;
 }

intel-staging/pKVM-IA

commit 7d13f3076cadf0f5204761f23240b31967d2345c

iommu/vt-d: pKVM: Reject use_first_level domains in SL setups
Prevent the host from configuring a second-stage-only translation
using a domain allocated with use_first_level=1.

If a domain has use_first_level=1, the unmap path
(cache_tag_flush_iotlb) keys its invalidation descriptor type on this
flag and issues a PASID-based IOTLB invalidation (type 0x6). However,
if this domain was installed as a second-stage-only (SL) translation
(via iommu_set_lm_ce or iommu_pasid_setup_sl), the hardware actually
caches it as DID-only tagged (second-stage).

Per VT-d spec, type 0x6 invalidation only invalidates first-stage or
nested entries, making it a no-op for second-stage entries.
Consequently, the stale second-stage entry survives unmap, allowing a
device to access the page after it is donated to a protected VM.

Fix this by rejecting domains with use_first_level set in both
iommu_set_lm_ce() and iommu_pasid_setup_sl().

Fixes: edc3db0f8b44 ("iommu/vt-d: pKVM: Legacy mode context update hypercalls")
Fixes: fc5e826f2af8 ("iommu/vt-d: pKVM: Pasid table hypercall for second level translation")
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -118,6 +118,11 @@ static int iommu_set_lm_ce(struct set_lm_ce_data *data)
 			 __func__, pgd);
 		return -EINVAL;
 	}
+	if (domain->use_first_level) {
+		pkvm_err("%s: Domain has use_first_level set, expected SL\n", __func__);
+		pkvm_put_iommu_domain(domain);
+		return -EINVAL;
+	}
 
 	pkvm_dbg("%s: dev[%x:%x], did: %d, pgd: %p, agaw: %d\n", __func__,
 		 data->bus, data->devfn, data->did, domain->pgd,
@@ -339,6 +344,11 @@ static int iommu_pasid_setup_sl(struct pasid_setup_sl_data *data)
 	if (!domain) {
 		pkvm_err("%s: Failed to locate domain with pgd: %p\n",
 			 __func__, pgd);
+		return -EINVAL;
+	}
+	if (domain->use_first_level) {
+		pkvm_err("%s: Domain has use_first_level set, expected SL\n", __func__);
+		pkvm_put_iommu_domain(domain);
 		return -EINVAL;
 	}
 