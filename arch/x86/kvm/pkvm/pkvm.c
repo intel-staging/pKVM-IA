cxdong/pub-pKVM-IA

commit 18ee4fa7df66fe63d7d08d6169c9419c632b5853

pKVM: x86: Enforce pvmfw as the pVM entry point
If a pVM is running with pvmfw, ensure that pvmfw is the first code
running in the pVM, by enforcing the pvmfw address as the initial value
of the RIP register on the primary vCPU. (Similarly to pKVM-ARM, assume
that the pvmfw entry point is at the beginning of the pvmfw memory
region.)

In order to achieve that, forbid running pVM's vCPUs until the host
finalizes the pVM, i.e. until pKVM knows which vCPU is primary and which
pvmfw entry address to enforce. If the host has finalized the VM and
thus the primary vCPU is allowed to run, enforce its RIP before running
it first time.

This temporarily makes secondary vCPUs never allowed to run (thus
temporarily makes SMP non-workable in pVMs), until we implement a
mechanism for letting a pVM itself allow a secondary vCPU to run, which
will be implemented in the next patches.

All of the above does not apply to npVMs, for which the host is allowed
to run any vCPU at any time.

[ Grzegorz: Align to 6.18 code structure, form new patch leaving the
  6.12 history behind. This patch is based on multiple [1], [2] 6.12
  patches. Also code reshuffle in `pkvm_vcpu_run` is based on [3]:
  [1] ANDROID: pkvm: x86: Refactor pvmfw handling as a part of new PV code
      https://android-review.googlesource.com/c/kernel/common/+/3632018
  [2] ANDROID: pkvm: x86: Enforce pvmfw as the pVM entry point
      https://android-review.googlesource.com/c/kernel/common/+/3485407
  [3] ANDROID: pkvm: x86: Prevent re-running vCPU after a triple fault ]

Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -336,7 +336,7 @@ static int pkvm_vm_finalize(int vm_handle)
 	struct kvm *kvm, *shared_kvm;
 	struct pkvm_vm *pkvm_vm;
 	u64 pvmfw_load_addr;
-	int ret = 0;
+	int ret = 0, i;
 
 	pkvm_vm = pkvm_get_vm(vm_handle);
 	if (!pkvm_vm)
@@ -367,6 +367,20 @@ static int pkvm_vm_finalize(int vm_handle)
 	}
 
 	kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
+
+	for (i = 0; i < kvm->created_vcpus; i++) {
+		struct kvm_vcpu *vcpu = &pkvm_vm->vcpus[i]->vcpu;
+
+		if (vcpu->vcpu_id == kvm->arch.bsp_vcpu_id) {
+			/*
+			 * Make sure pvmfw_load_addr and bsp_vcpu_id are updated before
+			 * updating mp_state, i.e. before allowing the primary vCPU
+			 * to run. Pairs with smp_load_acquire() in pkvm_vcpu_run().
+			 */
+			smp_store_release(&vcpu->arch.mp_state, KVM_MP_STATE_RUNNABLE);
+			break;
+		}
+	}
 
 	kvm->arch.pkvm.finalized = true;
 	shared_kvm->arch.pkvm.finalized = true;
@@ -461,6 +475,7 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 	vcpu->arch.last_vmentry_cpu = -1;
 	vcpu->arch.regs_avail = ~0;
 	vcpu->arch.regs_dirty = ~0;
+	vcpu->arch.mp_state = KVM_MP_STATE_UNINITIALIZED;
 	vcpu->arch.pat = MSR_IA32_CR_PAT_DEFAULT;
 	if (!pkvm_is_protected_vcpu(vcpu)) {
 		vcpu->arch.mce_banks = kern_pkvm_va(pkvm_vcpu->shared_vcpu->arch.mce_banks);
@@ -1245,6 +1260,12 @@ static int pkvm_load_mmu_pgd(struct kvm_vcpu *vcpu)
 	return 0;
 }
 
+static void pkvm_vcpu_pvmfw_entry_init(struct kvm_vcpu *vcpu)
+{
+	/* pvmfw entry point is at the beginning of the pvmfw image. */
+	kvm_rip_write(vcpu, vcpu->kvm->arch.pkvm.pvmfw_load_addr);
+}
+
 static void update_vcpu_state_from_host(struct kvm_vcpu *vcpu)
 {
 	struct kvm_vcpu *shared_vcpu = to_pkvm_vcpu(vcpu)->shared_vcpu;
@@ -1309,7 +1330,8 @@ static void update_vcpu_state_from_host(struct kvm_vcpu *vcpu)
 		 * If the host VMM boots the pVM directly, without pvmfw,
 		 * let it set the boot entry address.
 		 */
-		if (kvm_register_is_dirty(shared_vcpu, VCPU_REGS_RIP))
+		if (!pkvm_vcpu_is_pvmfw_bsp(vcpu) &&
+		    kvm_register_is_dirty(shared_vcpu, VCPU_REGS_RIP))
 			kvm_rip_write(vcpu, shared_vcpu->arch.regs[VCPU_REGS_RIP]);
 	} else if (unlikely(!kvm_vcpu_has_run(vcpu) && !kvm_vcpu_is_reset_bsp(vcpu))) {
 		/*
@@ -1376,6 +1398,21 @@ static int pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit,
 			 unsigned long *reqs_to_host)
 {
 	int ret;
+
+	if (pkvm_is_protected_vcpu(vcpu)) {
+		/*
+		 * Pairs with smp_store_release() in pkvm_vm_finalize() to make sure
+		 * that pvmfw_load_addr and bsp_vcpu_id are read after reading mp_state,
+		 * so they are read with up-to-date values.
+		 */
+		if (smp_load_acquire(&vcpu->arch.mp_state) != KVM_MP_STATE_RUNNABLE)
+			return -EPERM;
+
+		if (unlikely(!kvm_vcpu_has_run(vcpu))) {
+			if (pkvm_vcpu_is_pvmfw_bsp(vcpu))
+				pkvm_vcpu_pvmfw_entry_init(vcpu);
+		}
+	}
 
 	if (unlikely(!kvm_vcpu_has_run(vcpu)))
 		pkvm_load_mmu_pgd(vcpu);

arch/x86/kvm/pkvm/pkvm.h
@@ -130,6 +130,16 @@ static inline bool pkvm_has_req_to_host(int req, struct kvm_vcpu *vcpu)
 	return test_bit(req, &to_pkvm_vcpu(vcpu)->reqs_to_host);
 }
 
+static inline bool pkvm_vm_has_pvmfw(struct kvm *kvm)
+{
+	return kvm->arch.pkvm.pvmfw_load_addr != INVALID_GPA;
+}
+
+static inline bool pkvm_vcpu_is_pvmfw_bsp(struct kvm_vcpu *vcpu)
+{
+	return kvm_vcpu_is_reset_bsp(vcpu) && pkvm_vm_has_pvmfw(vcpu->kvm);
+}
+
 struct pkvm_x86_ops {
 	void (*update_vcpu_state_from_host)(struct kvm_vcpu *vcpu);
 	void (*share_vcpu_state_with_host)(struct kvm_vcpu *vcpu);

intel-staging/pKVM-IA

commit 22e59d24899362ffe1894a6a56164e5963afec2d

pKVM: x86: Validate if pvmfw is within 32-bit space
Since pKVM enforces starting pvmfw in 32-bit protected mode, any value
of pvmfw_load_addr outside the 32-bit space is invalid. So validate
pvmfw_load_addr against that, thus preventing configuring GUEST_RIP with
an invalid value before starting a pVM with pvmfw (even if that would
only result in a VM-entry failure, not in a security issue).

Additionally validate if the entire pvmfw image (not just
pvmfw_load_addr which is the start address of it) fits within the 32-bit
space, to prevent the host from causing scenarios like: pvmfw starts
running e.g. at address 4G-4 and thus quickly reaches the end of the
addressable space and crashes with #GP before it has a chance to switch
to 64-bit mode. (Even if that would only result in the pVM dying with a
triple fault thanks to the invalid initial IDT ensured by pKVM, not in a
security issue.)

Fixes: 18ee4fa7df66 ("pKVM: x86: Enforce pvmfw as the pVM entry point")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -397,7 +397,8 @@ static int pkvm_vm_finalize(int vm_handle)
 		gpa_t pvmfw_end = pvmfw_load_addr + pvmfw_size;
 
 		if (!pvmfw_present || pvmfw_end < pvmfw_load_addr ||
-		    pvmfw_end > pkvm_pgtable_max_size(&pkvm_vm->mmu)) {
+		    pvmfw_end > pkvm_pgtable_max_size(&pkvm_vm->mmu) ||
+		    pvmfw_end > SZ_4G) {
 			ret = -EINVAL;
 			goto unlock;
 		}