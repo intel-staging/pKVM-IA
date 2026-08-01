cxdong/pub-pKVM-IA

commit 01d1296d3072ce4c246ca5a1d528a9ebd5ac6cf4

pKVM: x86: Implement vm_finalize PV interface
Introduce a new hypercall vm_finalize to let the host "finalize" a pVM,
which means: let pKVM know which of the pVM's vCPUs is the primary vCPU
(BSP) and what is the address of pvmfw in the pVM memory. The host will
issue this hypercall after it has set up all vCPUs of the pVM, before
running them. pKVM will use this information for enforcing secure
startup of pVM's vCPUs, in particular for enforcing pvmfw as the entry
point on the primary vCPU. pKVM will not allow vCPUs to run until the
pVM is finalized. These enforcements will be implemented in the next
patches.

This hypercall will not be used for npVMs, as there is no need to, since
pKVM always allows npVM's vCPUs to run and doesn't need to enforce their
initial state.

This aligns x86 pKVM with the lifecycle model used in pKVM-ARM.

[ Grzegorz: Fully based on Dmytro 6.12 implementation but adjusted to
  6.18 code structure and formed this patch differently, leaving the
  history behind. This patch is based on multiple [1-4] 6.12
  patches:
  [1] ANDROID: pkvm: x86: Check pvmfw load address for overflow
      https://android-review.googlesource.com/c/kernel/common/+/3790427
  [2] ANDROID: pkvm: x86: Refactor pvmfw handling as a part of new PV code
      https://android-review.googlesource.com/c/kernel/common/+/3632018
  [3] ANDROID: pkvm: x86: Enforce pvmfw as the pVM entry point
      https://android-review.googlesource.com/c/kernel/common/+/3485407
  [4] ANDROID: pkvm: x86: Properly return error in pvmfw address set ioctl
      https://android-review.googlesource.com/c/kernel/common/+/3632020 ]

Co-developed-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -163,6 +163,7 @@ static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa,
 		kvm->arch.disabled_quirks = (kvm_caps.inapplicable_quirks |
 					     pkvm_vm->shared_kvm->arch.disabled_quirks) &
 					    kvm_caps.supported_quirks;
+	kvm->arch.pkvm.pvmfw_load_addr = INVALID_GPA;
 
 	pkvm_spin_lock_init(&pkvm_vm->lock);
 
@@ -330,6 +331,52 @@ unshare_apic:
 	return ret;
 }
 
+static int pkvm_vm_finalize(int vm_handle)
+{
+	struct kvm *kvm, *shared_kvm;
+	struct pkvm_vm *pkvm_vm;
+	u64 pvmfw_load_addr;
+	int ret = 0;
+
+	pkvm_vm = pkvm_get_vm(vm_handle);
+	if (!pkvm_vm)
+		return -EINVAL;
+
+	kvm = &pkvm_vm->kvm;
+	shared_kvm = pkvm_vm->shared_kvm;
+
+	if (!pkvm_is_protected_vm(kvm)) {
+		ret = -EINVAL;
+		goto put_pkvm_vm;
+	}
+
+	pkvm_spin_lock(&pkvm_vm->lock);
+
+	if (kvm->arch.pkvm.finalized) {
+		ret = -EBUSY;
+		goto unlock;
+	}
+
+	pvmfw_load_addr = READ_ONCE(shared_kvm->arch.pkvm.pvmfw_load_addr);
+	if (pvmfw_load_addr != INVALID_GPA) {
+		if (!pvmfw_present || U64_MAX - pvmfw_load_addr < pvmfw_size) {
+			ret = -EINVAL;
+			goto unlock;
+		}
+		kvm->arch.pkvm.pvmfw_load_addr = pvmfw_load_addr;
+	}
+
+	kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
+
+	kvm->arch.pkvm.finalized = true;
+	shared_kvm->arch.pkvm.finalized = true;
+unlock:
+	pkvm_spin_unlock(&pkvm_vm->lock);
+put_pkvm_vm:
+	pkvm_put_vm(pkvm_vm);
+	return ret;
+}
+
 static void unsetup_vcpu_lapic(struct kvm_vcpu *vcpu)
 {
 	struct kvm_lapic *apic = vcpu->arch.apic;
@@ -415,7 +462,6 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 	vcpu->arch.regs_avail = ~0;
 	vcpu->arch.regs_dirty = ~0;
 	vcpu->arch.pat = MSR_IA32_CR_PAT_DEFAULT;
-
 	if (!pkvm_is_protected_vcpu(vcpu)) {
 		vcpu->arch.mce_banks = kern_pkvm_va(pkvm_vcpu->shared_vcpu->arch.mce_banks);
 		vcpu->arch.mci_ctl2_banks =
@@ -1683,6 +1729,9 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = pkvm_vm_init(pkvm_host_gpa_to_phys(pkvm_hc_input1(vcpu)),
 				   pkvm_host_gpa_to_phys(pkvm_hc_input2(vcpu)),
 				   pkvm_host_gpa_to_phys(pkvm_hc_input3(vcpu)));
+		break;
+	case __pkvm__vm_finalize:
+		ret = pkvm_vm_finalize(pkvm_hc_input1(vcpu));
 		break;
 	case __pkvm__vm_destroy:
 		pkvm_vm_destroy(pkvm_hc_input1(vcpu), &out.vm_destroy.memcache);

arch/x86/kvm/pkvm.c
@@ -137,11 +137,21 @@ void kvm_free_pkvm_memcache(struct pkvm_memcache *mc)
 
 static int pkvm_vm_ioctl_set_fw_gpa(struct kvm *kvm, u64 gpa)
 {
+	struct kvm_pkvm_vm *pkvm = &kvm->arch.pkvm;
+	int ret = 0;
+
 	if (!pvmfw_present)
 		return -EINVAL;
 
-	WRITE_ONCE(kvm->arch.pkvm.pvmfw_load_addr, gpa);
-	return 0;
+	mutex_lock(&pkvm->finalized_lock);
+	if (pkvm->finalized) {
+		ret = -EBUSY;
+		goto out;
+	}
+	pkvm->pvmfw_load_addr = gpa;
+out:
+	mutex_unlock(&pkvm->finalized_lock);
+	return ret;
 }
 
 static int pkvm_vm_ioctl_info(struct kvm *kvm,

arch/x86/include/asm/kvm_host.h
@@ -929,6 +929,8 @@ struct kvm_pkvm_vm {
 	struct rb_root_cached mappings;
 
 	gpa_t pvmfw_load_addr;
+	bool finalized;
+	struct mutex finalized_lock;
 };
 
 struct kvm_pkvm_vcpu {

arch/x86/include/asm/pkvm_hypercalls.h
@@ -24,6 +24,7 @@ PKVM_HC(dump_vmexit_trace)
 PKVM_HC(check_processor_compatibility)
 PKVM_HC(enable_virtualization_cpu)
 PKVM_HC(vm_init)
+PKVM_HC(vm_finalize)
 PKVM_HC_OUT(vm_destroy)
 PKVM_HC(vcpu_create)
 PKVM_HC_OUT(vcpu_free)

arch/x86/kvm/x86.c
@@ -13598,6 +13598,7 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 
 #ifdef CONFIG_PKVM_X86
 	kvm->arch.pkvm.pvmfw_load_addr = INVALID_GPA;
+	mutex_init(&kvm->arch.pkvm.finalized_lock);
 #endif
 
 	INIT_DELAYED_WORK(&kvm->arch.kvmclock_update_work, kvmclock_update_fn);

intel-staging/pKVM-IA

commit a725b0538f1e3f3a2d477fa52748a02d47d236af

pKVM: x86: Return error for invalid bsp_vcpu_id
Leverage the latest changes in postponed_per_vm_setup() to add
validation of the host-provided bsp_vcpu_id value. For now this is
basically a cosmetic improvement, since pKVM is using the bsp_vcpu_id
value in a limited way, so if the host sets it to an invalid value,
it will only result in the host being unable to run any vCPU of a pVM
(since none of the created vCPUs will have vcpu_id matching the
bsp_vcpu_id).

Fixes: 01d1296d3072 ("pKVM: x86: Implement vm_finalize PV interface")
Fixes: 8a724a4bd0e7 ("pKVM: x86: Enable lapic reset in the pKVM")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -492,8 +492,8 @@ static int postponed_per_vm_setup(struct kvm *kvm)
 	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
 	struct kvm *shared_kvm = pkvm_vm->shared_kvm;
 	enum kvm_irqchip_mode irqchip_mode;
+	u32 max_vcpu_ids, bsp_vcpu_id;
 	u64 apic_bus_cycle_ns;
-	u32 max_vcpu_ids;
 
 	if (pkvm_vm->postponed_setup_done)
 		return 0;
@@ -508,6 +508,10 @@ static int postponed_per_vm_setup(struct kvm *kvm)
 	if (!max_vcpu_ids || max_vcpu_ids > KVM_MAX_VCPU_IDS)
 		return -EINVAL;
 
+	bsp_vcpu_id = READ_ONCE(shared_kvm->arch.bsp_vcpu_id);
+	if (bsp_vcpu_id >= max_vcpu_ids)
+		return -EINVAL;
+
 	/*
 	 * The following setup is per VM, not per vCPU, however it cannot be
 	 * done during VM creation, since these values are set by the host VMM
@@ -520,6 +524,7 @@ static int postponed_per_vm_setup(struct kvm *kvm)
 
 	kvm->arch.irqchip_mode = irqchip_mode;
 	kvm->arch.max_vcpu_ids = max_vcpu_ids;
+	kvm->arch.bsp_vcpu_id = bsp_vcpu_id;
 
 	if (kvm_caps.has_bus_lock_exit)
 		kvm->arch.bus_lock_detection_enabled =
@@ -536,8 +541,6 @@ static int postponed_per_vm_setup(struct kvm *kvm)
 
 	if (!pkvm_is_protected_vm(kvm))
 		kvm->arch.disabled_exits = shared_kvm->arch.disabled_exits;
-
-	kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
 
 	pkvm_vm->postponed_setup_done = true;
 	return 0;