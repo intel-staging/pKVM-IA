cxdong/pub-pKVM-IA

commit 9a7f23f3782615ff5154d800eac315fca62a5dcb

pKVM: x86: Introduce macro for_each_pkvm_guest_vcpu
Introduce for_each_pkvm_guest_vcpu(i, vcpu, vm) to iterate over all
non-NULL vCPUs of a guest VM. This replaces the for loops in the
pkvm_vm_finalize(), pkvm_start_secondary_vcpu(), guest_ept_flush_tlb(),
and the trace helpers.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.h
@@ -83,6 +83,23 @@ struct pkvm_vm {
 /* The pkvm_vm structure size w/o struct kvm */
 #define PKVM_VM_BASE_SIZE		offsetof(struct pkvm_vm, kvm)
 
+/**
+ * for_each_pkvm_guest_vcpu - iterate over non-NULL guest vCPUs
+ * @i: loop counter
+ * @vcpu: struct kvm_vcpu pointer, assigned by macro and guaranteed non-NULL.
+ * @vm: struct pkvm_vm pointer, evaluated multiple times. Don't use expressions.
+ *
+ * Iterates over all vCPUs in the VM, automatically skipping if vCPU pointer is
+ * NULL.
+ */
+#define for_each_pkvm_guest_vcpu(i, vcpu, vm)							\
+	for ((i) = 0; (i) < (vm)->kvm.created_vcpus &&						\
+		      ({ (vcpu) = (vm)->vcpus[(i)] ?						\
+				  &(vm)->vcpus[(i)]->vcpu : NULL; true; }); (i)++)		\
+		if (!(vcpu))									\
+			continue;								\
+		else
+
 static inline struct pkvm_vm *to_pkvm(struct kvm *kvm)
 {
 	/*

arch/x86/kvm/pkvm/pkvm.c
@@ -364,6 +364,7 @@ static int pkvm_vm_finalize(int vm_handle)
 {
 	struct kvm *kvm, *shared_kvm;
 	struct pkvm_vm *pkvm_vm;
+	struct kvm_vcpu *vcpu;
 	u64 pvmfw_load_addr;
 	int ret = 0, i;
 
@@ -395,9 +396,7 @@ static int pkvm_vm_finalize(int vm_handle)
 		kvm->arch.pkvm.pvmfw_load_addr = pvmfw_load_addr;
 	}
 
-	for (i = 0; i < kvm->created_vcpus; i++) {
-		struct kvm_vcpu *vcpu = &pkvm_vm->vcpus[i]->vcpu;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm) {
 		if (vcpu->vcpu_id == kvm->arch.bsp_vcpu_id) {
 			/*
 			 * Make sure pvmfw_load_addr and bsp_vcpu_id are updated before
@@ -2240,6 +2239,7 @@ unsigned long pkvm_pcpu_tss(int cpu)
 int pkvm_start_secondary_vcpu(struct kvm *kvm, u32 apic_id, unsigned long start_ip)
 {
 	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
+	struct kvm_vcpu *vcpu;
 	int ret = -EINVAL;
 	int i;
 
@@ -2251,9 +2251,7 @@ int pkvm_start_secondary_vcpu(struct kvm *kvm, u32 apic_id, unsigned long start_
 
 	pkvm_spin_lock(&pkvm_vm->lock);
 
-	for (i = 0; i < kvm->created_vcpus; i++) {
-		struct kvm_vcpu *vcpu = &pkvm_vm->vcpus[i]->vcpu;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm) {
 		if (vcpu->vcpu_id != apic_id)
 			continue;
 

arch/x86/kvm/pkvm/trace.c
@@ -39,15 +39,12 @@ static void refresh_host_vm_trace(void)
 
 static int __refresh_guest_vm_trace(struct pkvm_vm *vm, void *param)
 {
+	struct kvm_vcpu *vcpu;
 	int i;
 
 	pkvm_spin_lock(&vm->lock);
-	for (i = 0; i < vm->kvm.created_vcpus; i++) {
-		if (!vm->vcpus[i])
-			continue;
-
-		refresh_vmexit_perf(&vm->vcpus[i]->perf);
-	}
+	for_each_pkvm_guest_vcpu(i, vcpu, vm)
+		refresh_vmexit_perf(&to_pkvm_vcpu(vcpu)->perf);
 	pkvm_spin_unlock(&vm->lock);
 
 	return 0;
@@ -89,6 +86,7 @@ struct copy_arg {
 static int __copy_guest_vm_trace(struct pkvm_vm *vm, void *param)
 {
 	struct copy_arg *arg = param;
+	struct kvm_vcpu *vcpu;
 	int i;
 
 	if (arg->vm_handle != PKVM_HOST_VM_HANDLE &&
@@ -96,16 +94,13 @@ static int __copy_guest_vm_trace(struct pkvm_vm *vm, void *param)
 		return 0;
 
 	pkvm_spin_lock(&vm->lock);
-	for (i = 0; i < vm->kvm.created_vcpus; i++) {
-		if (!vm->vcpus[i])
-			continue;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, vm) {
 		if (arg->size < sizeof(struct perf_data)) {
 			pkvm_spin_unlock(&vm->lock);
 			return -ENOSPC;
 		}
 
-		copy_vmexit_perf_data(arg->dst, &vm->vcpus[i]->perf);
+		copy_vmexit_perf_data(arg->dst, &to_pkvm_vcpu(vcpu)->perf);
 		arg->dst += sizeof(struct perf_data);
 		arg->size -= sizeof(struct perf_data);
 	}

arch/x86/kvm/pkvm/vmx/ept.c
@@ -214,20 +214,12 @@ static void guest_ept_flush_tlb(struct pkvm_pgtable *pgt,
 				unsigned long vaddr, unsigned long size)
 {
 	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(pgt);
+	struct kvm_vcpu *vcpu;
 	int i;
 
 	pkvm_spin_lock(&pkvm_vm->lock);
 
-	for (i = 0; i < pkvm_vm->kvm.created_vcpus; i++) {
-		struct pkvm_vcpu *pkvm_vcpu;
-		struct kvm_vcpu *vcpu;
-
-		pkvm_vcpu = pkvm_vm->vcpus[i];
-		if (WARN_ON_ONCE(!pkvm_vcpu))
-			continue;
-
-		vcpu = &pkvm_vcpu->vcpu;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm) {
 		kvm_make_request(KVM_REQ_TLB_FLUSH_CURRENT, vcpu);
 		pkvm_kick_vcpu(vcpu);
 	}

intel-staging/pKVM-IA

commit 0ee3f118e1c2ad9b653a23feafd0910958ab6d7e

pKVM: x86: Fix name clash in for_each_pkvm_guest_vcpu()
for_each_pkvm_guest_vcpu() macro takes an argument named 'vcpu', while
also references the 'vcpu' field of struct pkvm_vcpu. As a result, this
struct field name is mistakenly substituted with the value of the 'vcpu'
argument passed to the macro, just like any other occurrencess of 'vcpu'
in the macro's implementation. So unless this passed value itself is
'vcpu' too, it doesn't compile.

Prevent this by changing the argument name to '_vcpu'.

Fixes: 9a7f23f37826 ("pKVM: x86: Introduce macro for_each_pkvm_guest_vcpu")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.h
@@ -87,17 +87,17 @@ struct pkvm_vm {
 /**
  * for_each_pkvm_guest_vcpu - iterate over non-NULL guest vCPUs
  * @i: loop counter
- * @vcpu: struct kvm_vcpu pointer, assigned by macro and guaranteed non-NULL.
+ * @_vcpu: struct kvm_vcpu pointer, assigned by macro and guaranteed non-NULL.
  * @vm: struct pkvm_vm pointer, evaluated multiple times. Don't use expressions.
  *
  * Iterates over all vCPUs in the VM, automatically skipping if vCPU pointer is
  * NULL.
  */
-#define for_each_pkvm_guest_vcpu(i, vcpu, vm)							\
+#define for_each_pkvm_guest_vcpu(i, _vcpu, vm)							\
 	for ((i) = 0; (i) < (vm)->kvm.created_vcpus &&						\
-		      ({ (vcpu) = (vm)->vcpus[(i)] ?						\
+		      ({ (_vcpu) = (vm)->vcpus[(i)] ?						\
 				  &(vm)->vcpus[(i)]->vcpu : NULL; true; }); (i)++)		\
-		if (!(vcpu))									\
+		if (!(_vcpu))									\
 			continue;								\
 		else
 