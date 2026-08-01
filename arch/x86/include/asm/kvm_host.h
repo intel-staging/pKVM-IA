cxdong/pub-pKVM-IA

commit 492416a60fce0c4df94b337da22fdffab19a85a0

pKVM: x86: Define the host VM handle as INT_MAX
Define INT_MAX as the pKVM VM handle for the host VM, which does not
conflict with the guest VM handles. This value can be used to identify
if a VM handle is for a guest VM or for the host VM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/kvm_host.h
@@ -837,6 +837,8 @@ static inline void free_pkvm_memcache(struct pkvm_memcache *mc,
 		free(pop_pkvm_memcache(mc, to_va));
 }
 
+#define PKVM_HOST_VM_HANDLE	INT_MAX
+
 struct kvm_pkvm_vm {
 	int handle;
 };

arch/x86/kvm/vmx/pkvm_init.c
@@ -155,6 +155,7 @@ static __init int pkvm_setup_host_vm(struct pkvm_hyp *pkvm)
 		return -ENOMEM;
 	}
 
+	kvmx->kvm.arch.pkvm.handle = PKVM_HOST_VM_HANDLE;
 	/*
 	 * Only a few fields in the kvm structure will be used, e.g.,
 	 * hlt_in_guest for exception injection code to clear hlt state.

intel-staging/pKVM-IA

commit 98fc42688a622a5f44f4763f8ba991badc45ea76

pKVM: x86: Move PKVM_HOST_VM_HANDLE definition to kvm_pkvm.h
Similarly to the previous commits, move PKVM_HOST_VM_HANDLE definition
to the pKVM-specific header kvm_pkvm.h, since there is no need for it
to be in the generic header kvm_host.h.

Fixes: 492416a60fce ("pKVM: x86: Define the host VM handle as INT_MAX")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_host.h
@@ -795,8 +795,6 @@ struct pkvm_memcache {
 	unsigned long flags;
 };
 
-#define PKVM_HOST_VM_HANDLE	INT_MAX
-
 struct kvm_pkvm_vm {
 	int handle;
 	struct pkvm_memcache guest_mmu_teardown_mc;

arch/x86/include/asm/kvm_pkvm.h
@@ -56,6 +56,8 @@ struct pkvm_mem_info {
 	u64 prot;
 };
 
+#define PKVM_HOST_VM_HANDLE	INT_MAX
+
 #ifdef CONFIG_PKVM_INTEL
 struct clear_ce_data {
 	u64 phys;