cxdong/pub-pKVM-IA

commit 1e97b74c93f829ad90d7861009bd6f70b42e05ff

pKVM: x86: Add page table walk support
Add page table walk infrastructure, inspired by the pkvm-arm's
kvm_pgtable_walk() to facilitate the page table operations e.g., map and
unmap. The page table walking will invoke a callback function for each
visited page table entry within the minimum PAGE_SIZE aligned memory
range covering the input [vaddr, vaddr + size), including present
leaves, non-present leaves and present non-leaves. The callback will be
executed on leaves based on the specified walk_flags. If the callback
returns zero, the walk continues to the next entry; a non-zero value
terminates the walk immediately and will be used as the return of the
walk.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pgtable.c
@@ -10,6 +10,86 @@ static void *pgtable_alloc_page(const struct pkvm_pgtable_mm_ops *mm_ops)
 	return mm_ops->zalloc_page();
 }
 
+struct pgt_walk_data {
+	struct pkvm_pgtable *pgt;
+	unsigned long addr;
+	const unsigned long start;
+	const unsigned long end;
+	struct pkvm_pgtable_walker *walker;
+};
+
+static int _pgtable_walk(struct pgt_walk_data *data, void *ptep, int level);
+static int pgtable_visit(struct pgt_walk_data *data, void *ptep, int level)
+{
+	const struct pkvm_pgtable_ops *pgt_ops = data->pgt->pgt_ops;
+	struct pkvm_pgtable_walker *walker = data->walker;
+	bool leaf = pgt_ops->pte_is_leaf(ptep, level);
+	unsigned long walk_flags = walker->walk_flags;
+	struct pkvm_pgtable_visit_ctx ctx = {
+		.pgt = data->pgt,
+		.start = data->start,
+		.end = data->end,
+		.addr = data->addr,
+		.level = level,
+		.ptep = ptep,
+	};
+	void *child_ptep;
+	int ret = 0;
+
+	if (!leaf && (walk_flags & PKVM_PGTABLE_WALK_TABLE_PRE))
+		ret = walker->cb(&ctx, PKVM_PGTABLE_WALK_TABLE_PRE, walker->arg);
+
+	if (leaf && (walk_flags & PKVM_PGTABLE_WALK_LEAF)) {
+		ret = walker->cb(&ctx, PKVM_PGTABLE_WALK_LEAF, walker->arg);
+		/*
+		 * The ptep may be updated by the walker->cb. Revisit the ptep
+		 * to see if it is still leaf or not.
+		 */
+		leaf = pgt_ops->pte_is_leaf(ptep, level);
+	}
+
+	if (ret)
+		return ret;
+
+	if (leaf) {
+		unsigned long size = pgt_ops->level_to_size(level);
+
+		data->addr = ALIGN_DOWN(data->addr, size);
+		data->addr += size;
+		return ret;
+	}
+
+	child_ptep = __pkvm_va(pgt_ops->pte_to_phys(ptep));
+	ret = _pgtable_walk(data, child_ptep, level - 1);
+	if (ret)
+		return ret;
+
+	if (walk_flags & PKVM_PGTABLE_WALK_TABLE_POST)
+		ret = walker->cb(&ctx, PKVM_PGTABLE_WALK_TABLE_POST, walker->arg);
+
+	return ret;
+}
+
+static int _pgtable_walk(struct pgt_walk_data *data, void *ptep, int level)
+{
+	const struct pkvm_pgtable_ops *pgt_ops = data->pgt->pgt_ops;
+	int idx = pgt_ops->vaddr_to_index(data->addr, level);
+	int entry_size = pgt_ops->pte_size(level);
+	int entries = pgt_ops->pte_count(level);
+	int ret;
+
+	for (; idx < entries; idx++) {
+		if (data->addr >= data->end)
+			break;
+
+		ret = pgtable_visit(data, (ptep + idx * entry_size), level);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 /**
  * pkvm_pgtable_init() - Initialize a pKVM page table.
  * @pgt:	The page table to be initialized.
@@ -39,4 +119,44 @@ int pkvm_pgtable_init(struct pkvm_pgtable *pgt,
 	pgt->pgt_ops = pgt_ops;
 
 	return 0;
+}
+
+/**
+ * pkvm_pgtable_walk() - Walk a pKVM page table
+ * @pgt:	The page table to walk.
+ * @vaddr:	The virtual address for the start of the walk.
+ * @size:	The walk range size.
+ * @walker:	The callback descriptor which will be executed
+ *		during the walk.
+ *
+ * The walked range is the minimum PAGE_SIZE-aligned range covering
+ * [@vaddr, @vaddr + @size).
+ *
+ * Each page table entry within the memory range [@vaddr, @vaddr + @size) is
+ * visited during the walk, including present leaves, non-present leaves, and
+ * present non-leaves. The callback in @walker is executed on leaves based on
+ * the walk_flags in @walker. If the callback returns zero, the walk will
+ * continue to the next entry. If the callback returns a non-zero, the walk will
+ * be terminated immediately and return that value.
+ *
+ * Return: 0 on success, negative error code on failure.
+ */
+int pkvm_pgtable_walk(struct pkvm_pgtable *pgt, unsigned long vaddr,
+		      unsigned long size, struct pkvm_pgtable_walker *walker)
+{
+	struct pgt_walk_data data = {
+		.pgt = pgt,
+		.addr = ALIGN_DOWN(vaddr, PAGE_SIZE),
+		.start = ALIGN_DOWN(vaddr, PAGE_SIZE),
+		.end = ALIGN(vaddr + size, PAGE_SIZE),
+		.walker = walker,
+	};
+
+	if (!pgt->root_pa)
+		return -EINVAL;
+
+	if (data.start == data.end)
+		return 0;
+
+	return _pgtable_walk(&data, __pkvm_va(pgt->root_pa), pgt->cap.level);
 }

arch/x86/kvm/pkvm/pgtable.h
@@ -77,9 +77,51 @@ struct pkvm_pgtable {
 	const struct pkvm_pgtable_ops *pgt_ops;
 };
 
+struct pkvm_pgtable_visit_ctx {
+	struct pkvm_pgtable *pgt;
+	const unsigned long start;
+	const unsigned long end;
+	unsigned long addr;
+	int level;
+	void *ptep;
+};
+
+typedef int (*pgtable_visit_fn_t)(struct pkvm_pgtable_visit_ctx *ctx,
+				  unsigned long walk_flags, void *const arg);
+
+/**
+ * enum pkvm_pgtable_walk_flags - Flags to control page table walk.
+ * @PKVM_PGTABLE_WALK_TABLE_PRE:	Execute the callback on a non-leaf
+ *					entry before visiting its children.
+ * @PKVM_PGTABLE_WALK_LEAF:		Execute the callback on a leaf entry
+ *					(either present or non-present).
+ * @PKVM_PGTABLE_WALK_TABLE_POST:	Execute the callback on a non-leaf
+ *					entry after visiting its children.
+ */
+enum pkvm_pgtable_walk_flags {
+	PKVM_PGTABLE_WALK_TABLE_PRE	= BIT(0),
+	PKVM_PGTABLE_WALK_LEAF		= BIT(1),
+	PKVM_PGTABLE_WALK_TABLE_POST	= BIT(2),
+};
+
+/**
+ * struct pkvm_pgtable_walker - The page table walk callback descriptor.
+ * @cb:		The callback executed during the page table walking.
+ * @arg:	The argument passed to the callback.
+ * @walk_flags:	A bitwise-or of enum pkvm_pgtable_walk_flags to indicate the
+ *		walking behaviors.
+ */
+struct pkvm_pgtable_walker {
+	const pgtable_visit_fn_t cb;
+	void *const arg;
+	const unsigned long walk_flags;
+};
+
 int pkvm_pgtable_init(struct pkvm_pgtable *pgt,
 		      struct pkvm_pgtable_cap cap,
 		      const struct pkvm_pgtable_mm_ops *mm_ops,
 		      const struct pkvm_pgtable_ops *pgt_ops);
+int pkvm_pgtable_walk(struct pkvm_pgtable *pgt, unsigned long vaddr,
+		      unsigned long size, struct pkvm_pgtable_walker *walker);
 
 #endif /* __PKVM_X86_PGTABLE_H */

intel-staging/pKVM-IA

commit 62bf6be6c9f1cddcdaa90a4c91c1c425da8af56e

pKVM: x86: Fix address range check in pkvm_pgtable_walk()
Commit 1d546469ae0c ("pKVM: x86: Add address range validation
to page table walker") added a check if the walked range is within the
page table's addressable space. However, this check only checks if the
size of range doesn't exceed the address space size, instead of checking
if both start and end addresses are within the address space. So it
still allows the caller to incorrectly pass a range which is small
enough but crosses the upper boundary of the address space.

Even though pkvm_pgtable_walk() itself effectively ignores upper bits
of the VA (since those bits are dropped by pgt_ops->vaddr_to_index()),
this insufficient checking is still causing at least the following
issue: if the host donates a page to a pVM passing an incorrect GPA
with upper bits set, while its lower bits, for example, match the
pvmfw_load_addr of the pVM, then gpa_range_overlaps_pvmfw() compares
this GPA against pvmfw_load_addr and returns false (since it doesn't
ignore the upper bits) and then pkvm_host_donate_guest() proceeds to
map pvmfw_load_addr in the guest MMU (since pkvm_pgtable_walk() does
ignore the upper bits), _without_ copying pvmfw into the guest page.

So fix this, to make the check sufficient.

Also document this requirement for the passed range in the documentation
of pkvm_pgtable_walk().

Fixes: 1e97b74c93f8 ("pKVM: x86: Add page table walk support")
Fixes: 1d546469ae0c ("pKVM: x86: Add address range validation to page table walker")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pgtable.c
@@ -654,6 +654,11 @@ int pkvm_pgtable_init(struct pkvm_pgtable *pgt,
  * continue to the next entry. If the callback returns a non-zero, the walk will
  * be terminated immediately and return that value.
  *
+ * The range [@vaddr, @vaddr + @size) must be within [0, 2^N) where N is the
+ * address width in bits, e.g. 48 or 57. If there are extra upper bits set
+ * (e.g. due to sign extension in canonical addressing), those bits must be
+ * cleared by the caller.
+ *
  * Return: 0 on success, negative error code on failure.
  */
 int pkvm_pgtable_walk(struct pkvm_pgtable *pgt, unsigned long vaddr,
@@ -671,7 +676,7 @@ int pkvm_pgtable_walk(struct pkvm_pgtable *pgt, unsigned long vaddr,
 	int ret;
 
 	if (!pgt->root_pa || vaddr + size <= vaddr || data.end <= data.start ||
-	    data.end - data.start > pkvm_pgtable_max_size(pgt))
+	    data.end > pkvm_pgtable_max_size(pgt))
 		return -EINVAL;
 
 	ret = _pgtable_walk(&data, __pkvm_va(pgt->root_pa), pgt->cap.level);