cxdong/pub-pKVM-IA

commit 3c6cfa18172f74ead2664e6e5cd965ae8eff0bac

pKVM: VMX: Implement vcpu_precreate operation
Import vmx_vcpu_precreate from KVM to implement vcpu_precreate operation
with the customization that, donate the pid_table pages from the host to
the pKVM hypervisor if ipi virtualization is supported, as the pid_table
is controlled inside the pKVM hypervisor and the host doesn't need to
access.

The pid_table will be donated back to the host when destroy the VM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -74,6 +74,8 @@
 #include "vmx_onhyperv.h"
 #include "posted_intr.h"
 #ifdef __PKVM_HYP__
+#include "mem_protect.h"
+#include "memory.h"
 #include "pkvm.h"
 
 #undef module_param_named
@@ -4710,6 +4712,7 @@ static u32 vmx_secondary_exec_control(struct vcpu_vmx *vmx)
 
 	return exec_control;
 }
+#endif /* !__PKVM_HYP__ */
 
 static inline int vmx_get_pid_table_order(struct kvm *kvm)
 {
@@ -4718,6 +4721,7 @@ static inline int vmx_get_pid_table_order(struct kvm *kvm)
 
 static int vmx_alloc_ipiv_pid_table(struct kvm *kvm)
 {
+#ifndef __PKVM_HYP__
 	struct page *pages;
 	struct kvm_vmx *kvm_vmx = to_kvm_vmx(kvm);
 
@@ -4734,6 +4738,52 @@ static int vmx_alloc_ipiv_pid_table(struct kvm *kvm)
 
 	kvm_vmx->pid_table = (void *)page_address(pages);
 	return 0;
+#else
+	struct kvm *shared_kvm = to_pkvm(kvm)->shared_kvm;
+	struct kvm_vmx *kvm_vmx = to_kvm_vmx(kvm);
+	struct kvm_vmx *shared_kvm_vmx;
+	u64 *pid_table;
+	int ret;
+
+	if (!irqchip_in_kernel(shared_kvm) || !enable_ipiv)
+		return 0;
+
+	pkvm_spin_lock(&to_pkvm(kvm)->lock);
+
+	if (kvm_vmx->pid_table) {
+		/* The pid_table is already set. */
+		ret = 0;
+		goto unlock;
+	}
+
+	shared_kvm_vmx = to_kvm_vmx(shared_kvm);
+
+	if (shared_kvm->arch.max_vcpu_ids > KVM_MAX_VCPU_IDS ||
+	    !shared_kvm_vmx->pid_table) {
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	kvm->arch.irqchip_mode = shared_kvm->arch.irqchip_mode;
+	kvm->arch.max_vcpu_ids = shared_kvm->arch.max_vcpu_ids;
+	pid_table = kern_pkvm_va(shared_kvm_vmx->pid_table);
+	/*
+	 * Although the contents in pid_table is not secret since it is
+	 * constructed following the SDM, still donate the pid_table pages to
+	 * the pKVM hypervisor as the host doesn't need to access these pages
+	 * until the guest is destroyed.
+	 */
+	ret = pkvm_host_donate_hyp(__pkvm_pa(pid_table),
+				   PAGE_SIZE << vmx_get_pid_table_order(kvm),
+				   true);
+	if (ret)
+		goto unlock;
+
+	kvm_vmx->pid_table = pid_table;
+unlock:
+	pkvm_spin_unlock(&to_pkvm(kvm)->lock);
+	return ret;
+#endif
 }
 
 int vmx_vcpu_precreate(struct kvm *kvm)
@@ -4741,6 +4791,7 @@ int vmx_vcpu_precreate(struct kvm *kvm)
 	return vmx_alloc_ipiv_pid_table(kvm);
 }
 
+#ifndef __PKVM_HYP__
 #define VMX_XSS_EXIT_BITMAP 0
 
 static void init_vmcs(struct vcpu_vmx *vmx)
@@ -8435,11 +8486,19 @@ void vmx_hardware_unsetup(void)
 
 void vmx_vm_destroy(struct kvm *kvm)
 {
-	/* TODO: Enable this for pKVM hypervisor when enabling ipiv */
-#ifndef __PKVM_HYP__
 	struct kvm_vmx *kvm_vmx = to_kvm_vmx(kvm);
 
+#ifndef __PKVM_HYP__
 	free_pages((unsigned long)kvm_vmx->pid_table, vmx_get_pid_table_order(kvm));
+#else
+	if (kvm_vmx->pid_table) {
+		/*
+		 * No need to clear the pid_table as its contents is following the SDM
+		 * which is not a secret.
+		 */
+		pkvm_hyp_donate_host(__pkvm_pa(kvm_vmx->pid_table),
+				     PAGE_SIZE << vmx_get_pid_table_order(kvm), false);
+	}
 #endif
 }
 

arch/x86/kvm/vmx/main.c
@@ -879,8 +879,10 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.vm_destroy = vt_op(vm_destroy),
 #ifndef __PKVM_HYP__
 	.vm_pre_destroy = vt_op_tdx_only(vm_pre_destroy),
+#endif
 
 	.vcpu_precreate = vt_op(vcpu_precreate),
+#ifndef __PKVM_HYP__
 	.vcpu_create = vt_op(vcpu_create),
 	.vcpu_free = vt_op(vcpu_free),
 	.vcpu_reset = vt_op(vcpu_reset),

intel-staging/pKVM-IA

commit 2fbc1593a176fd8df3969aab34ab01fe31b0486f

pKVM: VMX: Move irqchip_mode & max_vcpu_ids setup to x86-generic code
irqchip_mode and max_vcpu_ids are x86-generic things, not VMX or IPIv
specific (even though currently pKVM hypervisor is only using them in
vmx_alloc_ipiv_pid_table()). So move their setup to
postponed_per_vm_setup() in the generic code.

The immediate motivation for this refactoring is to initialize
max_vcpu_ids unconditionally regardless of IPIv enabled or not, in order
to add a bounds check of vcpu_id against max_vcpu_ids in the next patch.

As a bonus, simplify the code in vmx_alloc_ipiv_pid_table() by moving
locking pkvm->lock outside it, into __vcpu_create() which calls
.vcpu_precreate() which calls vmx_alloc_ipiv_pid_table(). This also
aligns it with the host KVM which calls .vcpu_precreate() with kvm->lock
already locked.

Fixes: 3c6cfa18172f ("pKVM: VMX: Implement vcpu_precreate operation")
Fixes: ccdb8aaef294 ("pKVM: VMX: Fix TOCTOU races on shared_kvm reads in vmx_alloc_ipiv_pid_table()")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -487,20 +487,27 @@ static void pkvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 	}
 }
 
-static void postponed_per_vm_setup(struct kvm *kvm)
+static int postponed_per_vm_setup(struct kvm *kvm)
 {
 	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
-	struct kvm *shared_kvm;
+	struct kvm *shared_kvm = pkvm_vm->shared_kvm;
+	enum kvm_irqchip_mode irqchip_mode;
 	u64 apic_bus_cycle_ns;
+	u32 max_vcpu_ids;
 
-	pkvm_spin_lock(&pkvm_vm->lock);
+	if (pkvm_vm->postponed_setup_done)
+		return 0;
 
-	if (pkvm_vm->postponed_setup_done) {
-		pkvm_spin_unlock(&pkvm_vm->lock);
-		return;
-	}
+	irqchip_mode = READ_ONCE(shared_kvm->arch.irqchip_mode);
+	if (irqchip_mode != KVM_IRQCHIP_NONE &&
+	    irqchip_mode != KVM_IRQCHIP_KERNEL &&
+	    irqchip_mode != KVM_IRQCHIP_SPLIT)
+		return -EINVAL;
 
-	shared_kvm = pkvm_vm->shared_kvm;
+	max_vcpu_ids = READ_ONCE(shared_kvm->arch.max_vcpu_ids);
+	if (!max_vcpu_ids || max_vcpu_ids > KVM_MAX_VCPU_IDS)
+		return -EINVAL;
+
 	/*
 	 * The following setup is per VM, not per vCPU, however it cannot be
 	 * done during VM creation, since these values are set by the host VMM
@@ -510,6 +517,9 @@ static void postponed_per_vm_setup(struct kvm *kvm)
 	 * creating vCPUs. So it is ok to assume these host's values here are
 	 * up-to-date.
 	 */
+
+	kvm->arch.irqchip_mode = irqchip_mode;
+	kvm->arch.max_vcpu_ids = max_vcpu_ids;
 
 	if (kvm_caps.has_bus_lock_exit)
 		kvm->arch.bus_lock_detection_enabled =
@@ -530,25 +540,35 @@ static void postponed_per_vm_setup(struct kvm *kvm)
 	kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
 
 	pkvm_vm->postponed_setup_done = true;
-
-	pkvm_spin_unlock(&pkvm_vm->lock);
+	return 0;
 }
 
 static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate *fps,
 			 struct kvm_lapic *shared_apic)
 {
 	struct pkvm_vcpu *pkvm_vcpu = to_pkvm_vcpu(vcpu);
-	int ret = kvm_x86_call(vcpu_precreate)(kvm);
 	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
 	void *unused = (void *)pkvm_vcpu +
 		       PKVM_VCPU_BASE_SIZE +
 		       kvm_vcpu_sz;
 	int cpu = raw_smp_processor_id();
+	int ret;
 
-	if (ret)
+	pkvm_spin_lock(&pkvm_vm->lock);
+
+	ret = postponed_per_vm_setup(kvm);
+	if (ret) {
+		pkvm_spin_unlock(&pkvm_vm->lock);
 		return ret;
+	}
 
-	postponed_per_vm_setup(kvm);
+	ret = kvm_x86_call(vcpu_precreate)(kvm);
+	if (ret) {
+		pkvm_spin_unlock(&pkvm_vm->lock);
+		return ret;
+	}
+
+	pkvm_spin_unlock(&pkvm_vm->lock);
 
 	vcpu->kvm = kvm;
 	/* Set cpu to -1 to indicate it is not loaded on any CPU */

arch/x86/kvm/vmx/vmx.c
@@ -5021,6 +5021,11 @@ static int vmx_alloc_ipiv_pid_table(struct kvm *kvm)
 {
 #ifndef __PKVM_HYP__
 	struct page *pages;
+#else
+	struct kvm *shared_kvm = to_pkvm(kvm)->shared_kvm;
+	u64 *pid_table;
+	int ret;
+#endif
 	struct kvm_vmx *kvm_vmx = to_kvm_vmx(kvm);
 
 	if (!irqchip_in_kernel(kvm) || !enable_ipiv)
@@ -5029,45 +5034,18 @@ static int vmx_alloc_ipiv_pid_table(struct kvm *kvm)
 	if (kvm_vmx->pid_table)
 		return 0;
 
+#ifndef __PKVM_HYP__
 	pages = alloc_pages(GFP_KERNEL_ACCOUNT | __GFP_ZERO,
 			    vmx_get_pid_table_order(kvm));
 	if (!pages)
 		return -ENOMEM;
 
 	kvm_vmx->pid_table = (void *)page_address(pages);
-	return 0;
 #else
-	struct kvm *shared_kvm = to_pkvm(kvm)->shared_kvm;
-	struct kvm_vmx *kvm_vmx = to_kvm_vmx(kvm);
-	struct kvm_vmx *shared_kvm_vmx;
-	u32 max_vcpu_ids;
-	u64 *pid_table;
-	int ret, mode;
+	pid_table = kern_pkvm_va(READ_ONCE(to_kvm_vmx(shared_kvm)->pid_table));
+	if (!pid_table)
+		return -EINVAL;
 
-	mode = READ_ONCE(shared_kvm->arch.irqchip_mode);
-
-	if (!enable_ipiv || (mode != KVM_IRQCHIP_KERNEL && mode != KVM_IRQCHIP_SPLIT))
-		return 0;
-
-	pkvm_spin_lock(&to_pkvm(kvm)->lock);
-
-	if (kvm_vmx->pid_table) {
-		/* The pid_table is already set. */
-		ret = 0;
-		goto unlock;
-	}
-
-	max_vcpu_ids = READ_ONCE(shared_kvm->arch.max_vcpu_ids);
-	shared_kvm_vmx = to_kvm_vmx(shared_kvm);
-	pid_table = kern_pkvm_va(READ_ONCE(shared_kvm_vmx->pid_table));
-
-	if (max_vcpu_ids == 0 || max_vcpu_ids > KVM_MAX_VCPU_IDS || !pid_table) {
-		ret = -EINVAL;
-		goto unlock;
-	}
-
-	kvm->arch.irqchip_mode = mode;
-	kvm->arch.max_vcpu_ids = max_vcpu_ids;
 	/*
 	 * Although the contents in pid_table is not secret since it is
 	 * constructed following the SDM, still donate the pid_table pages to
@@ -5078,13 +5056,11 @@ static int vmx_alloc_ipiv_pid_table(struct kvm *kvm)
 				   PAGE_SIZE << vmx_get_pid_table_order(kvm),
 				   true);
 	if (ret)
-		goto unlock;
+		return ret;
 
 	kvm_vmx->pid_table = pid_table;
-unlock:
-	pkvm_spin_unlock(&to_pkvm(kvm)->lock);
-	return ret;
 #endif
+	return 0;
 }
 
 int vmx_vcpu_precreate(struct kvm *kvm)