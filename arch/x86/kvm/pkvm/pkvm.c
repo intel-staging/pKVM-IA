cxdong/pub-pKVM-IA

commit 21d14afb789cef4a917034aa1d8df5c764add171

pKVM: x86: Add vcpu_after_set_cpuid PV interface
Add vcpu_after_set_cpuid PV interface for the host to send the CPUID
entries to the pKVM hypervisor, which will be used as the CPUID entries
for the pkvm_vcpu. The CPUID entries memory pages will be donated to
prevent the host from accessing. If the pkvm_vcpu already has a CPUID
entries, the old one's physical addresses and size will be return to the
host via pkvm_memcache.

For a pVM, this PV interface allows the host to set CPUID entries before
the pVM has started running, but doesn't allow the host to set if the
pVM is already running.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -524,6 +524,10 @@ static int __pkvm_vcpu_free(struct pkvm_vm *pkvm_vm, int vcpu_handle,
 
 	fps = pkvm_vcpu->vcpu.arch.guest_fpu.fpstate;
 	teardown_donated_memory(mc, fps, fps->size);
+	if (pkvm_vcpu->vcpu.arch.cpuid_entries)
+		teardown_donated_memory(mc, pkvm_vcpu->vcpu.arch.cpuid_entries,
+					PAGE_ALIGN(sizeof(struct kvm_cpuid_entry2) *
+					pkvm_vcpu->vcpu.arch.cpuid_nent));
 	teardown_donated_memory(mc, pkvm_vcpu, pkvm_vcpu->size);
 
 	pkvm_host_unshare_hyp(shared_vcpu_pa, kvm_vcpu_sz);
@@ -692,6 +696,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__flush_tlb_current:
 	case __pkvm__flush_tlb_gva:
 	case __pkvm__flush_tlb_guest:
+	case __pkvm__vcpu_after_set_cpuid:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -948,6 +953,43 @@ static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
 	kvm_x86_call(sync_pir_to_irr)(vcpu);
 }
 
+static int pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu,
+				     phys_addr_t cpuid_pa,
+				     struct pkvm_memcache *mc)
+{
+	struct kvm_cpuid_entry2 *new, *old;
+	int new_nent, old_nent, ret;
+	u64 size;
+
+	new_nent = to_pkvm_vcpu(vcpu)->shared_vcpu->arch.cpuid_nent;
+	size = PAGE_ALIGN(sizeof(struct kvm_cpuid_entry2) * new_nent);
+	ret = pkvm_host_donate_hyp(cpuid_pa, size, false);
+	if (ret)
+		return ret;
+
+	new = __pkvm_va(cpuid_pa);
+	old = vcpu->arch.cpuid_entries;
+	old_nent = vcpu->arch.cpuid_nent;
+
+	ret = kvm_set_cpuid(vcpu, new, new_nent);
+	if (ret) {
+		pkvm_hyp_donate_host(__pkvm_pa(new), size, false);
+		return ret;
+	}
+
+	memset(mc, 0, sizeof(*mc));
+	/*
+	 * New cpuid entries memory is consumed. Tear down the old cpuid
+	 * entries memory if there is.
+	 */
+	if (old)
+		teardown_donated_memory(mc, (void *)old,
+					PAGE_ALIGN(sizeof(struct kvm_cpuid_entry2) *
+						   old_nent));
+
+	return 0;
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1105,6 +1147,10 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__sync_pir_to_irr:
 		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__vcpu_after_set_cpuid:
+		ret = pkvm_vcpu_after_set_cpuid(vcpu, pkvm_host_gpa_to_phys(pkvm_hc_input1(hvcpu)),
+						&out->vcpu_after_set_cpuid.memcache);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -113,6 +113,9 @@ union pkvm_hc_data {
 		bool data;
 	} get_nmi_mask;
 	struct {
+		struct pkvm_memcache memcache;
+	} vcpu_after_set_cpuid;
+	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];
 	} raw;
 };

arch/x86/include/asm/pkvm_hypercalls.h
@@ -69,6 +69,7 @@ PKVM_HC(refresh_apicv_exec_ctrl)
 PKVM_HC(load_eoi_exitmap)
 PKVM_HC(hwapic_isr_update)
 PKVM_HC(sync_pir_to_irr)
+PKVM_HC_OUT(vcpu_after_set_cpuid)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

intel-staging/pKVM-IA

commit 2377fb532a5481765b7e531d0f9a76849b7753e8

pKVM: x86: Add bounds check for cpuid_nent + fix TOCTOU
When handling the vcpu_after_set_cpuid PV interface, the number of CPUID
entries (cpuid_nent) is provided by the untrusted host, so it should be
validated before using. Otherwise the host may, for example, pass a large
or negative value, causing an integer overflow with unpredicted
consequences.

Although luckily an overflow check for it is already immediately done by
pkvm_host_donate_hyp(), so maybe the lack of a more explicit check is
not causing any security issues for now. Nevertheless, it is a good idea
to add an easy explicit check as well, especially since the only sane
values of cpuid_nent are non-negative values below KVM_MAX_CPUID_ENTRIES.

While we're at it, fix potential TOCTOU by using READ_ONCE when reading
the host's cpuid_nent value, since this value is used in multiple places
within pkvm_vcpu_after_set_cpuid().

Fixes: 21d14afb789c ("pKVM: x86: Add vcpu_after_set_cpuid PV interface")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1303,7 +1303,10 @@ static int pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu,
 	int new_nent, old_nent, ret;
 	u64 size, aligned_size;
 
-	new_nent = to_pkvm_vcpu(vcpu)->shared_vcpu->arch.cpuid_nent;
+	new_nent = READ_ONCE(to_pkvm_vcpu(vcpu)->shared_vcpu->arch.cpuid_nent);
+	if (new_nent < 0 || new_nent > KVM_MAX_CPUID_ENTRIES)
+		return -EINVAL;
+
 	size = sizeof(struct kvm_cpuid_entry2) * new_nent;
 	aligned_size = PAGE_ALIGN(size);
 	ret = pkvm_host_donate_hyp(cpuid_pa, aligned_size, false);