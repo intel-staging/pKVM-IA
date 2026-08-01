cxdong/pub-pKVM-IA

commit 585f279490acc405bde146492a181a20ed2772bd

pKVM: x86: Introduce pkvm_vm handle
Introduce pkvm_vm handle to identify a guest VM instance created by
the pKVM hypervisor. The handle is allocated during VM initialization
from a global pkvm_vms_bitmap, which is protected by a lock to ensure
safe concurrent access. And the handle is returned to the host via the
vm_init PV interface.

For simplicity, pkvm_vms_bitmap is statically defined to support up to
64 guest VMs.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
 #include <asm/fpu/xcr.h>
+#include <asm/pkvm_spinlock.h>
+#include "debug.h"
 #include "init.h"
 #include "lapic.h"
 #include "mem_protect.h"
@@ -27,11 +29,73 @@ struct pkvm_hyp *pkvm_hyp;
 DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
+/* The maximum number of VMs under pkvm. */
+#define MAX_PKVM_VMS		64
+
+static DECLARE_BITMAP(pkvm_vms_bitmap, MAX_PKVM_VMS);
+static DEFINE_PKVM_SPINLOCK(pkvm_vms_lock);
+static struct pkvm_vm_ref {
+	/* Reference counter to indicate if pkvm_vm is in use */
+	atomic_t refcount;
+	/* Point to pkvm_vm in pkvm */
+	struct pkvm_vm *pkvm_vm;
+} pkvm_vms_ref[MAX_PKVM_VMS];
+
 static int pkvm_enable_virtualization_cpu(void)
 {
 	kvm_user_return_msr_cpu_online();
 
 	return kvm_x86_call(enable_virtualization_cpu)();
+}
+
+static int allocate_pkvm_vm_handle(struct pkvm_vm *pkvm_vm)
+{
+	struct pkvm_vm_ref *pkvm_vm_ref;
+	int idx;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	idx = find_first_zero_bit(pkvm_vms_bitmap, MAX_PKVM_VMS);
+	if (idx == MAX_PKVM_VMS) {
+		pkvm_spin_unlock(&pkvm_vms_lock);
+		return -ENOMEM;
+	}
+	__set_bit(idx, pkvm_vms_bitmap);
+
+	pkvm_vm_ref = &pkvm_vms_ref[idx];
+	pkvm_vm_ref->pkvm_vm = pkvm_vm;
+	atomic_set(&pkvm_vm_ref->refcount, 1);
+
+	pkvm_spin_unlock(&pkvm_vms_lock);
+
+	return idx;
+}
+
+static struct pkvm_vm *free_pkvm_vm_handle(int handle)
+{
+	struct pkvm_vm_ref *pkvm_vm_ref;
+	struct pkvm_vm *pkvm_vm = NULL;
+	int idx = handle;
+
+	if (idx < 0 || idx >= MAX_PKVM_VMS)
+		return NULL;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	pkvm_vm_ref = &pkvm_vms_ref[idx];
+	if (atomic_cmpxchg(&pkvm_vm_ref->refcount, 1, 0) != 1) {
+		pkvm_err("VM%d is busy, refcount %d\n", handle,
+			 atomic_read(&pkvm_vm_ref->refcount));
+		goto out;
+	}
+
+	pkvm_vm = pkvm_vm_ref->pkvm_vm;
+	pkvm_vm_ref->pkvm_vm = NULL;
+
+	__clear_bit(idx, pkvm_vms_bitmap);
+out:
+	pkvm_spin_unlock(&pkvm_vms_lock);
+	return pkvm_vm;
 }
 
 static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa)
@@ -71,12 +135,20 @@ static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa)
 					     pkvm_vm->shared_kvm->arch.disabled_quirks) &
 					    kvm_caps.supported_quirks;
 
-	ret = kvm_x86_call(vm_init)(kvm);
-	if (ret)
+	ret = allocate_pkvm_vm_handle(pkvm_vm);
+	if (ret < 0)
 		goto undonate;
 
-	return 0;
+	kvm->arch.pkvm.handle = ret;
 
+	ret = kvm_x86_call(vm_init)(kvm);
+	if (ret)
+		goto free_handle;
+
+	return kvm->arch.pkvm.handle;
+
+free_handle:
+	free_pkvm_vm_handle(kvm->arch.pkvm.handle);
 undonate:
 	pkvm_hyp_donate_host(__pkvm_pa(pkvm_vm), size, false);
 unshare:

arch/x86/include/asm/kvm_host.h
@@ -781,6 +781,12 @@ enum kvm_only_cpuid_leafs {
 	NKVMCAPINTS = NR_KVM_CPU_CAPS - NCAPINTS,
 };
 
+#ifdef CONFIG_PKVM_X86
+struct kvm_pkvm_vm {
+	int handle;
+};
+#endif
+
 struct kvm_vcpu_arch {
 	/*
 	 * rip and regs accesses must go through
@@ -1618,6 +1624,10 @@ struct kvm_arch {
 	 * current VM.
 	 */
 	int cpu_dirty_log_size;
+
+#ifdef CONFIG_PKVM_X86
+	struct kvm_pkvm_vm pkvm;
+#endif
 };
 
 struct kvm_vm_stat {

intel-staging/pKVM-IA

commit 513560f1d79577394f3a25c86d15199047217eb5

pKVM: x86: Fix publish-before-initialize of VM handle
pkvm_put_vm() assumes that as long as a VM's refcount is non-zero, the
VM handle is valid. However, the VM handle is initialized after
initializing its refcount to 1, not before. So if pkvm_put_vm() is
triggered for this VM immediately after its refcount becomes 1 (for
example, via some PV interface handled on another CPU, which succeeds
to pkvm_get_vm() this VM since its refcount is >0 and then tries to
pkvm_put_vm() it), it will use its uninitialized handle value i.e. 0,
thus it will try to put a different VM (with index 0, i.e. the first
VM), leading to unpredictable consequences.

Fix this by moving the VM handle initialization before setting the
refcount to 1, as a part of allocate_pkvm_vm_handle().

Fixes: 585f279490ac ("pKVM: x86: Introduce pkvm_vm handle")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -98,6 +98,7 @@ static int allocate_pkvm_vm_handle(struct pkvm_vm *pkvm_vm)
 	}
 	__set_bit(idx, pkvm_vms_bitmap);
 
+	pkvm_vm->kvm.arch.pkvm.handle = idx;
 	pkvm_vm_ref = &pkvm_vms_ref[idx];
 	pkvm_vm_ref->pkvm_vm = pkvm_vm;
 	atomic_set(&pkvm_vm_ref->refcount, 1);
@@ -184,8 +185,6 @@ static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa,
 	ret = allocate_pkvm_vm_handle(pkvm_vm);
 	if (ret < 0)
 		goto mmu_destroy;
-
-	kvm->arch.pkvm.handle = ret;
 
 	ret = kvm_x86_call(vm_init)(kvm);
 	if (ret)