cxdong/pub-pKVM-IA

commit 4eaeb82d2892d1175bc654454bc63e81efcd5d82

pKVM: x86: Introduce struct pkvm_memcache
Introduce struct pkvm_memcache, which can be used to store a list of
page-aligned physical memory ranges.

The helper push_pkvm_memcache() is to store a memory range's physical
address and size in the pkvm_memcache.

The helper pop_pkvm_memcache() is to retrieve the last stored memory
range's physical address and size from the pkvm_memcache.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/kvm_host.h
@@ -782,10 +782,57 @@ enum kvm_only_cpuid_leafs {
 };
 
 #ifdef CONFIG_PKVM_X86
+struct pkvm_memcache {
+	struct pkvm_page_range {
+		phys_addr_t addr;
+		u64 nr_pages;
+	} head;
+	unsigned long count;
+};
+
+static inline void push_pkvm_memcache(struct pkvm_memcache *mc,
+				      void *addr, size_t size,
+				      phys_addr_t (*to_pa)(void *virt))
+{
+	struct pkvm_page_range *head = addr;
+
+	if (WARN_ON_ONCE(!PAGE_ALIGNED(addr) || !PAGE_ALIGNED(size)))
+		return;
+
+	*head = mc->head;
+	mc->head.addr = to_pa(addr);
+	mc->head.nr_pages = size >> PAGE_SHIFT;
+
+	mc->count++;
+}
+
+static inline struct pkvm_page_range
+pop_pkvm_memcache(struct pkvm_memcache *mc, void *(*to_va)(phys_addr_t phys))
+{
+	struct pkvm_page_range head = { 0 };
+
+	if (!mc->count)
+		return head;
+
+	/*
+	 * The popped memory should be at least one page which contains the
+	 * next head.
+	 */
+	if (WARN_ON_ONCE(!mc->head.nr_pages))
+		return head;
+
+	head = mc->head;
+	mc->head = *(struct pkvm_page_range *)to_va(head.addr);
+
+	mc->count--;
+
+	return head;
+}
+
 struct kvm_pkvm_vm {
 	int handle;
 };
-#endif
+#endif /* CONFIG_PKVM_X86 */
 
 struct kvm_vcpu_arch {
 	/*

intel-staging/pKVM-IA

commit 7ca66056a64d0b91884176c0c5f8adeff949da0c

pKVM: x86: Move memcache helpers to kvm_pkvm.h
pKVM memcache helpers push_pkvm_memcache(), pop_pkvm_memcache() etc are
using WARN_ON_ONCE. At the same time, since kvm_host.h is included by
kvm_pkvm.h (not the other way around), these helpers are defined before
WARN_ON_ONCE is redefined in kvm_pkvm.h, not after. As a result, when
these helpers are used by the pKVM hypervisor, not by the host, they are
erroneously using the original kernel's definition of WARN_ON_ONCE
instead of the pKVM hypervisor's redefined one. As a result, if this
WARN_ON_ONCE is triggered, it unexpectedly triggers ud2 -> hyp panic
even in non-debug mode (CONFIG_PKVM_X86_DEBUG=n) when it is supposed to
do nothing.

Fix this by moving these memcache helpers from kvm_host.h to kvm_pkvm.h,
after the WARN_ON_ONCE redefinition. As a bonus, this helps to trim down
the amount of pKVM-specific stuff in the generic kvm_host.h header.

The pkvm_memcache struct definition still needs to remain in kvm_host.h
since it is used by kvm_pkvm_vm and kvm_pkvm_vcpu structs which are used
by kvm_arch and kvm_vcpu_arch structs.

Fixes: 4eaeb82d2892 ("pKVM: x86: Introduce struct pkvm_memcache")
Fixes: 556dd3a8e183 ("pKVM: x86: Add support for the host to free pkvm_memcache")
Fixes: 9e8a96245218 ("pKVM: x86: Add push/pop_pkvm_memcache_page() helpers")
Fixes: 94c0fbc6ca89 ("pKVM: x86: extend memcache API to support guest MMU")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_host.h
@@ -795,100 +795,6 @@ struct pkvm_memcache {
 	unsigned long flags;
 };
 
-static inline void push_pkvm_memcache(struct pkvm_memcache *mc,
-				      void *addr, size_t size,
-				      phys_addr_t (*to_pa)(void *virt))
-{
-	struct pkvm_page_range *head = addr;
-
-	if (WARN_ON_ONCE(!PAGE_ALIGNED(addr) || !PAGE_ALIGNED(size)))
-		return;
-
-	*head = mc->head;
-	mc->head.addr = to_pa(addr);
-	mc->head.nr_pages = size >> PAGE_SHIFT;
-
-	mc->count++;
-}
-
-static inline struct pkvm_page_range
-pop_pkvm_memcache(struct pkvm_memcache *mc, void *(*to_va)(phys_addr_t phys))
-{
-	struct pkvm_page_range head = { 0 };
-
-	if (!mc->count)
-		return head;
-
-	/*
-	 * The popped memory should be at least one page which contains the
-	 * next head.
-	 */
-	if (WARN_ON_ONCE(!mc->head.nr_pages))
-		return head;
-
-	head = mc->head;
-	mc->head = *(struct pkvm_page_range *)to_va(head.addr);
-
-	mc->count--;
-
-	return head;
-}
-
-static inline void push_pkvm_memcache_page(struct pkvm_memcache *mc,
-					   void *addr,
-					   phys_addr_t (*to_pa)(void *virt))
-{
-	push_pkvm_memcache(mc, addr, PAGE_SIZE, to_pa);
-}
-
-/* For memcaches containing single-page ranges only. */
-static inline void *pop_pkvm_memcache_page(struct pkvm_memcache *mc,
-					   void *(*to_va)(phys_addr_t phys))
-{
-	if (!mc->count)
-		return NULL;
-
-	if (WARN_ON_ONCE(mc->head.nr_pages != 1))
-		return NULL;
-
-	return to_va(pop_pkvm_memcache(mc, to_va).addr);
-}
-
-/* For memcaches containing single-page ranges only. */
-static inline int topup_pkvm_memcache(struct pkvm_memcache *mc,
-				      unsigned long min_pages,
-				      void *(*alloc_pg)(void *arg),
-				      phys_addr_t (*to_pa)(void *virt),
-				      void *arg)
-{
-	while (mc->count < min_pages) {
-		void *p = alloc_pg(arg);
-
-		if (!p)
-			return -ENOMEM;
-
-		push_pkvm_memcache_page(mc, p, to_pa);
-	}
-
-	return 0;
-}
-
-static inline void free_pkvm_memcache(struct pkvm_memcache *mc,
-				      void (*free)(struct pkvm_page_range range,
-						   void *arg),
-				      void *(*to_va)(phys_addr_t phys),
-				      void *arg)
-{
-	while (mc->count)
-		free(pop_pkvm_memcache(mc, to_va), arg);
-}
-
-static inline void init_pkvm_mmu_memcache(struct pkvm_memcache *mc)
-{
-	memset(mc, 0, sizeof(*mc));
-	mc->flags = PKVM_MC_ACCOUNT_PGTABLE_PAGES;
-}
-
 struct pkvm_mapping {
 	struct rb_node node;
 	gfn_t gfn;

arch/x86/include/asm/kvm_pkvm.h
@@ -826,6 +826,100 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 	return PAGE_ALIGN(size);
 }
 
+static inline void push_pkvm_memcache(struct pkvm_memcache *mc,
+				      void *addr, size_t size,
+				      phys_addr_t (*to_pa)(void *virt))
+{
+	struct pkvm_page_range *head = addr;
+
+	if (WARN_ON_ONCE(!PAGE_ALIGNED(addr) || !PAGE_ALIGNED(size)))
+		return;
+
+	*head = mc->head;
+	mc->head.addr = to_pa(addr);
+	mc->head.nr_pages = size >> PAGE_SHIFT;
+
+	mc->count++;
+}
+
+static inline struct pkvm_page_range
+pop_pkvm_memcache(struct pkvm_memcache *mc, void *(*to_va)(phys_addr_t phys))
+{
+	struct pkvm_page_range head = { 0 };
+
+	if (!mc->count)
+		return head;
+
+	/*
+	 * The popped memory should be at least one page which contains the
+	 * next head.
+	 */
+	if (WARN_ON_ONCE(!mc->head.nr_pages))
+		return head;
+
+	head = mc->head;
+	mc->head = *(struct pkvm_page_range *)to_va(head.addr);
+
+	mc->count--;
+
+	return head;
+}
+
+static inline void push_pkvm_memcache_page(struct pkvm_memcache *mc,
+					   void *addr,
+					   phys_addr_t (*to_pa)(void *virt))
+{
+	push_pkvm_memcache(mc, addr, PAGE_SIZE, to_pa);
+}
+
+/* For memcaches containing single-page ranges only. */
+static inline void *pop_pkvm_memcache_page(struct pkvm_memcache *mc,
+					   void *(*to_va)(phys_addr_t phys))
+{
+	if (!mc->count)
+		return NULL;
+
+	if (WARN_ON_ONCE(mc->head.nr_pages != 1))
+		return NULL;
+
+	return to_va(pop_pkvm_memcache(mc, to_va).addr);
+}
+
+/* For memcaches containing single-page ranges only. */
+static inline int topup_pkvm_memcache(struct pkvm_memcache *mc,
+				      unsigned long min_pages,
+				      void *(*alloc_pg)(void *arg),
+				      phys_addr_t (*to_pa)(void *virt),
+				      void *arg)
+{
+	while (mc->count < min_pages) {
+		void *p = alloc_pg(arg);
+
+		if (!p)
+			return -ENOMEM;
+
+		push_pkvm_memcache_page(mc, p, to_pa);
+	}
+
+	return 0;
+}
+
+static inline void free_pkvm_memcache(struct pkvm_memcache *mc,
+				      void (*free)(struct pkvm_page_range range,
+						   void *arg),
+				      void *(*to_va)(phys_addr_t phys),
+				      void *arg)
+{
+	while (mc->count)
+		free(pop_pkvm_memcache(mc, to_va), arg);
+}
+
+static inline void init_pkvm_mmu_memcache(struct pkvm_memcache *mc)
+{
+	memset(mc, 0, sizeof(*mc));
+	mc->flags = PKVM_MC_ACCOUNT_PGTABLE_PAGES;
+}
+
 #else /* !CONFIG_PKVM_X86 */
 
 static inline bool pkvm_is_protected_vm(struct kvm *kvm) { return false; }