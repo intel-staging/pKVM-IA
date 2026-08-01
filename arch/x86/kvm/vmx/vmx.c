cxdong/pub-pKVM-IA

commit 16d18acaaa993ad36cee84ca7ca02b651ce29041

pKVM: VMX: Implement cancel injection operation
Import vmx_cancle_injection() from KVM to implement the cancel_injection
PV interface for pKVM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -7473,6 +7473,7 @@ static void vmx_recover_nmi_blocking(struct vcpu_vmx *vmx)
 			ktime_to_ns(ktime_sub(ktime_get(),
 					      vmx->loaded_vmcs->entry_time));
 }
+#endif /* !__PKVM_HYP__ */
 
 static void __vmx_complete_interrupts(struct kvm_vcpu *vcpu,
 				      u32 idt_vectoring_info,
@@ -7532,12 +7533,14 @@ static void __vmx_complete_interrupts(struct kvm_vcpu *vcpu,
 	}
 }
 
+#ifndef __PKVM_HYP__
 static void vmx_complete_interrupts(struct vcpu_vmx *vmx)
 {
 	__vmx_complete_interrupts(&vmx->vcpu, vmx->idt_vectoring_info,
 				  VM_EXIT_INSTRUCTION_LEN,
 				  IDT_VECTORING_ERROR_CODE);
 }
+#endif /* !__PKVM_HYP__ */
 
 void vmx_cancel_injection(struct kvm_vcpu *vcpu)
 {
@@ -7549,6 +7552,7 @@ void vmx_cancel_injection(struct kvm_vcpu *vcpu)
 	vmcs_write32(VM_ENTRY_INTR_INFO_FIELD, 0);
 }
 
+#ifndef __PKVM_HYP__
 static void atomic_switch_perf_msrs(struct vcpu_vmx *vmx)
 {
 	int i, nr_msrs;

arch/x86/kvm/vmx/main.c
@@ -945,9 +945,7 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.inject_irq = vt_op(inject_irq),
 	.inject_nmi = vt_op(inject_nmi),
 	.inject_exception = vt_op(inject_exception),
-#ifndef __PKVM_HYP__
 	.cancel_injection = vt_op(cancel_injection),
-#endif
 	.interrupt_allowed = vt_op(interrupt_allowed),
 	.nmi_allowed = vt_op(nmi_allowed),
 	.get_nmi_mask = vt_op(get_nmi_mask),

arch/x86/kvm/x86.c
@@ -936,6 +936,7 @@ static void kvm_queue_exception_e_p(struct kvm_vcpu *vcpu, unsigned nr,
 {
 	kvm_multiple_exception(vcpu, nr, true, error_code, true, payload);
 }
+#endif /* !__PKVM_HYP__ */
 
 void kvm_requeue_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 			   bool has_error_code, u32 error_code)
@@ -966,6 +967,7 @@ void kvm_requeue_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_requeue_exception);
 
+#ifndef __PKVM_HYP__
 int kvm_complete_insn_gp(struct kvm_vcpu *vcpu, int err)
 {
 	if (err)

intel-staging/pKVM-IA

commit ac3a2abc63b6af0d402ef98bf9b1b863ab03243e

pKVM: VMX: Prevent double-call bypass of software interrupt cancel guard
A malicious host can call the cancel_injection hypercall twice to cancel
a pVM's soft interrupts or exceptions, as the second call finds the VMCS
field already zeroed, and unconditionally clear previous canceled
events.

Fix this by adding an early return in vmx_cancel_injection() for
pVM when the VMCS VM_ENTRY_INTR_INFO_FIELD has no valid entry (valid bit
not set). If there is nothing to cancel in the VMCS, skip the entire
operation to avoid clearing previously-preserved state.

[ Dmytro: add comment. ]

Fixes: 16d18acaaa99 ("pKVM: VMX: Implement cancel injection operation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/vmx/vmx.c
@@ -7909,6 +7909,19 @@ static void vmx_complete_interrupts(struct vcpu_vmx *vmx)
 
 void vmx_cancel_injection(struct kvm_vcpu *vcpu)
 {
+#ifdef __PKVM_HYP__
+	/*
+	 * A malicious host may request cancel_injection twice to cancel
+	 * a pVM's soft interrupts or exceptions, as the second call finds
+	 * VM_ENTRY_INTR_INFO_FIELD already zeroed and unconditionally clears
+	 * previous canceled events. Prevent this by returning early if
+	 * VM_ENTRY_INTR_INFO_FIELD is already zeroed and thus there is
+	 * nothing to do.
+	 */
+	if (pkvm_is_protected_vcpu(vcpu) &&
+	    !(vmcs_read32(VM_ENTRY_INTR_INFO_FIELD) & VECTORING_INFO_VALID_MASK))
+		return;
+#endif
 	__vmx_complete_interrupts(vcpu,
 				  vmcs_read32(VM_ENTRY_INTR_INFO_FIELD),
 				  VM_ENTRY_INSTRUCTION_LEN,