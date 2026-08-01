cxdong/pub-pKVM-IA

commit e729e31e292f4918ab14a1bca453cf517297fab2

KVM: x86/mmu: pKVM: Track guest mappings in an interval tree
Similarly to pKVM-ARM, let the host remember mapped guest pages in an
interval tree, to use this information afterwards in various cases (e.g.
for detecting spurious page faults, or for unmapping mapped pages within
the given range when handling MMU invalidate notifiers).

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_host.h
@@ -28,6 +28,7 @@
 #include <linux/sched/vhost_task.h>
 #include <linux/call_once.h>
 #include <linux/atomic.h>
+#include <linux/interval_tree_generic.h>
 
 #include <asm/apic.h>
 #include <asm/pvclock-abi.h>
@@ -886,11 +887,44 @@ static inline void init_pkvm_mmu_memcache(struct pkvm_memcache *mc)
 	mc->flags = PKVM_MC_ACCOUNT_PGTABLE_PAGES;
 }
 
+struct pkvm_mapping {
+	struct rb_node node;
+	gfn_t gfn;
+	kvm_pfn_t pfn;
+	gfn_t nr_pages;
+	gfn_t __subtree_last;	/* Internal member for interval tree */
+};
+
+void pkvm_mapping_insert(struct pkvm_mapping *node,
+			 struct rb_root_cached *root);
+void pkvm_mapping_remove(struct pkvm_mapping *node,
+			 struct rb_root_cached *root);
+struct pkvm_mapping *pkvm_mapping_iter_first(struct rb_root_cached *root,
+					     gfn_t start, gfn_t last);
+struct pkvm_mapping *pkvm_mapping_iter_next(struct pkvm_mapping *node,
+					    gfn_t start, gfn_t last);
+
+/*
+ * Iterates the interval tree safely, allowing removing __map node from it
+ * while iterating.
+ * Caution: __start and __end are evaluated multiple times.
+ */
+#define for_each_pkvm_mapping(__kvm, __start, __end, __map)					\
+	for (struct pkvm_mapping *__tmp = pkvm_mapping_iter_first(&(__kvm)->arch.pkvm.mappings,	\
+								  (__start), (__end) - 1);	\
+	     __tmp && ({									\
+				__map = __tmp;							\
+				__tmp = pkvm_mapping_iter_next(__map, (__start), (__end) - 1);	\
+				true;								\
+		       });									\
+	    )
+
 #define PKVM_HOST_VM_HANDLE	INT_MAX
 
 struct kvm_pkvm_vm {
 	int handle;
 	struct pkvm_memcache guest_mmu_teardown_mc;
+	struct rb_root_cached mappings;
 };
 
 struct kvm_pkvm_vcpu {

arch/x86/kvm/mmu/mmu.c
@@ -4927,8 +4927,22 @@ static unsigned long pkvm_mmu_cache_min_pages(void)
 	return kvm_mmu_get_max_tdp_level() - 1;
 }
 
+static gfn_t __pkvm_mapping_start(struct pkvm_mapping *m)
+{
+	return m->gfn;
+}
+
+static gfn_t __pkvm_mapping_end(struct pkvm_mapping *m)
+{
+	return m->gfn + m->nr_pages - 1;
+}
+
+INTERVAL_TREE_DEFINE(struct pkvm_mapping, node, gfn_t, __subtree_last,
+		     __pkvm_mapping_start, __pkvm_mapping_end, , pkvm_mapping)
+
 static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
+	struct pkvm_mapping *mapping;
 	gfn_t base_gfn;
 	gfn_t nr_pages;
 	int r;
@@ -4954,6 +4968,11 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	if (r)
 		return r;
 
+	/* Allocate non-atomically before taking mmu_lock. */
+	mapping = kzalloc(sizeof(struct pkvm_mapping), GFP_KERNEL_ACCOUNT);
+	if (!mapping)
+		return -ENOMEM;
+
 	r = RET_PF_RETRY;
 	write_lock(&vcpu->kvm->mmu_lock);
 
@@ -4973,9 +4992,17 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 
 	r = RET_PF_FIXED;
 
+	mapping->gfn = base_gfn;
+	mapping->pfn = fault->pfn;
+	mapping->nr_pages = nr_pages;
+	pkvm_mapping_insert(mapping, &vcpu->kvm->arch.pkvm.mappings);
+
 out_unlock:
 	kvm_mmu_finish_page_fault(vcpu, fault, r);
 	write_unlock(&vcpu->kvm->mmu_lock);
+
+	if (r != RET_PF_FIXED)
+		kfree(mapping);
 	return r;
 }
 #endif /* CONFIG_PKVM_X86 */
@@ -6837,6 +6864,11 @@ static void kvm_mmu_zap_all_fast(struct kvm *kvm)
 int kvm_mmu_init_vm(struct kvm *kvm)
 {
 	int r, i;
+
+#ifdef CONFIG_PKVM_X86
+	if (enable_pkvm)
+		kvm->arch.pkvm.mappings = RB_ROOT_CACHED;
+#endif
 
 	kvm->arch.shadow_mmio_value = shadow_mmio_value;
 	INIT_LIST_HEAD(&kvm->arch.active_mmu_pages);

arch/x86/kvm/vmx/pkvm_host.c
@@ -701,6 +701,7 @@ free_page:
 static void pkvm_vm_destroy(struct kvm *kvm)
 {
 	int vm_handle = kvm->arch.pkvm.handle;
+	struct pkvm_mapping *mapping;
 	union pkvm_hc_data out;
 	int ret;
 
@@ -712,6 +713,17 @@ static void pkvm_vm_destroy(struct kvm *kvm)
 
 	kvm_free_pkvm_memcache(&out.vm_destroy.memcache);
 	kvm_free_pkvm_memcache(&kvm->arch.pkvm.guest_mmu_teardown_mc);
+
+	/*
+	 * TODO: do this in the generic code in kvm_mmu_uninit_vm() instead.
+	 * For now we cannot do that, since kvm_mmu_uninit_vm() is called too
+	 * early, when pKVM has not released guest pages to the host yet
+	 * (which is currently completely postponed until vm_destroy).
+	 */
+	for_each_pkvm_mapping(kvm, 0, U64_MAX, mapping) {
+		pkvm_mapping_remove(mapping, &kvm->arch.pkvm.mappings);
+		kfree(mapping);
+	}
 
 	vmx_vm_destroy(kvm);
 }

intel-staging/pKVM-IA

commit bf591556061130b4cdb42056d1f1ec4271df38f0

pKVM: x86: Move pkvm_mapping definitions to kvm_pkvm.h
The definition of struct pkvm_mapping as well as the associated
functions and macros do not need to be defined in kvm_host.h since
(unlike struct pkvm_memcache) they are not used by any other definitions
in kvm_host.h. So move them to kvm_pkvm.h. This does not fix any known
issues but it helps to reduce the amount of pKVM-specific stuff in the
generic kvm_host.h header.

Fixes: e729e31e292f ("KVM: x86/mmu: pKVM: Track guest mappings in an interval tree")
Fixes: 8d2c5f3c5f2e ("KVM: x86/mmu: pKVM: Pin pVM pages")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_host.h
@@ -795,40 +795,6 @@ struct pkvm_memcache {
 	unsigned long flags;
 };
 
-struct pkvm_mapping {
-	struct rb_node node;
-	gfn_t gfn;
-	kvm_pfn_t pfn;
-	gfn_t nr_pages;
-	gfn_t __subtree_last;	/* Internal member for interval tree */
-
-	struct page *pinned_page;
-};
-
-void pkvm_mapping_insert(struct pkvm_mapping *node,
-			 struct rb_root_cached *root);
-void pkvm_mapping_remove(struct pkvm_mapping *node,
-			 struct rb_root_cached *root);
-struct pkvm_mapping *pkvm_mapping_iter_first(struct rb_root_cached *root,
-					     gfn_t start, gfn_t last);
-struct pkvm_mapping *pkvm_mapping_iter_next(struct pkvm_mapping *node,
-					    gfn_t start, gfn_t last);
-
-/*
- * Iterates the interval tree safely, allowing removing __map node from it
- * while iterating.
- * Caution: __start and __end are evaluated multiple times.
- */
-#define for_each_pkvm_mapping(__kvm, __start, __end, __map)					\
-	for (struct pkvm_mapping *__tmp = pkvm_mapping_iter_first(&(__kvm)->arch.pkvm.mappings,	\
-								  (__start), (__end) - 1);	\
-	     __tmp && ({									\
-				__map = __tmp;							\
-				__tmp = pkvm_mapping_iter_next(__map, (__start), (__end) - 1);	\
-				true;								\
-		       });									\
-	    )
-
 #define PKVM_HOST_VM_HANDLE	INT_MAX
 
 struct kvm_pkvm_vm {

arch/x86/include/asm/kvm_pkvm.h
@@ -920,6 +920,40 @@ static inline void init_pkvm_mmu_memcache(struct pkvm_memcache *mc)
 	mc->flags = PKVM_MC_ACCOUNT_PGTABLE_PAGES;
 }
 
+struct pkvm_mapping {
+	struct rb_node node;
+	gfn_t gfn;
+	kvm_pfn_t pfn;
+	gfn_t nr_pages;
+	gfn_t __subtree_last;	/* Internal member for interval tree */
+
+	struct page *pinned_page;
+};
+
+void pkvm_mapping_insert(struct pkvm_mapping *node,
+			 struct rb_root_cached *root);
+void pkvm_mapping_remove(struct pkvm_mapping *node,
+			 struct rb_root_cached *root);
+struct pkvm_mapping *pkvm_mapping_iter_first(struct rb_root_cached *root,
+					     gfn_t start, gfn_t last);
+struct pkvm_mapping *pkvm_mapping_iter_next(struct pkvm_mapping *node,
+					    gfn_t start, gfn_t last);
+
+/*
+ * Iterates the interval tree safely, allowing removing __map node from it
+ * while iterating.
+ * Caution: __start and __end are evaluated multiple times.
+ */
+#define for_each_pkvm_mapping(__kvm, __start, __end, __map)					\
+	for (struct pkvm_mapping *__tmp = pkvm_mapping_iter_first(&(__kvm)->arch.pkvm.mappings,	\
+								  (__start), (__end) - 1);	\
+	     __tmp && ({									\
+				__map = __tmp;							\
+				__tmp = pkvm_mapping_iter_next(__map, (__start), (__end) - 1);	\
+				true;								\
+		       });									\
+	    )
+
 #else /* !CONFIG_PKVM_X86 */
 
 static inline bool pkvm_is_protected_vm(struct kvm *kvm) { return false; }