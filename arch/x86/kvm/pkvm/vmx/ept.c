cxdong/pub-pKVM-IA

commit f818dbd77dad9316df2d8564b99d6faf0b436ac7

pKVM: VMX: Flush cpu cache on EPT updates if its required by IOMMU
If any IOMMU on the system does not have cpu cache snoop capability,
flush cpu cache on EPT update as host EPT is used as second stage
pagetable for passthrough devices.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -162,6 +162,24 @@ static void ept_pte_set(void *ptep, u64 spte)
 	WRITE_ONCE(*(u64 *)ptep, spte);
 }
 
+static void host_ept_pte_set(void *ptep, u64 spte)
+{
+	ept_pte_set(ptep, spte);
+
+	/*
+	 * TODO: no need to flush cache if none of the
+	 * devices behind non-coherent IOMMUs are configured
+	 * for passthrough mode.
+	 */
+	if (!pkvm_iommu_paging_structure_coherency())
+		clflush_cache_range(ptep, sizeof(u64));
+}
+
+static void guest_ept_pte_set(void *ptep, u64 spte)
+{
+	ept_pte_set(ptep, spte);
+}
+
 static u64 ept_pte_get(void *ptep)
 {
 	return READ_ONCE(*(u64 *)ptep);
@@ -243,7 +261,7 @@ static const struct pkvm_pgtable_ops host_ept_pgt_ops = {
 	.pte_is_leaf = ept_pte_is_leaf,
 	.pte_size = ept_pte_size,
 	.pte_count = ept_pte_count,
-	.pte_set = ept_pte_set,
+	.pte_set = host_ept_pte_set,
 	.pte_get = ept_pte_get,
 	.flush_tlb = host_ept_flush_tlb,
 	.pte_mk_pgstate = ept_pte_mk_pgstate,
@@ -268,7 +286,7 @@ static const struct pkvm_pgtable_ops guest_ept_pgt_ops = {
 	.pte_is_leaf = ept_pte_is_leaf,
 	.pte_size = ept_pte_size,
 	.pte_count = ept_pte_count,
-	.pte_set = ept_pte_set,
+	.pte_set = guest_ept_pte_set,
 	.pte_get = ept_pte_get,
 	.flush_tlb = guest_ept_flush_tlb,
 	.pte_mk_pgstate = ept_pte_mk_pgstate,

intel-staging/pKVM-IA

commit 1cc6dbe8caf4540015efc189a8533ffad1908eb4

pKVM: VMX: Flush CPU cache on EPT page table allocation for non-coherent IOMMUs
When IOMMU hardware does not support paging structure coherency, the
CPU cache must be flushed for newly allocated EPT page table pages.
Otherwise, the IOMMU reads stale data from memory when walking the
EPT, causing translation failures for pass-through devices.

Add clflush_cache_range() in host_ept_zalloc_page() when
pkvm_iommu_paging_structure_coherency() indicates the IOMMU requires
explicit cache maintenance.

Fixes: f818dbd77dad ("pKVM: VMX: Flush cpu cache on EPT updates if its required by IOMMU")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -36,7 +36,17 @@ int pkvm_host_ept_level(void)
 
 static void *host_ept_zalloc_page(struct pkvm_memcache *mc)
 {
-	return pkvm_alloc_pages(&host_ept_pool, 0);
+	void *page = pkvm_alloc_pages(&host_ept_pool, 0);
+
+	/*
+	 * TODO: no need to flush cache if none of the
+	 * devices behind non-coherent IOMMUs are configured
+	 * for passthrough mode.
+	 */
+	if (page && !pkvm_iommu_paging_structure_coherency())
+		clflush_cache_range(page, PAGE_SIZE);
+
+	return page;
 }
 
 static void host_ept_get_page(void *vaddr)