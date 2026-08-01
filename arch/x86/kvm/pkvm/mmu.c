cxdong/pub-pKVM-IA

commit 8406c78378be06553da91a7c1125a6fad7939c32

pKVM: x86: Implement copying pvmfw into pVM memory
While most of the initial contents of pVM memory is prepared by the
host, pvmfw is an exception: it must be loaded into pVM memory by the
pKVM hypervisor, not by the host which is untrusted and has no access
to the pvmfw memory.

So implement loading pvmfw into pVM memory, similarly to pKVM-ARM: when
donating a page which overlaps with the pvmfw address region in the
guest memory (specified previously by the host), copy the corresponding
portion of the pvmfw image (resident in the hypervisor's reserved
memory) into this newly donated guest page.

This ensures the pVM starts with the correct firmware code in memory.

[ Grzegorz: align to 6.18: s/kvm_protected_vm/kvm_pkvm_vm/; update the
  commit log ]

Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -704,6 +704,31 @@ static int __guest_unshare_host(unsigned long gpa, unsigned long hpa,
 	return pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot, NULL);
 }
 
+static bool gpa_range_overlaps_pvmfw(struct kvm *kvm,
+				     unsigned long gpa, unsigned long size,
+				     unsigned long *gpa_offset,
+				     unsigned long *pvmfw_offset,
+				     unsigned long *ovlp_size)
+{
+	struct kvm_pkvm_vm *pkvm = &kvm->arch.pkvm;
+	unsigned long start, end;
+
+	if (!pkvm_vm_has_pvmfw(kvm))
+		return false;
+
+	/* intersection between [gpa, gpa + size) and pvmfw region */
+	start = max(gpa, pkvm->pvmfw_load_addr);
+	end = min(gpa + size, pkvm->pvmfw_load_addr + pvmfw_size);
+
+	if (end <= start)
+		return false;
+
+	*gpa_offset = start - gpa;
+	*pvmfw_offset = start - pkvm->pvmfw_load_addr;
+	*ovlp_size = end - start;
+	return true;
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -1236,6 +1261,7 @@ int pkvm_host_donate_guest(struct kvm_vcpu *vcpu, unsigned long gpa,
 {
 	u64 prot = guest_mmu_pte_prot(vcpu, gpa, true, PKVM_PAGE_OWNED);
 	struct pkvm_vm *pkvm_vm = to_pkvm_vcpu(vcpu)->pkvm_vm;
+	unsigned long gpa_offset, pvmfw_offset, load_size;
 	int ret;
 
 	if (!PAGE_ALIGNED(gpa) || !PAGE_ALIGNED(hpa) ||
@@ -1265,6 +1291,26 @@ int pkvm_host_donate_guest(struct kvm_vcpu *vcpu, unsigned long gpa,
 	BUG_ON(ret);
 
 	set_host_mem_pgstate(hpa, size, PKVM_PAGE_NONE);
+
+	if (gpa_range_overlaps_pvmfw(&pkvm_vm->kvm, gpa, size, &gpa_offset,
+				     &pvmfw_offset, &load_size)) {
+		/*
+		 * Make sure pvmfw is loaded into the guest memory pages after
+		 * enabling protection of these pages from the host, not before.
+		 */
+		smp_wmb();
+
+		memcpy(__pkvm_va(hpa + gpa_offset),
+		       __pkvm_va(pvmfw_base + pvmfw_offset),
+		       load_size);
+
+		/*
+		 * Make sure pvmfw is loaded into the guest memory pages before
+		 * mapping these pages for the guest, not after, to prevent the
+		 * guest from seeing old contents of these pages on another CPU.
+		 */
+		smp_wmb();
+	}
 
 	ret = pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot,
 			       &vcpu->arch.pkvm.guest_mmu_memcache);

intel-staging/pKVM-IA

commit 559dd05a75fda1960b4c5300959ab71af05ac63f

pKVM: x86: Ensure pVM is finalized when donating pages
When donating a host page to a pVM, pKVM needs to be sure if the pVM's
pvmfw_load_addr is already set to its final value or not, so that pKVM
knows if it needs to copy pvmfw into the donated page. Otherwise,
malicious host may load arbitrary contents of a pvmfw page, by donating
this page before starting running the pVM. In particular, it may cause
the pVM to start running arbitrary code instead of the pvmfw code at its
entry point. And this arbitrary code may get access to pvmfw secrets, by
touching _other_ pvmfw pages and thus triggering donating them when
pvmfw_load_addr is already known.

So forbid donating pages if the VM is not finalized yet, i.e. if
pvmfw_load_addr is not known yet.

Fixes: 8406c78378be ("pKVM: x86: Implement copying pvmfw into pVM memory")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -1395,6 +1395,10 @@ int pkvm_host_donate_guest(struct kvm_vcpu *vcpu, unsigned long gpa,
 	if (WARN_ON_ONCE(!pkvm_is_protected_vcpu(vcpu)))
 		return -EPERM;
 
+	/* If the VM is not finalized yet, we don't know if we need to load pvmfw. */
+	if (!smp_load_acquire(&pkvm_vm->kvm.arch.pkvm.finalized))
+		return -EPERM;
+
 	pkvm_host_mmu_lock();
 	pkvm_guest_mmu_lock(pkvm_vm);
 

arch/x86/kvm/pkvm/pkvm.c
@@ -414,7 +414,8 @@ static int pkvm_vm_finalize(int vm_handle)
 		}
 	}
 
-	kvm->arch.pkvm.finalized = true;
+	/* Pairs with smp_load_acquire() in pkvm_host_donate_guest(). */
+	smp_store_release(&kvm->arch.pkvm.finalized, true);
 	shared_kvm->arch.pkvm.finalized = true;
 unlock:
 	pkvm_spin_unlock(&pkvm_vm->lock);