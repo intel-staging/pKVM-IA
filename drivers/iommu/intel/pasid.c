cxdong/pub-pKVM-IA

commit 55fa9d0e1751812916d1e4645f9e6ca09e81523f

iommu/vt-d: pKVM: Scalable mode context update hypercalls
Implement hypercalls for setting and clearing scalable mode context
table entries and host driver changes to use the hypercall.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/pasid.c
@@ -931,6 +931,15 @@ static void device_pasid_table_teardown(struct device *dev, u8 bus, u8 devfn)
 	struct context_entry *context;
 	u16 did;
 
+	if (pkvm_enabled()) {
+		int ret = pkvm_context_clear(iommu->reg_phys, bus, devfn, info);
+
+		if (ret)
+			pr_err("%s: iommu%d: pkvm_context_clear failed(err=%d)\n",
+			       __func__, iommu->seq_id, ret);
+		return;
+	}
+
 	spin_lock(&iommu->lock);
 	context = iommu_context_addr(iommu, bus, devfn, false);
 	if (!context) {
@@ -1026,6 +1035,17 @@ int device_pasid_table_setup(struct pkvm_device *dev, u8 bus, u8 devfn)
 	struct device_domain_info *info = dev_iommu_priv_get(dev);
 	struct intel_iommu *iommu = info->iommu;
 	struct context_entry *context;
+
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		int ret = pkvm_pasid_table_setup(iommu, info, bus, devfn);
+
+		if (ret)
+			pr_err("iommu%d: pkvm_pasid_table_setup failed(err=%d)\n",
+			       iommu->seq_id, ret);
+		return ret;
+	}
+#endif
 
 	spin_lock(&iommu->lock);
 	context = iommu_context_addr(iommu, bus, devfn, true);

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -11,6 +11,7 @@
 #include "pkvm/pkvm.h"
 #include "pkvm/debug.h"
 #include "../iommu.h"
+#include "../pasid.h"
 
 int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options)
 {
@@ -148,6 +149,79 @@ static int iommu_set_lm_ce(struct set_lm_ce_data *data)
 int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out)
 {
 	int ret = iommu_set_lm_ce(in);
+
+	*out = *in;
+	return ret;
+}
+
+/*
+ * Size of pasid directory in bytes, given the max pasid number
+ * A pasid directory entry can address 64 pasids and a pasid
+ * directory page holds 512 entries, hence one pasid dir page can
+ * address (64 * 512) entries.
+ * So pasid_dir_size = (max_pasid / (64 * 512)) * PAGE_SIZE
+ *                   => = (max_pasid >> 15) << PAGE_SHIFT
+ */
+#define pasid_dir_size(max_pasid) ((max_pasid) >> (15 - PAGE_SHIFT))
+
+static int iommu_set_sm_ce(struct set_sm_ce_data *data)
+{
+	struct intel_iommu *iommu = iommu_from_phys(data->phys);
+	u16 bdf = PCI_DEVID(data->bus, data->devfn);
+	struct device_domain_info info = { 0 };
+	struct pasid_table table = { 0 };
+	struct pkvm_device dev = { .info = &info };
+	int ret;
+
+	if (!iommu)
+		return -EINVAL;
+
+	if (data->ats_qdep > PCI_ATS_MAX_QDEP)
+		return -EINVAL;
+
+	if ((data->ats_supported || data->ats_enabled) &&
+	    !is_dev_in_satc(bdf))
+		return -EPERM;
+
+	info.bus = data->bus;
+	info.devfn = data->devfn;
+	info.ats_qdep = data->ats_qdep;
+	info.ats_supported = data->ats_supported;
+	info.ats_enabled = data->ats_enabled;
+	info.pasid_supported = data->pasid_supported;
+	info.pasid_enabled = data->pasid_enabled;
+	table.table = pkvm_host_gpa_to_virt(data->pasid_table_gpa);
+	table.max_pasid = data->max_pasid;
+	info.pasid_table = &table;
+	info.iommu = iommu;
+
+	ret = accept_page_donation(iommu, &data->donation_page_gpa);
+	if (ret)
+		return ret;
+
+	ret = pkvm_host_donate_hyp_share_ro(pkvm_host_gpa_to_phys(data->pasid_table_gpa),
+					    pasid_dir_size(data->max_pasid), true);
+	if (ret) {
+		pkvm_err("failed to write protect pasid dir for dev[%x:%x](err=%d)\n",
+			 data->bus, data->devfn, ret);
+		return ret;
+	}
+
+	__iommu_flush_cache(iommu, table.table, pasid_dir_size(data->max_pasid));
+
+	pkvm_dbg("%s: dev[%x:%x], ats_qdep: %d, pasid_table_gpa: %llx\n", __func__,
+		 data->bus, data->devfn, info.ats_qdep, data->pasid_table_gpa);
+	ret = device_pasid_table_setup(&dev, data->bus, data->devfn);
+
+	if (ret)
+		pkvm_hyp_donate_host(pkvm_host_gpa_to_phys(data->pasid_table_gpa),
+				     pasid_dir_size(data->max_pasid), false);
+	return ret;
+}
+
+int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *in, struct set_sm_ce_data *out)
+{
+	int ret = iommu_set_sm_ce(in);
 
 	*out = *in;
 	return ret;

arch/x86/include/asm/kvm_pkvm.h
@@ -73,6 +73,20 @@ struct set_lm_ce_data {
 	u8 ats_enabled: 1;
 	u8 ats_supported: 1;
 };
+
+struct set_sm_ce_data {
+	u64 phys;
+	u64 pasid_table_gpa;
+	u64 donation_page_gpa;
+	u32 max_pasid;
+	u8 bus;
+	u8 devfn;
+	u8 ats_qdep;
+	u8 ats_enabled: 1;
+	u8 ats_supported: 1;
+	u8 pasid_supported: 3;
+	u8 pasid_enabled: 1;
+};
 #endif
 
 #define TO_PKVM_HC(f)		CONCATENATE(__pkvm__, f)
@@ -160,6 +174,10 @@ union pkvm_hc_data {
 		struct set_lm_ce_data in;
 		struct set_lm_ce_data out;
 	} iommu_set_lm_ce;
+	union {
+		struct set_sm_ce_data in;
+		struct set_sm_ce_data out;
+	} iommu_set_sm_ce;
 #endif
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];

arch/x86/include/asm/pkvm_hypercalls.h
@@ -96,6 +96,7 @@ PKVM_HC(iommu_mmio_write)
 PKVM_HC(iommu_qi_submit)
 PKVM_HC_IN(iommu_clear_ce)
 PKVM_HC_INOUT(iommu_set_lm_ce)
+PKVM_HC_INOUT(iommu_set_sm_ce)
 #endif
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -1904,6 +1904,10 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = pkvm_iommu_set_lm_ce(&in.iommu_set_lm_ce.in,
 					   &out.iommu_set_lm_ce.out);
 		break;
+	case __pkvm__iommu_set_sm_ce:
+		ret = pkvm_iommu_set_sm_ce(&in.iommu_set_sm_ce.in,
+					   &out.iommu_set_sm_ce.out);
+		break;
 #endif
 	default:
 		ret = pkvm_vcpu_handle_host_hypercall(vcpu, hc, &in, &out);

drivers/iommu/intel/iommu.c
@@ -1745,12 +1745,31 @@ __domain_mapping(struct dmar_domain *domain, unsigned long iov_pfn,
 
 	return 0;
 }
+#else /* __PKVM_HYP__ */
+
+static void pasid_free_table(struct pasid_dir_entry *dir, int max_pde)
+{
+	for (int i = 0; i < max_pde; i++) {
+		struct pasid_entry *table = get_pasid_table_from_pde(&dir[i]);
+
+		if (!table)
+			continue;
+
+		pkvm_hyp_donate_host(__pkvm_pa(table), VTD_PAGE_SIZE, false);
+	}
+	pkvm_hyp_donate_host(__pkvm_pa(dir), ALIGN(max_pde * 8, VTD_PAGE_SIZE), false);
+}
 #endif /* !__PKVM_HYP__ */
 
 void domain_context_clear_one(struct device_domain_info *info, u8 bus, u8 devfn)
 {
 	struct intel_iommu *iommu = info->iommu;
 	struct context_entry *context;
+#ifdef __PKVM_HYP__
+	struct pasid_dir_entry *pasid_dir;
+	bool sm = sm_supported(iommu);
+	u64 pasid_dir_sz;
+#endif
 	u16 did;
 
 #ifndef __PKVM_HYP__
@@ -1771,11 +1790,23 @@ void domain_context_clear_one(struct device_domain_info *info, u8 bus, u8 devfn)
 		return;
 	}
 
+#ifdef __PKVM_HYP__
+	if (sm) {
+		pasid_dir = __pkvm_va(context->lo & VTD_PAGE_MASK);
+		pasid_dir_sz = get_pasid_dir_size(context);
+	}
+#endif
+
 	did = context_domain_id(context);
 	context_clear_entry(context);
 	__iommu_flush_cache(iommu, context, sizeof(*context));
 	spin_unlock(&iommu->lock);
 	intel_context_flush_no_pasid(info, context, did);
+
+#ifdef __PKVM_HYP__
+	if (sm)
+		pasid_free_table(pasid_dir, pasid_dir_sz);
+#endif
 }
 
 #ifndef __PKVM_HYP__

drivers/iommu/intel/pkvm_iommu.c
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include "iommu.h"
+#include "pasid.h"
 #include "../iommu-pages.h"
 
 int __init pkvm_host_prepare_iommu(void)
@@ -179,6 +180,46 @@ int pkvm_context_mapping(struct intel_iommu *iommu, struct device_domain_info *i
 		 * If the hypervisor used donation_gpa, it will be set to 0.
 		 * Free the page if hypervisor didn't use the page.
 		 */
+		if (data->donation_page_gpa)
+			iommu_free_pages(phys_to_virt(data->donation_page_gpa));
+	}
+	spin_unlock(&iommu->lock);
+
+	return ret;
+}
+
+int pkvm_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info *info,
+			   u8 bus, u8 devfn)
+{
+	union pkvm_hc_data d = { 0 };
+	struct set_sm_ce_data *data = &d.iommu_set_sm_ce.in;
+	int ret;
+
+	data->phys = iommu->reg_phys;
+	data->pasid_table_gpa = virt_to_phys(info->pasid_table->table);
+	data->max_pasid = info->pasid_table->max_pasid;
+	data->bus = bus;
+	data->devfn = devfn;
+	data->pasid_supported = info->pasid_supported;
+	data->pasid_enabled = info->pasid_enabled;
+	data->ats_supported = info->ats_supported;
+	data->ats_enabled = info->ats_enabled;
+	data->ats_qdep = info->ats_qdep;
+
+	spin_lock(&iommu->lock);
+	ret = pkvm_hypercall_inout(iommu_set_sm_ce, &d, &d);
+	if (ret == -ENOMEM) {
+		void *donation_page = iommu_alloc_pages_node_sz(iommu->node,
+								GFP_ATOMIC, SZ_4K);
+
+		if (!donation_page) {
+			pr_err("iommu%d: failed to allocate context page\n", iommu->seq_id);
+			spin_unlock(&iommu->lock);
+			return -ENOMEM;
+		}
+		data->donation_page_gpa = virt_to_phys(donation_page);
+		ret = pkvm_hypercall_inout(iommu_set_sm_ce, &d, &d);
+
 		if (data->donation_page_gpa)
 			iommu_free_pages(phys_to_virt(data->donation_page_gpa));
 	}

drivers/iommu/intel/pkvm_iommu.h
@@ -117,6 +117,8 @@ int pkvm_qi_submit_sync(struct intel_iommu *iommu, struct qi_desc *desc,
 int pkvm_context_clear(u64 phys, u8 bus, u8 devfn, struct device_domain_info *info);
 int pkvm_context_mapping(struct intel_iommu *iommu, struct device_domain_info *info,
 			 u8 bus, u8 devfn, u64 pgd_gpa, u16 did);
+int pkvm_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info *info,
+			   u8 bus, u8 devfn);
 #else /* __PKVM_HYP__ */
 /*
  * dev_iommu_priv_get is called from quite a few places in code re-used by
@@ -168,6 +170,7 @@ int pkvm_iommu_mmio_write(u64 phys, int len, u64 val);
 int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options);
 int pkvm_iommu_clear_ce(struct clear_ce_data *data);
 int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *in, struct set_lm_ce_data *out);
+int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *in, struct set_sm_ce_data *out);
 #endif /* !__PKVM_HYP__ */
 #else /* !CONFIG_PKVM_INTEL */
 static inline int pkvm_qi_submit_sync(struct intel_iommu *iommu,
@@ -184,6 +187,12 @@ static inline int pkvm_context_clear(u64 phys, u8 bus, u8 devfn,
 static inline int pkvm_context_mapping(struct intel_iommu *iommu,
 				       struct device_domain_info *info,
 				       u8 bus, u8 devfn, u64 pgd_gpa, u16 did)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_pasid_table_setup(struct intel_iommu *iommu,
+					 struct device_domain_info *info,
+					 u8 bus, u8 devfn)
 {
 	return -EOPNOTSUPP;
 }

intel-staging/pKVM-IA

commit a68d04ca43779e743328326b23620b6f523575e7

iommu/vt-d: Reject PASID table setup on present CE in hypervisor
Return -EEXIST from device_pasid_table_setup() under __PKVM_HYP__ when a
context entry for the target device (bus, devfn) is already present.

In hypervisor mode, iommu_set_sm_ce() donates the host PASID directory
range before calling device_pasid_table_setup(). When a context entry
is already present, device_pasid_table_setup() previously returned
success (0) without attaching the newly donated table. Because ret == 0,
iommu_set_sm_ce() skipped its error un-donate path, permanently leaking
the host pages into hypervisor ownership.

Returning -EEXIST in hypervisor mode triggers the un-donate error path in
iommu_set_sm_ce(), returning ownership of the donated pages to host.

Fixes: 55fa9d0e1751 ("iommu/vt-d: pKVM: Scalable mode context update hypercalls")
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/pasid.c
@@ -1054,7 +1054,11 @@ int device_pasid_table_setup(struct pkvm_device *dev, u8 bus, u8 devfn)
 
 	if (context_present(context) && !context_copied(iommu, bus, devfn)) {
 		spin_unlock(&iommu->lock);
+#ifndef __PKVM_HYP__
 		return 0;
+#else
+		return -EEXIST;
+#endif
 	}
 
 #ifndef __PKVM_HYP__

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -212,9 +212,16 @@ static int iommu_set_sm_ce(struct set_sm_ce_data *data)
 		 data->bus, data->devfn, info.ats_qdep, data->pasid_table_gpa);
 	ret = device_pasid_table_setup(&dev, data->bus, data->devfn);
 
-	if (ret)
+	if (ret) {
 		pkvm_hyp_donate_host(pkvm_host_gpa_to_phys(data->pasid_table_gpa),
 				     pasid_dir_size(data->max_pasid), false);
+		/*
+		 * If entry already exists, the host expects
+		 * the hypercall to succeed.
+		 */
+		if (ret == -EEXIST)
+			ret = 0;
+	}
 	return ret;
 }
 