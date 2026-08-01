cxdong/pub-pKVM-IA

commit addc9b4d505a8d5043f7e6700c4e828e61030199

pKVM: x86 Add cancel injection PV interface
Add cancel injection PV interface for the host to cancel injection for
an irq/nmi/exception via vendor specific operation. As pVM's exception
is emulated by the pKVM, the PV interface doesn't allow to cancel
exception injection for pVM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -660,6 +660,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__get_nmi_mask:
 	case __pkvm__inject_irq:
 	case __pkvm__inject_nmi:
+	case __pkvm__cancel_injection:
 		/*
 		 * The host is responsible for running vCPU, injecting
 		 * interrupts, emulating lapic etc. Always allow the related PV
@@ -874,6 +875,33 @@ static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
 	kvm_x86_call(inject_exception)(vcpu);
 }
 
+static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
+{
+	struct pkvm_vcpu *pkvm_vcpu = to_pkvm_vcpu(vcpu);
+	struct kvm_vcpu *shared_vcpu;
+
+	kvm_x86_call(cancel_injection)(vcpu);
+
+	shared_vcpu = pkvm_vcpu->shared_vcpu;
+	if (vcpu->arch.nmi_injected) {
+		shared_vcpu->arch.nmi_injected = true;
+		vcpu->arch.nmi_injected = false;
+	} else if (vcpu->arch.interrupt.injected) {
+		kvm_queue_interrupt(shared_vcpu, vcpu->arch.interrupt.nr,
+				    vcpu->arch.interrupt.soft);
+		kvm_clear_interrupt_queue(vcpu);
+	} else if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
+		/*
+		 * For the pVM, the exception can only be injected and canceled
+		 * by the pkvm hypervisor.
+		 * For the npVM, the exception can be injected and canceled by
+		 * both sides.
+		 */
+		shared_vcpu->arch.exception = vcpu->arch.exception;
+		kvm_clear_exception_queue(vcpu);
+	}
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1008,6 +1036,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__inject_exception:
 		pkvm_inject_exception(vcpu);
+		break;
+	case __pkvm__cancel_injection:
+		pkvm_cancel_injection(vcpu);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -62,6 +62,7 @@ PKVM_HC(set_nmi_mask)
 PKVM_HC(inject_irq)
 PKVM_HC(inject_nmi)
 PKVM_HC(inject_exception)
+PKVM_HC(cancel_injection)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

intel-staging/pKVM-IA

commit 06bf23840983fdee9d4a5327174c6922ba79c578

pKVM: x86: Improve comments in pkvm_cancel_injection()
Improve the comments in pkvm_cancel_injection() to state that the
canceled pVM software interrupt or exception should not be handed over
to the host.

[ Dmytro: move out of the next (VMX-specific) commit to separate one. ]

Fixes: addc9b4d505a ("pKVM: x86 Add cancel injection PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1260,7 +1260,8 @@ static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
 		 * is not allowed to inject the pVM's software interrupt, and
 		 * the pending pVM's software interrupt (exits during delivering
 		 * a software interrupt) should be injected by the pKVM, thus
-		 * the host cannot cancel such injection.
+		 * the canceled software interrupt should not be handed over to
+		 * the host.
 		 */
 		if (!pkvm_is_protected_vcpu(vcpu) || !vcpu->arch.interrupt.soft) {
 			kvm_queue_interrupt(shared_vcpu, vcpu->arch.interrupt.nr,
@@ -1269,10 +1270,10 @@ static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
 		}
 	} else if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
 		/*
-		 * For the pVM, the exception can only be injected and canceled
-		 * by the pkvm hypervisor.
-		 * For the npVM, the exception can be injected and canceled by
-		 * both sides.
+		 * For the pVM, the exception can only be injected by the pKVM
+		 * thus the canceled exception should not be handed over to the
+		 * host.
+		 * For the npVM, the exception can be injected by both sides.
 		 */
 		shared_vcpu->arch.exception = vcpu->arch.exception;
 		kvm_clear_exception_queue(vcpu);