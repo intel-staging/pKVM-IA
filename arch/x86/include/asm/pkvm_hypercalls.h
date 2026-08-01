cxdong/pub-pKVM-IA

commit fab4b63a8dadc2abac84419848a0e69aa573c231

pKVM: x86: Add enable_virtualization_cpu PV interface
Add enable_virtualization_cpu PV interface for the host to enable
virtualization features for running guest VMs on a physical CPU via the
vendor-specific operation. The percpu kvm uret MSRs which were added
during the hardware setup are initialized as this is also a one time
enabling.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_hypercalls.h
@@ -14,5 +14,6 @@ PKVM_HC(dump_vmexit_trace)
 
 /* KVM ops */
 PKVM_HC(check_processor_compatibility)
+PKVM_HC(enable_virtualization_cpu)
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -26,6 +26,13 @@ struct pkvm_hyp *pkvm_hyp;
 DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
+static int pkvm_enable_virtualization_cpu(void)
+{
+	kvm_user_return_msr_cpu_online();
+
+	return kvm_x86_call(enable_virtualization_cpu)();
+}
+
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 {
 	int ret = 0;
@@ -50,6 +57,9 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		break;
 	case __pkvm__check_processor_compatibility:
 		ret = kvm_x86_call(check_processor_compatibility)();
+		break;
+	case __pkvm__enable_virtualization_cpu:
+		ret = pkvm_enable_virtualization_cpu();
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/kvm/x86.c
@@ -227,7 +227,10 @@ struct kvm_user_return_msrs {
 u32 __read_mostly kvm_nr_uret_msrs;
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_nr_uret_msrs);
 static u32 __read_mostly kvm_uret_msrs_list[KVM_MAX_NR_USER_RETURN_MSRS];
-#ifndef __PKVM_HYP__
+#ifdef __PKVM_HYP__
+static DEFINE_PER_CPU(struct kvm_user_return_msrs, __user_return_msrs);
+static struct kvm_user_return_msrs __percpu *user_return_msrs = &__user_return_msrs;
+#else
 static struct kvm_user_return_msrs __percpu *user_return_msrs;
 #endif /* !__PKVM_HYP__ */
 
@@ -646,8 +649,7 @@ int kvm_find_user_return_msr(u32 msr)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_find_user_return_msr);
 
-#ifndef __PKVM_HYP__
-static void kvm_user_return_msr_cpu_online(void)
+void kvm_user_return_msr_cpu_online(void)
 {
 	struct kvm_user_return_msrs *msrs = this_cpu_ptr(user_return_msrs);
 	u64 value;
@@ -660,6 +662,7 @@ static void kvm_user_return_msr_cpu_online(void)
 	}
 }
 
+#ifndef __PKVM_HYP__
 static void kvm_user_return_register_notifier(struct kvm_user_return_msrs *msrs)
 {
 	if (!msrs->registered) {

arch/x86/kvm/x86.h
@@ -726,6 +726,7 @@ int ____kvm_emulate_hypercall(struct kvm_vcpu *vcpu, int cpl,
 })
 
 int kvm_emulate_hypercall(struct kvm_vcpu *vcpu);
+void kvm_user_return_msr_cpu_online(void);
 
 #define CET_US_RESERVED_BITS		GENMASK(9, 6)
 #define CET_US_SHSTK_MASK_BITS		GENMASK(1, 0)

intel-staging/pKVM-IA

commit 95c0d5e74f69982145eded9a9669e05af0138722

pKVM: x86: Initialize user-return MSR caches during pkvm_init
Move the invocation of kvm_user_return_msr_cpu_online() from the runtime
enable_virtualization_cpu hypercall into pkvm_init().

By initializing user-return MSR caches (.host and .curr) during
pkvm_init(), the hypervisor's .curr cache is seeded with untampered
values of those MSRs registers.

This ensures that .curr is already valid when host KVM (pKVM-high) later
executes boot-time probing (kvm_probe_user_return_msr()), allowing
intercepted MSR writes to be validated against the hardware boot values.

Furthermore, moving this initialization into pkvm_init() eliminates the
need for the __pkvm__enable_virtualization_cpu hypercall entirely,
removing any possibility of an untrusted host re-invoking initialization
hypercalls at runtime to re-seed .host and .curr.

Fixes: fab4b63a8dad ("pKVM: x86: Add enable_virtualization_cpu PV interface")

Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/include/asm/pkvm_hypercalls.h
@@ -26,7 +26,6 @@ PKVM_HC(dump_vmexit_trace)
 
 /* KVM ops */
 PKVM_HC(check_processor_compatibility)
-PKVM_HC(enable_virtualization_cpu)
 PKVM_HC(vm_init)
 PKVM_HC(vm_finalize)
 PKVM_HC_OUT(vm_destroy)

arch/x86/kvm/pkvm/init.c
@@ -3,6 +3,7 @@
 #include <asm/fpu/xstate.h>
 #include <asm/kvm_pkvm.h>
 #include "../cpuid.h"
+#include "../x86.h"
 #include "early_alloc.h"
 #include "fpu.h"
 #include "init.h"
@@ -300,6 +301,8 @@ int pkvm_init(struct pkvm_mem_info infos[], int nr_infos)
 		return ret;
 
 	pkvm_vcpu_perf_init(this_cpu_read(host_vcpu));
+
+	kvm_user_return_msr_cpu_online();
 
 	this_cpu_write(cpu_initialized, true);
 	return 0;

arch/x86/kvm/pkvm/pkvm.c
@@ -77,13 +77,6 @@ static struct pkvm_x86_ops pkvm_x86_ops __read_mostly;
 static int __pkvm_vcpu_free(struct pkvm_vm *pkvm_vm, int vcpu_handle,
 			    struct pkvm_memcache *mc);
 
-static int pkvm_enable_virtualization_cpu(void)
-{
-	kvm_user_return_msr_cpu_online();
-
-	return kvm_x86_call(enable_virtualization_cpu)();
-}
-
 static int allocate_pkvm_vm_handle(struct pkvm_vm *pkvm_vm)
 {
 	struct pkvm_vm_ref *pkvm_vm_ref;
@@ -2038,9 +2031,6 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		break;
 	case __pkvm__check_processor_compatibility:
 		ret = kvm_x86_call(check_processor_compatibility)();
-		break;
-	case __pkvm__enable_virtualization_cpu:
-		ret = pkvm_enable_virtualization_cpu();
 		break;
 	case __pkvm__vm_init:
 		ret = pkvm_vm_init(pkvm_host_gpa_to_phys(pkvm_hc_input1(vcpu)),

arch/x86/kvm/vmx/pkvm_host.c
@@ -605,7 +605,12 @@ static int pkvm_check_processor_compat(void)
 
 static int pkvm_enable_virtualization_cpu(void)
 {
-	return pkvm_hypercall(enable_virtualization_cpu);
+	/*
+	 * There is nothing to do here as virtualization was already enabled
+	 * during pKVM initialization and is never disabled or re-enabled later.
+	 */
+
+	return 0;
 }
 
 static void pkvm_disable_virtualization_cpu(void)