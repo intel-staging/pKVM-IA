cxdong/pub-pKVM-IA

commit 119a4e6a827348239304081cd1b58a05ab2cfd24

iommu/vt-d: pKVM: Hypercalls for domain pagetable management
Implement hypercalls for mapping and unmapping of IOVA addresses in the
IOMMU page tables.

When pKVM enabled, use hypercalls for iommu map and unmap instead of
directly updating page tables as they are write protected by pKVM.

Security Note:
The host IOMMU driver typically optimizes performance by delaying IOTLB
cache flushes on unmap. However, this violates pKVM security guarantees:
pages freed by an unmap might be immediately donated to another VM.
Stale IOTLB entries could allow a device to access sensitive data in
the newly repurposed pages. Consequently, this implementation enforces
unconditional IOTLB flushing on every unmap operation.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/iommu.c
@@ -3860,6 +3860,11 @@ static int intel_iommu_map(struct iommu_domain *domain,
 	/* Round up size to next multiple of PAGE_SIZE, if it and
 	   the low bits of hpa would take us onto the next page */
 	size = aligned_nrpages(hpa, size);
+
+	if (pkvm_enabled())
+		return pkvm_domain_map(dmar_domain, iova >> VTD_PAGE_SHIFT,
+				       hpa >> VTD_PAGE_SHIFT, size, prot, gfp);
+
 	return domain_map(dmar_domain, iova >> VTD_PAGE_SHIFT,
 			  hpa >> VTD_PAGE_SHIFT, size, prot, gfp);
 }
@@ -3905,6 +3910,15 @@ static size_t intel_iommu_unmap(struct iommu_domain *domain,
 
 	start_pfn = iova >> VTD_PAGE_SHIFT;
 	last_pfn = (iova + size - 1) >> VTD_PAGE_SHIFT;
+
+	if (pkvm_enabled()) {
+		int ret = pkvm_domain_unmap(dmar_domain, start_pfn, last_pfn);
+
+		if (ret)
+			pr_err("%s: domain unmap IOVA[start: %lx, end: %lx] failed (err=%d)\n",
+			       __func__, start_pfn, last_pfn, ret);
+		return size;
+	}
 
 	domain_unmap(dmar_domain, start_pfn, last_pfn, &gather->freelist);
 

arch/x86/include/asm/kvm_pkvm.h
@@ -138,6 +138,15 @@ struct alloc_domain_data {
 	u8 iommu_coherency;
 	u8 use_first_level;
 };
+
+struct domain_map_data {
+	u64 pgd_gpa;
+	u64 iov_pfn;
+	u64 phys_pfn;
+	u64 nr_pages;
+	u64 prot;
+	struct pkvm_memcache mc;
+};
 #endif
 
 #define TO_PKVM_HC(f)		CONCATENATE(__pkvm__, f)
@@ -246,6 +255,10 @@ union pkvm_hc_data {
 	struct {
 		struct pkvm_memcache memcache;
 	} iommu_free_domain;
+	union {
+		struct domain_map_data in;
+		struct domain_map_data out;
+	} iommu_domain_map;
 #endif
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];

arch/x86/include/asm/pkvm_hypercalls.h
@@ -102,6 +102,8 @@ PKVM_HC_INOUT(iommu_pasid_setup_sl)
 PKVM_HC_IN(iommu_pasid_teardown)
 PKVM_HC_IN(iommu_alloc_domain)
 PKVM_HC_OUT(iommu_free_domain)
+PKVM_HC_INOUT(iommu_domain_map)
+PKVM_HC(iommu_domain_unmap)
 #endif
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -1926,6 +1926,15 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = pkvm_iommu_free_domain(pkvm_hc_input1(vcpu),
 					     &out.iommu_free_domain.memcache);
 		break;
+	case __pkvm__iommu_domain_map:
+		ret = pkvm_iommu_domain_map(&in.iommu_domain_map.in,
+					    &out.iommu_domain_map.out);
+		break;
+	case __pkvm__iommu_domain_unmap:
+		ret = pkvm_iommu_domain_unmap(pkvm_hc_input1(vcpu),
+					      pkvm_hc_input2(vcpu),
+					      pkvm_hc_input3(vcpu));
+		break;
 #endif
 	default:
 		ret = pkvm_vcpu_handle_host_hypercall(vcpu, hc, &in, &out);

drivers/iommu/intel/pkvm/domain.c
@@ -119,6 +119,42 @@ void pkvm_put_domain_cache_tag_unassign(void *pgd, int did, u32 pasid,
 	pkvm_put_iommu_domain(domain);
 }
 
+static void *admit_host_domain_page(void *arg)
+{
+	struct pkvm_memcache *host_mc = (struct pkvm_memcache *)arg;
+	void *page;
+
+	page = pop_pkvm_memcache_page(host_mc, pkvm_host_gpa_to_virt);
+	if (!page)
+		return NULL;
+
+	if (WARN_ON(pkvm_host_donate_hyp_share_ro(__pkvm_pa(page), VTD_PAGE_SIZE, true))) {
+		push_pkvm_memcache_page(host_mc, page, pkvm_virt_to_host_gpa);
+		return NULL;
+	}
+
+	return page;
+}
+
+static int refill_domain_memcache(struct dmar_domain *domain,
+				  struct pkvm_memcache *host_mc)
+{
+	struct pkvm_memcache *mc = &domain->mc;
+	unsigned long min_pages;
+
+	/*
+	 * The host expects pKVM to drain the memcache fully and use it until
+	 * the domain is freed, as the host itself doesn't store the memcache
+	 * in any persistent data structure. This will not result in the
+	 * hypervisor's memcache growing endlessly, since the host only
+	 * provides a memcache if the hypervisor's memcache doesn't already
+	 * have enough pages.
+	 */
+	min_pages = mc->count + host_mc->count;
+	return topup_pkvm_memcache(mc, min_pages, admit_host_domain_page,
+				   pkvm_virt_to_phys, host_mc);
+}
+
 static void free_domain_memcache(struct dmar_domain *domain,
 				 struct pkvm_memcache *teardown_mc)
 {
@@ -201,4 +237,81 @@ struct dmar_domain *pkvm_alloc_iommu_domain(struct alloc_domain_data *data)
 	pkvm_spin_unlock(&iommu_domain_lock);
 
 	return domain;
+}
+
+static int iommu_domain_map(struct domain_map_data *data)
+{
+	struct dmar_domain *domain;
+	u64 iova, phys, size, end;
+	int ret;
+
+	/* Check for possible overfows that may have security implications */
+	if (check_shl_overflow(data->iov_pfn, VTD_PAGE_SHIFT, &iova))
+		return -EINVAL;
+	if (check_shl_overflow(data->phys_pfn, VTD_PAGE_SHIFT, &phys))
+		return -EINVAL;
+	if (check_mul_overflow(data->nr_pages, VTD_PAGE_SIZE, &size))
+		return -EINVAL;
+	if (check_add_overflow(iova, size, &end))
+		return -EINVAL;
+	if (check_add_overflow(phys, size, &end))
+		return -EINVAL;
+
+	domain = pkvm_get_iommu_domain(pkvm_host_gpa_to_virt(data->pgd_gpa));
+	if (!domain) {
+		pkvm_err("%s: failed to get the domain [pgd:%llx]\n",
+			 __func__, data->pgd_gpa);
+		return -EINVAL;
+	}
+
+	pkvm_spin_lock(&domain->lock);
+	if (data->mc.count) {
+		ret = refill_domain_memcache(domain, &data->mc);
+		if (ret) {
+			pkvm_err("%s: failed to refill memcache for domain[pgd: %p] (err=%d)\n",
+				 __func__, domain->pgd, ret);
+			goto out_unlock;
+		}
+	}
+	if (domain->mc.count < __pkvm_pgtable_max_pages(data->nr_pages)) {
+		ret = -ENOMEM;
+		goto out_unlock;
+	}
+
+	ret = domain_map(domain, data->iov_pfn, data->phys_pfn,
+			 data->nr_pages, data->prot, 0);
+
+out_unlock:
+	pkvm_spin_unlock(&domain->lock);
+	pkvm_put_iommu_domain(domain);
+
+	return ret;
+}
+
+int pkvm_iommu_domain_map(struct domain_map_data *in, struct domain_map_data *out)
+{
+	int ret = iommu_domain_map(in);
+
+	*out = *in;
+	return ret;
+}
+
+int pkvm_iommu_domain_unmap(u64 pgd_gpa, u64 start_pfn, u64 last_pfn)
+{
+	struct dmar_domain *domain;
+
+	domain = pkvm_get_iommu_domain(pkvm_host_gpa_to_virt(pgd_gpa));
+	if (!domain) {
+		pkvm_err("%s, failed to get the domain [pgd:%llx]\n",
+			 __func__, pgd_gpa);
+		return -EINVAL;
+	}
+
+	pkvm_spin_lock(&domain->lock);
+	domain_unmap(domain, start_pfn, last_pfn, NULL);
+	pkvm_spin_unlock(&domain->lock);
+
+	pkvm_put_iommu_domain(domain);
+
+	return 0;
 }

drivers/iommu/intel/pkvm_iommu.c
@@ -350,9 +350,26 @@ int pkvm_alloc_domain(struct device_domain_info *info, struct dmar_domain *domai
 	return ret;
 }
 
+static phys_addr_t host_pa(void *addr)
+{
+	return __pa(addr);
+}
+
 static void *host_va(phys_addr_t phys)
 {
 	return __va(phys);
+}
+
+struct mc_alloc_arg {
+	int nid;
+	gfp_t gfp;
+};
+
+static void *host_mc_alloc_page(void *alloc_arg)
+{
+	struct mc_alloc_arg *arg = (struct mc_alloc_arg *)alloc_arg;
+
+	return iommu_alloc_pages_node_sz(arg->nid, arg->gfp, SZ_4K);
 }
 
 static void free_domain_memcache(struct pkvm_memcache *mc)
@@ -368,5 +385,60 @@ int pkvm_free_domain(struct dmar_domain *domain)
 
 	ret = pkvm_hypercall_out(iommu_free_domain, &out, virt_to_phys(domain->pgd));
 	free_domain_memcache(&out.iommu_free_domain.memcache);
+	return ret;
+}
+
+int pkvm_domain_map(struct dmar_domain *domain, unsigned long iov_pfn,
+		    unsigned long phys_pfn, unsigned long nr_pages,
+		    int prot, int gfp)
+{
+	union pkvm_hc_data d = { 0 };
+	struct domain_map_data *data = &d.iommu_domain_map.in;
+	int ret;
+
+	data->pgd_gpa = virt_to_phys(domain->pgd),
+	data->iov_pfn = iov_pfn,
+	data->phys_pfn = phys_pfn,
+	data->nr_pages = nr_pages,
+	data->prot = prot,
+
+	ret = pkvm_hypercall_inout(iommu_domain_map, &d, &d);
+	if (ret == -ENOMEM) {
+		struct mc_alloc_arg arg = {
+			.nid = domain->nid,
+			.gfp = gfp,
+		};
+
+		ret = topup_pkvm_memcache(&data->mc, __pkvm_pgtable_max_pages(nr_pages),
+					  host_mc_alloc_page, host_pa, &arg);
+		if (!ret)
+			ret = pkvm_hypercall_inout(iommu_domain_map, &d, &d);
+		else
+			pr_err("%s: memcache topup failed(err=%d)\n", __func__, ret);
+	}
+
+	if (ret) {
+		pr_err("%s: domain map[iov_pfn: %lx, pfn: %lx, nr_pages: %lu] failed (err=%d)\n",
+		       __func__, iov_pfn, phys_pfn, nr_pages, ret);
+
+		/*
+		 * pKVM would not have drained the memcache on
+		 * hypercall failure. Free it if not empty.
+		 */
+		free_domain_memcache(&data->mc);
+	}
+
+	domain->has_mappings = true;
+	return ret;
+}
+
+int pkvm_domain_unmap(struct dmar_domain *domain, unsigned long start_pfn, unsigned long last_pfn)
+{
+	int ret = pkvm_hypercall(iommu_domain_unmap, virt_to_phys(domain->pgd),
+				 start_pfn, last_pfn);
+
+	if (ret)
+		pr_err("%s: domain unmap[start_pfn: %lx, last_pfn: %lx] failed (err=%d)\n",
+		       __func__, start_pfn, last_pfn, ret);
 	return ret;
 }

drivers/iommu/intel/pkvm_iommu.h
@@ -127,6 +127,11 @@ int pkvm_pasid_setup_sl(struct device_domain_info *info, phys_addr_t ssptptr,
 int pkvm_pasid_teardown(struct device_domain_info *info, u32 pasid);
 int pkvm_alloc_domain(struct device_domain_info *info, struct dmar_domain *domain);
 int pkvm_free_domain(struct dmar_domain *domain);
+int pkvm_domain_map(struct dmar_domain *domain, unsigned long iov_pfn,
+		    unsigned long phys_pfn, unsigned long nr_pages,
+		    int prot, int gfp);
+int pkvm_domain_unmap(struct dmar_domain *domain, unsigned long start_pfn,
+		      unsigned long last_pfn);
 #else /* __PKVM_HYP__ */
 /*
  * dev_iommu_priv_get is called from quite a few places in code re-used by
@@ -202,6 +207,8 @@ int pkvm_iommu_pasid_setup_sl(struct pasid_setup_sl_data *in, struct pasid_setup
 int pkvm_iommu_pasid_teardown(struct pasid_teardown_data *data);
 int pkvm_iommu_alloc_domain(struct alloc_domain_data *data);
 int pkvm_iommu_free_domain(u64 pgd_gpa, struct pkvm_memcache *mc);
+int pkvm_iommu_domain_map(struct domain_map_data *in, struct domain_map_data *out);
+int pkvm_iommu_domain_unmap(u64 pgd_gpa, u64 start_pfn, u64 last_pfn);
 #endif /* !__PKVM_HYP__ */
 #else /* !CONFIG_PKVM_INTEL */
 static inline int pkvm_qi_submit_sync(struct intel_iommu *iommu,
@@ -249,6 +256,17 @@ static inline int pkvm_alloc_domain(struct device_domain_info *info,
 	return -EOPNOTSUPP;
 }
 static inline int pkvm_free_domain(struct dmar_domain *domain)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_domain_map(struct dmar_domain *domain, unsigned long iov_pfn,
+				  unsigned long phys_pfn, unsigned long nr_pages,
+				  int prot, int gfp)
+{
+	return -EOPNOTSUPP;
+}
+static inline int pkvm_domain_unmap(struct dmar_domain *domain, unsigned long start_pfn,
+				    unsigned long last_pfn)
 {
 	return -EOPNOTSUPP;
 }

intel-staging/pKVM-IA

commit a9d10c01525dcbaa43d4fd52f9492134cb8fc68b

iommu/vt-d: Fix walk collision in pfn_to_dma_pte
Mitigate a security vulnerability where the page table walker could be
tricked into treating a superpage leaf entry as a directory page. This
allowed a compromised host to construct fake page tables and bypass
IOMMU isolation.

This condition is triggered when the host incorrectly (or maliciously)
requests mapping an IOVA range that overlaps with an already mapped
superpage. When attempting to map the new, overlapping range at a
deeper page table level, the walker encounters the existing superpage
leaf entry and previously attempted to recurse through it as a directory.

To fix this:
- In pfn_to_dma_pte(), check if we encounter a superpage entry before
  reaching the target walk level. If so, return ERR_PTR(-EEXIST) to
  block the walk and prevent the invalid traversal.
- Refactor pfn_to_dma_pte() to return ERR_PTR instead of NULL on
  failure to allow propagating specific error codes (like -EEXIST,
  -ENOMEM, and -EINVAL) to callers.
- In domain_map(), propagate the -EEXIST error code back to the host.
  This is critical for pKVM security and stability: returning -ENOMEM
  (the old default) would signal the host to donate more pages and
  retry the mapping, leading to an infinite hypercall loop (DoS) since
  the conflict cannot be resolved by adding memory.
- Update all other callers of pfn_to_dma_pte() to use IS_ERR() and
  handle the error codes appropriately.

Note on partial mappings:
Because the Intel IOMMU driver does not update the 'mapped' output
parameter on error to report partial success, any pages mapped during
domain_map() before encountering the -EEXIST collision will not be
rolled back by the host. While this is an inconsistency limitation of
the Intel driver, it does not affect hypervisor security as the
malicious mapping is still blocked and the leaked mappings are
confined to the guest's own domain.

Fixes: 119a4e6a8273 ("iommu/vt-d: pKVM: Hypercalls for domain pagetable management")

drivers/iommu/intel/iommu.c
@@ -747,7 +747,7 @@ static struct dma_pte *pfn_to_dma_pte(struct dmar_domain *domain,
 
 	if (!domain_pfn_supported(domain, pfn))
 		/* Address beyond IOMMU's addressing capabilities. */
-		return NULL;
+		return ERR_PTR(-EINVAL);
 
 	parent = domain->pgd;
 
@@ -769,11 +769,11 @@ static struct dma_pte *pfn_to_dma_pte(struct dmar_domain *domain,
 							     SZ_4K);
 
 			if (!tmp_page)
-				return NULL;
+				return ERR_PTR(-ENOMEM);
 #else
 			tmp_page = pop_pkvm_memcache_page(&domain->mc, pkvm_phys_to_virt);
 			if (!tmp_page)
-				return NULL;
+				return ERR_PTR(-ENOMEM);
 			memset(tmp_page, 0, VTD_PAGE_SIZE);
 #endif
 
@@ -798,7 +798,10 @@ static struct dma_pte *pfn_to_dma_pte(struct dmar_domain *domain,
 			}
 			else
 				domain_flush_cache(domain, pte, sizeof(*pte));
+		} else if (WARN_ON_ONCE(dma_pte_superpage(pte))) {
+			return ERR_PTR(-EEXIST);
 		}
+
 		if (level == 1)
 			break;
 
@@ -1883,8 +1886,8 @@ int domain_map(struct dmar_domain *domain, unsigned long iov_pfn,
 
 			pte = pfn_to_dma_pte(domain, iov_pfn, &largepage_lvl,
 					     gfp);
-			if (!pte) {
-				ret = -ENOMEM;
+			if (IS_ERR(pte)) {
+				ret = PTR_ERR(pte);
 				goto out;
 			}
 			first_pte = pte;
@@ -4077,12 +4080,14 @@ static size_t intel_iommu_unmap(struct iommu_domain *domain,
 {
 	struct dmar_domain *dmar_domain = to_dmar_domain(domain);
 	unsigned long start_pfn, last_pfn;
+	struct dma_pte *pte;
 	int level = 0;
 
 	/* Cope with horrid API which requires us to unmap more than the
 	   size argument if it happens to be a large-page mapping. */
-	if (unlikely(!pfn_to_dma_pte(dmar_domain, iova >> VTD_PAGE_SHIFT,
-				     &level, GFP_ATOMIC)))
+	pte = pfn_to_dma_pte(dmar_domain, iova >> VTD_PAGE_SHIFT, &level,
+			     GFP_ATOMIC);
+	if (IS_ERR(pte))
 		return 0;
 
 	if (size < VTD_PAGE_SIZE << level_to_offset_bits(level))
@@ -4153,7 +4158,7 @@ static phys_addr_t intel_iommu_iova_to_phys(struct iommu_domain *domain,
 
 	pte = pfn_to_dma_pte(dmar_domain, iova >> VTD_PAGE_SHIFT, &level,
 			     GFP_ATOMIC);
-	if (pte && dma_pte_present(pte))
+	if (!IS_ERR(pte) && dma_pte_present(pte))
 		phys = dma_pte_addr(pte) +
 			(iova & (BIT_MASK(level_to_offset_bits(level) +
 						VTD_PAGE_SHIFT) - 1));
@@ -4804,7 +4809,7 @@ static int intel_iommu_read_and_clear_dirty(struct iommu_domain *domain,
 		pte = pfn_to_dma_pte(dmar_domain, iova >> VTD_PAGE_SHIFT, &lvl,
 				     GFP_ATOMIC);
 		pgsize = level_size(lvl) << VTD_PAGE_SHIFT;
-		if (!pte || !dma_pte_present(pte)) {
+		if (IS_ERR(pte) || !dma_pte_present(pte)) {
 			iova += pgsize;
 			continue;
 		}