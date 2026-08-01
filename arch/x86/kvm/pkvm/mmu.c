cxdong/pub-pKVM-IA

commit 321d904fd8b1f67a3ad03fa52c3d142c396b7ee9

pKVM: x86: Add hyp mmu map
Add hyp mmu map support to install vaddr to phys mappings, which is
implemented via the generic pkvm_pgtable_map api provided by the pkvm
pgtable infrastructure.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -119,3 +119,9 @@ int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 
 	return pkvm_pgtable_init(&hyp_mmu, cap, &pkvm_early_alloc_mm_ops, &hyp_mmu_pgt_ops);
 }
+
+int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
+		     unsigned long size, u64 prot)
+{
+	return pkvm_pgtable_map(&hyp_mmu, vaddr, phys, size, prot);
+}

arch/x86/kvm/pkvm/mmu.h
@@ -3,5 +3,7 @@
 #define __PKVM_X86_MMU_H
 
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages);
+int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
+		     unsigned long size, u64 prot);
 
 #endif /* __PKVM_X86_MMU_H */

intel-staging/pKVM-IA

commit c15c92d391dbb943de83988b8b2587890b8fa580

pKVM: x86: Strip upper bits of canonical VA in pkvm_hyp_mmu_map()
pkvm_hyp_mmu_map() directly passes kernel's / pKVM's virtual addresses
to pkvm_pgtable_map() in their canonical form, i.e. with the highest
address bit (e.g. bit 47 with 4-level paging) sign-extended. So since
the highest bit in those addresses is usually 1, the passed address has
all upper bits set as well (e.g. 0xffff888000000000-0xffffc87fffffffff
with the standard layout of the direct map with 4-level paging), i.e.
the address values exceed the address space size (e.g. 2^48).

This is not causing problems in the operation of pkvm_pgtable_map()
itself, since those upper bits are effectively ignored by
pgt_ops->vaddr_to_index(). However, this prevents adding a bounds check
of the passed vaddr against the address space size (see next patch).

Since canonical addressing applies to the 1st-stage MMU only and thus
luckily this pkvm_hyp_mmu_map() helper is the only user of the pgtable
API affected by this issue, fix the issue in a simple way: clear the
upper bits in pkvm_hyp_mmu_map() itself, before passing the address to
the pgtable API.

It is tempting to address this more "generically" at the pgtable API
level instead, i.e. let pkvm_pgtable_walk() check if vaddr is a valid
canonical address, instead of just bounds-checking it against
pkvm_pgtable_max_size(). However, at least one problem with that is that
pkvm_pgtable_walk() takes a [vaddr, vaddr + size) range rather than just
the address, so if pkvm_pgtable_walk() required the range to be
canonical, it would not allow ranges that span both the lower and the
upper halves of the address space (while we have a use case for that in
fix_hyp_mmu_page_refcnt() where we use pkvm_pgtable_walk() to walk the
entire address space).

Fixes: 321d904fd8b1 ("pKVM: x86: Add hyp mmu map")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -784,6 +784,10 @@ int pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn_t fn)
 int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 		     unsigned long size, u64 prot)
 {
+	if (WARN_ON(!__is_canonical_address(vaddr, boot_cpu_data.x86_virt_bits)))
+		return -EINVAL;
+	vaddr &= (1ULL << boot_cpu_data.x86_virt_bits) - 1;
+
 	return pkvm_pgtable_map(&hyp_mmu, vaddr, phys, size, prot, NULL);
 }
 