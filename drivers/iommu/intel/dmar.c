cxdong/pub-pKVM-IA

commit 4d6d18de29046312b7aa83400c7998bff87db8d2

iommu/vt-d: pKVM: Port QI functionality to pKVM
Port the major QI helper functions to help implement the invalidation
logic fully in pKVM.

Key architectural changes:
- Environment-specific macro overrides: To facilitate code reuse without
  duplicating core logic, standard host synchronization primitives
  (like raw_spin_lock and its variants) are seamlessly redefined to use
  pKVM's spinlock implementation (pkvm_spin_lock) when compiled under
  __PKVM_HYP__. This avoids the need for custom wrapper macros. Additional
  overrides handle virtual-to-physical memory translation and stub out
  unsupported host tracing.

This ensures that the hypervisor can submit and wait for invalidation
descriptors independently of the host.

Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/dmar.c
@@ -31,7 +31,14 @@
 #include <asm/irq_remapping.h>
 #include <asm/kvm_host.h>
 
+#ifdef __PKVM_HYP__
+#include <asm/pkvm_spinlock.h>
+#include "memory.h"
+#include "debug.h"
+#endif
+
 #include "iommu.h"
+#ifndef __PKVM_HYP__
 #include "../irq_remapping.h"
 #include "../iommu-pages.h"
 #include "perf.h"
@@ -1219,6 +1226,7 @@ static void free_iommu(struct intel_iommu *iommu)
 	ida_free(&dmar_seq_ids, iommu->seq_id);
 	kfree(iommu);
 }
+#endif /* !__PKVM_HYP__ */
 
 /*
  * Reclaim all the submitted descriptors which have completed its work.
@@ -1292,8 +1300,10 @@ static int qi_check_fault(struct intel_iommu *iommu, int index, int wait_index)
 {
 	u32 fault;
 	int head, tail;
+#ifndef __PKVM_HYP__
 	struct device *dev;
 	u64 iqe_err, ite_sid;
+#endif
 	struct q_inval *qi = iommu->qi;
 	int shift = qi_shift(iommu);
 
@@ -1338,12 +1348,14 @@ static int qi_check_fault(struct intel_iommu *iommu, int index, int wait_index)
 		tail = dmar_readl(iommu, DMAR_IQT_REG);
 		tail = ((tail >> shift) - 1 + QI_LENGTH) % QI_LENGTH;
 
+#ifndef __PKVM_HYP__
 		/*
 		 * SID field is valid only when the ITE field is Set in FSTS_REG
 		 * see Intel VT-d spec r4.1, section 11.4.9.9
 		 */
 		iqe_err = dmar_readq(iommu, DMAR_IQER_REG);
 		ite_sid = DMAR_IQER_REG_ITESID(iqe_err);
+#endif
 
 		dmar_writel(iommu, DMAR_FSTS_REG, DMA_FSTS_ITE);
 		pr_info("Invalidation Time-out Error (ITE) cleared\n");
@@ -1354,6 +1366,7 @@ static int qi_check_fault(struct intel_iommu *iommu, int index, int wait_index)
 			head = (head - 2 + QI_LENGTH) % QI_LENGTH;
 		} while (head != tail);
 
+#ifndef __PKVM_HYP__
 		/*
 		 * If device was released or isn't present, no need to retry
 		 * the ATS invalidate request anymore.
@@ -1367,6 +1380,7 @@ static int qi_check_fault(struct intel_iommu *iommu, int index, int wait_index)
 			    !pci_device_is_present(to_pci_dev(dev)))
 				return -ETIMEDOUT;
 		}
+#endif
 		if (qi->desc_status[wait_index] == QI_ABORT)
 			return -EAGAIN;
 	}
@@ -1379,6 +1393,21 @@ static int qi_check_fault(struct intel_iommu *iommu, int index, int wait_index)
 	return 0;
 }
 
+#ifdef __PKVM_HYP__
+#undef raw_spin_lock
+#define raw_spin_lock(lock) pkvm_spin_lock(lock)
+#undef raw_spin_unlock
+#define raw_spin_unlock(lock) pkvm_spin_unlock(lock)
+#undef raw_spin_lock_irqsave
+#define raw_spin_lock_irqsave(lock, flags) pkvm_spin_lock(lock)
+#undef raw_spin_unlock_irqrestore
+#define raw_spin_unlock_irqrestore(lock, flags) pkvm_spin_unlock(lock)
+
+#define trace_qi_submit(...)
+#undef virt_to_phys
+#define virt_to_phys(ptr) __pkvm_pa(ptr)
+#endif
+
 /*
  * Function to submit invalidation descriptors of all types to the queued
  * invalidation interface(QI). Multiple descriptors can be submitted at a
@@ -1390,19 +1419,26 @@ int qi_submit_sync(struct intel_iommu *iommu, struct qi_desc *desc,
 		   unsigned int count, unsigned long options)
 {
 	struct q_inval *qi = iommu->qi;
+#ifndef __PKVM_HYP__
 	s64 devtlb_start_ktime = 0;
 	s64 iotlb_start_ktime = 0;
 	s64 iec_start_ktime = 0;
+#endif
 	struct qi_desc wait_desc;
 	int wait_index, index;
+#ifndef __PKVM_HYP__
 	unsigned long flags;
+#endif
 	int offset, shift;
 	int rc, i;
+#ifndef __PKVM_HYP__
 	u64 type;
+#endif
 
 	if (!qi)
 		return 0;
 
+#ifndef __PKVM_HYP__
 	type = desc->qw0 & GENMASK_ULL(3, 0);
 
 	if ((type == QI_IOTLB_TYPE || type == QI_EIOTLB_TYPE) &&
@@ -1416,6 +1452,7 @@ int qi_submit_sync(struct intel_iommu *iommu, struct qi_desc *desc,
 	if (type == QI_IEC_TYPE &&
 	    dmar_latency_enabled(iommu, DMAR_LATENCY_INV_IEC))
 		iec_start_ktime = ktime_to_ns(ktime_get());
+#endif /* !__PKVM_HYP__ */
 
 restart:
 	rc = 0;
@@ -1499,6 +1536,7 @@ restart:
 	if (rc == -EAGAIN)
 		goto restart;
 
+#ifndef __PKVM_HYP__
 	if (iotlb_start_ktime)
 		dmar_latency_update(iommu, DMAR_LATENCY_INV_IOTLB,
 				ktime_to_ns(ktime_get()) - iotlb_start_ktime);
@@ -1510,6 +1548,7 @@ restart:
 	if (iec_start_ktime)
 		dmar_latency_update(iommu, DMAR_LATENCY_INV_IEC,
 				ktime_to_ns(ktime_get()) - iec_start_ktime);
+#endif
 
 	return rc;
 }
@@ -1564,8 +1603,13 @@ void qi_flush_dev_iotlb(struct intel_iommu *iommu, u16 sid, u16 pfsid,
 	 * Software is recommended to not submit any Device-TLB invalidation
 	 * requests while address remapping hardware is disabled.
 	 */
+#ifndef __PKVM_HYP__
 	if (!(iommu->gcmd & DMA_GCMD_TE))
 		return;
+#else
+	if (!(iommu->vgsts & DMA_GSTS_TES))
+		return;
+#endif
 
 	qi_desc_dev_iotlb(sid, pfsid, qdep, addr, mask, &desc);
 	qi_submit_sync(iommu, &desc, 1, 0);
@@ -1603,8 +1647,13 @@ void qi_flush_dev_iotlb_pasid(struct intel_iommu *iommu, u16 sid, u16 pfsid,
 	 * Software is recommended to not submit any Device-TLB invalidation
 	 * requests while address remapping hardware is disabled.
 	 */
+#ifndef __PKVM_HYP__
 	if (!(iommu->gcmd & DMA_GCMD_TE))
 		return;
+#else
+	if (!(iommu->vgsts & DMA_GSTS_TES))
+		return;
+#endif
 
 	qi_desc_dev_iotlb_pasid(sid, pfsid, pasid,
 				qdep, addr, size_order,
@@ -1622,6 +1671,7 @@ void qi_flush_pasid_cache(struct intel_iommu *iommu, u16 did,
 	qi_submit_sync(iommu, &desc, 1, 0);
 }
 
+#ifndef __PKVM_HYP__
 /*
  * Disable Queued Invalidation interface.
  */
@@ -2418,3 +2468,4 @@ bool dmar_platform_optin(void)
 	return ret;
 }
 EXPORT_SYMBOL_GPL(dmar_platform_optin);
+#endif /* !__PKVM_HYP__ */

drivers/iommu/intel/pkvm/Makefile.pkvm
@@ -7,4 +7,5 @@ ccflags-$(CONFIG_PKVM_INTEL) += -I $(srctree)/drivers/iommu/intel
 pkvm-iommu = ../../../../drivers/iommu/intel/pkvm
 intel-iommu = ../../../../drivers/iommu/intel
 
-pkvm-hyp-$(CONFIG_PKVM_INTEL) += $(pkvm-iommu)/iommu.o $(intel-iommu)/iommu.o
+pkvm-hyp-$(CONFIG_PKVM_INTEL) += $(pkvm-iommu)/iommu.o $(intel-iommu)/iommu.o \
+				 $(intel-iommu)/dmar.o

intel-staging/pKVM-IA

commit cdb262654d5f483d6e7ea550f937b76d370c1575

iommu/vt-d: pKVM: Assert translation is enabled in Device-TLB flushes
During Device-TLB invalidations (ATC flushes) in scalable mode, the
hypervisor checks if IOMMU translation has been enabled by checking the
virtual status shadow maintained by hypervisor(iommu->vgsts)

Under pKVM, the physical IOMMU translation is always enabled once
initialized and is never disabled, as doing so would compromise memory
isolation. Since translation is always active, Device-TLB flushes must
always be executed when requested.

However, since the host is blocked from disabling translation (returning
an error for DMAR_GCMD_TE=0 writes), the virtual status vgsts.TES
should always be 1 under normal operation. If vgsts.TES is ever
observed to be 0 here, it indicates a pKVM bug that could lead to stale
ATC entries and a guest memory isolation bypass.

Replace the early-return check on vgsts.TES with a BUG_ON assertion to
immediately halt the hypervisor and protect guest memory confidentiality
if a state desynchronization is detected.

Fixes: 4d6d18de2904 ("iommu/vt-d: pKVM: Port QI functionality to pKVM")
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/dmar.c
@@ -1645,8 +1645,12 @@ void qi_flush_dev_iotlb(struct intel_iommu *iommu, u16 sid, u16 pfsid,
 	if (!(iommu->gcmd & DMA_GCMD_TE))
 		return;
 #else
-	if (!(iommu->vgsts & DMA_GSTS_TES))
-		return;
+	/*
+	 * Translation is never disabled after enabling during initialization.
+	 * And this is not called before IOMMU is initialized in pKVM.
+	 * If this is called with translation disabled, it means a bug in pKVM!
+	 */
+	BUG_ON(!(iommu->vgsts & DMA_GSTS_TES));
 #endif
 
 	qi_desc_dev_iotlb(sid, pfsid, qdep, addr, mask, &desc);
@@ -1689,8 +1693,12 @@ void qi_flush_dev_iotlb_pasid(struct intel_iommu *iommu, u16 sid, u16 pfsid,
 	if (!(iommu->gcmd & DMA_GCMD_TE))
 		return;
 #else
-	if (!(iommu->vgsts & DMA_GSTS_TES))
-		return;
+	/*
+	 * Translation is never disabled after enabling during initialization.
+	 * And this is not called before IOMMU is initialized in pKVM.
+	 * If this is called with translation disabled, it means a bug in pKVM!
+	 */
+	BUG_ON(!(iommu->vgsts & DMA_GSTS_TES));
 #endif
 
 	qi_desc_dev_iotlb_pasid(sid, pfsid, pasid,