cxdong/pub-pKVM-IA

commit d087fdec3fc31a5f3afeb06c8f822728b844b969

pKVM: VMX: Handle EXIT_REASON_NOTIFY
Enable the instruction timeout feature if this is supported and
requested by the host. The exit is handled by the pKVM hypervisor first
if the exit happened while executing iret from NMI, then returns back to
the host from the vcpu_run PV interface, which will handle this vmexit
similarly to the native KVM VMX host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -385,6 +385,12 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 	    pkvm_vm->shared_kvm->arch.bus_lock_detection_enabled &&
 	    kvm_caps.has_bus_lock_exit)
 		kvm->arch.bus_lock_detection_enabled = true;
+	if (!kvm->arch.notify_vmexit_flags &&
+	    pkvm_vm->shared_kvm->arch.notify_vmexit_flags &&
+	    kvm_caps.has_notify_vmexit) {
+		kvm->arch.notify_window = pkvm_vm->shared_kvm->arch.notify_window;
+		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
+	}
 
 	pkvm_spin_unlock(&pkvm_vm->lock);
 

arch/x86/kvm/vmx/pkvm_host.c
@@ -477,6 +477,24 @@ static int handle_bus_lock_vmexit(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+static int handle_notify(struct kvm_vcpu *vcpu)
+{
+	unsigned long exit_qual = vmx_get_exit_qual(vcpu);
+	bool context_invalid = exit_qual & NOTIFY_VM_CONTEXT_INVALID;
+
+	++vcpu->stat.notify_window_exits;
+
+	if (vcpu->kvm->arch.notify_vmexit_flags & KVM_X86_NOTIFY_VMEXIT_USER ||
+	    context_invalid) {
+		vcpu->run->exit_reason = KVM_EXIT_NOTIFY;
+		vcpu->run->notify.flags = context_invalid ?
+					  KVM_NOTIFY_CONTEXT_INVALID : 0;
+		return 0;
+	}
+
+	return 1;
+}
+
 /*
  * The exit handlers return 1 if the exit was handled fully and guest execution
  * may resume.  Otherwise they set the kvm_run parameter to indicate what needs
@@ -504,6 +522,7 @@ static int (*pkvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_EPT_MISCONFIG]           = handle_ept_misconfig,
 	[EXIT_REASON_PAUSE_INSTRUCTION]       = handle_pause,
 	[EXIT_REASON_BUS_LOCK]                = handle_bus_lock_vmexit,
+	[EXIT_REASON_NOTIFY]		      = handle_notify,
 };
 
 static const int pkvm_vmx_max_exit_handlers = ARRAY_SIZE(pkvm_vmx_exit_handlers);

arch/x86/kvm/vmx/vmx.c
@@ -6672,13 +6672,14 @@ static int handle_bus_lock_vmexit(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
-#ifndef __PKVM_HYP__
 static int handle_notify(struct kvm_vcpu *vcpu)
 {
 	unsigned long exit_qual = vmx_get_exit_qual(vcpu);
+#ifndef __PKVM_HYP__
 	bool context_invalid = exit_qual & NOTIFY_VM_CONTEXT_INVALID;
 
 	++vcpu->stat.notify_window_exits;
+#endif
 
 	/*
 	 * Notify VM exit happened while executing iret from NMI,
@@ -6688,6 +6689,7 @@ static int handle_notify(struct kvm_vcpu *vcpu)
 		vmcs_set_bits(GUEST_INTERRUPTIBILITY_INFO,
 			      GUEST_INTR_STATE_NMI);
 
+#ifndef __PKVM_HYP__
 	if (vcpu->kvm->arch.notify_vmexit_flags & KVM_X86_NOTIFY_VMEXIT_USER ||
 	    context_invalid) {
 		vcpu->run->exit_reason = KVM_EXIT_NOTIFY;
@@ -6697,8 +6699,12 @@ static int handle_notify(struct kvm_vcpu *vcpu)
 	}
 
 	return 1;
+#else
+	return 0;
+#endif
 }
 
+#ifndef __PKVM_HYP__
 static int vmx_get_msr_imm_reg(struct kvm_vcpu *vcpu)
 {
 	return vmx_get_instr_info_reg(vmcs_read32(VMX_INSTRUCTION_INFO));
@@ -6807,8 +6813,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 #endif
 	[EXIT_REASON_ENCLS]		      = handle_encls,
 	[EXIT_REASON_BUS_LOCK]                = handle_bus_lock_vmexit,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_NOTIFY]		      = handle_notify,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_SEAMCALL]		      = handle_tdx_instruction,
 	[EXIT_REASON_TDCALL]		      = handle_tdx_instruction,
 	[EXIT_REASON_MSR_READ_IMM]            = handle_rdmsr_imm,

intel-staging/pKVM-IA

commit 9d0e688d7aa58ebd87fc10796c9827d65cc91c3f

pKVM: x86: Do postponed_per_vm_setup() no more than once per VM
Make sure that the per-VM setup in postponed_per_vm_setup() is applied
exactly once per VM, before creating its first vCPU, to avoid changing
parameters of an already running VM and thus avoid any potential issues
caused by that.

Fixes: 549835379856 ("pKVM: VMX: Handle EXIT_REASON_BUS_LOCK")
Fixes: d087fdec3fc3 ("pKVM: VMX: Handle EXIT_REASON_NOTIFY")
Fixes: 6fdd05cd1022 ("pKVM: VMX: Synchronize host disabled_exits to pKVM in npVMs")
Fixes: 2f1769168899 ("pKVM: x86: Expose TSC frequency to pVM")
Fixes: 590abffcb6d6 ("pKVM: x86: Fix TOCTOU race reading apic_bus_cycle_ns from shared memory")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -495,6 +495,11 @@ static void postponed_per_vm_setup(struct kvm *kvm)
 
 	pkvm_spin_lock(&pkvm_vm->lock);
 
+	if (pkvm_vm->postponed_setup_done) {
+		pkvm_spin_unlock(&pkvm_vm->lock);
+		return;
+	}
+
 	shared_kvm = pkvm_vm->shared_kvm;
 	/*
 	 * The following setup is per VM, not per vCPU, however it cannot be
@@ -505,14 +510,12 @@ static void postponed_per_vm_setup(struct kvm *kvm)
 	 * creating vCPUs. So it is ok to assume these host's values here are
 	 * up-to-date.
 	 */
-	if (!kvm->arch.bus_lock_detection_enabled &&
-	    shared_kvm->arch.bus_lock_detection_enabled &&
-	    kvm_caps.has_bus_lock_exit)
-		kvm->arch.bus_lock_detection_enabled = true;
 
-	if (!kvm->arch.notify_vmexit_flags &&
-	    shared_kvm->arch.notify_vmexit_flags &&
-	    kvm_caps.has_notify_vmexit) {
+	if (kvm_caps.has_bus_lock_exit)
+		kvm->arch.bus_lock_detection_enabled =
+			shared_kvm->arch.bus_lock_detection_enabled;
+
+	if (kvm_caps.has_notify_vmexit) {
 		kvm->arch.notify_window = shared_kvm->arch.notify_window;
 		kvm->arch.notify_vmexit_flags = shared_kvm->arch.notify_vmexit_flags;
 	}
@@ -524,8 +527,9 @@ static void postponed_per_vm_setup(struct kvm *kvm)
 	if (!pkvm_is_protected_vm(kvm))
 		kvm->arch.disabled_exits = shared_kvm->arch.disabled_exits;
 
-	if (!kvm->created_vcpus)
-		kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
+	kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
+
+	pkvm_vm->postponed_setup_done = true;
 
 	pkvm_spin_unlock(&pkvm_vm->lock);
 }

arch/x86/kvm/pkvm/pkvm.h
@@ -62,6 +62,7 @@ struct pkvm_vm {
 	pkvm_spinlock_t lock;
 	struct pkvm_vcpu *vcpus[KVM_MAX_VCPUS];
 	atomic_t vcpu_refs[KVM_MAX_VCPUS];
+	bool postponed_setup_done;
 	/* Guest MMU (stage-2) page table managed by the hypervisor */
 	struct pkvm_pgtable mmu;
 	struct pkvm_pool mmu_pool;