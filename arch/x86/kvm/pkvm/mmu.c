cxdong/pub-pKVM-IA

commit 3eb6f747ab9d36a5229480cfb9c5e8b50c5ed26a

pKVM: x86: Add support for sharing/unsharing pVM pages with host
Introduce API for sharing pVM pages with the host (i.e. allowing the
host to access them) and unsharing them back (i.e. restoring protection
of those pages from the host). It will be used for letting pVMs
themselves deliberately share their specific pages with the host, which
is needed e.g. for allowing pVMs to use virtio devices provided by the
host.

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -671,6 +671,39 @@ static int __host_unshare_guest(unsigned long gpa, unsigned long hpa,
 	return 0;
 }
 
+static int __guest_share_host(unsigned long gpa, unsigned long hpa,
+			      unsigned long size, u64 prot, void *arg)
+{
+	struct kvm_vcpu *vcpu = arg;
+	struct pkvm_vm *pkvm_vm = to_pkvm_vcpu(vcpu)->pkvm_vm;
+	int ret;
+
+	prot = pkvm_pte_set_pgstate(prot, &pkvm_vm->mmu, PKVM_PAGE_SHARED_OWNED);
+	ret = pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot,
+			       &vcpu->arch.pkvm.guest_mmu_memcache);
+	if (ret)
+		return ret;
+
+	BUG_ON(pkvm_pgtable_map(&host_mmu, hpa, hpa, size,
+				host_mmu_pte_prot(false), NULL));
+	set_host_mem_pgstate(hpa, size, PKVM_PAGE_SHARED_BORROWED);
+
+	return 0;
+}
+
+static int __guest_unshare_host(unsigned long gpa, unsigned long hpa,
+				unsigned long size, u64 prot, void *arg)
+{
+	struct kvm_vcpu *vcpu = arg;
+	struct pkvm_vm *pkvm_vm = to_pkvm_vcpu(vcpu)->pkvm_vm;
+
+	BUG_ON(pkvm_pgtable_set_owner(&host_mmu, hpa, size, PKVM_ID_GUEST));
+	set_host_mem_pgstate(hpa, size, PKVM_PAGE_NONE);
+
+	prot = pkvm_pte_set_pgstate(prot, &pkvm_vm->mmu, PKVM_PAGE_OWNED);
+	return pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot, NULL);
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -1392,6 +1425,94 @@ int pkvm_host_test_clear_young_guest(struct kvm *kvm, unsigned long gpa,
 	pkvm_guest_mmu_lock(pkvm_vm);
 	ret = pkvm_pgtable_test_clear_young(&pkvm_vm->mmu, gpa, size, mkold);
 	pkvm_guest_mmu_unlock(pkvm_vm);
+
+	return ret;
+}
+
+/**
+ * pkvm_guest_share_host() - Share guest pages with the host.
+ * @vcpu:	Guest's vCPU in whose context the sharing is requested.
+ * @gpa:	Guest physical address of the memory region to share.
+ * @size:	Size of the memory region to share.
+ *
+ * Changes the ownership state of the pages mapped by the GPA range [@gpa,
+ * @gpa + @size) in the guest mmu from exclusively owned by the guest to shared
+ * with the host, thus allowing the host to access them, with RWX permissions.
+ * The guest must be a protected VM and the pages in the range must have been
+ * previously donated to this guest. The @gpa and @size are required to be
+ * PAGE_SIZE aligned.
+ *
+ * Returns: 0 on success, or a negative error code on failure.
+ */
+int pkvm_guest_share_host(struct kvm_vcpu *vcpu, unsigned long gpa,
+			  unsigned long size)
+{
+	struct pkvm_vm *pkvm_vm = to_pkvm_vcpu(vcpu)->pkvm_vm;
+	int ret;
+
+	if (!PAGE_ALIGNED(gpa) || !PAGE_ALIGNED(size))
+		return -EINVAL;
+
+	pkvm_host_mmu_lock();
+	pkvm_guest_mmu_lock(pkvm_vm);
+
+	ret = check_guest_host_state(pkvm_vm, gpa, size, PKVM_PAGE_OWNED,
+				     PKVM_PAGE_NONE);
+	if (ret)
+		goto unlock;
+
+	ret = for_each_contig_range(&pkvm_vm->mmu, gpa, size,
+				    __guest_share_host, vcpu);
+	if (ret) {
+		/* Unshare already shared pages, if any. */
+		BUG_ON(for_each_contig_range(&pkvm_vm->mmu, gpa, size,
+					     __guest_unshare_host, vcpu));
+	}
+unlock:
+	pkvm_guest_mmu_unlock(pkvm_vm);
+	pkvm_host_mmu_unlock();
+
+	return ret;
+}
+
+/**
+ * pkvm_guest_unshare_host() - Un-share guest pages with the host.
+ * @vcpu:	Guest's vCPU in whose context the unsharing is requested.
+ * @gpa:	Guest physical address of the memory region to unshare.
+ * @size:	Size of the memory region to unshare.
+ *
+ * Changes the ownership state of the pages mapped by the GPA range [@gpa,
+ * @gpa + @size) in the guest mmu from shared with the host to exclusively
+ * owned by the guest, thus protecting them from accessing by the host.
+ * The guest must be a protected VM and the pages in the range must have been
+ * previously donated to this guest and then shared by it with the host.
+ * The @gpa and @size are required to be PAGE_SIZE aligned.
+ *
+ * Returns: 0 on success, or a negative error code on failure.
+ */
+int pkvm_guest_unshare_host(struct kvm_vcpu *vcpu, unsigned long gpa,
+			    unsigned long size)
+{
+	struct pkvm_vm *pkvm_vm = to_pkvm_vcpu(vcpu)->pkvm_vm;
+	int ret;
+
+	if (!PAGE_ALIGNED(gpa) || !PAGE_ALIGNED(size))
+		return -EINVAL;
+
+	pkvm_host_mmu_lock();
+	pkvm_guest_mmu_lock(pkvm_vm);
+
+	ret = check_guest_host_state(pkvm_vm, gpa, size, PKVM_PAGE_SHARED_OWNED,
+				     PKVM_PAGE_SHARED_BORROWED);
+	if (ret)
+		goto unlock;
+
+	ret = for_each_contig_range(&pkvm_vm->mmu, gpa, size,
+				    __guest_unshare_host, vcpu);
+	BUG_ON(ret);
+unlock:
+	pkvm_guest_mmu_unlock(pkvm_vm);
+	pkvm_host_mmu_unlock();
 
 	return ret;
 }

arch/x86/kvm/pkvm/mem_protect.h
@@ -90,5 +90,9 @@ int pkvm_host_unshare_guest(struct kvm *kvm, unsigned long gpa,
 			    unsigned long size);
 int pkvm_host_test_clear_young_guest(struct kvm *kvm, unsigned long gpa,
 				     unsigned long size, bool mkold);
+int pkvm_guest_share_host(struct kvm_vcpu *vcpu, unsigned long gpa,
+			  unsigned long size);
+int pkvm_guest_unshare_host(struct kvm_vcpu *vcpu, unsigned long gpa,
+			    unsigned long size);
 
 #endif /* __PKVM_X86_MEM_PROTECT_H */

arch/x86/kvm/pkvm/pgtable.h
@@ -193,6 +193,15 @@ static inline enum pkvm_page_state pkvm_pte_pgstate(struct pkvm_pgtable *pgt, vo
 	return pgt->pgt_ops->pte_pgstate(ptep);
 }
 
+static inline u64 pkvm_pte_set_pgstate(u64 val, struct pkvm_pgtable *pgt,
+				       enum pkvm_page_state state)
+{
+	val &= ~pkvm_pgt_pgstate_mask(pgt);
+	val |= pkvm_pte_mk_pgstate(pgt, state);
+
+	return val;
+}
+
 /*
  * Use bit31 ~ bi12 as owner ID bits to avoid conflicting w/ low 12 property
  * bits for all kinds of page table.

intel-staging/pKVM-IA

commit 400b983e103be5204d15ad5ef8b7bf2fb16df3e1

pKVM: x86: Use memcache on guest unshare
pKVM doesn't use memcache when unsharing pVM pages with host, assuming
that a pVM will only unshare a whole previously shared range, not a part
of it, so it will not require splitting a huge page mapping and thus
will not require allocating memory for extra page table pages.

So if a pVM still requests a partial unshare, it may fail with -ENOMEM,
which will in turn trigger the BUG_ON in pkvm_guest_unshare_host().
Which means that an arbitrary pVM can crash the entire system.

There are no known pVM use cases for such partial unshares, so we could
address this problem just by removing that BUG_ON and just returning the
error to the pVM instead. However, that would be still problematic,
since it would leave inconsistent page state info: the host state of the
pages already updated to PKVM_PAGE_NONE, while their guest state remains
PKVM_PAGE_SHARED_OWNED instead of PKVM_PAGE_OWNED, since
pkvm_pgtable_map() failed with -ENOMEM. That would, in particular,
trigger the BUG_ON in host_reclaim_guest_walker() when destroying the
pVM.

So instead fix the problem by ensuring that partial unshare will
succeed, by using memcache for unshare (refilling it before unshare if
needed), the same way as we use it for share.

This patch enables using the memcache. The next patch will implement
refilling the memcache when needed, to ensure that partial unshare will
actually succeed.

Fixes: 3eb6f747ab9d ("pKVM: x86: Add support for sharing/unsharing pVM pages with host")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -688,7 +688,8 @@ static int __guest_unshare_host(unsigned long gpa, unsigned long hpa,
 	set_host_mem_pgstate(hpa, size, PKVM_PAGE_NONE, PKVM_ID_GUEST);
 
 	prot = pkvm_pte_set_pgstate(prot, &pkvm_vm->mmu, PKVM_PAGE_OWNED);
-	return pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot, NULL);
+	return pkvm_pgtable_map(&pkvm_vm->mmu, gpa, hpa, size, prot,
+				&vcpu->arch.pkvm.guest_mmu_memcache);
 }
 
 static bool gpa_range_overlaps_pvmfw(struct kvm *kvm,